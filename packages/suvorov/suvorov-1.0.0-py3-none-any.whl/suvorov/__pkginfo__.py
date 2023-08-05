ec60011324946b110a664cf3c2c546f217543b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:33 +0000

    Add package suvorov/1.0.0/suvorov-1.0.0-py3-none-any.whl

---
+++ packages/suvorov/suvorov-1.0.0-py3-none-any.whl/suvorov/__pkginfo__.py
@@ -0,0 +1,23 @@
+name = "suvorov"
+desc = "Tool to enable a more concise and thematically structured creation of mods for Crusader Kings 2"
+author = {
+	"name":"Johannes Krattenmacher",
+	"email":"misc@krateng.dev",
+	"github": "krateng"
+}
+links = {
+	"github":"ck2suite"
+}
+version = 1,0,0
+versionstr = ".".join(str(n) for n in version)
+
+
+requires = [
+	"doreah>=1.6.1"
+]
+resources = [
+]
+
+commands = {
+	"suvorov":"main:main"
+}
