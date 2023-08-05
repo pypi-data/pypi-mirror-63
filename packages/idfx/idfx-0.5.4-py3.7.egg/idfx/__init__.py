10c95b84dfe5aab6d9d3709d9d823fe46e75ab51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package idfx/0.5.4/idfx-0.5.4-py3.7.egg

---
+++ packages/idfx/idfx-0.5.4-py3.7.egg/idfx/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__author__ = "d01"
+__email__ = "jungflor@gmail.com"
+__copyright__ = "Copyright (C) 2017, Florian JUNG"
+__license__ = "MIT"
+__version__ = "0.5.4"
+__date__ = "2018-12-01"
+# Created: 2017-11-27 21:08
+
+from .controller import IDFXManga
+from .model import Manga, User
+
+__all__ = ["IDFXManga", "Manga", "User"]
