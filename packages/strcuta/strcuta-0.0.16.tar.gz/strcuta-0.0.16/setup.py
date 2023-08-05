130f76121fb7ce2f88f2e46097cfbc2a4b531ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:16 +0000

    Add package strcuta/0.0.16/strcuta-0.0.16.tar.gz

---
+++ packages/strcuta/strcuta-0.0.16.tar.gz/strcuta-0.0.16/setup.py
@@ -0,0 +1,36 @@
+# Copyright (C) Yuki Takagi 2020
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# https://www.boost.org/LICENSE_1_0.txt)
+
+from setuptools import setup
+from setuptools import find_packages
+
+from os import path
+pkgdir = path.abspath(path.dirname(__file__))
+with open(path.join(pkgdir, "README.md"), encoding="utf-8") as f:
+    readme_md = f.read()
+
+setup(
+        name="strcuta",
+        version="0.0.16",
+        url="https://github.com/takagiy/strcuta-py",
+        project_urls={
+            "Repository": "https://github.com/takagiy/strcuta-py",
+            "Bug reports": "https://github.com/takagiy/strcuta-py/issues"
+            },
+        description="Loading UTAU voice banks into the Python data structure.",
+        long_description=readme_md,
+        long_description_content_type="text/markdown",
+        author="takagiy",
+        author_email="takagiy.4dev@gmail.com",
+        classifiers=[
+            "License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)",
+            "Development Status :: 3 - Alpha"
+            ],
+        packages=find_packages(),
+        include_package_data=False,
+        install_requires=[],
+        extras_require={
+            "play": ["sounddevice"]
+            })
---
+++ packages/strcuta/strcuta-0.0.16.tar.gz/strcuta-0.0.16/strcuta/wav.py
@@ -0,0 +1,52 @@
+# Copyright (C) Yuki Takagi 2020
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# https://www.boost.org/LICENSE_1_0.txt)
+
+import wave as _wave
+from strcuta import cursor as _cursor
+
+class Type:
+    def __init__(self, parameter, frames):
+        assert parameter.nchannels == 1
+        self.parameter = parameter
+        self.frames = frames
+
+    def __len__(self):
+        return self.parameter.nframes
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            k = _cursor.resolve_slice(key, self.parameter.framerate, self.parameter.sampwidth)
+            f = self.frames[k]
+            return Type(
+                    parameter=self.parameter._replace(nframes=int(len(f) / self.parameter.sampwidth)),
+                    frames=f)
+        else:
+            return self.frames[_cursor.resolve_to_slice(key, self.parameter.framerate, self.parameter.sampwidth)]
+
+    def write(self, outputpath):
+        with _wave.open(outputpath, mode="wb") as w:
+            w.setparams(self.parameter)
+            w.writeframes(self.frames)
+
+    _audio_stream = None
+
+    def play(self):
+        from sounddevice import RawOutputStream
+        Type._audio_stream = Type._audio_stream or RawOutputStream(
+                channels=self.parameter.nchannels,
+                dtype='int' + str(8 * self.parameter.sampwidth),
+                samplerate=self.parameter.framerate * self.parameter.nchannels,)
+        Type._audio_stream.start()
+        Type._audio_stream.write(self.frames)
+
+def load(path):
+    with _wave.open(path, mode="rb") as w:
+        return Type(
+                parameter=w.getparams(),
+                frames=w.readframes(w.getnframes())
+                )
+
+def write(wav, outputpath):
+    wav.write(outputpath)
