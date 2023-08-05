c36f9a675ac78fcf660794d20b1ca410d64edc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:50 +0000

    Add package pmdatapipelines-light/0.0.5/pmdatapipelines-light-0.0.5.tar.gz

---
+++ packages/pmdatapipelines-light/pmdatapipelines-light-0.0.5.tar.gz/pmdatapipelines-light-0.0.5/README.md
@@ -0,0 +1,3 @@
+# Polaris Media Data Pipeline Library
+
+This is a light weight library for Polaris Media. Does not require libraries for converting to Parquet, and will not be able to work with Parquet files.
---
+++ packages/pmdatapipelines-light/pmdatapipelines-light-0.0.5.tar.gz/pmdatapipelines-light-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pmdatapipelines-light",
+	version="0.0.5",
+	author="Jørgen Frøland",
+	author_email="jorgen.froland@polarismedia.no",
+	description="Lightweight version of data pipeline library for Polaris Media",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
