a98a9231e9e731f937a89f4034544cbc954a5bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package sws-py-sdk/0.7/sws_py_sdk-0.7-py3-none-any.whl

---
+++ packages/sws-py-sdk/sws_py_sdk-0.7-py3-none-any.whl/sws_py_sdk/service.py
@@ -0,0 +1,113 @@
+""" This is the base class for the Web Service definitions -- includes common functions
+"""
+import json
+from requests import Request, Session
+from requests.auth import HTTPBasicAuth
+
+class Service(object):
+    def __init__(self, sws):
+        """ Initializes the service object with a reference to the base SWS instance.
+            sws : Sws instance
+        """
+        self.sws = sws
+        self.service_uri = ''
+        self.last_request = None
+        self.invalid_access_token_handler = None
+
+    def fetch(self,
+    auth,
+    endpoint,
+    body={},
+    params={},
+    method='GET',
+    timeout=None,
+    headers={'Accept': 'application/json',
+            'Content-Type': 'application/json'}):
+        """ Highest level function for handling requests
+            auth : object | string
+                Will either contain a configured form of authentication - like the well supported
+                HTTPDigestAuth that is inbuilt to requests or  simply a string that will notify the
+                service of which type of authentication to use.
+            endpoint : string
+                The URI path to send the request to
+            body : dict
+                If a GET request, will be used for the URL params.
+                If a POST request this will be the data in the body of the packet.
+            method : string
+                The HTTP method
+            timeout : float
+                The time that the request will wait before timing out if there is
+                no data from the server.
+            headers : dict
+                The headers that will be sent in the request
+            
+        """
+        self.last_request = self.build_request(
+            auth=auth,
+            endpoint=('' if self.service_uri.find('://') != -1 else 'https://') + self.service_uri + endpoint,
+            body=body, 
+            method=method,
+            params=params,
+            timeout=timeout,
+            headers=headers)
+        
+        return self.fetch_request(self.last_request)
+
+    def fetch_request(self, request):
+        """ Responsible for taking the Request object and sending it - includes error handling
+            request : Requests.Request
+                The HTTP request that is being sent.
+        """
+        # First, prepare request
+        session = Session()
+        prepared_request = session.prepare_request(request)
+        response = session.send(prepared_request)
+        if not response.ok:
+            data = response.json()
+            if ((response.status_code == 403 and data['code'] == 2001)
+                or (response.status_code == 401 and data['code'] == 2002)):
+                # Access token is invalid or expired
+                # 403 2001 - Invalid access token
+                # 401 2002 - Expired access token
+                return self.sws.invalid_access_token_handler(self, response)
+                   
+        return response
+
+    def build_request(self, auth, endpoint, body, params, method, timeout, headers):
+        """ Build up the request object.
+            auth : object | string
+                Will either contain a configured form of authentication - like the well supported
+                HTTPDigestAuth that is inbuilt to requests or  simply a string that will notify the
+                service of which type of authentication to use.
+            endpoint : string
+                The URI path to send the request to
+            body : dict
+                If a GET request, will be used for the URL params.
+                If a POST request this will be the data in the body of the packet.
+            method : string
+                The HTTP method
+            timeout : float
+                The time that the request will wait before timing out if there is
+                no data from the server.
+            headers : dict
+                The headers that will be sent in the request
+        """
+
+        request = Request(method=method, url=endpoint, headers=headers)
+
+        if auth is 'bearer':
+            request.headers['Authorization'] = f'Bearer {self.sws.access_token}'
+        elif isinstance(auth, HTTPBasicAuth):
+            request.auth = auth
+
+        if method is 'GET' or method is 'DELETE':
+            request.params = body
+
+        if method == 'PUT' or method == 'PATCH' or method == 'POST':
+            if 'Content-Type' in headers and headers['Content-Type'] == 'application/x-www-form-urlencoded':
+                request.data = body
+            else:
+                request.data = json.dumps(body)  # JSON in the body by default
+            if params != {}:
+                request.params = params
+        return request
---
+++ packages/sws-py-sdk/sws_py_sdk-0.7-py3-none-any.whl/sws_py_sdk/sws_client.py
@@ -0,0 +1,80 @@
+import datetime
+
+from .sws import Sws
+""" This is the client.
+    It needs a nice description.
+"""
+class SwsClient(Sws):
+
+    def __init__(self, app_id, secret=None, user_id=0, timeout=3000, service_uri={}, auto_refresh=True):
+        """
+        Here we set up a mechanism for token refresh to be handled and triggered
+        Create SWS object
+        config : object
+            Configuration options
+        appId : str
+            Application ID
+        secret : str
+            Application secret
+        user_id : int
+            End user ID
+        timeout : float
+            Request timeout
+        service_uri : object
+            Base URIs for SWS services
+        service_uri.id : str
+            Base URI for SWS ID Service
+        service_uri.license : str
+            Base URI for SWS License Service
+        auto_refresh : boolean
+            Determines if the client will attempt to refresh the access token if invalid or expired.
+        """
+        super().__init__(app_id=app_id, secret=secret, user_id=user_id, timeout=timeout, service_uri=service_uri, invalid_access_token_handler=self._handle_invalid_access_token)
+        self.auto_refresh = auto_refresh
+        self.access_token_updated_callback = None
+
+    def set_access_token_updated_callback(self, callback):
+        """
+        Sets a callback for when the access token has been updated when auto_refresh is true.
+        :param function callback: Callback to execute when the access token has been updated.
+        :return: Nothing.
+        """
+        self.access_token_updated_callback = callback
+
+    def _handle_invalid_access_token(self, service, response):
+        """
+        Handles the case where a response came back from a service where the access token was not valid or was expired.
+
+        If `auto_refresh` is true, will attempt to refresh the access token then attempt to make the last request again,
+        otherwise returns the original response.
+
+        :param Service service: The service which failed to fulfill a request due to an invalid access token.
+        :param requests.Response response: The response given due to the invalid access token.
+        :return: A HTTP response.
+        :rtype: requests.Response
+        """
+        if self.auto_refresh:
+            last_request = service.last_request
+            response = self.identity().token_refresh(self.refresh_token)
+            #   This token refresh request may have resulted in an error that was
+            #   handled by a custom error handler.
+
+            data = response.json()
+
+            if 'tokens' in data:
+                #   Update the access token property
+                self.access_token = data['tokens']['access']['token']
+                #   Call the callback
+                if self.access_token_updated_callback:
+                    self.access_token_updated_callback(
+                      token=self.access_token,
+                      expires=datetime.datetime.utcfromtimestamp(data['tokens']['access']['expires_at'])
+                    )
+
+                #   Set a new Authorization header for the request
+                last_request.headers['Authorization'] = f'Bearer {self.access_token}'
+
+                #   Re-execute the 'last request'
+                return service.fetch_request(last_request)
+        return response
+
