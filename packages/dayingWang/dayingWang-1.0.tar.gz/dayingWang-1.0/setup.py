dd0412ce1e885f91c31f2868b633b736bc210098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package dayingWang/1.0/dayingWang-1.0.tar.gz

---
+++ packages/dayingWang/dayingWang-1.0.tar.gz/dayingWang-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import  setup
+
+setup(
+    name='dayingWang',
+    version='1.0',
+    description='对外发布',
+    author='wangjin',
+    py_modules=['dayingWang.demo1','dayingWang.demo2']
+
+
+)
