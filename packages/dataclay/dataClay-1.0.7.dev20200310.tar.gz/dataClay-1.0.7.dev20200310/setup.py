1cf63ff21c619277481faf73b4ab2a995fb56c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:29 +0000

    Add package dataclay/1.0.7.dev20200310/dataClay-1.0.7.dev20200310.tar.gz

---
+++ packages/dataclay/dataClay-1.0.7.dev20200310.tar.gz/dataClay-1.0.7.dev20200310/setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+from distutils.command.sdist import sdist
+from datetime import date
+import os 
+
+class sdist_hg(sdist):
+
+    user_options = sdist.user_options + [
+            ('dev', None, "Add a dev marker")
+            ]
+
+    def initialize_options(self):
+        sdist.initialize_options(self)
+        self.dev = 0
+
+    def run(self):
+        if self.dev:
+            today = date.today()
+            d = today.strftime("%Y%m%d")
+            suffix = '.dev%s' % str(d)
+            self.distribution.metadata.version += suffix
+        sdist.run(self)
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+# parse_requirements
+install_reqs = parse_requirements("./requirements.txt")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='dataClay',
+      version="1.0.7",
+      maintainer='Barcelona Supercomputing Center',
+      maintainer_email='support-dataclay@bsc.es',
+      install_requires=install_reqs,
+      description='Python library for dataClay',
+      packages=find_packages("src"),
+      package_dir={'':'src'},
+      package_data={
+        # All .properties files are valuable "package data"
+        '': ['*.properties'],
+      },
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://www.bsc.es/dataclay",
+      project_urls={
+          'Documentation': 'https://pyclay.readthedocs.io/en/latest/',
+          'Source': 'https://github.com/bsc-dom/pyclay',
+      },
+      cmdclass={'sdist': sdist_hg},
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Science/Research",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python",
+          "Topic :: Database :: Database Engines/Servers",
+          "Topic :: System :: Distributed Computing",
+          "Topic :: Software Development :: Libraries :: Application Frameworks",
+      ],
+      )
+
