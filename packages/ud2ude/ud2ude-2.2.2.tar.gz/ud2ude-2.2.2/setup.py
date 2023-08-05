a2144461ca588008b09e9bb4af3fc2e24833649b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package ud2ude/2.2.2/ud2ude-2.2.2.tar.gz

---
+++ packages/ud2ude/ud2ude-2.2.2.tar.gz/ud2ude-2.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ud2ude",
+    version="2.2.2",
+    author="Aryeh Tiktinsky",
+    author_email="aryehgigi@gmail.com",
+    description="python converter from UD to enhanced UD representations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/allenai/UD2UDE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
