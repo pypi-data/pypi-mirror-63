403360caa823a6b40e134ce40bdff456ed7dc5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:27 +0000

    Add package detectree/0.3.1/detectree-0.3.1.tar.gz

---
+++ packages/detectree/detectree-0.3.1.tar.gz/detectree-0.3.1/detectree/filters.py
@@ -0,0 +1,67 @@
+import numpy as np
+from scipy.ndimage.filters import _gaussian_kernel1d
+from skimage.filters import gabor_kernel
+
+__all__ = ['get_texture_kernel', 'get_gabor_filter_bank']
+
+
+def _get_gaussian_kernel1d(sigma, order=0, truncate=4.0):
+    """
+    Based on scipy.ndimage.filters.gaussian_filter1d
+    """
+    truncate = 4
+    sd = float(sigma)
+    # make the radius of the filter equal to truncate standard deviations
+    lw = int(truncate * sd + 0.5)
+    # # Since we are calling correlate, not convolve, revert the kernel
+    # weights = _gaussian_kernel1d(sigma, order, lw)[::-1]
+    weights = _gaussian_kernel1d(sigma, order, lw)
+    return weights
+
+
+def get_texture_kernel(sigma):
+    """
+    Parameters
+    ----------
+    sigma : numeric
+        Scale parameter to build a texture kernel, based on a Gaussian on the
+        X dimension and a second-derivative Gaussian in the Y dimension
+
+    Returns
+    -------
+    texture_kernel : array-like
+    """
+    g0_kernel_arr = _get_gaussian_kernel1d(sigma, 0)
+    g2_kernel_arr = _get_gaussian_kernel1d(sigma, 2)
+
+    return np.dot(g2_kernel_arr.reshape(1, -1).T, g0_kernel_arr.reshape(1, -1))
+
+
+def get_gabor_filter_bank(frequencies, num_orientations):
+    """
+    Parameters
+    ----------
+    frequencies : list-like
+        Set of frequencies used to build the Gabor filter bank.
+    num_orientations : int or list-like
+        Number of orientations used to build the Gabor filter bank. If an
+        integer is provided, the corresponding number of orientations will be
+        used for each scale (determined by `gabor_frequencies`). If a tuple is
+        provided, each element will determine the number of orientations that
+        must be used at its matching scale (determined by `gabor_frequencies`)
+        - thus the tuple must match the length of `frequencies`.
+
+    Returns
+    -------
+    kernels : list-like
+        List of kernel 2-D arrays that correspond to the filter bank
+    """
+    kernels = []
+
+    for frequency, _num_orientations in zip(frequencies, num_orientations):
+        for orientation_i in range(_num_orientations):
+            theta = orientation_i / _num_orientations * np.pi
+            kernel = np.real(gabor_kernel(frequency, theta=theta))
+            kernels.append(kernel)
+
+    return kernels
---
+++ packages/detectree/detectree-0.3.1.tar.gz/detectree-0.3.1/detectree/image_descriptor.py
@@ -0,0 +1,115 @@
+import numpy as np
+from PIL import Image
+from scipy import ndimage as ndi
+from skimage import color
+from skimage.util import shape
+from sklearn import preprocessing
+
+from . import utils
+
+__all__ = [
+    'compute_image_descriptor', 'compute_image_descriptor_from_filepath'
+]
+
+
+def compute_image_descriptor(img_rgb, kernels, response_bins_per_axis,
+                             num_color_bins):
+    """
+    Compute a GIST descriptor for `img_rgb`. See the `background
+    <https://bit.ly/2KlCICO>`_ example notebook for more details.
+
+    Parameters
+    ----------
+    img_rgb : array-like
+        The image in RGB format, i.e., in a 3-D array
+    kernels : list-like
+        List of kernel 2-D arrays that correspond to the filter bank
+    response_bins_per_axis : int
+        Number of spatial bins per axis into which the responses to the filter
+        bank will be aggreated. For example, a value of 2 will aggregate the
+        responses into the four quadrants of the image (i.e., 2x2, 2 bins in
+        each axis of the image).
+    num_color_bins : int
+        Number of color bins per axis of the L*a*b color space with which
+        the joint color histogram will be computed
+
+    Returns
+    -------
+    img_descr : array-like
+        Vector representing GIST descriptor of `img_rgb`
+    """
+
+    # gist descriptor
+    num_blocks = response_bins_per_axis**2
+    gist_descr = np.zeros(len(kernels) * num_blocks)
+    img_gray = color.rgb2gray(img_rgb)
+    block_shape = tuple(size // response_bins_per_axis
+                        for size in img_gray.shape)
+    divides_evenly = True
+    for size in img_gray.shape:
+        if size % response_bins_per_axis != 0:
+            divides_evenly = False
+            break
+
+    if not divides_evenly:
+        # TODO: warn?
+        target_height, target_width = (size * response_bins_per_axis
+                                       for size in block_shape)
+        img_gray = np.array(
+            Image.fromarray(img_gray).resize((target_width, target_height)))
+
+    for i, kernel in enumerate(kernels):
+        filter_response = ndi.convolve(img_gray, kernel)
+        response_bins = shape.view_as_blocks(filter_response, block_shape)
+        bin_sum = response_bins.sum(axis=(2, 3)).flatten()
+        gist_descr[i * num_blocks:(i + 1) * num_blocks] = bin_sum
+
+    # color descriptor
+    img_lab = color.rgb2lab(img_rgb)
+    img_lab_dn = img_lab.reshape(img_lab.shape[0] * img_lab.shape[1],
+                                 img_lab.shape[2])
+    H, _ = np.histogramdd(img_lab_dn, bins=num_color_bins)
+    color_descr = H.flatten()
+
+    # normalize the gist and color descriptors to the l1 norm and concatenate
+    # them
+    img_descr = np.concatenate([
+        preprocessing.normalize(row.reshape(1, -1), norm='l1').flatten()
+        for row in [gist_descr, color_descr]
+    ])
+
+    return img_descr
+
+
+def compute_image_descriptor_from_filepath(img_filepath, kernels,
+                                           response_bins_per_axis,
+                                           num_color_bins):
+    """
+    Compute a GIST descriptor for `img_filepath`. See the `background
+    <https://bit.ly/2KlCICO>`_ example notebook for more details.
+
+    Parameters
+    ----------
+    img_filepath : str, file object or pathlib.Path object
+        Path to a file, URI, file object opened in binary ('rb') mode, or a
+        Path object representing the image for which a GIST descriptor will be
+        computed. The value will be passed to `rasterio.open`.
+    kernels : list-like
+        List of kernel 2-D arrays that correspond to the filter bank
+    response_bins_per_axis : int
+        Number of spatial bins per axis into which the responses to the filter
+        bank will be aggreated. For example, a value of 2 will aggregate the
+        responses into the four quadrants of the image (i.e., 2x2, 2 bins in
+        each axis of the image).
+    num_color_bins : int
+        Number of color bins per axis of the L*a*b color space with which
+        the joint color histogram will be computed
+
+    Returns
+    -------
+    img_descr : array-like
+        Vector representing GIST descriptor of `img_rgb`
+    """
+    img_rgb = utils.img_rgb_from_filepath(img_filepath)
+    return compute_image_descriptor(img_rgb, kernels, response_bins_per_axis,
+                                    num_color_bins)
