609e644f33e082a093eb623e11f490a81c19f41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:32 +0000

    Add package virtualenv/20.0.10/virtualenv-20.0.10.tar.gz

---
+++ packages/virtualenv/virtualenv-20.0.10.tar.gz/virtualenv-20.0.10/tests/unit/seed/test_pip_invoke.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import, unicode_literals
+
+import pytest
+
+from virtualenv.discovery.py_info import PythonInfo
+from virtualenv.run import cli_run
+from virtualenv.seed.embed.wheels import BUNDLE_SUPPORT
+
+
+@pytest.mark.slow
+@pytest.mark.timeout(timeout=60)
+def test_base_bootstrap_via_pip_invoke(tmp_path, coverage_env, current_fastest):
+    bundle_ver = BUNDLE_SUPPORT[PythonInfo.current_system().version_release_str]
+    create_cmd = [
+        "--seeder",
+        "pip",
+        str(tmp_path / "env"),
+        "--download",
+        "--pip",
+        bundle_ver["pip"].split("-")[1],
+        "--setuptools",
+        bundle_ver["setuptools"].split("-")[1],
+        "--creator",
+        current_fastest,
+    ]
+    result = cli_run(create_cmd)
+    coverage_env()
+    assert result
+
+    site_package = result.creator.purelib
+    pip = site_package / "pip"
+    setuptools = site_package / "setuptools"
+    wheel = site_package / "wheel"
+
+    files_post_first_create = list(site_package.iterdir())
+    assert pip in files_post_first_create
+    assert setuptools in files_post_first_create
+    assert wheel in files_post_first_create
