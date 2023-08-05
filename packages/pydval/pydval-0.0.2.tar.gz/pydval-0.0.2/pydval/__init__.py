a861bda4e85391a0dc3d779240e1c88e932c0aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package pydval/0.0.2/pydval-0.0.2.tar.gz

---
+++ packages/pydval/pydval-0.0.2.tar.gz/pydval-0.0.2/pydval/__init__.py
@@ -0,0 +1,1 @@
+from pydval import validator
---
+++ packages/pydval/pydval-0.0.2.tar.gz/pydval-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name = 'pydval',
+  packages = ['pydval'],
+  version = '0.0.2',
+  license='MIT',
+  description = 'Python Data Validator',
+  author = 'Afrizal F.A',
+  author_email = 'afrizal.fadillah46@gmail.com',
+  url = 'https://github.com/ICWR-TECH/Pydval',
+  download_url = 'https://github.com/ICWR-TECH/Pydval/raw/master/pydval-0.0.2.tar.gz',
+  keywords = ['pydval'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
