3bf02d78c5defba5d01c62294ab07811fd1ed9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:26 +0000

    Add package vectorio/1.2.4/vectorio-1.2.4.tar.gz

---
+++ packages/vectorio/vectorio-1.2.4.tar.gz/vectorio-1.2.4/vectorio/vector/exceptions.py
@@ -0,0 +1,54 @@
+#!-*-coding:utf-8-*-
+
+
+class CharDecodeError(Exception):
+    pass
+
+
+class FileNotFound(Exception):
+    pass
+
+
+class GeojsonInvalid(Exception):
+    pass
+
+
+class WKTInvalid(Exception):
+    pass
+
+
+class GeometryTypeInvalid(Exception):
+    pass
+
+
+class DataSourceWithoutSpatialRef(Exception):
+    pass
+
+
+class TypeFromFileIsNotSupported(Exception):
+    pass
+
+
+class ShapefileInvalid(Exception):
+    pass
+
+
+class ShapefileIsEmpty(Exception):
+    pass
+
+
+class DirectoryDoesNotExists(Exception):
+    pass
+
+
+class ImpossibleCreateShapefileFromGeometryCollection(Exception):
+    pass
+
+
+class ErrorOnIntersection(Exception):
+    pass
+
+
+class ExistsManyGeometriesTypes(Exception):
+    pass
+
