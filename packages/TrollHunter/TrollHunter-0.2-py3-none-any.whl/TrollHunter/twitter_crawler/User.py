86e8ae9e7fa2937fe669b30b53a006df2d7aeaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:13 +0000

    Add package TrollHunter/0.2/TrollHunter-0.2-py3-none-any.whl

---
+++ packages/TrollHunter/TrollHunter-0.2-py3-none-any.whl/TrollHunter/twitter_crawler/User.py
@@ -0,0 +1,17 @@
+import pandas as pd
+class User:
+    def __init__(self,username):
+        self.username = username
+
+
+    def set_info_to_df(self,user_obj):
+        self.info_df = pd.DataFrame.from_dict([vars(user_obj)])
+
+    def set_follower_df(self,follow):
+        self.follower_df = follow
+
+    def set_following_df(self,follow):
+        self.following_df = follow
+
+    def set_tweet_df(self,tweet):
+        self.tweets_df = tweet
