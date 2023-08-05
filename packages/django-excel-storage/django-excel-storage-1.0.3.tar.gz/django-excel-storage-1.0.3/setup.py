cae3cb73140df3739b62311bc23e0fe073648d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:54 +0000

    Add package django-excel-storage/1.0.3/django-excel-storage-1.0.3.tar.gz

---
+++ packages/django-excel-storage/django-excel-storage-1.0.3.tar.gz/django-excel-storage-1.0.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+from setuptools import setup
+
+
+version = '1.0.3'
+
+
+setup(
+    name='django-excel-storage',
+    version=version,
+    keywords='django-excel-storage',
+    description='Django Excel Storage',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/django-xxx/django-excel-storage',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    py_modules=['excel_storage'],
+    install_requires=['xlwt', 'pytz', 'screen', 'django-excel-base', 'django-six>=1.0.4'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
