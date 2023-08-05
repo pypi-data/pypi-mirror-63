b3d54a20e20f6c4061259a771c7aa0fc489c3d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:30 +0000

    Add package tlbpy/0.0.4/tlbpy-0.0.4.tar.gz

---
+++ packages/tlbpy/tlbpy-0.0.4.tar.gz/tlbpy-0.0.4/tlbpy/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    My personnal toolbox for Python
+"""
+
+__version__ = "0.0.4"
+
+from tlbpy.helloworld import helloworld
---
+++ packages/tlbpy/tlbpy-0.0.4.tar.gz/tlbpy-0.0.4/tlbpy/helloworld.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+    Implémentation de la proclamation de la bonne parole.
+ 
+    Usage:
+ 
+    >>> from tlbpy import helloworld
+    >>> helloworld()
+"""
+__all__ = ['helloworld']
+ 
+def helloworld():
+    print("Hello world ! Comment ça va ? Bien et toi ?")
+
+if __name__ == "__main__":
+    helloworld()
