f82a0ccd3b7a4732fceeadfa6fd94b7b964af643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:26 +0000

    Add package game-of-life-MUGOH/0.0.13/game_of_life_MUGOH-0.0.13-py3-none-any.whl

---
+++ packages/game-of-life-MUGOH/game_of_life_MUGOH-0.0.13-py3-none-any.whl/game_of_life/run.py
@@ -0,0 +1,15 @@
+"""
+    Starter file
+"""
+from .controller import Runner
+
+
+def run_():
+    """
+        Starts the game
+    """
+    Runner().run()
+
+
+if __name__ == '__main__':
+    run_()
