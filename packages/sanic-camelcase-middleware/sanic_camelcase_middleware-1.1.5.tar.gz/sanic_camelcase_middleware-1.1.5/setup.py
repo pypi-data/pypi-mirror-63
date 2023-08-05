06e71af147b3eb9255fea4ef9a9ab62b48f1b3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package sanic-camelcase-middleware/1.1.5/sanic_camelcase_middleware-1.1.5.tar.gz

---
+++ packages/sanic-camelcase-middleware/sanic_camelcase_middleware-1.1.5.tar.gz/sanic_camelcase_middleware-1.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+VERSION = "1.1.5"
+
+setup(
+    name="sanic_camelcase_middleware",
+    version=VERSION,
+    description="Middleware for camelizing request and response bodies for Sanic",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://ahmednafies.github.io/sanic_camelcase_middleware/",
+    project_urls={
+        "Documentation": "https://ahmednafies.github.io/sanic_camelcase_middleware/",
+        "Source": "https://github.com/ahmednafies/sanic_camelcase_middleware",
+    },
+    author="Ahmed Nafies",
+    author_email="ahmed.nafies@gmail.com",
+    license="MIT",
+    packages=["sanic_camelcase_middleware"],
+    install_requires=["sanic", "pyhumps"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    zip_safe=False,
+)
