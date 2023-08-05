cf2a03d7d8b4df0c238272df96459d3f8452f325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package pyrin/0.3.13/pyrin-0.3.13-py3-none-any.whl

---
+++ packages/pyrin/pyrin-0.3.13-py3-none-any.whl/pyrin/application/services.py
@@ -0,0 +1,367 @@
+# -*- coding: utf-8 -*-
+"""
+application services module.
+"""
+
+from pyrin.application.container import _get_app
+
+
+def add_context(key, value, **options):
+    """
+    adds the given key and it's value into the application context.
+
+    :param str key: related key for storing application context.
+    :param object value: related value for storing in application context.
+
+    :keyword bool replace: specifies that if there is already a value with
+                           the same key in application context, it should be updated
+                           with new value, otherwise raise an error. defaults to False.
+
+    :raises DuplicateContextKeyError: duplicate context key error.
+    """
+
+    get_current_app().add_context(key, value, **options)
+
+
+def get_context(key):
+    """
+    gets the application context value that belongs to given key.
+
+    :param str key: key for requested application context.
+
+    :returns: related value to given key.
+    """
+
+    return get_current_app().get_context(key)
+
+
+def register_component(component, **options):
+    """
+    registers given application component or replaces the existing one
+    if `replace=True` is provided. otherwise, it raises an error
+    on adding an instance which it's id is already available
+    in registered components.
+
+    :param Component component: component instance.
+
+    :keyword bool replace: specifies that if there is another registered
+                           component with the same id, replace it with the new one.
+                           otherwise raise an error. defaults to False.
+
+    :raises InvalidComponentTypeError: invalid component type error.
+    :raises InvalidComponentIDError: invalid component id error.
+    :raises DuplicateComponentIDError: duplicate component id error.
+    """
+
+    get_current_app().register_component(component, **options)
+
+
+def remove_component(component_id):
+    """
+    removes application component with given id.
+
+    :param tuple component_id: component id to be removed.
+    :type component_id: tuple(str, object)
+
+    :raises ComponentAttributeError: component attribute error.
+    """
+
+    get_current_app().remove_component(component_id)
+
+
+def get_component(component_name, **options):
+    """
+    gets the specified application component.
+
+    :param str component_name: component name.
+
+    :keyword object component_custom_key: component custom key.
+                                          if not provided, tries to get it from
+                                          request object, if not found,
+                                          `DEFAULT_COMPONENT_KEY` will be used.
+
+    :raises InvalidComponentNameError: invalid component name error.
+    :raises ComponentAttributeError: component attribute error.
+
+    :rtype: Component
+    """
+
+    return get_current_app().get_component(component_name, **options)
+
+
+def register_error_handler(code_or_exception, func):
+    """
+    registers the given function as an error handler for given code or exception type.
+
+    :param Union[int, Exception] code_or_exception: code or exception to
+                                                    register error handler for.
+
+    :param callable func: function to register it as an error handler.
+    """
+
+    get_current_app().register_error_handler(code_or_exception, func)
+
+
+def register_before_request_handler(func):
+    """
+    registers the given function into application before request handlers.
+
+    :param callable func: function to register it into before request handlers.
+    """
+
+    get_current_app().before_request(func)
+
+
+def register_after_request_handler(func):
+    """
+    registers the given function into application after request handlers.
+
+    :param callable func: function to register it into after request handlers.
+    """
+
+    get_current_app().after_request(func)
+
+
+def register_teardown_request_handler(func):
+    """
+    registers the given function into application teardown request handlers.
+    teardown request handlers should not return any value
+    and also should not raise any exception.
+
+    :param callable func: function to register it into teardown request handlers.
+    """
+
+    get_current_app().teardown_request(func)
+
+
+def add_url_rule(rule, endpoint=None, view_func=None,
+                 provide_automatic_options=None, **options):
+    """
+    connects a url rule. if a view_func is provided it will be registered with the endpoint.
+    if there is another rule with the same url and `replace=True` option is provided,
+    it will be replaced, otherwise an error will be raised.
+
+    :param str rule: the url rule as string.
+
+    :param str endpoint: the endpoint for the registered url rule.
+                         pyrin itself assumes the url rule as endpoint.
+
+    :param callable view_func: the function to call when serving a request to the
+                               provided endpoint.
+
+    :param bool provide_automatic_options: controls whether the `OPTIONS` method should be
+                                           added automatically.
+                                           this can also be controlled by setting the
+                                           `view_func.provide_automatic_options = False`
+                                           before adding the rule.
+
+    :keyword tuple(str) methods: http methods that this rule should handle.
+                                 if not provided, defaults to `GET`.
+
+    :keyword tuple(PermissionBase) permissions: tuple of all required permissions
+                                                to access this route's resource.
+
+    :keyword bool login_required: specifies that this route could not be accessed
+                                  if the requester has not a valid token.
+                                  defaults to True if not provided.
+
+    :keyword bool replace: specifies that this route must replace
+                           any existing route with the same url or raise
+                           an error if not provided. defaults to False.
+
+    :raises DuplicateRouteURLError: duplicate route url error.
+    """
+
+    get_current_app().add_url_rule(rule, endpoint, view_func,
+                                   provide_automatic_options, **options)
+
+
+def register_route_factory(factory):
+    """
+    registers a route factory as application url rule class.
+
+    :param callable factory: route factory.
+                             it could be a class or a factory method.
+
+    :raises InvalidRouteFactoryTypeError: invalid route factory type error.
+    """
+
+    get_current_app().register_route_factory(factory)
+
+
+def get_current_route_factory():
+    """
+    gets current route factory in use.
+
+    :rtype: callable
+    """
+
+    return get_current_app().get_current_route_factory()
+
+
+def get_default_settings_path():
+    """
+    gets the pyrin default settings path.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_default_settings_path()
+
+
+def get_settings_path():
+    """
+    gets the application settings path.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_settings_path()
+
+
+def get_migrations_path():
+    """
+    gets the application migrations path.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_migrations_path()
+
+
+def get_locale_path():
+    """
+    gets the application locale path.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_locale_path()
+
+
+def get_application_main_package_path():
+    """
+    gets the application main package path.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_application_main_package_path()
+
+
+def get_pyrin_root_path():
+    """
+    gets pyrin root path in which pyrin package is located.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_pyrin_root_path()
+
+
+def get_application_root_path():
+    """
+    gets the application root path in which application package is located.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_application_root_path()
+
+
+def get_pyrin_main_package_path():
+    """
+    gets pyrin main package path.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_pyrin_main_package_path()
+
+
+def get_configs():
+    """
+    gets a shallow copy of application's configuration dictionary.
+
+    :rtype: dict
+    """
+
+    return get_current_app().get_configs()
+
+
+def configure(config_store):
+    """
+    configures the application with given dict.
+    all keys will be converted to uppercase for flask compatibility.
+
+    :param dict config_store: a dictionary containing configuration key/values.
+    """
+
+    get_current_app().configure(config_store)
+
+
+def get_current_app():
+    """
+    gets the instance of current running application.
+
+    :rtype: Application
+    """
+
+    return _get_app()
+
+
+def register_hook(instance):
+    """
+    registers the given instance into application hooks.
+
+    :param ApplicationHookBase instance: application hook instance to be registered.
+
+    :raises InvalidApplicationHookTypeError: invalid application hook type error.
+    """
+
+    get_current_app().register_hook(instance)
+
+
+def is_scripting_mode():
+    """
+    gets a value indicating that application has been started in scripting mode.
+    some application hooks will not fire in this mode. like 'before_application_start'.
+    """
+
+    return get_current_app().is_scripting_mode()
+
+
+def get_application_name():
+    """
+    gets the application name.
+
+    it is actually the application main package name.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_application_name()
+
+
+def get_class_name():
+    """
+    gets the application class name.
+
+    it is required for template generation.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_class_name()
+
+
+def get_module_name():
+    """
+    gets the application module name.
+
+    it is required for template generation.
+
+    :rtype: str
+    """
+
+    return get_current_app().get_module_name()
---
+++ packages/pyrin/pyrin-0.3.13-py3-none-any.whl/pyrin/database/migration/hooks.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+database migration hooks module.
+"""
+
+import pyrin.database.migration.services as migration_services
+import pyrin.configuration.services as config_services
+import pyrin.application.services as application_services
+
+from pyrin.application.decorators import application_hook
+from pyrin.application.hooks import ApplicationHookBase
+from pyrin.database.decorators import database_hook
+from pyrin.database.hooks import DatabaseHookBase
+from pyrin.utils.custom_print import print_warning, print_info
+
+
+@database_hook()
+class DatabaseHook(DatabaseHookBase):
+    """
+    database hook class.
+    """
+
+    def __init__(self):
+        """
+        initializes an instance of DatabaseHook.
+        """
+
+        super().__init__()
+
+    def after_session_factories_configured(self):
+        """
+        this method will be called after all database
+        session factories have been configured.
+        """
+
+        migration_services.configure_migration_data()
+
+
+@application_hook()
+class ApplicationHook(ApplicationHookBase):
+    """
+    application hook class.
+    """
+
+    def __init__(self):
+        """
+        initializes an instance of ApplicationHook.
+        """
+
+        super().__init__()
+
+    def after_application_loaded(self):
+        """
+        this method will be called after application has been loaded.
+        """
+
+        if application_services.is_scripting_mode() is False:
+            if config_services.get_active('database', 'drop_on_startup') is True:
+                environment = config_services.get_active('environment', 'env')
+                debug = config_services.get_active('environment', 'debug')
+                unit_testing = config_services.get_active('environment', 'unit_testing')
+
+                if (environment == 'development' and debug is True) or \
+                        (environment == 'testing' and unit_testing is True):
+                    print_warning('Dropping all models...')
+                    migration_services.drop_all()
+
+            if config_services.get_active('database', 'create_on_startup') is True:
+                print_info('Creating all models...')
+                migration_services.create_all()
