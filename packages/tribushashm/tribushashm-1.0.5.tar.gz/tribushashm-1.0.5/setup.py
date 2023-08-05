4f69cdddaf50490429903b098f7bb0467d4aa614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:26 +0000

    Add package tribushashm/1.0.5/tribushashm-1.0.5.tar.gz

---
+++ packages/tribushashm/tribushashm-1.0.5.tar.gz/tribushashm-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+
+tribushashm_module = Extension('tribushashm',
+                               sources = ['tribusmodule.c',
+                                          'tribus.c',
+										  'sph/aes_helper.c',
+										  'sph/sph_jh.c',
+										  'sph/sph_keccak.c',
+										  'sph/sph_echo.c'],
+                               include_dirs=['.', './sph'])
+
+setup (name = 'tribushashm',
+       version = '1.0.5',
+       description = 'Bindings for tribus proof of work used by Denarius',
+       ext_modules = [tribushashm_module])
