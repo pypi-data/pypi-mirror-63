a940d72888691a5a4c2f5b209cb41e8961b5a3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:36 +0000

    Add package datacol/1.0.2/datacol-1.0.2.tar.gz

---
+++ packages/datacol/datacol-1.0.2.tar.gz/datacol-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# The directory containing this file
+
+
+# The text of the README file
+desc = open('DESC.md')
+desc = desc.read()
+
+# This call to setup() does all the work
+setup(
+    name="datacol",
+    version="1.0.2",
+    packages=['datacol'],
+    description="Localized, file-based database module.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EKHolmes/DataCol/",
+    author="Erik Holmes",
+    author_email="zv.eevee6718@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
