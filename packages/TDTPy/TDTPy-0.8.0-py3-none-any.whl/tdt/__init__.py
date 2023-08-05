dc84a13be97d0cac6ff814b45a2a000501468ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:19 +0000

    Add package TDTPy/0.8.0/TDTPy-0.8.0-py3-none-any.whl

---
+++ packages/TDTPy/TDTPy-0.8.0-py3-none-any.whl/tdt/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+# Although Python 2.7+ has a logging.NullHandler class available, we should at
+# least maintain backwards-compatibility with Python 2.6 so that
+# ReadTheDocs.org can generate our autodocumentation.
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
+from .dsp_project import DSPProject
+from .dsp_process import DSPProcess
+from .dsp_circuit import DSPCircuit
+from .dsp_buffer import DSPBuffer
+from .dsp_error import DSPError
+
+__version__ = '0.8.0.dev0'
