ee4381141ea04c27dfb17ae89cd5324a014aaaed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:27 +0000

    Add package dinglingling/0.2.0/dinglingling-0.2.0.tar.gz

---
+++ packages/dinglingling/dinglingling-0.2.0.tar.gz/dinglingling-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from io import open
+
+setup(
+	name='dinglingling',
+	version='0.2.0',
+	description='remind you the status of the program',
+	long_description=open('readme.md', 'r', encoding='utf-8').read(),
+	long_description_content_type='text/markdown',
+	url='https://github.com/PeijiYang/dinglingling',
+	author='Peiji',
+	author_email='peiji.yang@foxmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	python_requires='>=3.6',
+	install_requires=[],
+	classifiers=[
+          'Intended Audience :: Science/Research',
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
+)
