a0573b5b5c84e0213094257c95d402e1bb54bb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:16 +0000

    Add package CodeforcesApiPy/1.1.1/CodeforcesApiPy-1.1.1.tar.gz

---
+++ packages/CodeforcesApiPy/CodeforcesApiPy-1.1.1.tar.gz/CodeforcesApiPy-1.1.1/codeforces_api/__init__.py
@@ -0,0 +1,6 @@
+"""
+Importing classes from modules
+"""
+__all__ = ["CodeforcesApi"]
+from codeforces_api.api_requests import CodeforcesApi
+from codeforces_api.api_request_maker import CodeforcesApiRequestMaker
