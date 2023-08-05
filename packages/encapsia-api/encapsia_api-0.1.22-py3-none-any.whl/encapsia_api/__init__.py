6fdff58e55916689c16880462500b22ea9330b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:19 +0000

    Add package encapsia-api/0.1.22/encapsia_api-0.1.22-py3-none-any.whl

---
+++ packages/encapsia-api/encapsia_api-0.1.22-py3-none-any.whl/encapsia_api/__init__.py
@@ -0,0 +1,21 @@
+#: Keep in sync with git tag and package version in pyproject.toml.
+__version__ = "0.1.22"
+
+
+class EncapsiaApiError(RuntimeError):  # NOQA
+    def __init__(self, message, payload=None):
+        super().__init__(message)
+        self.message = message
+        self.payload = payload
+
+
+class EncapsiaApiFailedTaskError(EncapsiaApiError):  # NOQA
+    pass
+
+
+from encapsia_api.analytics import analytics_connect  # NOQA
+from encapsia_api.credentials import CredentialsStore  # NOQA
+from encapsia_api.credentials import discover_credentials  # NOQA
+from encapsia_api.package import PackageMaker  # NOQA
+from encapsia_api.rest import EncapsiaApi  # NOQA
+from encapsia_api.rest import FileDownloadResponse  # NOQA
