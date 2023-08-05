34c5c3183955ca3bebdcdde05fdfe3eb2738a7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package mozzarilla/1.9.1/mozzarilla-1.9.1-py3-none-any.whl

---
+++ packages/mozzarilla/mozzarilla-1.9.1-py3-none-any.whl/mozzarilla/__init__.py
@@ -0,0 +1,17 @@
+#
+# This file is part of Mozzarilla.
+#
+# For authors and copyright check AUTHORS.TXT
+#
+# Mozzarilla is free software under the GNU General Public License v3.0.
+# See LICENSE for more information.
+#
+
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla, Michelle van der Graaf"
+#           YYYY.MM.DD
+__date__ = "2020.03.07"
+__version__ = (1, 9, 1)
+__website__ = "https://github.com/Sigmmma/mozzarilla"
