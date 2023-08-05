83ec5200712c0f2253e05073dea8558506cfadbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package medium-crosspost/0.1.1/medium-crosspost-0.1.1.tar.gz

---
+++ packages/medium-crosspost/medium-crosspost-0.1.1.tar.gz/medium-crosspost-0.1.1/setup.py
@@ -0,0 +1,40 @@
+from os import path
+import re
+import io
+from setuptools import setup
+
+PACKAGE_NAME = "medium-crosspost"
+PACKAGE_PATH = "medium_crosspost"
+HERE = path.abspath(path.dirname(__file__))
+
+with io.open(path.join(HERE, "README.md"), encoding="utf-8") as fp:
+    README = fp.read()
+
+with io.open(path.join(HERE, PACKAGE_PATH, "__init__.py"), encoding="utf-8") as fp:
+    VERSION = re.search("__version__ = \"([^\"]+)\"", fp.read()).group(1)
+
+setup(
+    name=PACKAGE_NAME,
+    packages=[PACKAGE_PATH],
+    version=VERSION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description="Easily crosspost articles to Medium",
+    author="Matt White",
+    author_email="code@typenil.com",
+    url="https://github.com/typenil/ghost-crosspost-medium",
+    license="MIT License",
+    install_requires=["requests"],
+    keywords="medium ghost crosspost blog zapier",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities",
+    ],
+)
