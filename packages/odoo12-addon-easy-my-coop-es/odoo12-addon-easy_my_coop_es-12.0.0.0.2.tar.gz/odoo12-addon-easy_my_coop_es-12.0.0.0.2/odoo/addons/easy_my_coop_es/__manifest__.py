71c52e767be3e9b8fc727b48807056460ab44bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:20 +0000

    Add package odoo12-addon-easy-my-coop-es/12.0.0.0.2/odoo12-addon-easy_my_coop_es-12.0.0.0.2.tar.gz

---
+++ packages/odoo12-addon-easy-my-coop-es/odoo12-addon-easy_my_coop_es-12.0.0.0.2.tar.gz/odoo12-addon-easy_my_coop_es-12.0.0.0.2/odoo/addons/easy_my_coop_es/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Easy My Coop Spain",
+    'version': '12.0.0.0.2',
+    'depends': ['easy_my_coop', 'easy_my_coop_website'],
+    'author': "Coopdevs Treball SCCL",
+    'category': "Cooperative management",
+    'description': """
+    Easy My Coop localization for Spain
+    """,
+    "license": "AGPL-3",
+    'data': [
+        "views/become_cooperator_view.xml",
+        "views/subscription_request_view.xml",
+    ],
+}
