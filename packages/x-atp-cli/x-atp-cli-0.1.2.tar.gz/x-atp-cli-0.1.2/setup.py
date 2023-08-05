a346198500be88a85af7095373532fcf89f7b0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package x-atp-cli/0.1.2/x-atp-cli-0.1.2.tar.gz

---
+++ packages/x-atp-cli/x-atp-cli-0.1.2.tar.gz/x-atp-cli-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='x-atp-cli',
+    version='0.1.2',
+    keywords=['x', 'atp', 'test', 'platform'],
+    description='X automated test platform command line client',
+    long_description='X automated test platform command line client',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/hekaiyou/x-atp-cli',
+    author="HeKaiYou",
+    author_email="hekaiyou@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'x-atp-cli = atp.cli:main'
+        ]
+    },
+)
