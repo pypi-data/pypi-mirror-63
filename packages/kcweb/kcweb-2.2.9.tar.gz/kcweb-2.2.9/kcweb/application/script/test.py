e7a326f718dd7b1df5e6392ad39c4b7ae76d6e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package kcweb/2.2.9/kcweb-2.2.9.tar.gz

---
+++ packages/kcweb/kcweb-2.2.9.tar.gz/kcweb-2.2.9/kcweb/application/script/test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("../../")
+from ${appname}.cli.common import *
+from selenium import webdriver
+class TEST:
+    def aa():
+        print('aa')
+test=TEST()
+test.aa()
---
+++ packages/kcweb/kcweb-2.2.9.tar.gz/kcweb-2.2.9/test/test5.py
@@ -0,0 +1,8 @@
+import nude
+#图片快速简单签黄
+print(nude.is_nude("11.jpg"))
+print(nude.is_nude("50.jpg"))
+# print(nude.is_nude("2.jpg"))
+# print(nude.is_nude("3.jpg"))
+# print(nude.is_nude("4.jpg"))
+# print(nude.is_nude("11.jpg"))
