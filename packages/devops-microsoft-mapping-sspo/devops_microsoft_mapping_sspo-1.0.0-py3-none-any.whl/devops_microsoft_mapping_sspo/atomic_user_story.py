de18b4e1742fc0b54807d31e7c46d8b04205a693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package devops-microsoft-mapping-sspo/1.0.0/devops_microsoft_mapping_sspo-1.0.0-py3-none-any.whl

---
+++ packages/devops-microsoft-mapping-sspo/devops_microsoft_mapping_sspo-1.0.0-py3-none-any.whl/devops_microsoft_mapping_sspo/atomic_user_story.py
@@ -0,0 +1,23 @@
+from .user_story import UserStory
+from sspo_db.application import factories as application_factories
+from sspo_db.model import factories as model_factories
+import logging
+logging.basicConfig(level=logging.INFO)
+import re  
+
+class AtomicUserStory(UserStory):
+
+    def create(self, element):
+        try:
+            logging.info("User Story: Create Atomic User Story")
+            self.element = element
+            self.user_story = model_factories.AtomicUserStoryFactory()
+            self.user_story_application = application_factories.AtomicUserStoryFactory()
+            
+            super().create()
+            logging.info("User Story: End")
+            return self.user_story
+            
+        except Exception as e: 
+            logging.error("OS error: {0}".format(e))
+            logging.error(e.__dict__)
