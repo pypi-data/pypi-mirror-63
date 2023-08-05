8dc53eaab1ae409e19c618573ab13685824de368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:37 +0000

    Add package cs18-api-client/0.0.2.1263/cs18-api-client-0.0.2.1263.tar.gz

---
+++ packages/cs18-api-client/cs18-api-client-0.0.2.1263.tar.gz/cs18-api-client-0.0.2.1263/gateways/common/cs18_api_errors.py
@@ -0,0 +1,82 @@
+"""
+Container for Error classes used in cs18-api-client
+"""
+import json
+
+from requests import Response
+
+
+class Error:
+    def __init__(self, json_data: dict):
+        self.code = json_data["code"]
+        self.name = json_data["name"]
+        self.message = json_data["message"]
+
+    def __eq__(self, other):
+        if isinstance(other, Error):
+            return (
+                self.code == other.code
+                and self.name == other.name
+                and self.message == other.message
+            )
+        return False
+
+
+class SandboxError:
+    def __init__(self, time: str, code: str, message: str):
+        self.message = message
+        self.code = code
+        self.time = time
+
+
+class GeneralHttpException(Exception):
+    """
+    General HTTP Exception.
+    Loads additional information we want to be passed with the error.
+    """
+
+    def __init__(self, response: Response):
+        super(GeneralHttpException, self).__init__(
+            "Request to '{url}' ended with wrong status_code '{status_code}'. "
+            "Message {message}. "
+            "TraceID: {trace}.".format(
+                url=response.url,
+                status_code=response.status_code,
+                message=response.text,
+                trace=response.headers.get("X-Correlation-ID"),
+            )
+        )
+        self.status_code = response.status_code
+        self.trace_id = response.headers.get("X-Correlation-ID")
+
+
+class DevboxHttpException(GeneralHttpException):
+    """
+    Devbox HTTP Exception
+    """
+
+    def __init__(self, response: Response):
+        super(DevboxHttpException, self).__init__(response)
+        self.errors = [Error(x) for x in json.loads(response.text).get("errors")]
+
+
+class UnauthorizedException(Exception):
+    def __init__(
+            self,
+            credentials: dict
+    ):
+        super(UnauthorizedException, self).__init__(
+            "Failed to authenticate Colony client. Should provide either an access_token or both email and password."
+            "Provided credentials: {credentials}".format(credentials=credentials)
+        )
+
+
+class MaxRetriesException(Exception):
+    def __init__(self, max_retries, subject):
+        super(MaxRetriesException, self).__init__(
+            f"Timeout: {max_retries} attempts were exhausted waiting for '{subject}'"
+        )
+
+
+class SandboxNotFound(Exception):
+    pass
