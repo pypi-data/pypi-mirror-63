7553820a6e43c312328efdcc430bb93189abad16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:51 +0000

    Add package fhirpath/0.5.0/fhirpath-0.5.0-py2.py3-none-any.whl

---
+++ packages/fhirpath/fhirpath-0.5.0-py2.py3-none-any.whl/fhirpath/enums.py
@@ -0,0 +1,125 @@
+# _*_ coding: utf-8 _*_
+import enum
+import operator
+from typing import Any
+from typing import Callable
+from typing import Text
+
+
+__author__ = "Md Nazrul Islam<email2nazrul@gmail.com>"
+
+
+class FHIR_VERSION(enum.Enum):
+    """ """
+
+    DEFAULT: Text = "R4"
+    STU3: Text = "STU3"
+    R4: Text = "R4"
+    DSTU2: Text = "DSTU2"
+
+
+@enum.unique
+class SortOrderType(enum.Enum):
+    """ """
+
+    ASC: Text = "asc"
+    DESC: Text = "desc"
+
+
+@enum.unique
+class MatchType(enum.Enum):
+    """ """
+
+    ANY: Text = "ANY"
+    ALL: Text = "ALL"
+    ONE: Text = "ONE"
+    NONE: Text = "NONE"
+
+
+@enum.unique
+class TermMatchType(enum.Enum):
+    """ """
+
+    EXACT: Text = "EXACT"
+    STARTWITH: Text = "STARTWITH"
+    ENDWITH: Text = "ENDWITH"
+    FULLTEXT: Text = "FULLTEXT"
+
+
+@enum.unique
+class GroupType(enum.Enum):
+    DECOUPLED: Text = "DECOUPLED"
+    COUPLED: Text = "COUPLED"
+
+
+@enum.unique
+class WhereConstraintType(enum.Enum):
+    """ """
+
+    # normal key, value conditional
+    T1: Text = "T1"
+    # constraint certain FHIR Resource type
+    T2: Text = "T2"
+    # complex constraint with subpath
+    T3: Text = "T3"
+
+
+@enum.unique
+class EngineQueryType(enum.Enum):
+    """" """
+
+    DDL: Text = "DDL"
+    DML: Text = "DML"
+    COUNT: Text = "COUNT"
+
+
+def sa(a: Any, b: Any) -> Any:
+    """starts-after
+    the value for the parameter in the resource starts after the provided value
+    the range of the search value does not overlap with the range of the target value,
+    and the range above the search value contains the range of the target value
+    """
+    pass
+
+
+def eb(a: Any, b: Any) -> Any:
+    """ends-before
+    the value for the parameter in the resource ends before the provided value
+    the range of the search value does overlap not with the range of the target
+    value, and the range below the search value contains the range of the target value
+    """
+
+
+def ap(a: Any, b: Any) -> Any:
+    """approximately
+    the value for the parameter in the resource is approximately the same
+    to the provided value. Note that the recommended value for the approximation
+    is 10% of the stated value (or for a date, 10% of the gap between now and the date),
+    but systems may choose other values where appropriate
+    """
+
+
+@enum.unique
+class OPERATOR(enum.Enum):
+    """ """
+
+    # built-in
+    eq: Callable = operator.eq
+    ne: Callable = operator.ne
+    le: Callable = operator.le
+    lt: Callable = operator.lt
+    ge: Callable = operator.ge
+    gt: Callable = operator.gt
+    pos: Callable = operator.pos
+    neg: Callable = operator.neg
+    contains: Callable = operator.contains
+    concat: Callable = operator.concat
+    sub: Callable = operator.sub
+    xor: Callable = operator.xor
+    or_: Callable = operator.or_
+    and_: Callable = operator.and_
+    not_: Callable = operator.not_
+    # custom (FHIR)
+    ap: Callable = ap
+    sa: Callable = sa
+    eb: Callable = eb
---
+++ packages/fhirpath/fhirpath-0.5.0-py2.py3-none-any.whl/fhirpath/fhirspec/downloader.py
@@ -0,0 +1,83 @@
+# _*_ coding: utf-8 _*_
+import io
+import logging
+import os
+import shutil
+import tempfile
+import zipfile
+from http.client import HTTPResponse
+from typing import Text
+from urllib.error import HTTPError
+from urllib.request import Request
+from urllib.request import urlopen
+
+from fhirpath.enums import FHIR_VERSION
+
+
+__author__ = "Md Nazrul Islam<email2nazrul@gmail.com>"
+
+logger = logging.getLogger("fhirpath.fhirspec.downloader")
+BASE_URL: Text = (
+    "https://github.com/nazrulworld/fhirpath-scripts/blob/master/resources"
+    "/fhirspec/{release}.zip?raw=true"
+)
+
+
+def write_stream(
+    outputdir: Text, response: HTTPResponse, release: FHIR_VERSION
+) -> Text:
+    """ """
+    filename = ".".join([release.value, "zip"])
+
+    with io.open(os.path.join(outputdir, filename), "wb") as fp:
+
+        while not response.closed:
+            chunk = response.read(io.DEFAULT_BUFFER_SIZE)
+            if not chunk:
+                break
+
+            fp.write(chunk)
+    return os.path.join(outputdir, filename)
+
+
+def download_archive(release: FHIR_VERSION, temp_location: Text) -> Text:
+    """ """
+    fullurl = BASE_URL.format(release=release.value)
+
+    request = Request(url=fullurl, method="GET", unverifiable=False)
+
+    response: HTTPResponse
+    try:
+        response = urlopen(request)
+        assert response.status == 200
+    except HTTPError:
+        # xxx: handle nicely later
+        raise
+    logger.info("Archive file has been downloaded from {0}".format(fullurl))
+    return write_stream(temp_location, response, release)
+
+
+def extract_spec_files(extract_location, archive_file):
+    """ """
+    with zipfile.ZipFile(str(archive_file), "r") as zip_ref:
+        zip_ref.extractall(extract_location)
+
+
+def download_and_extract(release: FHIR_VERSION, output_dir: str):
+    """ """
+    logger.info(
+        "FHIR Resources Specification json files for release '{0}' "
+        "are not found in local disk. "
+        "Going to download...".format(release.value)
+    )
+    temp_dir = tempfile.mkdtemp()
+
+    zip_file = download_archive(release, temp_dir)
+
+    extract_spec_files(output_dir, zip_file)
+    logger.info(
+        "Downloaded archive has been extracted successfully, "
+        "now all json files are available at {0}/{1}".format(output_dir, release.value)
+    )
+    # clean up
+    shutil.rmtree(temp_dir)
