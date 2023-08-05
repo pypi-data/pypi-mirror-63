7a8ffa580d7e277a82c65539fff77023cd8b2e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package brewblox-service/0.23.1/brewblox-service-0.23.1.tar.gz

---
+++ packages/brewblox-service/brewblox-service-0.23.1.tar.gz/brewblox-service-0.23.1/test/test_main.py
@@ -0,0 +1,22 @@
+"""
+Tests brewblox_service.__main__.py
+"""
+
+from brewblox_service import __main__ as main
+
+TESTED = main.__name__
+
+
+def test_main(mocker):
+    create_mock = mocker.patch(TESTED + '.service.create_app')
+    furnish_mock = mocker.patch(TESTED + '.service.furnish')
+    run_mock = mocker.patch(TESTED + '.service.run')
+    events_mock = mocker.patch(TESTED + '.events.setup')
+    app_mock = create_mock.return_value
+
+    main.main()
+
+    create_mock.assert_called_once_with(default_name='_service')
+    furnish_mock.assert_called_once_with(app_mock)
+    run_mock.assert_called_once_with(app_mock)
+    events_mock.assert_called_once_with(app_mock)
