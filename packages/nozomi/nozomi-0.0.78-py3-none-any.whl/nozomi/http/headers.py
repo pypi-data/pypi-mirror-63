1b6bb55cdd52dd470bfad9b84134b883060eb79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package nozomi/0.0.78/nozomi-0.0.78-py3-none-any.whl

---
+++ packages/nozomi/nozomi-0.0.78-py3-none-any.whl/nozomi/http/headers.py
@@ -0,0 +1,51 @@
+"""
+Nozomi
+HTTP Headers Module
+Copyright Amatino Pty Ltd
+"""
+from typing import Optional, List
+from collections.abc import Mapping
+from nozomi.errors.error import NozomiError
+from nozomi.ancillary.immutable import Immutable
+from nozomi.http.status_code import HTTPStatusCode
+
+
+class Headers:
+    """
+    A set of HTTP headers. Feed Headers a Mapping-conformant data type, for
+    example a Dict or a Werkzeug ImmutableMultiDict.
+    """
+
+    def __init__(self, raw: Mapping = {}) -> None:
+        self._raw = raw
+        return
+
+    dictionary = Immutable(lambda s: s._raw)
+
+    def value_for(self, key: str) -> Optional[str]:
+        """
+        Return the value of a supplied header key, or None if no value
+        exists for that key.
+        """
+        value = self._raw.get(key)
+        if value is None:
+            return None
+
+        if not isinstance(value, str):
+            raise NozomiError('The data structure underlying a Headers instance\
+unexpectedly contained non-string data for key "{k}", of type {t}. Headers \
+must only contain string values. Consider examining the data you fed to the \
+Headers initialiser.', HTTPStatusCode.BAD_REQUEST)
+
+        return value
+
+    def getlist(self, key: str) -> List[str]:
+
+        if not hasattr(self._raw, 'getlist'):
+            return list()
+
+        return self._raw.getlist(key)
+
+    def add(self, key: str, value: str) -> None:
+        self._raw.add(key, value)
+        return
