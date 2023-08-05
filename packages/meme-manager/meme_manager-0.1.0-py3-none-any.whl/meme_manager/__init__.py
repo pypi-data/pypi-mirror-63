36d51cd3ee8988f8f36ff3353b952103d6967430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package meme-manager/0.1.0/meme_manager-0.1.0-py3-none-any.whl

---
+++ packages/meme-manager/meme_manager-0.1.0-py3-none-any.whl/meme_manager/__init__.py
@@ -0,0 +1,3 @@
+from .create_app import create_app
+from .cli import cli
+from .models import db, Image
---
+++ packages/meme-manager/meme_manager-0.1.0-py3-none-any.whl/meme_manager/frontend/precache-manifest.c17c3c5f04ebd13cbb6ae3544f021ca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d6e3c102a9bc50f17ecb7c78f24394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53afed548752a8d7db15",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "9645888e3ab7005bec46",
+    "url": "/static/css/main.e5797911.chunk.css"
+  },
+  {
+    "revision": "53afed548752a8d7db15",
+    "url": "/static/js/2.8c4e5516.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.8c4e5516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9645888e3ab7005bec46",
+    "url": "/static/js/main.55e8cf15.chunk.js"
+  },
+  {
+    "revision": "8226152eef1e82b6d617",
+    "url": "/static/js/runtime-main.a763cf63.js"
+  }
+]);
