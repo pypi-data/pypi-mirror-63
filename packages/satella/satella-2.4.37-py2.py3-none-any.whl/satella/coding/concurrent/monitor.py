5828ee8fe66b3077d3866901a972c63a90fec899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package satella/2.4.37/satella-2.4.37-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.4.37-py2.py3-none-any.whl/satella/coding/concurrent/monitor.py
@@ -0,0 +1,141 @@
+import logging
+import threading
+
+from ..decorators import wraps
+
+__all__ = [
+    'Monitor', 'RMonitor'
+]
+
+logger = logging.getLogger(__name__)
+
+
+class Monitor:
+    """
+    Base utility class for creating monitors (the synchronization thingies!)
+
+    These are NOT re-entrant!
+
+    Use it like that:
+
+    >>> class MyProtectedObject(Monitor):
+    >>>     def __init__(self, *args, **kwargs):
+    >>>         Monitor.__init__(self)
+    >>>         ... do your job ..
+
+    >>>     @Monitor.synchronized
+    >>>     def function_that_needs_mutual_exclusion(self):
+    >>>         .. do your threadsafe jobs ..
+
+    >>>     def function_that_partially_needs_protection(self):
+    >>>         .. do your jobs ..
+    >>>         with Monitor.acquire(self):
+    >>>             .. do your threadsafe jobs ..
+    """
+
+    def __init__(self, obj=None):
+        """You need to invoke this at your constructor
+        You can also use it to release locks of other objects."""
+        self._monitor_lock = threading.Lock()
+
+    @staticmethod
+    def synchronized(fun):
+        """
+        This is a decorator. Class method decorated with that will lock the
+        global lock of given instance, making it threadsafe. Depending on
+        usage pattern of your class and it's data semantics, your performance
+        may vary
+        """
+
+        @wraps(fun)
+        def monitored(*args, **kwargs):
+            # noinspection PyProtectedMember
+            with args[0]._monitor_lock:
+                return fun(*args, **kwargs)
+
+        return monitored
+
+    class release:
+        """
+        Returns a context manager object that can release another object
+        as long as that object is a monitor.
+
+        Consider foo, which is a monitor. You have a protected function,
+        but you feel that you can release it for a while as it would
+        improve parallelism. You can use it as such:
+
+        >>> @Monitor.synchronized
+        >>> def protected_function(self):
+        >>>     .. do some stuff that needs mutual exclusion ..
+        >>>     with Monitor.release(self):
+        >>>         .. do some I/O that doesn't need mutual exclusion ..
+        >>>     .. back to protected stuff ..
+        """
+
+        def __init__(self, foo: 'Monitor'):
+            self.foo = foo
+
+        def __enter__(self):
+            # noinspection PyProtectedMember
+            self.foo._monitor_lock.release()
+
+        def __exit__(self, e1, e2, e3):
+            # noinspection PyProtectedMember
+            self.foo._monitor_lock.acquire()
+            return False
+
+    class acquire:
+        """
+        Returns a context manager object that can lock another object,
+        as long as that object is a monitor.
+
+        Consider foo, which is a monitor. If you needed to lock it from
+        outside, you would do:
+
+        >>> with Monitor.acquire(foo):
+        >>>     .. do operations on foo that need mutual exclusion ..
+        """
+
+        def __init__(self, foo: 'Monitor'):
+            self.foo = foo
+
+        def __enter__(self):
+            # noinspection PyProtectedMember
+            self.foo._monitor_lock.acquire()
+
+        def __exit__(self, e1, e2, e3):
+            # noinspection PyProtectedMember
+            self.foo._monitor_lock.release()
+            return False
+
+    @classmethod
+    def synchronize_on(cls, monitor):
+        """
+        A decorator for locking on non-self Monitor objects
+
+        Use it like:
+
+        >>> class MasterClass(Monitor):
+        >>>     def get_object(self):
+        >>>         class SlaveClass:
+        >>>             @Monitor.synchronize_on(self)
+        >>>             def get_object(self2):
+        >>>                 ...
+        >>>         return SlaveClass
+        """
+        def outer(fun):
+            @wraps(fun)
+            def inner(*args, **kwargs):
+                with cls.acquire(monitor):
+                    return fun(*args, **kwargs)
+            return inner
+        return outer
+
+
+class RMonitor(Monitor):
+    """
+    Monitor, but using an reentrant lock instead of a normal one
+    """
+
+    def __init__(self):
+        self._monitor_lock = threading.RLock()
---
+++ packages/satella/satella-2.4.37-py2.py3-none-any.whl/satella/configuration/sources/base.py
@@ -0,0 +1,31 @@
+__all__ = [
+    'BaseSource',
+    'StaticSource'
+]
+
+
+class BaseSource(object):
+    """Base class for all configuration sources"""
+
+    def provide(self) -> dict:
+        """
+        Return your configuration, as a dict
+
+        :raise ConfigurationError: on invalid configuration
+        """
+        return {}
+
+    def __repr__(self):
+        return '<%s>' % (self.__class__.__qualname__,)
+
+
+class StaticSource(BaseSource):
+    """
+    A static piece of configuration. Returns exactly what is passed
+    """
+
+    def __init__(self, config: dict):
+        self.config = config
+
+    def provide(self) -> dict:
+        return self.config
