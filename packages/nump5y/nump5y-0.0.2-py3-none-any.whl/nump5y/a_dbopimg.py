9f96344ac36eb1ccaabc51061f4dc9ee2f6145b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package nump5y/0.0.2/nump5y-0.0.2-py3-none-any.whl

---
+++ packages/nump5y/nump5y-0.0.2-py3-none-any.whl/nump5y/a_dbopimg.py
@@ -0,0 +1,33 @@
+Ans="""
+import java.io.*;
+import java.sql.*;
+ 
+public class DatabaseImageExample {
+	public static void main(String args[]){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost/demo","root","root");
+			
+			File file=new File("E:\\image1.png");
+			FileOutputStream fos=new FileOutputStream(file);
+			byte b[];
+			Blob blob;
+			
+			PreparedStatement ps=con.prepareStatement("select * from image_table"); 
+			ResultSet rs=ps.executeQuery();
+			
+			while(rs.next()){
+				blob=rs.getBlob("image");
+				b=blob.getBytes(1,(int)blob.length());
+				fos.write(b);
+			}
+			
+			ps.close();
+			fos.close();
+			con.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
+"""
