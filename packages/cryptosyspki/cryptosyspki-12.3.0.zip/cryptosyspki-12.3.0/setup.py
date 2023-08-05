c0ea10d2c9e0f68cccf493638fb6e276587df0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package cryptosyspki/12.3.0/cryptosyspki-12.3.0.zip

---
+++ packages/cryptosyspki/cryptosyspki-12.3.0.zip/cryptosyspki-12.3.0/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+readme = read('README.rst')
+changelog = read('CHANGELOG.rst')
+
+setup(name='cryptosyspki',
+      version='12.3.0',
+      description='Python interface to CryptoSys PKI (py 3)',
+      long_description=readme + '\n\n' + changelog,
+      author='David Ireland',
+      url='https://www.cryptosys.net/pki/',
+      platforms=['Windows'],
+      py_modules=['cryptosyspki'],
+      )
