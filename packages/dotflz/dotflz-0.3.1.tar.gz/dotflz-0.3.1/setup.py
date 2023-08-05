02c7a0741695f1215f721f8cfea94e70fc2bd8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package dotflz/0.3.1/dotflz-0.3.1.tar.gz

---
+++ packages/dotflz/dotflz-0.3.1.tar.gz/dotflz-0.3.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Use README for the PyPI page
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='dotflz',
+    version='0.3.1',
+    packages=['dotflz'],
+    url='https://github.com/ivanjermakov/dotflz',
+    license='MIT',
+    author='Ivan Ermakov',
+    author_email='ivanjermakov1@gmail.com',
+    description='Utility to keep copies of dotfiles in one place',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3',
+    install_requires=['wheel', 'pyyaml', 'glob3', 'more-itertools', 'datetime', 'click'],
+    entry_points='''
+        [console_scripts]
+        dotflz=dotflz.dotflz:dotflz
+    '''
+)
