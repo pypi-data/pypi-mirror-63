9f47be609039e6addbfcea9850864871f44df276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package arjuna/0.9.7/arjuna-0.9.7.tar.gz

---
+++ packages/arjuna/arjuna-0.9.7.tar.gz/arjuna-0.9.7/arjuna/interact/gui/auto/finder/emd.py
@@ -0,0 +1,310 @@
+'''
+This file is a part of Arjuna
+Copyright 2015-2020 Rahul Verma
+
+Website: www.RahulVerma.net
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import re
+from enum import Enum, auto
+from collections import namedtuple
+
+from arjuna.core.enums import GuiElementType
+
+class ImplWith:
+    
+    def __init__(self, *, wtype, wvalue, named_args, has_content_locator):
+        self.wtype = wtype
+        self.wvalue = wvalue
+        self.named_args = named_args
+        self.has_content_locator = has_content_locator
+
+class GenericLocateWith(Enum):
+    ID = auto()
+    NAME = auto() 
+    XPATH = auto() 
+    SELECTOR = auto()
+    CLASSES = auto() 
+    LINK = auto() 
+    FLINK = auto()
+    PLINK = auto() 
+    TAG = auto()
+
+    INDEX = auto()
+    WINDOW_TITLE = auto()
+    WINDOW_PTITLE = auto()
+    ELEMENT = auto()
+    POINT = auto()
+    JS = auto() 
+
+    # Translated to XPath
+    TEXT = auto() 
+    FTEXT = auto() 
+    TITLE = auto() 
+    TYPE = auto() 
+    VALUE = auto() 
+    ATTR = auto()
+    FATTR = auto()
+    IMAGE_SRC = auto()
+    IMAGE = auto()
+
+    # Translated 1-1 to Selenium
+    PARTIAL_LINK_TEXT = auto()
+    LINK_TEXT = auto()
+    TAG_NAME = auto()
+    CSS_SELECTOR = auto()
+
+class Locator:
+
+    def __init__(self, ltype, lvalue, named_args):
+        self.ltype = ltype
+        self.lvalue = lvalue
+        self.__named_args = named_args
+
+    def set_args(self,  named_args):
+        self.__named_args = named_args
+
+    @property
+    def named_args(self):
+        return self.__named_args
+
+    def __str__(self):
+        return str(vars(self))
+
+    def is_layered_locator(self):
+        return False
+
+class GuiGenericLocator(Locator):
+
+    def __init__(self, ltype, lvalue, named_args):
+        super().__init__(ltype, lvalue, named_args)
+    
+    def set_value(self, value):
+        self.lvalue = value
+
+    def as_map(self):
+        map = dict()
+        map["withType"] = self.ltype.name
+        if self.ltype.name.lower() != "content_locator":
+            map["withValue"] = self.lvalue
+        else:
+            map["withValue"] = [l.as_map() for l in self.lvalue.locators]
+
+        # if self.__named_args:
+        #     map["named_args"] = self.__named_args
+        
+        return map
+
+# class GuiGenericChildLocator(Locator):
+#     def __init__(self, ltype, lvalue, named_args):
+#         super().__init__(ltype, lvalue, named_args)
+    
+#     def set_value(self, value):
+#         self.lvalue = value    
+
+#     def is_layered_locator(self):
+#         return True
+
+class GuiElementMetaData:
+    XPATH_TWO_ARG_VALUE_PATTERN = r'^\s*\[\s*(\w+)\s*\]\s*\[\s*(\w+)\s*\]$'
+
+    BASIC_LOCATORS = {
+        GenericLocateWith.ID,
+        GenericLocateWith.NAME,
+        GenericLocateWith.XPATH,
+        GenericLocateWith.IMAGE,
+        GenericLocateWith.INDEX,
+        GenericLocateWith.WINDOW_TITLE,
+        GenericLocateWith.WINDOW_PTITLE,
+        GenericLocateWith.POINT,
+        GenericLocateWith.JS,
+    }
+
+    NEED_TRANSLATION = {
+        GenericLocateWith.TAG : GenericLocateWith.TAG_NAME,
+        GenericLocateWith.LINK : GenericLocateWith.PARTIAL_LINK_TEXT,
+        GenericLocateWith.FLINK : GenericLocateWith.LINK_TEXT,
+        GenericLocateWith.SELECTOR : GenericLocateWith.CSS_SELECTOR,
+    }
+
+    XTYPE_LOCATORS = {
+        GuiElementType.TEXTBOX: "//input[@type='text']",
+        GuiElementType.PASSWORD: "//input[@type='password']",
+        GuiElementType.LINK: "//a",
+        GuiElementType.BUTTON: "//input[@type='button']",
+        GuiElementType.SUBMIT_BUTTON: "//input[@type='submit']",
+        GuiElementType.DROPDOWN: "//select",
+        GuiElementType.CHECKBOX: "//input[@type='checkbox']",
+        GuiElementType.RADIO: "//input[@type='radio']",
+        GuiElementType.IMAGE: "//img",
+    }
+
+    XPATH_LOCATORS = {
+        GenericLocateWith.TEXT : "//*[contains(text(),'{}')]",
+        GenericLocateWith.FTEXT : "//*[text()='{}']",
+        GenericLocateWith.VALUE : "//*[@value='{}']",
+        GenericLocateWith.TITLE : "//*[@title='{}']",
+        GenericLocateWith.IMAGE_SRC : "//img[@src='{}']"
+    }
+
+    XPATH_TWO_ARG_LOCATORS = {
+        GenericLocateWith.ATTR : "//*[contains(@{},'{}')]",
+        GenericLocateWith.FATTR : "//*[@{}='{}']",
+    }
+
+    def __init__(self, raw_locators, process_args=True):
+        self.__raw_locators = raw_locators
+        self.__locators = []
+        self.__process()
+        if process_args:
+            self.process_args()
+
+    def __str__(self):
+        return str([str(l) for l in self.__locators])
+
+    def print_locators(self):
+        print(str(self))
+
+    @property
+    def locators(self):
+        return self.__locators
+
+    @property
+    def raw_locators(self):
+        return self.__raw_locators
+
+    def __process(self):
+        for raw_locator in self.__raw_locators:
+            rltype = raw_locator.ltype
+            rlvalue = raw_locator.lvalue
+            named_args = raw_locator.named_args
+            try:
+                generic_locate_with = GenericLocateWith[rltype.upper()]
+            except:
+                raise Exception("Invalid locator across all automators: {}".format(rltype))
+            else:
+                if generic_locate_with == GenericLocateWith.ELEMENT:
+                    self.__add_locator(generic_locate_with, rlvalue, named_args)
+                elif generic_locate_with in self.BASIC_LOCATORS:
+                    self.__add_locator(generic_locate_with, rlvalue, named_args)
+                elif generic_locate_with in self.NEED_TRANSLATION:
+                    self.__add_locator(self.NEED_TRANSLATION[generic_locate_with], rlvalue, named_args)
+                elif generic_locate_with in self.XPATH_LOCATORS:
+                    self.__add_locator(GenericLocateWith.XPATH, self.XPATH_LOCATORS[generic_locate_with].format(rlvalue), named_args)
+                elif generic_locate_with in self.XPATH_TWO_ARG_LOCATORS:
+                    parts = None
+                    try:
+                        parts =  re.search(GuiElementMetaData.XPATH_TWO_ARG_VALUE_PATTERN, rlvalue).groups()
+                    except:
+                        raise Exception("Value {} for {} is misformatted. Attribute name and attribute value should be supplied as: [<arg>][<value>]".format(rlvalue, rltype))
+                    self.__add_locator(GenericLocateWith.XPATH, self.XPATH_TWO_ARG_LOCATORS[generic_locate_with].format(parts[0], parts[1]), named_args)
+                elif generic_locate_with == GenericLocateWith.TYPE:
+                    try:
+                        elem_type = GuiElementType[rlvalue.upper()]
+                    except:
+                        raise Exception("Unsupported element type for XTYPE locator: " + rlvalue)
+                    else:
+                        self.__add_locator(GenericLocateWith.XPATH, self.XTYPE_LOCATORS[elem_type], named_args)
+                # elif generic_locate_with == GenericLocateWith.COMPOUND_CLASS:
+                #     self.__add_locator(GenericLocateWith.CSS_SELECTOR, re.sub(r'\s+', '.', ), named_args)
+                elif generic_locate_with == GenericLocateWith.CLASSES:
+                    css_string = None
+                    if len(rlvalue) == 0:
+                        raise Exception("You must pass atleast one class name.")
+                    elif len(rlvalue) == 1:
+                        css_string = "." + rlvalue[0].replace('.', ' ').strip()
+                    else:
+                        css_string = "." + ".".join(rlvalue)
+                    self.__add_locator(GenericLocateWith.CSS_SELECTOR, re.sub(r'\s+', '.', css_string), named_args)
+                else:
+                    raise Exception("Locator not supported yet by Arjuna: " + rltype)
+
+    def __add_locator(self, locator_type, locator_value, named_args):
+        self.locators.append(GuiGenericLocator(locator_type, locator_value, named_args))
+
+    def process_args(self):
+        '''
+            It consumes impl locator list.
+        '''
+        pattern = r"\$(\s*\w*?\s*)\$"
+        for locator in self.locators:
+            if locator.ltype == GenericLocateWith.ELEMENT:
+                locator.lvalue.process_args()
+
+            if not locator.named_args: continue
+
+            fmt_locator_value = locator.lvalue.replace("\{", "__LB__").replace("}", "__RB__")
+
+            # Find params
+            matches = re.findall(pattern, fmt_locator_value)
+            
+            for match in matches:
+                target = "${}$".format(match)
+                repl = "{" + match.lower().strip() + "}"
+                fmt_locator_value = fmt_locator_value.replace(target, repl)
+
+            repl = {k.lower():v for k,v in locator.named_args.items()}
+            fmt_locator_value = fmt_locator_value.format(**repl)
+            fmt_locator_value = fmt_locator_value.replace("__LB__", "{").replace("__RB__", "}")
+            locator.set_value(fmt_locator_value)
+
+
+    @classmethod
+    def __process_single_raw_locator(cls, impl_locator):
+        ltype = impl_locator.wtype
+        lvalue = impl_locator.wvalue
+        p_locator = Locator(ltype=ltype, lvalue=lvalue, named_args=impl_locator.named_args)
+        return p_locator
+
+    @classmethod
+    def convert_to_impl_with_locators(cls, *with_locators):
+        out_list = []
+        for locator in with_locators:
+            l = isinstance(locator, ImplWith) and locator or locator.as_impl_locator()
+            out_list.append(l)
+        return out_list
+
+    @classmethod
+    def create_lmd(cls, *locators):
+        impl_locators = cls.convert_to_impl_with_locators(*locators)
+        processed_locators = []
+        for locator in impl_locators:
+            ltype = locator.wtype.lower()
+            if ltype == "content_locator":
+                CONTENT_LOCATOR = cls.create_lmd(locator.wvalue)
+                p_locator = Locator(ltype=locator.wtype, lvalue=CONTENT_LOCATOR, named_args=None)
+            else:
+                p_locator = cls.__process_single_raw_locator(locator)
+            processed_locators.append(p_locator)
+        return GuiElementMetaData(processed_locators)
+
+    @staticmethod
+    def locators_as_str(locators):
+        if not locators:
+            return list()
+
+        from arjuna.interact.gui.helpers import With
+        out_list = []
+        for l in locators:
+            if isinstance(l, GuiGenericLocator) or isinstance(l, With):
+                out_list.append(l.as_map())
+            else:
+                out_list.append(str(l))
+        return out_list
+
+class SimpleGuiElementMetaData(GuiElementMetaData):
+
+    def __init__(self, locator_type, locator_value, named_args=dict()):
+        super().__init__([Locator(ltype=locator_type, lvalue=locator_value, named_args=named_args)])
---
+++ packages/arjuna/arjuna-0.9.7.tar.gz/arjuna-0.9.7/arjuna/interact/gui/gom/__init__.py
@@ -0,0 +1,170 @@
+'''
+This file is a part of Arjuna
+Copyright 2015-2020 Rahul Verma
+
+Website: www.RahulVerma.net
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+from .gui import *
+from arjuna.interact.gui.auto.finder.emd import GuiElementMetaData
+
+class Page(AppContent):
+
+    def __init__(self, *args, source_gui, label=None, **kwargs):
+        # app = isinstance(source_gui, App) and source_gui or source_gui.app
+        super().__init__(automator=source_gui.automator, label=label)
+        self.app.ui = self
+        self._load(*args, **kwargs)
+
+class Section(AppContent):
+
+    def __init__(self, gui, *args, root_element_locators=None, label=None, **kwargs):
+        super().__init__(automator=gui.automator, label=label)   
+        self.__root_element_locators = root_element_locators
+        self.__root_element = None
+        self._load(*args, **kwargs)
+        self.__parent = gui
+
+    def load_root_element(self):
+        gns_re_locators = None
+        coded_re_locators = None
+        # From GUI Def
+        if self.externalized:
+            gns_re_locators = self.gui_def.root_element_with_locators
+
+        # From constructor. Overrides Gui Def if both places have the def.        
+        if self.__root_element_locators is not None:
+            if type(self.__root_element_locators) not in {list, tuple}:
+                self.__root_element_locators = (self.__root_element_locators,)
+            coded_re_locators = self.__root_element_locators
+
+        self.__root_element_locators = self.__root_element_locators and coded_re_locators or coded_re_locators or gns_re_locators
+
+        from arjuna import Arjuna
+        Arjuna.get_logger().debug("Loaded Root Element for {} widget. Loaded as: {}. Externalized: {}. RE locators in GNS: {}. RE in __init__: {}.".format(
+            self.label,
+            GuiElementMetaData.locators_as_str(self.__root_element_locators),
+            self.externalized,
+            GuiElementMetaData.locators_as_str(gns_re_locators),
+            GuiElementMetaData.locators_as_str(coded_re_locators)
+        ))
+
+        if self.__root_element_locators is not None:
+            self.__root_element = super().element(*self.__root_element_locators)
+
+    @property
+    def root(self):
+        return self.__root_element
+
+    def element(self, *str_or_with_locators, iconfig=None):
+        if self.root:
+            return self.root.element(*str_or_with_locators, iconfig=iconfig)
+        else:
+            return super().element(*str_or_with_locators, iconfig=iconfig)
+
+    def multi_element(self, *str_or_with_locators, iconfig=None):
+        return self.automator.multi_element(self, self.convert_to_with_lmd(*str_or_with_locators), iconfig=iconfig)
+
+    @property
+    def parent(self):
+        return self.__parent
+
+Widget = Section
+Dialog = Section
+
+class App(Gui, metaclass=abc.ABCMeta):
+
+    def __init__(self, *, config=None, ext_config=None, label=None, gns_dir=None):
+        super().__init__(config=config, ext_config=ext_config, label=label)
+        self.__ui = None
+        self.__automator = None
+        self.__gns_dir = gns_dir
+
+    @property
+    def automator(self):
+        return self.__automator
+
+    @property
+    def gns_dir(self):
+        return self.__gns_dir
+
+    @gns_dir.setter
+    def gns_dir(self, d):
+        self.__gns_dir = d
+
+    def _launchautomator(self):
+        # Default Gui automation engine is Selenium
+        from arjuna.interact.gui.auto.automator import GuiAutomator
+        self.__automator = GuiAutomator(self, self.config, self.ext_config)    
+
+    @property
+    def ui(self):
+        return self.__ui
+
+    @ui.setter
+    def ui(self, page):
+        self.__ui = page
+
+    def _create_default_ui(self):
+        self.__ui = Page(source_gui=self, label="{}-Def-UI".format(self.label))
+
+    @abc.abstractmethod
+    def launch(self):
+        pass
+
+    def prepare_widget(self, widget_object):
+        return widget_object
+
+
+class WebApp(App):
+
+    def __init__(self, *args, base_url=None, blank_slate=False, config=None, ext_config=None, label=None, gns_dir=None, **kwargs):
+        '''
+            Creates and returns GuiAutomator object for provided config.
+            If no configuration is provided reference configuration is used.
+            You can also provide GuiDriverExtendedConfig for extended configuration for WebDriver family of libs. 
+        '''
+        super().__init__(config=config, ext_config=ext_config, label=label, gns_dir=gns_dir)
+        from arjuna.core.enums import ArjunaOption
+        self.__base_url = base_url is not None and base_url or self.config.value(ArjunaOption.APP_URL)
+        # self._load(*args, **kwargs)
+        self.__args = args
+        self.__kwargs = kwargs
+
+    @property
+    def base_url(self):
+        return self.__base_url
+
+    def launch(self, blank_slate=False):
+        self._launchautomator()
+        if not blank_slate:
+            self.automator.browser.go_to_url(self.base_url)
+        self._create_default_ui()
+        self._load(*self.__args, **self.__kwargs)
+
+    def quit(self):
+        self.automator.quit()
+
+    def __getattr__(self, name):
+        return getattr(self.ui, name)
+
+    def externalize(self, *, gns_dir=None, gns_file_name=None):
+        self.__gns_file_name = gns_file_name is not None and gns_file_name or "{}.yaml".format(self.label)        
+        from arjuna.core.enums import ArjunaOption   
+        self.gns_dir = gns_dir and gns_dir or self.gns_dir
+        if not self.gns_dir:
+            self.gns_dir = ""
+        self.ui.externalize(gns_dir=self.gns_dir, gns_file_name=self.__gns_file_name)
+        self._set_externalized()
