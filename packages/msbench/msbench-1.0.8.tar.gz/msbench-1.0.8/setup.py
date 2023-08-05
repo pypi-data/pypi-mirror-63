afd028869791c40acf186fb5f94dbe314abd8ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:55 +0000

    Add package msbench/1.0.8/msbench-1.0.8.tar.gz

---
+++ packages/msbench/msbench-1.0.8.tar.gz/msbench-1.0.8/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from io import open
+from setuptools import setup, find_packages
+
+with open('README.md', encoding="utf-8") as f:
+    readme = f.read()
+
+with open('LICENSE', encoding="utf-8") as f:
+    license = f.read()
+
+setup(
+    name="msbench",
+    version="1.0.8",
+    description="Benchmarking tool for marketstore",
+    author="Daito Akimura",
+    install_requires=["pymarketstore", "argparse", ],
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        "console_scripts": [
+            "msbench = msbench.app:main",
+        ]
+    }
+)
