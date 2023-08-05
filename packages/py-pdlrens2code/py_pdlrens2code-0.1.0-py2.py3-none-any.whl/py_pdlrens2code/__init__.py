19883fbcf204dc7052ecac7ccee21bb6480bfdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package py-pdlrens2code/0.1.0/py_pdlrens2code-0.1.0-py2.py3-none-any.whl

---
+++ packages/py-pdlrens2code/py_pdlrens2code-0.1.0-py2.py3-none-any.whl/py_pdlrens2code/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for py_pdlrens2code."""
+
+__author__ = """Aritra Biswas"""
+__email__ = 'pandalearnstocode@gmail.com'
+__version__ = '0.1.0'
---
+++ packages/py-pdlrens2code/py_pdlrens2code-0.1.0-py2.py3-none-any.whl/py_pdlrens2code/cli.py
@@ -0,0 +1,16 @@
+"""Console script for py_pdlrens2code."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for py_pdlrens2code."""
+    click.echo("Replace this message by putting your code into "
+               "py_pdlrens2code.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
