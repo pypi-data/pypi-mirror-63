78383f244886c4595fa06a0c9979382b359c728e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:35 +0000

    Add package cfg-cli/0.1.2/cfg-cli-0.1.2.tar.gz

---
+++ packages/cfg-cli/cfg-cli-0.1.2.tar.gz/cfg-cli-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['cfg-cli']
+install_requires = \
+['better-exceptions>=0.2.2,<0.3.0',
+ 'boto3>=1.12.17,<2.0.0',
+ 'fire>=0.2.1,<0.3.0',
+ 'pandas==1.0.1',
+ 'requests>=2.23.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['cfg-cli = src.cfg-cli']}
+
+setup_kwargs = {
+    'name': 'cfg-cli',
+    'version': '0.1.2',
+    'description': "Command-line tool for Cofactor Genomics' products and services.",
+    'long_description': None,
+    'author': 'Alex Bode',
+    'author_email': 'alex_bode@cofactorgenomics.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
