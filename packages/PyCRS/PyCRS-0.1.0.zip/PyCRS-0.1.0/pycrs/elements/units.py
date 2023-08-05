4bf79a48d110a6fa1c13b7fe964b371fae8b0e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package PyCRS/0.1.0/PyCRS-0.1.0.zip

---
+++ packages/PyCRS/PyCRS-0.1.0.zip/PyCRS-0.1.0/pycrs/elements/units.py
@@ -0,0 +1,46 @@
+
+
+def find(unitname, crstype, strict=False):
+    if not strict:
+        unitname = unitname.lower()
+    for itemname,item in globals().items():
+        if itemname.startswith("_"):
+            continue
+        try:
+            if hasattr(item, crstype):
+                itemname = getattr(item, crstype)
+                # compare
+                if not strict:
+                    itemname = itemname.lower()
+                if unitname == itemname:
+                    return item
+                # special handling of wkt meters which has multiple possibilities
+                elif isinstance(item(), Meter) and crstype.endswith("wkt") and not strict and unitname in ("meters","meter","metre","m"):
+                    return item
+        except:
+            pass
+    else:
+        return None
+
+
+
+
+class Meter:
+    proj4 = "m"
+    ogc_wkt = "Meters" # or is it metre?? sometimes even Meter?
+    esri_wkt = "Meter"
+
+class Degree:
+    proj4 = "degrees"
+    ogc_wkt = "degree"
+    esri_wkt = "Degree"
+
+class Feet:
+    proj4 = "..."
+    ogc_wkt = "Foot_US"
+    esri_wkt = "Foot_US"
+
+class Unknown:
+    proj4 = ""
+    ogc_wkt = "Unknown"
+    esri_wkt = "Unknown"
