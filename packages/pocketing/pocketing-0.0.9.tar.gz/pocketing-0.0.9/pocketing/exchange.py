5974e3f8521dda07a01c25b3cdb859cbd151f322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:19 +0000

    Add package pocketing/0.0.9/pocketing-0.0.9.tar.gz

---
+++ packages/pocketing/pocketing-0.0.9.tar.gz/pocketing-0.0.9/pocketing/exchange.py
@@ -0,0 +1,23 @@
+"""
+exchange.py
+-------------
+
+Format toolpaths as JSON or G-Code (eventually)
+"""
+
+import json
+
+
+def to_json(paths, radius, **kwargs):
+    """
+    """
+    return json.dumps(
+        {'radius': radius,
+         'path': toolpath.tolist(),
+         'bounds': np.reshape(polygon.bounds, (2, 2)).tolist()}, **kwargs)
+
+
+def from_json(obj):
+    """
+    """
+    pass
