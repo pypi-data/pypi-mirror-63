9e10cdabbbef7b478af97d68df4fd885966501a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:59 +0000

    Add package plone.protect/3.1.5/plone.protect-3.1.5.tar.gz

---
+++ packages/plone.protect/plone.protect-3.1.5.tar.gz/plone.protect-3.1.5/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '3.1.5'
+
+setup(
+    name='plone.protect',
+    version=version,
+    description="Security for browser forms",
+    long_description='%s\n%s' % (
+        open("README.rst").read(),
+        open("CHANGES.rst").read()
+    ),
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='zope security CSRF',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/plone.protect',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.keyring >= 3.0dev',
+        'six',
+        'zope.component',
+        'zope.interface',
+        'Zope2',
+        'plone.transformchain',
+        'repoze.xmliter>=0.3',
+        'five.globalrequest',
+        'collective.monkeypatcher'
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'Products.CMFPlone'
+        ],
+    }
+)
