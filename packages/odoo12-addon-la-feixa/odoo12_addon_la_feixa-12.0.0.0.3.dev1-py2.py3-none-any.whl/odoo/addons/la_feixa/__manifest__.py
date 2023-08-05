c74e4cc675c92856289479b9c14c76c3f25a0ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package odoo12-addon-la-feixa/12.0.0.0.3.dev1/odoo12_addon_la_feixa-12.0.0.0.3.dev1-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-la-feixa/odoo12_addon_la_feixa-12.0.0.0.3.dev1-py2.py3-none-any.whl/odoo/addons/la_feixa/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Odoo La Feixa customizations",
+    'version': '12.0.0.0.3',
+    'depends': ['easy_my_coop_es'],
+    'author': "Coopdevs Treball SCCL",
+    'category': "Cooperative management",
+    'description': """
+    Odoo La Feixa customizations.
+    """,
+    "license": "AGPL-3",
+    'data': [
+        "views/become_cooperator_view.xml",
+        "views/subscription_request_view.xml",
+    ],
+}
