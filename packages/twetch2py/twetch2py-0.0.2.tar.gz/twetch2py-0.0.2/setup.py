cd22fcd3b5c2b9bd0f8e2097edc87df70100c0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package twetch2py/0.0.2/twetch2py-0.0.2.tar.gz

---
+++ packages/twetch2py/twetch2py-0.0.2.tar.gz/twetch2py-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'twetch2py',         
+  packages = ['twetch2py'],   
+  version = '0.0.2', 
+  license='MIT', 
+  description = 'A class-based implementation of the Twetch SDK for Python',
+  author = 'Cyphergato, LLC',
+  author_email = 'cyphergato@protonmail.com',
+  url = 'https://gitlab.com/cyphergato/twetch2py',
+  download_url = 'https://gitlab.com/cyphergato/twetch2py/-/archive/master/twetch2py-master.tar.gz',
+  keywords = ['bitcoin', 'twetch', 'sdk'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
