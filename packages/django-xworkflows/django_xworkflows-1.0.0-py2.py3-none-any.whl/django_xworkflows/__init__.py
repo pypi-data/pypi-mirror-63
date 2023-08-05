4107dced3cf573b1d11a96a9d4cd8bd29ec69c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:44 +0000

    Add package django-xworkflows/1.0.0/django_xworkflows-1.0.0-py2.py3-none-any.whl

---
+++ packages/django-xworkflows/django_xworkflows-1.0.0-py2.py3-none-any.whl/django_xworkflows/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2020 Raphaël Barrois
+# This code is distributed under the two-clause BSD license.
+
+__version__ = '1.0.0'
+__author__ = 'Raphaël Barrois <raphael.barrois+xworkflows@polytechnique.org>'
