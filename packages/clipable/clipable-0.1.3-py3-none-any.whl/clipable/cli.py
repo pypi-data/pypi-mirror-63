3a319bb4633104040c6783ac4fa543ac4a708be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package clipable/0.1.3/clipable-0.1.3-py3-none-any.whl

---
+++ packages/clipable/clipable-0.1.3-py3-none-any.whl/clipable/cli.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pyperclip
+import argparse
+import io
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Your clipboard(Excel or Google spreadsheet) change to markdown clipboard',
+        add_help=True,
+    )
+    parser.add_argument('-f', help='Please input format type(csv)', default=None, type=str)
+
+    try:
+
+        args = parser.parse_args()
+        if args.f is None:
+            data = pd.read_clipboard()
+        elif args.f == 'csv':
+            data = pd.read_csv(io.StringIO(pyperclip.paste()))
+        print(data.head(3).to_markdown())
+        print('......Succeeded!!')
+        pyperclip.copy(data.to_markdown())
+
+    except Exception as e:
+        print('Error: cannot clipboard to markdown. Please check your clipboard')
+
+
+if __name__ == '__main__':
+    main()
