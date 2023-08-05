d55c71365a490c7641d4a1c5c838be4401affee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:25 +0000

    Add package emora-stdm/1.30/emora-stdm-1.30.tar.gz

---
+++ packages/emora-stdm/emora-stdm-1.30.tar.gz/emora-stdm-1.30/emora_stdm/test_state_transition_dialogue_manager/interactive_json_style_dialogue_flow.py
@@ -0,0 +1,52 @@
+
+from emora_stdm import DialogueFlow
+
+
+df = DialogueFlow('root', initial_speaker=DialogueFlow.Speaker.SYSTEM)
+
+flow = {
+    'state': 'root',
+
+    'hello': {
+        '[hi]': {
+            'have you seen a good movie recently':{
+                '[yes]': {
+                    'cool what is it':{
+                        'error': 'root'
+                    }
+                },
+                '[no]':{
+                    'ok then': {
+                        'error': 'root'
+                    }
+                }
+            },
+            'how are you': {
+                '[good]': {
+                    'thats good': {
+                        'error': 'root'
+                    }
+                },
+                '[bad]': {
+                    'sorry to hear that':{
+                        'error': 'root'
+                    }
+                }
+            }
+        },
+        '[how, you]': {
+            'good': {
+                'error': 'root'
+            }
+        }
+    }
+}
+df.load_transitions(flow, DialogueFlow.Speaker.SYSTEM)
+
+df.state_settings('root').update(system_multi_hop=True)
+df.add_state('recovery_question', global_nlu='[!{do, who, what, when, where, why, how, is, can, should}, /.*/]')
+df.add_system_transition('recovery_question', 'root', '"Hmm.. I\'m not sure."')
+
+
+if __name__ == '__main__':
+    df.run(debugging=True)
