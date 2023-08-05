c56f65bb6c61beb8eb16bf292b6e894b164197db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:36 +0000

    Add package pmdatapipelines-light/0.0.2/pmdatapipelines-light-0.0.2.tar.gz

---
+++ packages/pmdatapipelines-light/pmdatapipelines-light-0.0.2.tar.gz/pmdatapipelines-light-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pmdatapipelines-light",
+	version="0.0.2",
+	author="Jørgen Frøland",
+	author_email="jorgen.froland@polarismedia.no",
+	description="Lightweight version of data pipeline library for Polaris Media",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'boto3'
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
