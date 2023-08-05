70f11b12adc3186729363139e098486acadb6422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package log-ext/1.0.0/log_ext-1.0.0.tar.gz

---
+++ packages/log-ext/log_ext-1.0.0.tar.gz/log_ext-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='log_ext',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/CpHarding/py_logging_extensions',
+    license='MIT License',
+    author='CpHarding',
+    author_email='',
+    description='An extension for standard python logging',
+    python_requires='>=3.6',
+)
