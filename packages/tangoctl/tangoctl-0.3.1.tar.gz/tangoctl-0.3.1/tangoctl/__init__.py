9543b0652af48e38e4d4a2d2e47200ca451cb7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:00 +0000

    Add package tangoctl/0.3.1/tangoctl-0.3.1.tar.gz

---
+++ packages/tangoctl/tangoctl-0.3.1.tar.gz/tangoctl-0.3.1/tangoctl/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the tangoctl project
+#
+# Copyright (c) 2018 Tiago Coutinho
+# Distributed under the MIT license. See LICENSE for more info.
+
+__version__ = '0.3.1'
---
+++ packages/tangoctl/tangoctl-0.3.1.tar.gz/tangoctl-0.3.1/tangoctl/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the tangoctl project
+#
+# Copyright (c) 2018 Tiago Coutinho
+# Distributed under the MIT license. See LICENSE for more info.
+
+from .cli import cli
+cli()
