6313e57b2571d47d77953187448dd7734cfca351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:42 +0000

    Add package django-db-obfuscator/0.1.3/django-db-obfuscator-0.1.3.tar.gz

---
+++ packages/django-db-obfuscator/django-db-obfuscator-0.1.3.tar.gz/django-db-obfuscator-0.1.3/README.md
@@ -0,0 +1,22 @@
+# Django DB Obfuscator
+
+[![Generic badge](https://img.shields.io/pypi/v/django-db-obfuscator.svg)](https://pypi.python.org/pypi/django-db-obfuscator)
+[![Generic badge](https://img.shields.io/travis/oesah/django-db-obfuscator.svg)](https://travis-ci.com/oesah/django-db-obfuscator)
+[![Generic badge](https://readthedocs.org/projects/django-db-obfuscator/badge/?version=latest)](https://django-db-obfuscator.readthedocs.io/en/latest/?badge=latest)
+[![Generic badge](https://pyup.io/repos/github/oesah/django-db-obfuscator/shield.svg)](https://pyup.io/repos/github/oesah/django-db-obfuscator/)
+
+Django app to obfuscate text data.
+
+-   Free software: MIT license
+-   Documentation: https://obfuscator.readthedocs.io.
+
+## Features
+
+-   TODO
+
+## Credits
+
+This package was created with:
+
+-   [Cookiecutter](https://github.com/audreyr/cookiecutter)
+-   [`audreyr/cookiecutter-pypackage`](https://github.com/audreyr/cookiecutter-pypackage)
---
+++ packages/django-db-obfuscator/django-db-obfuscator-0.1.3.tar.gz/django-db-obfuscator-0.1.3/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest>=3',
+]
+
+setup(
+    author="Oezer Sahin",
+    author_email='os@oesah.ch',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Django app to obfuscate text data.",
+    entry_points={
+        'console_scripts': [
+            'obfuscator=obfuscator.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords='obfuscator',
+    name='django-db-obfuscator',
+    packages=find_packages(include=['obfuscator', 'obfuscator.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/oesah/django-db-obfuscator',
+    version='0.1.3',
+    zip_safe=False,
+)
