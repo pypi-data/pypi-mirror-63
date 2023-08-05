b9ff20f09e638889fe28193091c21395660b8eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package pyfrost/0.1.1/pyfrost-0.1.1.tar.gz

---
+++ packages/pyfrost/pyfrost-0.1.1.tar.gz/pyfrost-0.1.1/frost/server/storage/__init__.py
@@ -0,0 +1,5 @@
+from frost.server.storage.base import Base
+from frost.server.storage.models import User, Room, Message
+
+
+__all__ = ('Base', 'User', 'Room', 'Message')
---
+++ packages/pyfrost/pyfrost-0.1.1.tar.gz/pyfrost-0.1.1/tests/run_client.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+from frost import FrostClient
+from frost.client.socketio import threaded
+
+
+@dataclass
+class Messages:
+    contents = dict()
+
+
+# Store messages in memory globally
+messages = Messages()
+
+
+@threaded(daemon=True)
+def listen(client: 'FrostClient') -> None:
+    while True:
+        msgs = client.get_new_msgs()
+
+        if msgs:
+            for msg_id, msg in msgs.items():
+                messages.contents[msg_id] = msg
+
+            print(messages.contents)
+
+
+@threaded()
+def send_msg(client: 'FrostClient'):
+    while True:
+        msg = input()
+        client.send_msg(msg)
+
+
+def run_client():
+    # with FrostClient() as client:
+    client = FrostClient()
+    client.connect()
+
+    client.login(
+        input('Username: '),
+        input('Password: ')
+    )
+    msgs = client.get_all_msgs()
+    messages.contents = msgs
+    print(msgs)
+
+    listen(client)
+    send_msg(client)
+
+    # client.close()
