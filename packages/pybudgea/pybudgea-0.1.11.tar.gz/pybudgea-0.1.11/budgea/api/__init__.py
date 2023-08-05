4b907abac0712116c84602b979e15b28bd18d802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:54 +0000

    Add package pybudgea/0.1.11/pybudgea-0.1.11.tar.gz

---
+++ packages/pybudgea/pybudgea-0.1.11.tar.gz/pybudgea-0.1.11/budgea/api/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from budgea.api.administration_api import AdministrationApi
+from budgea.api.authentication_api import AuthenticationApi
+from budgea.api.banks_api import BanksApi
+from budgea.api.connections_api import ConnectionsApi
+from budgea.api.documents_api import DocumentsApi
+from budgea.api.ocr_api import OCRApi
+from budgea.api.oidc_api import OIDCApi
+from budgea.api.pfm_api import PFMApi
+from budgea.api.providers_api import ProvidersApi
+from budgea.api.recipients_api import RecipientsApi
+from budgea.api.terms_api import TermsApi
+from budgea.api.transfer_api import TransferApi
+from budgea.api.transfers_api import TransfersApi
+from budgea.api.users_management_api import UsersManagementApi
+from budgea.api.wealth_api import WealthApi
+from budgea.api.default_api import DefaultApi
