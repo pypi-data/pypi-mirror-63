b4366020e907ca2f59b5da6e7a4064ebd8fa0919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package sehan-helloworld/0.0.2/sehan_helloworld-0.0.2.tar.gz

---
+++ packages/sehan-helloworld/sehan_helloworld-0.0.2.tar.gz/sehan_helloworld-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'sehan_helloworld',         # How you named your package folder (MyLib)
+  version='0.0.2',
+  description='nothing special',
+  package_dir={'':'src'},
+  py_modules=['helloworld']
+  
+)
