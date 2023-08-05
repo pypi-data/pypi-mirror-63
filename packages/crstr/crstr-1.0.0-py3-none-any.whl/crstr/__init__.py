eef9edb0a7ed2e658934a74ad20d61303103878b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:34 +0000

    Add package crstr/1.0.0/crstr-1.0.0-py3-none-any.whl

---
+++ packages/crstr/crstr-1.0.0-py3-none-any.whl/crstr/__init__.py
@@ -0,0 +1,18 @@
+'''
+shell wrapper for rstr (https://pypi.org/project/rstr/)
+
+usage
+
+  python -m crstr [options]
+
+You can give this command an alias to simplify the it:   
+Add the following line to you bashrc (Usually `~/.bashrc` (current user) or 
+`/etc/bashrc` (global); if you use Git For Windows, it's `/etc/bash.bashrc`)
+
+  alias crstr='python -m crstr'
+
+Then you can use the simplified command
+
+  crstr [options]
+
+'''
