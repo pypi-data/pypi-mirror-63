42bd26d9f6aefc1a86c7078cb5c8cc0c0dba3a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:10 +0000

    Add package localstack-ext/0.10.124/localstack-ext-0.10.124.tar.gz

---
+++ packages/localstack-ext/localstack-ext-0.10.124.tar.gz/localstack-ext-0.10.124/localstack_ext/bootstrap/aws_models.py
@@ -0,0 +1,18 @@
+from localstack.utils.aws import aws_models
+cHCue=super
+cHCuY=None
+cHCul=id
+class LambdaLayer(aws_models.LambdaFunction):
+ def __init__(self,arn):
+  cHCue(LambdaLayer,self).__init__(arn)
+  self.cwd=cHCuY
+  self.runtime=''
+  self.handler=''
+  self.envvars={}
+  self.versions={}
+class RDSDatabase(aws_models.Component):
+ def __init__(self,cHCul,env=cHCuY):
+  cHCue(RDSDatabase,self).__init__(cHCul,env=env)
+ def name(self):
+  return self.cHCul.split(':')[-1]
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
