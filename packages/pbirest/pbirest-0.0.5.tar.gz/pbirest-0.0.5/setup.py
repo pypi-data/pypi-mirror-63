055a2d691cd94e7381af5529cdfd8fb86845b35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package pbirest/0.0.5/pbirest-0.0.5.tar.gz

---
+++ packages/pbirest/pbirest-0.0.5.tar.gz/pbirest-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name = "pbirest",
+    packages = setuptools.find_packages(),
+    version = "0.0.5",
+    license = "MIT",
+    description = "A Python library created to easily use the Power BI REST API with Python",
+    long_description = "A Python library created to easily use the Power BI REST API with Python",
+    author = "Antoine DE WILDE",
+    author_email = "antoinedewilde7@gmail.com",
+    url = "https://github.com/AntoineDW/powerbi-rest-api-python",
+    keywords = ["power bi", "powerbi", "rest", "api", "rest api"],
+    install_requires = [
+        "requests"
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
