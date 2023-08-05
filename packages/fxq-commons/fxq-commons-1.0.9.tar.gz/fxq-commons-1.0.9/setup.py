ded989c2f997c387885ad79f0e72ec54e3aa725b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:22 +0000

    Add package fxq-commons/1.0.9/fxq-commons-1.0.9.tar.gz

---
+++ packages/fxq-commons/fxq-commons-1.0.9.tar.gz/fxq-commons-1.0.9/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+
+from setuptools import setup
+
+setup(
+    name='fxq-commons',
+    version='1.0.9',
+    packages=[
+        'fxq.core',
+        'fxq.core.beans',
+        'fxq.core.beans.factory',
+        'fxq.core.beans.factory.annotation',
+        'fxq.core.context',
+        'fxq.core.stereotype',
+        'fxq.commons',
+        'fxq.model'
+    ],
+    url='https://bitbucket.org/fxqlabs-oss/fxq-commons/',
+    license='MIT',
+    author='Jonathan Turnock',
+    author_email='jonathan.turnock@outlook.com',
+    description='Common python utilities and scripts for fxq labs',
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'stringcase',
+        'requests',
+        'multidispatch',
+        'jsonpickle'
+    ]
+)
