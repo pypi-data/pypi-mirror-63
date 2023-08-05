8dfc0a48aed147ce71fe74a687f0133a78b470a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package dbx-deploy/0.8.6/dbx-deploy-0.8.6.tar.gz

---
+++ packages/dbx-deploy/dbx-deploy-0.8.6.tar.gz/dbx-deploy-0.8.6/src/dbxdeploy/notebook/converter/PythonNotebookConverter.py
@@ -0,0 +1,62 @@
+import re
+from pathlib import Path, PurePosixPath
+from dbxdeploy.notebook.LibsRunPreparer import LibsRunPreparer
+from dbxdeploy.notebook.converter.CellsExtractor import CellsExtractor
+from dbxdeploy.notebook.converter.DbcScriptRenderer import DbcScriptRenderer
+from dbxdeploy.notebook.converter.JinjaTemplateLoader import JinjaTemplateLoader
+from dbxdeploy.notebook.converter.NotebookConverterInterface import NotebookConverterInterface
+
+class PythonNotebookConverter(NotebookConverterInterface):
+
+    def __init__(
+        self,
+        consumerGlobs: list,
+        jobGlobs: list,
+        libsRunPreparer: LibsRunPreparer,
+        cellsExtractor: CellsExtractor,
+        jinjaTemplateLoader: JinjaTemplateLoader,
+        dbcScriptRenderer: DbcScriptRenderer,
+    ):
+        self.__consumerGlobs = consumerGlobs
+        self.__jobGlobs = jobGlobs
+        self.__libsRunPreparer = libsRunPreparer
+        self.__cellsExtractor = cellsExtractor
+        self.__jinjaTemplateLoader = jinjaTemplateLoader
+        self.__dbcScriptRenderer = dbcScriptRenderer
+
+    def toDbcNotebook(self, notebookPath: Path, whlFilename: PurePosixPath) -> str:
+        originalScript = self.__loadNotebook(notebookPath)
+        cells = self.__cellsExtractor.extract(originalScript, r'#%%\n+')
+        template = self.__jinjaTemplateLoader.load()
+
+        return self.__dbcScriptRenderer.render(notebookPath, template, cells, whlFilename)
+
+    def toWorkspaceImportNotebook(self, notebookPath: Path, whlFilename: PurePosixPath) -> str:
+        script = self.__loadNotebook(notebookPath)
+
+        script = (
+            '# Databricks notebook source\n\n' +
+            self.__libsRunPreparer.prepare(whlFilename) + '\n\n' +
+            script
+        )
+
+        cellSeparatorRegex = re.compile(r'^#%%', re.MULTILINE)
+        script = re.sub(cellSeparatorRegex, '# COMMAND ----------', script)
+
+        return script
+
+    def resolves(self, fileExtension: str) -> bool:
+        return fileExtension == 'py'
+
+    def getGlobPatterns(self) -> list:
+        return self.__consumerGlobs + self.__jobGlobs
+
+    def getConsumerGlobPatterns(self) -> list:
+        return self.__consumerGlobs
+
+    def getDescription(self):
+        return 'Python consumers and jobs'
+
+    def __loadNotebook(self, notebookPath: Path):
+        with notebookPath.open('r', encoding='utf-8') as f:
+            return f.read()
