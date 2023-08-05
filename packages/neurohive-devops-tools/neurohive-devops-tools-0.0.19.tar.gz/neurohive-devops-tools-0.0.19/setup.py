db6f6c488cc1ff71d8ee82f14b218a248b5f355f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:07 +0000

    Add package neurohive-devops-tools/0.0.19/neurohive-devops-tools-0.0.19.tar.gz

---
+++ packages/neurohive-devops-tools/neurohive-devops-tools-0.0.19.tar.gz/neurohive-devops-tools-0.0.19/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neurohive-devops-tools",
+    version="0.0.19",
+    author="Dmitriy Shelestovskiy",
+    author_email="one@sonhador.ru",
+    description="Neurohive devops tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'bb-trigger=neurohive.integration.bitbucket:main',
+            'bb-check-prs-branch=neurohive.integration.bitbucket:check_branch',
+            'jira-add-comment=neurohive.integration.jirawrap:main',
+            'uc-run-build=neurohive.integration.unitycloud:run_build',
+            'uc-clone-build-from-branch=neurohive.cli.commands:create_and_build_from_prs',
+            "ac-cleanup-builds=neurohive.cli.commands:cleanup_old_appcenter_build"
+        ]
+    },
+    install_requires=[
+        "requests==2.22.0",
+        "jira==2.0.0"
+    ]
+)
