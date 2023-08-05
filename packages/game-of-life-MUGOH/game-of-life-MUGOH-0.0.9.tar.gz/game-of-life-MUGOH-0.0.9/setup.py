98c2b69a735b69d00951cb0710384b16fd78c39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:20 +0000

    Add package game-of-life-MUGOH/0.0.9/game-of-life-MUGOH-0.0.9.tar.gz

---
+++ packages/game-of-life-MUGOH/game-of-life-MUGOH-0.0.9.tar.gz/game-of-life-MUGOH-0.0.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+data = ['beacon',  'bee_hive',  'blinker', ' block', 'boat',  'diehard', ' glider', ' glider__',
+    'gosper_glider_gun',  '__init__.py', 'loaf'  'pulsar', 'r_pentominoi', 'toad', 'tub']
+setuptools.setup(
+    name="game-of-life-MUGOH",
+    # include_package_data=True,
+    version="0.0.9",
+    include_package_data=True,
+    package_data={'game_of_life/.data': data},
+    author="mugoh",
+    author_email="mugoh.ks@gmail.com",
+    description="Conway's game of life in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hogum/game-of-life",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=['numpy'],
+)
