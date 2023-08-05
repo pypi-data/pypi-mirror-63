7482d587f1aed1c2b15ec81ec78659dc50c55d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:35 +0000

    Add package software/0.0.0/software-0.0.0.tar.gz

---
+++ packages/software/software-0.0.0.tar.gz/software-0.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(url='',
+      name='software',
+      author='STEALTH',
+      version='0.0.0',
+      packages=find_packages(),
+      description='...',
+      python_requires='>=3.9',
+      long_description='TODO',
+      long_description_content_type="text/x-rst",
+      classifiers=['License :: Other/Proprietary License',
+                   'Development Status :: 1 - Planning',
+                   # 'Development Status :: 2 - Pre-Alpha',
+                   # 'Development Status :: 3 - Alpha',
+                   # 'Development Status :: 4 - Beta',
+                   # 'Development Status :: 5 - Production/Stable',
+                   # 'Development Status :: 6 - Mature',
+                   # 'Development Status :: 7 - Inactive',
+                   'Intended Audience :: Developers',
+                    'Programming Language :: Python :: 3.9',
+                   'Topic :: Software Development :: Libraries :: Python Modules'])
+
