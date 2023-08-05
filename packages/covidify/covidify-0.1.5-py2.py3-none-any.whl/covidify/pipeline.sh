433febe83b02269fa4457dc1455bf275a9df1085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:17 +0000

    Add package covidify/0.1.5/covidify-0.1.5-py2.py3-none-any.whl

---
+++ packages/covidify/covidify-0.1.5-py2.py3-none-any.whl/covidify/pipeline.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+function banner {
+    echo "###"
+    echo "### $1"
+    echo "###"
+}
+
+ENV=$1
+OUT_FDR=$2
+SOURCE=$3
+COUNTRY=$4
+
+set -e
+
+mkdir -p $OUT_FDR
+
+banner "Job arguments:"
+echo "ENV: $ENV"
+echo "OUTPUT FOLDER: $OUT_FDR"
+echo "DATA SOURCE: $SOURCE"
+
+banner "Data Extraction"
+python $ENV/data_prep.py --output_folder $OUT_FDR --country $COUNTRY
+
+banner "Data Exploration"
+python $ENV/data_exploration.py --output_folder $OUT_FDR --country $COUNTRY
+
+banner "Complete!"
+echo "* Results in: $OUT_FDR" 
