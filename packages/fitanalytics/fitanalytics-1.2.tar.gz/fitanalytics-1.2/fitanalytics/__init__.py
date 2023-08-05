3c4e414557aa1c723d87df52685d33b75a43cbd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package fitanalytics/1.2/fitanalytics-1.2.tar.gz

---
+++ packages/fitanalytics/fitanalytics-1.2.tar.gz/fitanalytics-1.2/fitanalytics/__init__.py
@@ -0,0 +1,3 @@
+from multiclass_classification import MulticlassClassification
+from preprocessing import create_emoticons_tags, create_url_tags, create_time_tags, clean_texts, \
+                                       stemming, oversampling, w2v, pipeline
