3bd81620b903a3df0579699d9bd1995f1c58fea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:13 +0000

    Add package jsonprocess-progroveriman/0.0.1/jsonprocess_progroveriman-0.0.1.tar.gz

---
+++ packages/jsonprocess-progroveriman/jsonprocess_progroveriman-0.0.1.tar.gz/jsonprocess_progroveriman-0.0.1/jsonprocess_progroveriman/tools.py
@@ -0,0 +1,16 @@
+import os, json, tqdm, difflib, copy
+from pprint import pprint
+import pandas as pd
+
+def read_api_rst(file_path):
+    """
+    file_path is a string which is a path to API rst file. 
+    """
+    with open(file_path, "r", encoding = "utf-8") as fp:
+        contents = json.load(fp)
+    return contents
+
+def dump_json_to_file(dic, file_path, indent = 4, sort_keys = True):
+    with open(file_path, "w", encoding = "utf-8") as fp:
+        contents = json.dump(dic, fp, indent=indent, sort_keys=sort_keys)
+    return
