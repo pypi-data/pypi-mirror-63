927aa786586de5a67c6d6bd940ca81dbe48d34d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package trio-asyncio/0.11.0/trio_asyncio-0.11.0.tar.gz

---
+++ packages/trio-asyncio/trio_asyncio-0.11.0.tar.gz/trio_asyncio-0.11.0/tests/test_misc.py
@@ -0,0 +1,353 @@
+import pytest
+import trio_asyncio
+import asyncio
+import trio
+import sys
+from . import utils as test_utils
+
+# from tests import aiotest
+
+
+class Seen:
+    flag = 0
+
+
+class TestMisc:
+    @pytest.mark.trio
+    async def test_close_no_stop(self):
+        with pytest.raises(RuntimeError):
+            async with trio_asyncio.open_loop() as loop:
+
+                def close_no_stop():
+                    loop.close()
+
+                loop.call_soon(close_no_stop)
+
+                await trio.sleep(0.1)
+                await loop.wait_closed()
+
+    @pytest.mark.trio
+    async def test_too_many_stops(self):
+        with trio.move_on_after(1) as scope:
+            async with trio_asyncio.open_loop() as loop:
+                await trio.hazmat.checkpoint()
+                loop.stop()
+        assert not scope.cancelled_caught, \
+            "Possible deadlock after manual call to loop.stop"
+
+    @pytest.mark.trio
+    async def test_err1(self, loop):
+        async def raise_err():
+            raise RuntimeError("Foo")
+
+        with pytest.raises(RuntimeError) as err:
+            await trio_asyncio.aio_as_trio(raise_err, loop=loop)()
+        assert err.value.args[0] == "Foo"
+
+    @pytest.mark.trio
+    async def test_err1_depr(self, loop):
+        async def raise_err():
+            raise RuntimeError("Foo")
+
+        with test_utils.deprecate(self):
+            with pytest.raises(RuntimeError) as err:
+                await loop.run_asyncio(raise_err)
+        assert err.value.args[0] == "Foo"
+
+    @pytest.mark.trio
+    async def test_err3(self, loop):
+        owch = 0
+
+        async def nest():
+            nonlocal owch
+            owch = 1
+            raise RuntimeError("Hello")
+
+        async def call_nested():
+            with pytest.raises(RuntimeError) as err:
+                await trio_asyncio.trio_as_aio(nest, loop=loop)()
+            assert err.value.args[0] == "Hello"
+
+        await trio_asyncio.aio_as_trio(call_nested, loop=loop)()
+        assert owch
+
+    @pytest.mark.trio
+    async def test_err3_depr(self, loop):
+        owch = 0
+
+        async def nest():
+            nonlocal owch
+            owch = 1
+            raise RuntimeError("Hello")
+
+        async def call_nested():
+            with pytest.raises(RuntimeError) as err:
+                await loop.run_trio(nest)
+            assert err.value.args[0] == "Hello"
+
+        with test_utils.deprecate(self):
+            await loop.run_asyncio(call_nested)
+        assert owch
+
+    @pytest.mark.trio
+    async def test_run(self, loop):
+        owch = 0
+
+        async def nest():
+            await trio.sleep(0.01)
+            nonlocal owch
+            owch = 1
+
+        async def call_nested():
+            await trio_asyncio.trio_as_aio(nest, loop=loop)()
+
+        await trio_asyncio.aio_as_trio(call_nested, loop=loop)()
+        assert owch
+
+    @pytest.mark.trio
+    async def test_run_depr(self, loop):
+        owch = 0
+
+        async def nest():
+            await trio.sleep(0.01)
+            nonlocal owch
+            owch = 1
+
+        async def call_nested():
+            with test_utils.deprecate(self):
+                await loop.run_trio(nest)
+
+        with test_utils.deprecate(self):
+            await loop.run_asyncio(call_nested)
+        assert owch
+
+    async def _test_run(self):
+        owch = 0
+
+        async def nest():
+            await trio.sleep(0.01)
+            nonlocal owch
+            owch = 1
+
+        async def call_nested():
+            await trio_asyncio.trio_as_aio(nest)()
+
+        await trio_asyncio.aio_as_trio(call_nested)()
+        assert owch
+
+    def test_run2(self):
+        trio_asyncio.run(self._test_run)
+
+    async def _test_run_depr(self):
+        owch = 0
+
+        async def nest():
+            await trio.sleep(0.01)
+            nonlocal owch
+            owch = 1
+
+        async def call_nested():
+            await trio_asyncio.run_trio(nest)
+
+        with test_utils.deprecate(self):
+            await trio_asyncio.run_asyncio(call_nested)
+        assert owch
+
+    def test_run2_depr(self):
+        trio_asyncio.run(self._test_run_depr)
+
+    @pytest.mark.trio
+    async def test_run_task(self):
+        owch = 0
+
+        async def nest(x):
+            nonlocal owch
+            owch += x
+
+        with pytest.raises(RuntimeError):
+            trio_asyncio.run_trio_task(nest, 100)
+
+        with pytest.raises(RuntimeError):
+            with trio_asyncio.open_loop():
+                nest(1000)
+
+        async with trio_asyncio.open_loop():
+            trio_asyncio.run_trio_task(nest, 1)
+        await trio.sleep(0.05)
+        assert owch == 1
+
+    @pytest.mark.trio
+    async def test_err2(self, loop):
+        owch = 0
+
+        async def nest():
+            nonlocal owch
+            owch = 1
+            raise RuntimeError("Hello")
+
+        async def call_nested():
+            await trio_asyncio.aio_as_trio(nest, loop=loop)()
+
+        async def call_more_nested():
+            with pytest.raises(RuntimeError) as err:
+                await trio_asyncio.trio_as_aio(call_nested, loop=loop)()
+            assert err.value.args[0] == "Hello"
+
+        await trio_asyncio.aio_as_trio(call_more_nested, loop=loop)()
+        assert owch
+
+    @pytest.mark.trio
+    async def test_err2_depr(self, loop):
+        owch = 0
+
+        async def nest():
+            nonlocal owch
+            owch = 1
+            raise RuntimeError("Hello")
+
+        async def call_nested():
+            await loop.run_asyncio(nest)
+
+        async def call_more_nested():
+            with pytest.raises(RuntimeError) as err:
+                await loop.run_trio(call_nested)
+            assert err.value.args[0] == "Hello"
+
+        with test_utils.deprecate(self):
+            await loop.run_asyncio(call_more_nested)
+        assert owch
+
+    @pytest.mark.trio
+    async def test_run3(self, loop):
+        owch = 0
+
+        async def nest():
+            nonlocal owch
+            owch = 1
+
+        async def call_nested():
+            await trio_asyncio.aio_as_trio(nest, loop=loop)()
+
+        async def call_more_nested():
+            await trio_asyncio.trio_as_aio(call_nested, loop=loop)()
+
+        await trio_asyncio.aio_as_trio(call_more_nested, loop=loop)()
+        assert owch
+
+    @pytest.mark.trio
+    async def test_run3_depr(self, loop):
+        owch = 0
+
+        async def nest():
+            nonlocal owch
+            owch = 1
+
+        async def call_nested():
+            await loop.run_asyncio(nest)
+
+        async def call_more_nested():
+            await loop.run_trio(call_nested)
+
+        with test_utils.deprecate(self):
+            await loop.run_asyncio(call_more_nested)
+        assert owch
+
+    @pytest.mark.trio
+    async def test_cancel_sleep(self, loop):
+        owch = 0
+
+        def do_not_run():
+            nonlocal owch
+            owch = 1
+            raise Exception("should not run")
+
+        async def cancel_sleep():
+            h = loop.call_later(0.2, do_not_run)
+            await asyncio.sleep(0.1, loop=loop)
+            h.cancel()
+            await asyncio.sleep(0.3, loop=loop)
+
+        await trio_asyncio.aio_as_trio(cancel_sleep, loop=loop)()
+        assert owch == 0
+
+    @pytest.mark.trio
+    async def test_cancel_sleep_depr(self, loop):
+        owch = 0
+
+        def do_not_run():
+            nonlocal owch
+            owch = 1
+            raise Exception("should not run")
+
+        async def cancel_sleep():
+            h = loop.call_later(0.2, do_not_run)
+            await asyncio.sleep(0.1, loop=loop)
+            h.cancel()
+            await asyncio.sleep(0.3, loop=loop)
+
+        with test_utils.deprecate(self):
+            await loop.run_asyncio(cancel_sleep)
+        assert owch == 0
+
+
+@pytest.mark.trio
+async def test_wrong_context_manager_order():
+    take_down = trio.Event()
+    async def work_in_asyncio():
+        await asyncio.sleep(0)
+
+    async def runner(*, task_status=trio.TASK_STATUS_IGNORED):
+        await trio_asyncio.aio_as_trio(work_in_asyncio)()
+        try:
+            task_status.started()
+            await take_down.wait()
+        finally:
+            await trio_asyncio.aio_as_trio(work_in_asyncio)()
+
+    async with trio.open_nursery() as nursery:
+        async with trio_asyncio.open_loop():
+            await nursery.start(runner)
+            take_down.set()
+
+
+@pytest.mark.trio
+@pytest.mark.skipif(sys.platform == 'win32',
+                    reason="Not supported on Windows")
+async def test_keyboard_interrupt_teardown():
+    asyncio_loop_closed = trio.Event()
+
+    async def work_in_trio_no_matter_what(*, task_status=trio.TASK_STATUS_IGNORED):
+        await trio_asyncio.aio_as_trio(work_in_asyncio)()
+        try:
+            # KeyboardInterrupt won't cancel this coroutine thanks to the shield
+            with trio.CancelScope(shield=True):
+                task_status.started()
+                await asyncio_loop_closed.wait()
+        finally:
+            # Hence this call will be exceuted after run_asyncio_loop is cancelled
+            with pytest.raises(RuntimeError):
+                await trio_asyncio.aio_as_trio(work_in_asyncio)()
+
+    async def work_in_asyncio():
+        await asyncio.sleep(0)
+
+    async def run_asyncio_loop(nursery, *, task_status=trio.TASK_STATUS_IGNORED):
+        with trio.CancelScope() as cancel_scope:
+            try:
+                async with trio_asyncio.open_loop():
+                    # Starting a coroutine from here make it inherit the access
+                    # to the asyncio loop context manager
+                    await nursery.start(work_in_trio_no_matter_what)
+                    task_status.started(cancel_scope)
+                    await trio.sleep_forever()
+            finally:
+                asyncio_loop_closed.set()
+
+    import signal
+    import threading
+    with pytest.raises(KeyboardInterrupt):
+        with pytest.warns(RuntimeWarning):
+            async with trio.open_nursery() as nursery:
+                await nursery.start(run_asyncio_loop, nursery)
+                # Trigger KeyboardInterrupt that should propagate accross the coroutines
+                signal.pthread_kill(threading.get_ident(), signal.SIGINT)
---
+++ packages/trio-asyncio/trio_asyncio-0.11.0.tar.gz/trio_asyncio-0.11.0/trio_asyncio/_handles.py
@@ -0,0 +1,166 @@
+import sys
+import trio
+import asyncio
+import sniffio
+try:
+    from asyncio.format_helpers import _format_callback, _get_function_source
+except ImportError:  # <3.7
+    from asyncio.events import _format_callback, _get_function_source
+
+
+def _format_callback_source(func, args):
+    func_repr = _format_callback(func, args, None)
+    source = _get_function_source(func)
+    if source:  # pragma: no cover
+        func_repr += ' at %s:%s' % source
+    return func_repr
+
+
+async def _set_sniff(proc, *args):
+    sniffio.current_async_library_cvar.set("trio")
+    return await proc(*args)
+
+
+class _TrioHandle:
+    """
+    This extends asyncio.Handle by providing:
+    * a way to cancel an async callback
+    * a way to declare the type of the callback function
+
+    ``is_sync`` may be
+    * True: sync function, use _call_sync()
+    * False: async function, use _call_async()
+    * None: also async, but the callback function accepts
+      the handle as its sole argument
+
+    The caller is responsible for checking whether the handle
+    has been cancelled before invoking ``call_[a]sync()``.
+    """
+
+    def _init(self, is_sync):
+        """Secondary init.
+        """
+        self._is_sync = is_sync
+        self._scope = None
+
+    def cancel(self):
+        try:
+            task = self._callback.__self__
+        except AttributeError:
+            task = None
+        else:
+            if not isinstance(task, asyncio.Task):
+                task = None
+        super().cancel()
+        if self._scope is not None:
+            self._scope.cancel()
+        elif task is not None:
+            task.cancel()
+
+    def _cb_future_cancel(self, f):
+        """If a Trio task completes an asyncio Future,
+        add this callback to the future
+        and set ``_scope`` to the Trio cancel scope
+        so that the task is terminated when the future gets canceled.
+
+        """
+        if f.cancelled():
+            self.cancel()
+
+    def _raise(self, exc):
+        """This is a copy of the exception handling in asyncio.events.Handle._run()
+        """
+        cb = _format_callback_source(self._callback, self._args)
+        msg = 'Exception in callback {}'.format(cb)
+        context = {
+            'message': msg,
+            'exception': exc,
+            'handle': self,
+        }
+        if self._source_traceback:
+            context['source_traceback'] = self._source_traceback
+        self._loop.call_exception_handler(context)
+
+    def _repr_info(self):
+        info = [self.__class__.__name__]
+        if self._cancelled:
+            info.append('cancelled')
+        if self._callback is not None:
+            info.append(_format_callback_source(self._callback, self._args))
+        if self._source_traceback:
+            frame = self._source_traceback[-1]
+            info.append('created at %s:%s' % (frame[0], frame[1]))
+        if self._scope is not None:
+            info.append('scope=%s' % repr(self._scope))
+        return info
+
+    def _call_sync(self):
+        assert self._is_sync
+        if self._cancelled:
+            return
+        self._run()
+
+    if sys.version_info >= (3, 7):
+
+        async def _call_async(self, task_status=trio.TASK_STATUS_IGNORED):
+            assert not self._is_sync
+            if self._cancelled:
+                return
+            task_status.started()
+            try:
+                with trio.CancelScope() as scope:
+                    self._scope = scope
+                    if self._is_sync is None:
+                        await self._context.run(_set_sniff, self._callback, self)
+                    else:
+                        await self._context.run(_set_sniff, self._callback, *self._args)
+            except Exception as exc:
+                self._raise(exc)
+            finally:
+                self._scope = None
+
+    else:  # no contextvars
+
+        async def _call_async(self, task_status=trio.TASK_STATUS_IGNORED):
+            assert not self._is_sync
+            if self._cancelled:
+                return
+            task_status.started()
+            try:
+                with trio.CancelScope() as scope:
+                    self._scope = scope
+                    if self._is_sync is None:
+                        await self._callback(self)
+                    else:
+                        await self._callback(*self._args)
+            except Exception as exc:
+                self._raise(exc)
+            finally:
+                self._scope = None
+
+
+if sys.version_info >= (3, 7):
+
+    class Handle(_TrioHandle, asyncio.Handle):
+        def __init__(self, callback, args, loop, context=None, is_sync=True):
+            assert not isinstance(context, bool)
+            super().__init__(callback, args, loop, context=context)
+            self._init(is_sync)
+
+    class TimerHandle(_TrioHandle, asyncio.TimerHandle):
+        def __init__(self, when, callback, args, loop, context=None, is_sync=True):
+            assert not isinstance(context, bool)
+            super().__init__(when, callback, args, loop, context=context)
+            self._init(is_sync)
+
+else:
+
+    class Handle(_TrioHandle, asyncio.Handle):
+        def __init__(self, callback, args, loop, context=None, is_sync=True):
+            super().__init__(callback, args, loop)
+            self._init(is_sync)
+
+    class TimerHandle(_TrioHandle, asyncio.TimerHandle):
+        def __init__(self, when, callback, args, loop, context=None, is_sync=True):
+            super().__init__(when, callback, args, loop)
+            self._init(is_sync)
