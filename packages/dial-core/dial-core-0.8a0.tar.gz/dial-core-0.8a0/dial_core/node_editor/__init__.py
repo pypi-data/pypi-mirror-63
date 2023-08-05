166d18cde91b3f6ca56be0d20f86b169fa9cfae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:43 +0000

    Add package dial-core/0.8a0/dial-core-0.8a0.tar.gz

---
+++ packages/dial-core/dial-core-0.8a0.tar.gz/dial-core-0.8a0/dial_core/node_editor/__init__.py
@@ -0,0 +1,20 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+from .containers import DefaultSceneFactory, NodeFactorySingleton
+from .input_port import InputPort
+from .node import Node
+from .node_factory import NodeFactory
+from .output_port import OutputPort
+from .port import Port
+from .scene import Scene
+
+__all__ = [
+    "Node",
+    "Port",
+    "InputPort",
+    "OutputPort",
+    "Scene",
+    "DefaultSceneFactory",
+    "NodeFactory",
+    "NodeFactorySingleton",
+]
---
+++ packages/dial-core/dial-core-0.8a0.tar.gz/dial-core-0.8a0/dial_core/project/project.py
@@ -0,0 +1,23 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+from dial_core.node_editor import Scene
+
+
+class Project:
+    def __init__(self, name: str, scene: "Scene"):
+        self.name = name
+        self.file_path = ""
+
+        self.__scene = scene
+
+    @property
+    def scene(self):
+        """Returns the nodes scene of the project."""
+        return self.__scene
+
+    def __eq__(self, other):
+        return (
+            self.name == other.name
+            and self.file_path == other.file_path
+            and self.scene == other.scene
+        )
