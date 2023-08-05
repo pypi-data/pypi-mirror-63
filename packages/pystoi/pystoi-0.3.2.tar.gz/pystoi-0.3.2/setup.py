492f5b3e348c78cd52ff461c1d280f2eee02a2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:31 +0000

    Add package pystoi/0.3.2/pystoi-0.3.2.tar.gz

---
+++ packages/pystoi/pystoi-0.3.2.tar.gz/pystoi-0.3.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pystoi',
+    version='0.3.2',
+    description='Computes Short Term Objective Intelligibility measure',
+    author='Manuel Pariente',
+    author_email='pariente.mnl@gmail.com',
+    url='https://github.com/mpariente/pystoi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    install_requires=['numpy', 'scipy'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=find_packages()
+)
