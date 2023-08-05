8fca5d2591de6817056194fd9e67afd844ee7f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:37 +0000

    Add package azureml-telemetry/1.1.5/azureml_telemetry-1.1.5-py3-none-any.whl

---
+++ packages/azureml-telemetry/azureml_telemetry-1.1.5-py3-none-any.whl/azureml/telemetry/logger/__init__.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Init for logger module."""
+
+from ._abstract_event_logger import AbstractEventLogger
+from ._applicationinsights_logger import ApplicationInsightsEventLogger
+from ._inmemory_event_logger import InMemoryEventLogger
+
+__all__ = [
+    'AbstractEventLogger',
+    'ApplicationInsightsEventLogger',
+    'InMemoryEventLogger'
+]
