f3f3553005a863655fb499ebc4c398051c7f0b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package mkdocs-bibtex/0.2.3/mkdocs-bibtex-0.2.3.tar.gz

---
+++ packages/mkdocs-bibtex/mkdocs-bibtex-0.2.3.tar.gz/mkdocs-bibtex-0.2.3/mkdocs_bibtex/__init__.py
@@ -0,0 +1,8 @@
+from mkdocs_bibtex.plugin import BibTexPlugin
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
