d98fc573984177118d770ea406ec9e937c2b3e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:40 +0000

    Add package user-tools/0.0.4/user_tools-0.0.4-py3-none-any.whl

---
+++ packages/user-tools/user_tools-0.0.4-py3-none-any.whl/user_tools/util_cmd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import subprocess
+
+
+def get_out_text(cmd):
+    """Run command with arguments and return its output.
+
+    cmd(list): Command to be executed.
+        The cmd format is ["command", ["arg", "arg1",...]]
+    """
+
+    code = 0
+    try:
+        out_bytes = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        out_bytes = e.output
+        code = e.returncode
+    out_text = out_bytes.decode('utf-8')
+    if code:
+        out_text = "Error code is " + code + "\n" +\
+                   "Output text is" + out_bytes.decode('utf-8')
+    else:
+        out_text = "Output text is" + out_bytes.decode('utf-8')
+    return out_text
