9777cec6bb627360aa8def7e52c1b7985a60e2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:06 +0000

    Add package rpa-public-package/1.0.2/rpa_public_package-1.0.2.tar.gz

---
+++ packages/rpa-public-package/rpa_public_package-1.0.2.tar.gz/rpa_public_package-1.0.2/rpa_public_package/__init__.py
@@ -0,0 +1,24 @@
+"""
+@Version: 1.0.0
+@Author: qi.you
+@File: __init__.py.py
+@Time: 2020/3/9 11:24
+@Describe: 引用RPA公共包
+"""
+import sys
+import os
+sys.path.append('%s\PythonPublicPackage'%os.path.abspath(os.path.join(sys.argv[0], "../..")))
+sys.path.append('%s\PythonPublicPackage'%os.path.abspath(os.path.join(sys.argv[0], "../../..")))
+sys.path.append('%s\PythonPublicPackage'%os.path.abspath(os.path.join(sys.argv[0], "../../../..")))
+sys.path.append('%s\PythonPublicPackage'%os.path.abspath(os.path.join(sys.argv[0], "../../../../..")))
+from GetCmd import getcmd
+import settings
+from GetOATData import GetCData,GetOATData
+from Loggers import Loggers
+from RecordCheck import recordcheck
+from YDMPython3 import YDM,balance,YDM_error,YDM_L
+from UploadEmail import uploademail
+from SendMessage import SendMessage
+from Futuaccount_Login import Futuaccount_Login
+from GetToken import GetToken
+from SG_MySQL import IX_MySQL
---
+++ packages/rpa-public-package/rpa_public_package-1.0.2.tar.gz/rpa_public_package-1.0.2/rpa_public_package/rpa_public_package.py
@@ -0,0 +1,7 @@
+"""
+@Version: 1.0.0
+@Author: qi.you
+@File: rpa_public_package.py
+@Time: 2020/3/9 10:27
+@Describe: 引用RPA公共包
+"""
