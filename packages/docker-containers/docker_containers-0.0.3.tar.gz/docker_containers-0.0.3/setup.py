590bf7fe6afa6a81806757e6f3a9d404cfd64367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:10 +0000

    Add package docker-containers/0.0.3/docker_containers-0.0.3.tar.gz

---
+++ packages/docker-containers/docker_containers-0.0.3.tar.gz/docker_containers-0.0.3/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup
+
+
+def get_long_description():
+    with open("README.md", encoding="utf8") as f:
+        return f.read()
+
+
+def get_packages(package):
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+
+setup(
+    name='docker_containers',
+    version='0.0.3',
+    author='V. Cheremushkin',
+    author_email='',
+    url='https://git.cashwagon.com/billing/docker_containers',
+    python_requires=">=3.7",
+    install_requires=[
+        'pytest',
+        'crayons>=0.3.0, <1',
+        'blindspin>=2.0.1, <3',
+        'docker>=4.2.0, <5',
+        'asyncpg>=0.19.0, <1'
+    ],
+    license="BSD",
+    description="Util for start containers",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    packages=get_packages('docker_containers'),
+    include_package_data=True,
+    data_files=[("", [])],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 3.7",
+    ],
+    zip_safe=False,
+)
