26b0cf9a1a578406d4952bf3b002713bd917caf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package responsum/0.1.3/responsum-0.1.3.tar.gz

---
+++ packages/responsum/responsum-0.1.3.tar.gz/responsum-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages, Command
+import os
+import io
+import sys
+from shutil import rmtree
+import versioneer
+
+setup_requires = ["setuptools >= 30.3.0"]
+
+
+setup(
+    setup_requires=setup_requires,
+    license="GPL3",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
