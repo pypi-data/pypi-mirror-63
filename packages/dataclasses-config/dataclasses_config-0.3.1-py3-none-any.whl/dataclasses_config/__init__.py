a74c7ecd2e84b1cb19167409637185ed71bdfa7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package dataclasses-config/0.3.1/dataclasses_config-0.3.1-py3-none-any.whl

---
+++ packages/dataclasses-config/dataclasses_config-0.3.1-py3-none-any.whl/dataclasses_config/__init__.py
@@ -0,0 +1,34 @@
+"""
+.. include:: ../../README.md
+"""
+
+from collections import namedtuple
+
+__title__ = 'dataclasses-config'
+__author__ = 'Peter Zaitcev / USSX Hares'
+__license__ = 'BSD 2-clause'
+__copyright__ = 'Copyright 2019 Peter Zaitcev'
+__version__ = '0.3.1'
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+version_info = VersionInfo(*__version__.split('.'), releaselevel='alpha', serial=0)
+
+from ._classes import *
+from ._config import *
+from ._decorations import *
+from .settings import *
+
+__all__ =  \
+[
+    'version_info',
+    '__title__',
+    '__author__',
+    '__license__',
+    '__copyright__',
+    '__version__',
+    
+    *_classes.__all__,
+    *_config.__all__,
+    *_decorations.__all__,
+    *settings.__all__,
+]
