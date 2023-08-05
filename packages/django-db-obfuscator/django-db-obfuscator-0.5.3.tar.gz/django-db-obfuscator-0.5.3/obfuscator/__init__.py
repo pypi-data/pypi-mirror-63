8cee3176f5808b9c367ade9c0b14bb37aae77c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package django-db-obfuscator/0.5.3/django-db-obfuscator-0.5.3.tar.gz

---
+++ packages/django-db-obfuscator/django-db-obfuscator-0.5.3.tar.gz/django-db-obfuscator-0.5.3/obfuscator/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Django DB Obfuscator."""
+
+__author__ = """Oezer Sahin"""
+__email__ = 'os@oesah.ch'
+__version__ = '0.5.3'
+__url__ = 'https://github.com/oesah/django-db-obfuscator/'
+
+default_app_config = 'obfuscator.apps.ObfuscatorConfig'
