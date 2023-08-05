995a39856a1a9619d76d4cc35d49ce001eb3bfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package bpp-iplweb/202003.21/bpp_iplweb-202003.21-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202003.21-py3-none-any.whl/raport_slotow/tables.py
@@ -0,0 +1,114 @@
+import django_tables2 as tables
+from django.template.defaultfilters import safe
+from django.urls import reverse
+from django_tables2 import Column
+
+from bpp.models.cache import Cache_Punktacja_Autora_Query
+from raport_slotow.columns import DecimalColumn, SummingColumn
+from raport_slotow.models import RaportZerowyEntry
+
+
+class RaportSlotowAutorTable(tables.Table):
+    class Meta:
+        empty_text = "Brak danych"
+        model = Cache_Punktacja_Autora_Query
+        fields = (
+            "tytul_oryginalny",
+            "autorzy",
+            "rok",
+            "zrodlo",
+            "dyscyplina",
+            "punkty_kbn",
+            "pkdaut",
+            "slot",
+        )
+
+    tytul_oryginalny = Column("Tytuł oryginalny", "rekord")
+    autorzy = Column(
+        "Autorzy", "rekord.opis_bibliograficzny_zapisani_autorzy_cache", orderable=False
+    )
+    rok = Column("Rok", "rekord.rok", orderable=True)
+    dyscyplina = Column(orderable=False)
+    punkty_kbn = Column("Punkty PK", "rekord.punkty_kbn")
+    zrodlo = Column("Źródło", "rekord.zrodlo", empty_values=())
+    pkdaut = SummingColumn("Punkty dla autora", "pkdaut")
+    slot = SummingColumn("Slot")
+
+    def render_tytul_oryginalny(self, value):
+        url = reverse("bpp:browse_rekord", args=(value.pk[0], value.pk[1]))
+        return safe("<a href=%s>%s</a>" % (url, value))
+
+    def value_tytul_oryginalny(self, value):
+        return value.tytul_oryginalny
+
+    def render_zrodlo(self, value):
+        if value is None:
+            return "-"
+        return value
+
+
+class RaportSlotowUczelniaBezJednostekIWydzialowTable(tables.Table):
+    class Meta:
+        empty_text = "Brak danych"
+        model = Cache_Punktacja_Autora_Query
+        fields = (
+            "autor",
+            "pbn_id",
+            "orcid",
+            "dyscyplina",
+            "pkdautsum",
+            "pkdautslotsum",
+            "avg",
+        )
+
+    pkdautsum = DecimalColumn("Suma punktów dla autora")
+    pkdautslotsum = DecimalColumn("Slot")
+    avg = Column("Średnio punktów dla autora na slot")
+    dyscyplina = Column()
+    pbn_id = Column("PBN ID", "autor.pbn_id")
+    orcid = Column("ORCID", "autor.orcid")
+
+    def __init__(self, od_roku, do_roku, *args, **kw):
+        self.od_roku = od_roku
+        self.do_roku = do_roku
+        super(RaportSlotowUczelniaBezJednostekIWydzialowTable, self).__init__(
+            *args, **kw
+        )
+
+    def render_avg(self, value):
+        return round(value, 4)
+
+    def render_autor(self, value):
+        url = reverse(
+            "raport_slotow:raport", args=(value.slug, self.od_roku, self.do_roku)
+        )
+        return safe("<a href=%s>%s</a>" % (url, value))
+
+    def value_autor(self, value):
+        return value
+
+
+class RaportSlotowUczelniaTable(RaportSlotowUczelniaBezJednostekIWydzialowTable):
+    class Meta:
+        empty_text = "Brak danych"
+        model = Cache_Punktacja_Autora_Query
+        fields = (
+            "autor",
+            "pbn_id",
+            "orcid",
+            "jednostka",
+            "wydzial",
+            "dyscyplina",
+            "pkdautsum",
+            "pkdautslotsum",
+            "avg",
+        )
+
+    wydzial = Column("Wydział", accessor="jednostka.wydzial.nazwa")
+
+
+class RaportSlotowZerowyTable(tables.Table):
+    class Meta:
+        empty_text = "Brak danych"
+        model = RaportZerowyEntry
+        fields = ("autor", "lata", "dyscyplina_naukowa")
