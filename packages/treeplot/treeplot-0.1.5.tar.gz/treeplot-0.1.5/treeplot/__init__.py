634fa3630077764a2b415242577adaf8fb748af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:29 +0000

    Add package treeplot/0.1.5/treeplot-0.1.5.tar.gz

---
+++ packages/treeplot/treeplot-0.1.5.tar.gz/treeplot-0.1.5/treeplot/__init__.py
@@ -0,0 +1,37 @@
+from treeplot.treeplot import (
+    plot,
+    randomforest,
+	xgboost,
+    import_example,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.5'
+
+# module level doc-string
+__doc__ = """
+treeplot
+==========================================
+
+Description
+-----------
+treeplot vizualizes a tree based on model.
+
+Example
+-------
+>>> import treeplot
+>>> X,y = treeplot.import_example()
+>>>
+>>> # randomforest model
+>>> model = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
+>>> fig = treeplot.plot(model)
+>>>
+>>> # xgboost model
+>>> model = XGBClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
+>>> fig = treeplot.plot(model)
+
+References
+----------
+https://github.com/erdogant/treeplot
+"""
