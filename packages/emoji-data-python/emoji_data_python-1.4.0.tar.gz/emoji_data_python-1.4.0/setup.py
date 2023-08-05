221984ef11f0209f133618472d18d3994463a224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package emoji-data-python/1.4.0/emoji_data_python-1.4.0.tar.gz

---
+++ packages/emoji-data-python/emoji_data_python-1.4.0.tar.gz/emoji_data_python-1.4.0/setup.py
@@ -0,0 +1,32 @@
+import os
+
+import setuptools
+
+module_path = os.path.join(os.path.dirname(__file__), 'emoji_data_python.py')
+
+setuptools.setup(
+    name="emoji_data_python",
+    version="1.4.0",
+    url="https://github.com/alexmick/emoji-data-python/",
+
+    author="Alexander Micklewright",
+
+    description="Python emoji toolkit",
+    long_description="Full documentation available on http://pythonhosted.org/emoji-data-python",
+
+    zip_safe=False,
+    platforms='any',
+
+    install_requires=[''],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
