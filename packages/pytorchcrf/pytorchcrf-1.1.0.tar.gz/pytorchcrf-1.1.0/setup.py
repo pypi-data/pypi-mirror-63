2b6cada0101464d3bd060653e5b3f4525aa50a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package pytorchcrf/1.1.0/pytorchcrf-1.1.0.tar.gz

---
+++ packages/pytorchcrf/pytorchcrf-1.1.0.tar.gz/pytorchcrf-1.1.0/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(ROOT_DIR, 'README.md')) as file:
+    readme = file.read()
+
+setup(
+    name='pytorchcrf',
+    version='1.1.0',
+    description='PyTorch CRF with N-best decoding',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='https://github.com/statech/pytorchCRF',
+    author='Feiyang Niu',
+    author_email='statech.forums@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='pytorch crf nbest',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
