afbbafd1ed06f612ae5ca711ed085a9deb3f26b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:43 +0000

    Add package mantis-cli/0.2.0/mantis-cli-0.2.0.tar.gz

---
+++ packages/mantis-cli/mantis-cli-0.2.0.tar.gz/mantis-cli-0.2.0/mantis/__main__.py
@@ -0,0 +1,5 @@
+from logic import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mantis-cli/mantis-cli-0.2.0.tar.gz/mantis-cli-0.2.0/mantis/command_line.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from mantis.logic import main as execute_from_command_line
+
+execute_from_command_line()
