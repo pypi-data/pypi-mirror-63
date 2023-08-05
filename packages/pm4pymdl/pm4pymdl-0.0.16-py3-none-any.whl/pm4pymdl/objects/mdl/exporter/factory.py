cb68571b27a4c78e56d82b88ae9f1be6cf9fe719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package pm4pymdl/0.0.16/pm4pymdl-0.0.16-py3-none-any.whl

---
+++ packages/pm4pymdl/pm4pymdl-0.0.16-py3-none-any.whl/pm4pymdl/objects/mdl/exporter/factory.py
@@ -0,0 +1,34 @@
+from pm4pymdl.algo.mvp.utils import exploded_mdl_to_succint_mdl, succint_mdl_to_exploded_mdl
+from copy import deepcopy
+
+def apply(df, file_path, parameters=None):
+    if parameters is None:
+        parameters = {}
+
+    if file_path.endswith(".csv") or file_path.endswith(".mdl"):
+        conversion_needed = True
+        try:
+            if df.type == "succint":
+                conversion_needed = False
+        except:
+            pass
+
+        if conversion_needed:
+            df = exploded_mdl_to_succint_mdl.apply(df)
+
+        df.to_csv(file_path, index=False, sep=',', quotechar='\"')
+    else:
+        from pm4py.objects.log.exporter.parquet import factory as parquet_exporter
+
+        conversion_needed = False
+        try:
+            if df.type == "succint":
+                conversion_needed = True
+        except:
+            pass
+
+        if conversion_needed:
+            df = succint_mdl_to_exploded_mdl.apply(df)
+        new_parameters = deepcopy(parameters)
+        new_parameters["compression"] = "gzip"
+        parquet_exporter.export_df(df, file_path, parameters=new_parameters)
