fbc40d14c3bf76cfae11c11aedacc8cb0bc4ebd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:52 +0000

    Add package fstd2nc-deps/0.20200304.0/fstd2nc_deps-0.20200304.0-py2.py3-none-win32.whl

---
+++ packages/fstd2nc-deps/fstd2nc_deps-0.20200304.0-py2.py3-none-win32.whl/fstd2nc_deps-0.20200304.0.data/purelib/fstd2nc_deps/rpnpy/librmn/all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Stephane Chamberland <stephane.chamberland@canada.ca>
+# Copyright: LGPL 2.1
+"""
+ Short hand to load all rpnpy.librmn submodules in the same namespace
+
+ See also:
+     rpnpy.librmn
+     rpnpy.librmn.proto
+     rpnpy.librmn.proto_burp
+     rpnpy.librmn.const
+     rpnpy.librmn.base
+     rpnpy.librmn.fstd98
+     rpnpy.librmn.interp
+     rpnpy.librmn.grids
+     rpnpy.librmn.burp
+     rpnpy.librmn.burp_const
+
+"""
+
+from . import *
+from .proto import *
+from .proto_burp import *
+from .const import *
+from .base import *
+from .fstd98 import *
+from .interp import *
+from .grids import *
+from .burp import *
+from .burp_const import *
---
+++ packages/fstd2nc-deps/fstd2nc_deps-0.20200304.0-py2.py3-none-win32.whl/fstd2nc_deps-0.20200304.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+Provides the Python-RPN_ package, along with the libraries_ needed for fstd2nc.
+
+.. _Python-RPN: https://github.com/meteokid/python-rpn
+.. _libraries: https://github.com/neishm/python-rpn-libsrc
+
+
