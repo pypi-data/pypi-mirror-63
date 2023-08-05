ccd748b3e19a1246dae77dd7494fdd0e8205c87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package tnng/0.2.0/tnng-0.2.0.tar.gz

---
+++ packages/tnng/tnng-0.2.0.tar.gz/tnng-0.2.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from pathlib import Path
+
+p = Path(__file__)
+
+
+setup_requires = [
+    'numpy',
+    'pytest-runner'
+]
+
+install_requires = [
+]
+test_require = [
+    'pytest-cov',
+    'pytest-html',
+    'pytest',
+]
+
+setuptools.setup(
+    name="tnng",
+    version='0.2.0',
+    python_requires='>3.5',
+    author="Koji Ono",
+    author_email="koji.ono@exwzd.com",
+    description="Toy Neural Network Generator.",
+    url='https://github.com/0h-n0/toy_neural_network_generator',
+    long_description=(p.parent / 'README.md').open(encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=test_require,
+    extras_require={
+        'docs': [
+            'sphinx >= 1.4',
+            'sphinx_rtd_theme']},
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: MIT License",
+    ],
+)
