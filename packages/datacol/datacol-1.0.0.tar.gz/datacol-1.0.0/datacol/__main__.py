8b9e064a8320ca58100188f97c6a68080498e69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:34 +0000

    Add package datacol/1.0.0/datacol-1.0.0.tar.gz

---
+++ packages/datacol/datacol-1.0.0.tar.gz/datacol-1.0.0/datacol/__main__.py
@@ -0,0 +1,62 @@
+dbr = None
+dbw = None
+dbl = None
+work = None
+file_name = None
+class NewDB:
+  def __init__(self, file_namek):
+    global dbr
+    global dbl
+    global dbw
+    global file_name
+    if file_namek[-3:-1] != '.db':
+        raise
+    file_name = file_namek
+    dbr = open(file_name, "r")
+    dbw = open(file_name, "a")
+    dbl = dbr.readlines()
+  def query(self, val, param):
+    for line in dbl:
+      linetype = line[0:4]
+      equal = line.find("=")
+      namespace = equal - 1
+      valuespace = equal + 1
+      if linetype == "nvp;" and val == "nvp":
+        name = line[5:namespace]
+        value = line[valuespace::]
+        if param == name:
+          return value
+      if linetype == "vlu;" and val == "vlu":
+        value = line[5::]
+        if value == param:
+          return True
+        else:
+          return False
+  def insert(self, val, param):
+    global dbw
+    global dbl
+    global dbr
+    if val == "nvp":
+      if dbl[-1] != "":
+        content = "\nnvp; " + param
+      else:
+        content = "nvp;" + param + "\n"
+      if "=" not in param:
+        print("Borealis Error: Invalid Parameter")
+      else:
+        dbw.write(content)
+        dbr = open(file_name, "r")
+        dbl = dbr.readlines()
+    elif val == "vlu":
+      if dbl[-1] != "":
+        content = "\nvlu; " + param
+      else:
+        content = "vlu; " + param + "\n"
+      dbw.write(content)
+      dbl = dbr.readlines()
+def help():
+  print("Welcome to DataBorealis!")
+  print("You can create a new database using the \"NewDB\" class. It takes only one parameter: the name of the file to be used as the database.")
+  print("You can search the database using the \"query\" method. This takes 2 parameters: the kind of data you are looking for, and the keywords to search for.")
+  print("You can insert data into the database using the insert function. This takes the same parameters as the query method: the kind of data to insert, and the data to insert.")
+  print("For more information, visit https://repl.it/@ekholmes/DBorealis or https://github.com/EKHolmes/DBorealis.")
