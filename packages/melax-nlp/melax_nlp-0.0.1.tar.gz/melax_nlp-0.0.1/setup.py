4c19c18d419b18a628eb276f4239d927a08a2f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package melax-nlp/0.0.1/melax_nlp-0.0.1.tar.gz

---
+++ packages/melax-nlp/melax_nlp-0.0.1.tar.gz/melax_nlp-0.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='melax_nlp',  # pip3 install get-time
+    version='0.0.1',  #
+    author='lj',  #
+    author_email='soda.lj@gmail.com',
+    url='https://github.com/sodalvjian/melax_nlp_lib.git',
+    description='nlp ner lib',
+    packages=['melaxapi'],
+    install_requires=['requests'],
+    entry_points={
+
+    }
+)
