fd5f7b45c573c8f6e3eb95ffc79b22c9eba0eb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:09 +0000

    Add package reclaimer/2.8.0/reclaimer-2.8.0-cp37-cp37m-win32.whl

---
+++ packages/reclaimer/reclaimer-2.8.0-cp37-cp37m-win32.whl/reclaimer/__init__.py
@@ -0,0 +1,25 @@
+#
+# This file is part of Reclaimer.
+#
+# For authors and copyright check AUTHORS.TXT
+#
+# Reclaimer is free software under the GNU General Public License v3.0.
+# See LICENSE for more information.
+#
+
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla, Michelle van der Graaf"
+#           YYYY.MM.DD
+__date__ = "2020.03.07"
+__version__ = (2, 8, 0)
+__website__ = "https://github.com/Sigmmma/reclaimer"
+__all__ = (
+    "animation", "bitmaps", "h2", "h3", "halo_script", "hek", "meta", "misc",
+    "model", "os_hek", "os_v3_hek", "os_v4_hek", "physics",
+    "shadowrun_prototype", "sounds", "strings", "stubbs", "util",
+
+    "common_descs", "constants", "data_extraction", "enums",
+    "field_type_methods", "field_types",
+    )
