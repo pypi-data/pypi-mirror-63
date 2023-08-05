6cfeaefba09b1a1cd619fcd848c2eb04ed423609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:27 +0000

    Add package readee/0.0.23/readee-0.0.23.tar.gz

---
+++ packages/readee/readee-0.0.23.tar.gz/readee-0.0.23/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="readee",
+    version="0.0.23",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Library for export webpage to reader mode html.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/readee",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'bs4',
+        'readability-lxml',
+        'telegram_util>=0.0.20',
+        'hanziconv>=0.3.2',
+    ],
+    python_requires='>=3.0',
+)
