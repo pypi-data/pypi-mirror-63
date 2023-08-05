a654f24db1a6c5bfe25e69d6882b65023292dea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package torchio/0.13.11/torchio-0.13.11-py2.py3-none-any.whl

---
+++ packages/torchio/torchio-0.13.11-py2.py3-none-any.whl/torchio/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for torchio."""
+
+__author__ = """Fernando Perez-Garcia"""
+__email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
+__version__ = '0.13.11'
+
+from . import utils
+from .torchio import *
+from .transforms import *
+from .data import io, sampler, inference, ImagesDataset, Image, Queue, Subject
+from . import datasets
