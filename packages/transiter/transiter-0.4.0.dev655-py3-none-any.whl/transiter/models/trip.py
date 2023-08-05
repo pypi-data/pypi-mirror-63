d3e8659ee02720fb9e376a813b3a846ee557e782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:58 +0000

    Add package transiter/0.4.0.dev655/transiter-0.4.0.dev655-py3-none-any.whl

---
+++ packages/transiter/transiter-0.4.0.dev655-py3-none-any.whl/transiter/models/trip.py
@@ -0,0 +1,66 @@
+import enum
+
+from sqlalchemy import (
+    Column,
+    Enum,
+    TIMESTAMP,
+    Integer,
+    String,
+    Boolean,
+    ForeignKey,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import relationship
+
+from .base import Base, ToDictMixin
+from .updatableentity import updatable_entity
+
+
+@updatable_entity
+class Trip(ToDictMixin, Base):
+    __tablename__ = "trip"
+
+    pk = Column(Integer, primary_key=True)
+    id = Column(String)
+    route_pk = Column(Integer, ForeignKey("route.pk"), nullable=False)
+    source_pk = Column(Integer, ForeignKey("feed_update.pk"), index=True)
+
+    class TripStatus(enum.Enum):
+        SCHEDULED = 1
+        INCOMING_AT = 2
+        STOPPED_AT = 3
+        IN_TRANSIT_TO = 4
+
+    direction_id = Column(Boolean)
+    start_time = Column(TIMESTAMP(timezone=True))
+
+    last_update_time = Column(TIMESTAMP(timezone=True))
+    vehicle_id = Column(String)
+    current_status = Column(Enum(TripStatus))
+    current_stop_sequence = Column(Integer)
+
+    route_id = None
+    stop_id = None
+
+    source = relationship("FeedUpdate", cascade="none")
+    route = relationship("Route", back_populates="trips", cascade="")
+    stop_times = relationship(
+        "TripStopTime",
+        back_populates="trip",
+        order_by="TripStopTime.stop_sequence",
+        cascade="all, delete-orphan",
+        cascade_backrefs=False,
+    )
+
+    __table_args__ = (UniqueConstraint(route_pk, id),)
+
+    __dict_columns__ = [id]
+    __large_dict_columns__ = [
+        id,
+        direction_id,
+        start_time,
+        last_update_time,
+        current_status,
+        current_stop_sequence,
+        vehicle_id,
+    ]
