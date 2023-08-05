8b8de3f9ad1d2634c051e0fc72aed077baef3cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:55 +0000

    Add package readee/0.0.24/readee-0.0.24-py3-none-any.whl

---
+++ packages/readee/readee-0.0.24-py3-none-any.whl/readee/article.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+from readability import Document
+from .content import _findMain
+import cached_url
+
+TO_REMOVE = [
+	'跳到导航', 
+	'跳到搜索', 
+	'Skip to main content'
+]
+
+def _findUrl(url, soup):
+	if 'telegra.ph' not in url:
+		return
+	address = soup.find('address')
+	if not address:
+		return
+	link = address.find('a')
+	return link and link.get('href')
+
+def _trimWebpage(raw):
+	for to_remove in TO_REMOVE:
+		raw = raw.replace(to_remove, '')
+	anchor = '<!-- detail_toolbox -->'
+	index = raw.find(anchor)
+	if index != -1:
+		return raw[:index]
+	return raw
+
+def getArticle(url, content, args = {}):
+	if not content:
+		content = cached_url.get(url)
+	soup = BeautifulSoup(_trimWebpage(content), 'html.parser')
+	article_url = _findUrl(url, soup) # may need to use
+	doc = Document(content)
+	return _findMain(soup, doc, url, args)
+
