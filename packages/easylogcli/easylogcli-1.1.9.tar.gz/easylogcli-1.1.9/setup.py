baf6494626ea837ba3160b0d67a4d76975d1a4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package easylogcli/1.1.9/easylogcli-1.1.9.tar.gz

---
+++ packages/easylogcli/easylogcli-1.1.9.tar.gz/easylogcli-1.1.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easylogcli",
+    version="1.1.9",
+    description="[easylog](https://github.com/prprprus/easylog) 的 Python 客户端实现",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prprprus/easylog-client",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
