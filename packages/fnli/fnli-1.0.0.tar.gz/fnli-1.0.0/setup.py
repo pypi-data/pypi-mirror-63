d8ee563fd2296172103ab2d3dfbdcc1859f8d69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:44 +0000

    Add package fnli/1.0.0/fnli-1.0.0.tar.gz

---
+++ packages/fnli/fnli-1.0.0.tar.gz/fnli-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fnli", # Replace with your own username
+    version="1.0.0",
+    author="mohammad azari jafari",
+    author_email="azari.jafari.m@gmail.com",
+    description="persian Natural Language Inference dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/m-azari/fnli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+		"Natural Language :: Persian",
+    ],
+    python_requires='>=3.6',
+)
