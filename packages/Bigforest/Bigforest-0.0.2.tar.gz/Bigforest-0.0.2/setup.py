1e830f712300fe4f574a9a3d81c2aa0a5b54e2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:33 +0000

    Add package Bigforest/0.0.2/Bigforest-0.0.2.tar.gz

---
+++ packages/Bigforest/Bigforest-0.0.2.tar.gz/Bigforest-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Bigforest",
+    version="0.0.2",
+    author="kenspy",
+    author_email="381717500@qq.com",
+    description="Distributed node router",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kensdm/forest/tree/master",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pickleshare",
+        "pymysql"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
