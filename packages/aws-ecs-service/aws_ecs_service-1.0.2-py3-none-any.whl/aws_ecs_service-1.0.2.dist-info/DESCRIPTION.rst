924e6f5fa53ccf89295b8b3aae360c49e691f0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:01 +0000

    Add package aws-ecs-service/1.0.2/aws_ecs_service-1.0.2-py3-none-any.whl

---
+++ packages/aws-ecs-service/aws_ecs_service-1.0.2-py3-none-any.whl/aws_ecs_service-1.0.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,24 @@
+## Aws Ecs Service
+
+A custom ecs service resource which does not have bugs that traditional ecs service resource does.
+
+#### Remarks
+
+The project is written by [Laimonas Sutkus](https://github.com/laimonassutkus) 
+and is owned by [iDenfy](https://github.com/idenfy). This is an open source
+library intended to be used by anyone. [iDenfy](https://github.com/idenfy) aims
+to share its knowledge and educate market for better and more secure IT infrastructure.
+
+
+# Release history
+
+#### 1.0.2
+Mitigate CloudFormation bug.
+
+#### 1.0.1
+Do not have global boto3 resources/clients.
+
+#### 1.0.0
+Initial project. Not tested. No extensive readme.
+
+
---
+++ packages/aws-ecs-service/aws_ecs_service-1.0.2-py3-none-any.whl/aws_ecs_service/package/index.py
@@ -0,0 +1,93 @@
+import json
+import boto3
+import botocore
+import logging
+
+from botocore.exceptions import ClientError
+from typing import Any, Dict, List
+
+try:
+    from aws_ecs_service.package.action import Action
+    from aws_ecs_service.package import cfnresponse
+except ImportError:
+    # Lambda specific import.
+    # noinspection PyUnresolvedReferences
+    import cfnresponse
+    # noinspection PyUnresolvedReferences
+    from action import Action
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info(f'Version of boto3 lib: {boto3.__version__}.')
+logger.info(f'Version of botocore lib: {botocore.__version__}.')
+
+
+def __try_get_key(dictionary: Dict[str, Any], keys: List[str]) -> Any:
+    for key in keys:
+        value = dictionary.get(key)
+
+        if value:
+            return value
+
+    raise KeyError(f'Non of the keys {keys} were found.')
+
+
+def __success(event, context, data):
+    logger.info('SUCCESS: {}'.format(data))
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, data)
+
+
+def __failed(event, context, data):
+    logger.info('FAIL: {}'.format(data))
+    cfnresponse.send(event, context, cfnresponse.FAILED, data)
+
+
+def __create(**kwargs) -> Dict[str, Any]:
+    response = Action.create(**kwargs)
+    logger.info(json.dumps(response, default=lambda o: '<not serializable>'))
+    return response
+
+
+def __update(**kwargs) -> Dict[str, Any]:
+    response = Action.update(**kwargs)
+    logger.info(json.dumps(response, default=lambda o: '<not serializable>'))
+    return response
+
+
+def __delete(**kwargs) -> Dict[str, Any]:
+    response = Action.delete(**kwargs)
+    logger.info(json.dumps(response, default=lambda o: '<not serializable>'))
+    return response
+
+
+def __handle(event, context):
+    logger.info('Got new request. Event: {}, Context: {}'.format(event, context))
+
+    kwargs = event['ResourceProperties']
+
+    create_args = __try_get_key(kwargs, ['onCreate', 'OnCreate', 'oncreate', 'on_create'])
+    update_args = __try_get_key(kwargs, ['onUpdate', 'OnUpdate', 'onupdate', 'on_update'])
+    delete_args = __try_get_key(kwargs, ['onDelete', 'OnDelete', 'ondelete', 'on_delete'])
+
+    if event['RequestType'] == 'Delete':
+        return __delete(**create_args)
+
+    if event['RequestType'] == 'Create':
+        return __create(**update_args)
+
+    if event['RequestType'] == 'Update':
+        return __update(**delete_args)
+
+    raise KeyError('Unsupported request type! Type: {}'.format(event['RequestType']))
+
+
+def handler(event, context):
+    try:
+        response = __handle(event, context)
+    except ClientError as ex:
+        return __failed(event, context, {'Error': str(ex.response)})
+    except Exception as ex:
+        return __failed(event, context, {'Error': str(ex)})
+
+    __success(event, context, response)
