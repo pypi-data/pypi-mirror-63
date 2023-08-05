02c80fd7fe56d16ec96155c134f102dede2e9fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package myimage/0.0.5/myimage-0.0.5.tar.gz

---
+++ packages/myimage/myimage-0.0.5.tar.gz/myimage-0.0.5/myimage/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------
+# @CreateTime   : 2020/3/10 3:59
+# @Author       : Mark Shawn
+# @Email        : shawninjuly@gmai.com
+# ------------------------------------
+
+QINIU_DOMAIN = 'img.nanchuan.site'
+QINIU_BUCKET = "pfa"
+QINIU_AK     = 'Li-8HoZaqUHFzZ8l87WL-JzBRpjK7c1HTFgSoWso'
+QINIU_SK     = '_Y_ywcqqrmpDskjCrpH2A35iOQMjMHdIsdTsTbz0'
