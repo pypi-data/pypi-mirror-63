c12d1f3efd3f8209a529146ed918df15440d4312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package pushshift.py/0.1.1/pushshift.py-0.1.1.tar.gz

---
+++ packages/pushshift.py/pushshift.py-0.1.1.tar.gz/pushshift.py-0.1.1/pushshift_py/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pushshift.io API Wrapper (for reddit.com public comment/submission search)
+
+https://github.com/typenil/pushshift.py
+"""
+
+from pushshift_py.pushshift_api import PushshiftAPI
+from pushshift_py.pushshift_api_minimal import PushshiftAPIMinimal
+
+__version__ = "0.1.1"
