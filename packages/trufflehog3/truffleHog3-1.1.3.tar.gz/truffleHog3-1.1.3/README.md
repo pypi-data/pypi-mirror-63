136f81e8984ab6ef2d9c1fcaf953000f41542ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package trufflehog3/1.1.3/truffleHog3-1.1.3.tar.gz

---
+++ packages/trufflehog3/truffleHog3-1.1.3.tar.gz/truffleHog3-1.1.3/README.md
@@ -0,0 +1,66 @@
+[![Package Version](https://img.shields.io/pypi/v/truffleHog3.svg)](https://pypi.org/project/truffleHog3)
+![Python Version](https://img.shields.io/badge/python-3.6%2B-informational.svg)
+[![Build Status](https://travis-ci.com/feeltheajf/truffleHog3.svg?branch=master)](https://travis-ci.com/feeltheajf/truffleHog3)
+[![Code Coverage](https://codecov.io/gh/feeltheajf/truffleHog3/branch/master/graph/badge.svg)](https://codecov.io/gh/feeltheajf/truffleHog3)
+[![Downloads](https://pepy.tech/badge/trufflehog3)](https://pepy.tech/project/trufflehog3)
+[![Known Vulnerabilities](https://snyk.io/test/github/feeltheajf/truffleHog3/badge.svg?targetFile=requirements.txt)](https://snyk.io/test/github/feeltheajf/truffleHog3?targetFile=requirements.txt)
+
+
+# truffleHog3
+This is an enhanced version of [truffleHog](https://github.com/dxa4481/truffleHog) scanner
+
+
+## New
+
+- Python 3.6
+- flake8 compliant code
+- output to file option
+- option to disable Git history checks - scan simple files/folders
+- option to exclude files/directories
+- config file support with automatic detection in source code directory, see [trufflehog.json](https://github.com/feeltheajf/truffleHog3/blob/master/trufflehog.json.example), [trufflehog.yaml](https://github.com/feeltheajf/truffleHog3/blob/master/trufflehog.yaml.example)
+
+
+## Installation
+
+Package is available on [PyPI](https://pypi.org/project/truffleHog3)
+
+```
+pip install truffleHog3
+```
+
+
+## Customizing
+
+List of regexes was moved into repository, see [regexes.json](https://github.com/feeltheajf/truffleHog3/blob/master/truffleHog3/regexes.json)
+
+
+## Help
+
+```
+usage: trufflehog3 [options] source
+
+Find secrets in your codebase.
+
+positional arguments:
+  source              URL or local path for secret searching
+
+optional arguments:
+  -h, --help          show this help message and exit
+  -c, --config        path to config file
+  -r, --rules         ignore default regexes and source from json
+  -o, --output        write report to file
+  -b, --branch        name of the branch to be scanned
+  -m, --max-depth     max commit depth for searching
+  -s, --since-commit  scan starting from a given commit hash
+  --json              output in JSON
+  --exclude           exclude paths from scan
+  --whitelist         skip matching strings
+  --no-regex          disable high signal regex checks
+  --no-entropy        disable entropy checks
+  --no-history        disable commit history check
+```
+
+
+## Thanks
+
+Special thanks to Dylan Ayrey ([@dxa4481](https://github.com/dxa4481)), developer of the original [truffleHog](https://github.com/dxa4481/truffleHog) scanner
---
+++ packages/trufflehog3/truffleHog3-1.1.3.tar.gz/truffleHog3-1.1.3/truffleHog3/cli.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+"""truffleHog3 scanner cli."""
+
+import argparse
+import git
+import os
+import shutil
+import sys
+
+from distutils import dir_util
+from signal import signal, SIGINT
+from tempfile import TemporaryDirectory
+from urllib import parse
+
+from truffleHog3 import core
+
+
+def run(**kwargs):
+    graceful_keyboard_interrupt()
+    args = get_cmdline_args()
+    args.__dict__.update(**kwargs)
+    core.config.update(**args.__dict__)
+    issues = []
+
+    for src in args.source:
+        with TemporaryDirectory() as tmp:
+            if args.no_history:
+                source = src.split("://")[-1]
+                if os.path.isdir(source):
+                    dir_util.copy_tree(source, tmp, preserve_symlinks=True)
+                else:
+                    shutil.copy2(source, tmp)
+            else:
+                try:
+                    git.Repo.clone_from(src, tmp)
+                except Exception:  # pragma: no cover
+                    error = "Failed to clone repository: {}".format(src)
+                    raise RuntimeError(error)
+
+                issues += core.search_history(tmp)
+
+            issues += core.search_current(tmp)
+
+    core.log(issues, output=args.output, json_output=args.json_output)
+    return bool(issues)
+
+
+def graceful_keyboard_interrupt():
+    def exit_on_keyboard_interrupt():  # pragma: no cover
+        sys.stdout.write("\rKeyboard interrupt. Exiting\n")
+        sys.exit(0)
+
+    signal(SIGINT, lambda signal, frame: exit_on_keyboard_interrupt())
+
+
+class HelpFormatter(argparse.HelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings:  # pragma: no cover
+            metavar, = self._metavar_formatter(action, action.dest)(1)
+            return metavar
+        else:
+            return ", ".join(action.option_strings)
+
+
+def check_source(source):
+    if not parse.urlparse(source).scheme:
+        source = "file://{}".format(os.path.abspath(source))
+    return source
+
+
+def get_cmdline_args():
+    parser = argparse.ArgumentParser(
+        description="Find secrets in your codebase.",
+        usage="trufflehog3 [options] source",
+        formatter_class=HelpFormatter,
+    )
+    parser.add_argument(
+        "source",
+        help="URLs or local paths for secret searching",
+        type=check_source,
+        nargs="+",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="path to config file",
+        dest="config",
+        type=argparse.FileType("r"),
+    )
+    parser.add_argument(
+        "-r",
+        "--rules",
+        help="ignore default regexes and source from json",
+        dest="rules",
+        type=argparse.FileType("r"),
+        default=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="write report to file",
+        dest="output",
+        type=argparse.FileType("w"),
+    )
+    parser.add_argument(
+        "-b",
+        "--branch",
+        help="name of the branch to be scanned",
+        dest="branch",
+    )
+    parser.add_argument(
+        "-m",
+        "--max-depth",
+        help="max commit depth for searching",
+        dest="max_depth",
+        type=int,
+    )
+    parser.add_argument(
+        "-s",
+        "--since-commit",
+        help="scan starting from a given commit hash",
+        dest="since_commit",
+    )
+    parser.add_argument(
+        "--json",
+        help="output in JSON",
+        dest="json_output",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--exclude", help="exclude paths from scan", dest="exclude", nargs="*"
+    )
+    parser.add_argument(
+        "--whitelist",
+        help="skip matching strings",
+        dest="whitelist",
+        nargs="*",
+    )
+    parser.add_argument(
+        "--no-regex",
+        help="disable high signal regex checks",
+        dest="no_regex",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--no-entropy",
+        help="disable entropy checks",
+        dest="no_entropy",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--no-history",
+        help="disable commit history check",
+        dest="no_history",
+        action="store_true",
+    )
+
+    args, _ = parser.parse_known_args()
+    if args.config:
+        core.config.load(args.config)
+    else:
+        path = args.source[0].split("://")[-1]
+        for filename in core.DEFAULT_CONFIGS:
+            config_path = os.path.join(path, filename)
+            if os.path.exists(config_path):  # pragma: no cover
+                core.config.load(open(config_path))
+
+    parser.set_defaults(**core.config.as_dict)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    sys.exit(run())  # pragma: no cover
