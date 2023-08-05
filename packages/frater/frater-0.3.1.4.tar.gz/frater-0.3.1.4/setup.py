07935834c3c7eb42579d7f90f894741f0f79c876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:20 +0000

    Add package frater/0.3.1.4/frater-0.3.1.4.tar.gz

---
+++ packages/frater/frater-0.3.1.4.tar.gz/frater-0.3.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+packages = find_packages(exclude=['tests*'])
+
+with open('requirements.txt') as f:
+    dependencies = list(line.strip() for line in f.readlines())
+
+with open('README.md') as f:
+    description = f.read()
+
+setup(
+    name='frater',
+    version='0.3.1.4',
+    packages=packages,
+    license='MIT',
+    url='https://github.com/frater-sdk/frater',
+    author='John Henning',
+    description='A Machine Learning and Data-Driven Systems Framework and Toolkit',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author_email='john.l.henning@ibm.com',
+    install_requires=dependencies,
+    python_requires='>=3.6'
+)
