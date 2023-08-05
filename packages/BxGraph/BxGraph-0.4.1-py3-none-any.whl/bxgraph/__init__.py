940a43e1cb00161b1fd781c357bd98b6e911d12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:17 +0000

    Add package BxGraph/0.4.1/BxGraph-0.4.1-py3-none-any.whl

---
+++ packages/BxGraph/BxGraph-0.4.1-py3-none-any.whl/bxgraph/__init__.py
@@ -0,0 +1,11 @@
+#
+#  __init__.py
+#  bxgraph
+#
+#  Created by Oliver Borchert on May 10, 2019.
+#  Copyright (c) 2019 Oliver Borchert. All rights reserved.
+#
+
+from .algorithms import build, func, stats
+from .backend import Graph
+from .walker import BiasedSecondOrderRandomWalker
---
+++ packages/BxGraph/BxGraph-0.4.1-py3-none-any.whl/bxgraph/walker/__init__.py
@@ -0,0 +1,9 @@
+#
+#  walker/__init__.py
+#  bxgraph
+#
+#  Created by Oliver Borchert on May 10, 2019.
+#  Copyright (c) 2019 Oliver Borchert. All rights reserved.
+#
+
+from .node2vec import BiasedSecondOrderRandomWalker
