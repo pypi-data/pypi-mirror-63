f944226e2c5b0ea9c7ca60b8b65441fd3979484c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package dataget/0.4.7/dataget-0.4.7-py3-none-any.whl

---
+++ packages/dataget/dataget-0.4.7-py3-none-any.whl/dataget/__init__.py
@@ -0,0 +1,9 @@
+# datasets
+from .dataset import Dataset
+from .kaggle import kaggle
+from . import image
+from . import toy
+from . import structured
+from . import text
+
+__version__ = "0.4.7"
---
+++ packages/dataget/dataget-0.4.7-py3-none-any.whl/dataget/image/__init__.py
@@ -0,0 +1,4 @@
+from .cifar import cifar10, cifar100
+from .fashion_mnist import fashion_mnist
+from .imagenet import imagenet
+from .mnist import mnist
