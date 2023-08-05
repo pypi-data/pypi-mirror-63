7ed0e623f18a72d3905e8a61a867beb4d15820b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package html-jparser/0.1/html_jparser-0.1.tar.gz

---
+++ packages/html-jparser/html_jparser-0.1.tar.gz/html_jparser-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='html_jparser',
+      version='0.1',
+      install_requires=["requests"],
+      packages=['html_jparser'],
+      zip_safe=False,
+      # Meta data
+      author='FellowCode',
+      author_email='sergo79_f1@mail.ru',
+      description='Easy html parser with Jquery selector',
+      keywords="html parser jquery select easy",
+      url='https://github.com/FellowCode/html-jparser')
