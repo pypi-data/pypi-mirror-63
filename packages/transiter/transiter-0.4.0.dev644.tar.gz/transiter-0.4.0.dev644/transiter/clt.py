b86064c9f1ae2322e035d118a5b62234e988686a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:30 +0000

    Add package transiter/0.4.0.dev644/transiter-0.4.0.dev644.tar.gz

---
+++ packages/transiter/transiter-0.4.0.dev644.tar.gz/transiter-0.4.0.dev644/transiter/clt.py
@@ -0,0 +1,99 @@
+"""
+This module contains the code for the transiterclt (Transiter command line
+tools) command line program.
+"""
+import time
+
+import click
+from sqlalchemy import exc
+
+from transiter import config
+from transiter.data import dbconnection
+from transiter.executor import celeryapp
+from transiter.scheduler import server as scheduler
+
+# noinspection PyUnresolvedReferences
+from transiter.services import feedservice
+
+# TODO: figure this out
+#  maybe use autodiscover_tasks
+# noinspection PyUnresolvedReferences
+from transiter.services import systemservice
+
+
+@click.group()
+def transiter_clt():
+    """
+    Transiter Command Line Tools
+    """
+    pass
+
+
+@transiter_clt.command()
+@click.option(
+    "-f",
+    "--force",
+    is_flag=True,
+    help="Force start by killing any process listening on the target port.",
+)
+@click.argument("server", type=click.Choice(["webservice", "scheduler", "executor"]))
+def launch(force, server):
+    """
+    Launch a Transiter service in debug mode.
+    """
+    if server == "webservice":
+        # NOTE: the flask app is imported here because otherwise the task server will
+        # use the app's logging configuration.
+        from transiter.http import flaskapp
+
+        flaskapp.launch(force)
+    if server == "scheduler":
+        app = scheduler.create_app()
+        app.run(host="0.0.0.0", port=config.SCHEDULER_PORT, debug=False)
+    if server == "executor":
+        celeryapp.run()
+
+
+@transiter_clt.group()
+def db():
+    """
+    Perform Transiter database operation.
+    """
+
+
+@db.command()
+def schema():
+    """
+    Dump the Transiter database's schema in SQL.
+    """
+    dbconnection.generate_schema()
+
+
+@db.command()
+def init():
+    """
+    Initialize the Transiter database.
+    """
+    while True:
+        try:
+            dbconnection.init_db()
+            print("DB ready")
+            return
+        except exc.SQLAlchemyError:
+            print("Failed to connect to the DB; Waiting 1 second")
+            time.sleep(1)
+
+
+@db.command()
+@click.confirmation_option(
+    prompt="This will result in all data in the current database being lost.\n"
+    "Are you sure?"
+)
+def reset():
+    """
+    Reset the Transiter database.
+
+    This operation drops all of the Transiter tables in the database, if they
+    exist, and then creates them. All existing data will be lost.
+    """
+    dbconnection.rebuild_db()
---
+++ packages/transiter/transiter-0.4.0.dev644.tar.gz/transiter-0.4.0.dev644/transiter/executor/celeryapp.py
@@ -0,0 +1,15 @@
+import os
+
+from celery import Celery
+
+host = os.environ.get("TRANSITER_RABBITMQ_HOST", "127.0.0.1")
+
+app = Celery("transiter", broker="amqp://{}".format(host))
+
+
+def run():
+    app.start(argv=["celeryapp", "worker", "-l", "info"])
+
+
+if __name__ == "__main__":
+    run()
