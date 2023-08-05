7ce1fb61e832dcbad514fd6effb8f9e65548f1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package spaceone-secret/0.1.1.post91/spaceone_secret-0.1.1.post91-py3-none-any.whl

---
+++ packages/spaceone-secret/spaceone_secret-0.1.1.post91-py3-none-any.whl/spaceone/secret/conf/proto_conf.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+PROTO = {
+    'v1': {
+        'credential': ['Credential'],
+        'credential_group': ['CredentialGroup']
+    }
+}
---
+++ packages/spaceone-secret/spaceone_secret-0.1.1.post91-py3-none-any.whl/spaceone/secret/model/__init__.py
@@ -0,0 +1,2 @@
+from spaceone.secret.model.credential_model import Credential
+from spaceone.secret.model.credential_group_model import CredentialGroup, CredentialGroupMap
