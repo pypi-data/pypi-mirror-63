948296ebb857c694f6723af5a9154461a056813e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package dataset/1.2.3/dataset-1.2.3.tar.gz

---
+++ packages/dataset/dataset-1.2.3.tar.gz/dataset-1.2.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dataset',
+    version='1.2.3',
+    description="Toolkit for Python-based database access.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='sql sqlalchemy etl loading utility',
+    author='Friedrich Lindenberg, Gregor Aisch, Stefan Wehrmeyer',
+    author_email='friedrich.lindenberg@gmail.com',
+    url='http://github.com/pudo/dataset',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'sqlalchemy >= 1.2.3',
+        'alembic >= 0.6.2'
+    ],
+    tests_require=[
+        'nose'
+    ],
+    test_suite='test',
+    entry_points={}
+)
