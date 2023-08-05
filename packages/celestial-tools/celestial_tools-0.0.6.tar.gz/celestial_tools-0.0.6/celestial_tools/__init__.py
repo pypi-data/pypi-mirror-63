4fafdd60e1047021a4c0b3ab68484968872bed3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package celestial-tools/0.0.6/celestial_tools-0.0.6.tar.gz

---
+++ packages/celestial-tools/celestial_tools-0.0.6.tar.gz/celestial_tools-0.0.6/celestial_tools/__init__.py
@@ -0,0 +1,9 @@
+"""
+The core celestial_tools package is broken out into two pieces.
+
+client - the target emebedded device
+
+server - host to serve files, etc to  the target device
+"""
+
+from celestial_tools import client
