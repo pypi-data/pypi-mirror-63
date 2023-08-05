c78c621dca65376e8c30243a2f5f83587048b490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:13 +0000

    Add package blackfox/0.1.0/blackfox-0.1.0-py3-none-any.whl

---
+++ packages/blackfox/blackfox-0.1.0-py3-none-any.whl/blackfox/log_writer.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import sys
+
+class LogWriter(object):
+    """LogWriter provides logging capabilities for an ongoing Black Fox optimization.
+
+    Parameters
+    ----------
+    log_file : str
+        Optional file or sys.stdout used for logging
+
+    """
+
+    def __init__(self, log_file=sys.stdout):
+        self.log_file = log_file
+
+    def write_status(self, id, status):
+
+        msg = ("%s -> %s, "
+               "Generation: %s/%s, "
+               "Validation set error: %f, "
+               "Training set error: %f, "
+               "Epoch: %d, "
+               "Optimization Id: %s") % (
+            datetime.now(),
+            status.state,
+            status.generation,
+            status.total_generations,
+            status.validation_set_error,
+            status.training_set_error,
+            status.epoch,
+            id
+        )
+        self.write_string(msg)
+
+    def write_string(self, msg):
+        if isinstance(self.log_file, str):
+            with open(self.log_file, mode='a', encoding='utf-8', buffering=1) as f:
+                f.write(msg+'\n')
+        else:
+            self.log_file.write(msg+'\n')
+            self.log_file.flush()
