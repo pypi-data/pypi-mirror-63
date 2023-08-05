a902c8626f033c59830d1e2fd5bd020ebabb7038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package pytablereader/0.28.0/pytablereader-0.28.0.tar.gz

---
+++ packages/pytablereader/pytablereader-0.28.0.tar.gz/pytablereader-0.28.0/pytablereader/factory/__init__.py
@@ -0,0 +1,6 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from ._file import TableFileLoaderFactory
+from ._url import TableUrlLoaderFactory
---
+++ packages/pytablereader/pytablereader-0.28.0.tar.gz/pytablereader-0.28.0/pytablereader/loadermanager/__init__.py
@@ -0,0 +1,6 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from ._file import TableFileLoader
+from ._url import TableUrlLoader
