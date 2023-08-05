6083bb571d46a240e1d31da4df9d7c54fab07551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package pydebootstrap/1.2.2/pydebootstrap-1.2.2.tar.gz

---
+++ packages/pydebootstrap/pydebootstrap-1.2.2.tar.gz/pydebootstrap-1.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pydebootstrap',
+      version='1.2.2',
+      description='Python wrapper for debootstrap',
+      long_description="Python Wrapper for debootstrap on systemd systems.",
+      long_description_content_type = "text/plain",
+      url='http://github.com/dfoulks1/pydebootstrap',
+      author='Drew Foulks',
+      author_email='dfoulks@gmail.com',
+      license='Apache v2 License',
+      packages=['pydebootstrap'],
+      zip_safe=False)
