bca0487dcdf2ee5b5b6fe9d900283292ba764be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package ai4drones/0.0.0/ai4drones-0.0.0.tar.gz

---
+++ packages/ai4drones/ai4drones-0.0.0.tar.gz/ai4drones-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ai4drones',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='',
+    author='Aly Shmahell',
+    author_email='aly.shmahell@gmail.com',
+    packages=[],
+)
