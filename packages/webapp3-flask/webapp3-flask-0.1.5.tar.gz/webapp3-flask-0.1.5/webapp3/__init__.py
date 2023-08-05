ba910fa158a38ccfd8200d67a5ae951c47d0a5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:32 +0000

    Add package webapp3-flask/0.1.5/webapp3-flask-0.1.5.tar.gz

---
+++ packages/webapp3-flask/webapp3-flask-0.1.5.tar.gz/webapp3-flask-0.1.5/webapp3/__init__.py
@@ -0,0 +1,3 @@
+#the Init file
+import webapp3.main
+from webapp3.main import *
---
+++ packages/webapp3-flask/webapp3-flask-0.1.5.tar.gz/webapp3-flask-0.1.5/webapp3/main.py
@@ -0,0 +1,53 @@
+from flask import Flask, session,request,redirect, url_for
+
+class Response:
+  def __init__(self, handler):
+    self.handler = handler
+
+  def write(self, stuffAdd):
+    self.handler.data += stuffAdd
+
+
+#this replaces your webapp2 base handler and makes it so that most of your webapp2 syntax will work and run on 
+#Python3 using Flask instead of webapp2. It allows you to do self.response.write("whatever"), sessions, and redirects
+
+class BaseHandler:
+  def __init__(self):
+    self.data = ""
+    self.ChangePage="None"
+    self.response = Response(self)
+    self.session = session
+    self.request= {}
+  def redirect(self,arg):
+    self.ChangePage=arg
+    
+    
+class RequestHandler(BaseHandler):
+  pass
+
+class webapp2:
+  RequestHandler = RequestHandler
+
+
+
+def webappGet(daPage):
+    darequests=request.form
+    daPage.request=darequests
+    daPage.get()
+    if daPage.ChangePage=="None":
+        return daPage.data
+    else:
+        return redirect(daPage.ChangePage)
+
+def webappPost(daPage):
+    darequests=request.form
+    daPage.request=darequests
+    daPage.post()
+    if daPage.ChangePage=="None":
+        return daPage.data
+    else:
+        return redirect(daPage.ChangePage)
+    
+
+
+
