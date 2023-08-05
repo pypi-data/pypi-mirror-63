4a41471425ca58712301802d3158cdcfcc9f377a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:23 +0000

    Add package robotframework-dictkeyallpaths/0.1.1/robotframework-dictkeyallpaths-0.1.1.tar.gz

---
+++ packages/robotframework-dictkeyallpaths/robotframework-dictkeyallpaths-0.1.1.tar.gz/robotframework-dictkeyallpaths-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import os.path
+
+
+setup(
+  name = 'robotframework-dictkeyallpaths',         # How you named your package folder (MyLib)
+  packages = ['robotframework-dictkeyallpaths'],   # Chose the same as "name"
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'robot framework - To get all paths of dictionary',   # Give a short description about your library
+  long_description='This is a function for robotframework that gets the keys of all values ​​in the dictionary .',
+  author = 'Yoochar',                   # Type in your name
+  author_email = 'yoochar@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/yoochar',   # Provide either the link to your github or to your website
+#  download_url = 'https://github.com/yoochar',    # I explain this later on
+  keywords = ['robot', 'robotframework', 'dictionary', 'path','key','jsonpath','dictpath','getallpath','getallkey'],   # Keywords that define your package best
+  install_requires=[
+          'json',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
