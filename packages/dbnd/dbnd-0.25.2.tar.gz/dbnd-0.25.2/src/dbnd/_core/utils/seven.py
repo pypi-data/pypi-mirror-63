e2c473216e70901721de953bda799540eece094f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:35 +0000

    Add package dbnd/0.25.2/dbnd-0.25.2.tar.gz

---
+++ packages/dbnd/dbnd-0.25.2.tar.gz/dbnd-0.25.2/src/dbnd/_core/utils/seven.py
@@ -0,0 +1,21 @@
+import six
+
+
+if six.PY3:
+    import contextlib
+else:
+    import contextlib2 as contextlib
+
+
+def qualname_func(func):
+    if six.PY3:
+        return func.__qualname__
+    return "%s.%s" % (func.__module__, func.__name__)
+
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+__all__ = ["contextlib", "qualname_func"]
