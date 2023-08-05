4670bb54abdd0c358883b59bb067fa8d421d81ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package ChineseTimeNLP/1.1.5/ChineseTimeNLP-1.1.5-py3-none-any.whl

---
+++ packages/ChineseTimeNLP/ChineseTimeNLP-1.1.5-py3-none-any.whl/chinese_time_nlp/__init__.py
@@ -0,0 +1,21 @@
+import sys
+
+from loguru import logger
+
+from .resource.pattern import r, pattern
+from .LunarSolarConverter import LunarSolarConverter
+from .StringPreHandler import StringPreHandler
+from .TimeNormalizer import TimeNormalizer
+
+logger.remove()
+default_logger = logger.add(sys.stdout, level="INFO")
+
+
+__all__ = [
+    "TimeNormalizer",
+    "StringPreHandler",
+    "LunarSolarConverter",
+    "default_logger",
+    "pattern",
+    "r",
+]
