7604b9489468e8d8f37272af60fc52e14395b72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package flake8-pytest-style/0.3.0/flake8-pytest-style-0.3.0.tar.gz

---
+++ packages/flake8-pytest-style/flake8-pytest-style-0.3.0.tar.gz/flake8-pytest-style-0.3.0/flake8_pytest_style/plugin.py
@@ -0,0 +1,30 @@
+import argparse
+from typing import List
+
+from flake8.options.manager import OptionManager
+from flake8_plugin_utils import Plugin
+
+from .config import Config
+from .visitors import PytestStyleVisitor
+
+__version__ = '0.3.0'
+
+
+class PytestStylePlugin(Plugin[Config]):
+    name = 'flake8-pytest-style'
+    version = __version__
+    visitors = [PytestStyleVisitor]
+
+    @classmethod
+    def add_options(cls, option_manager: OptionManager) -> None:  # pragma: no cover
+        option_manager.add_option(
+            '--pytest-raises-require-match-for',
+            comma_separated_list=True,
+            parse_from_config=True,
+        )
+
+    @classmethod
+    def parse_options_to_config(  # pylint: disable=unused-argument
+        cls, option_manager: OptionManager, options: argparse.Namespace, args: List[str]
+    ) -> Config:  # pragma: no cover
+        return Config(raises_require_match_for=options.pytest_raises_require_match_for)
