3cb1b71472678f1ae7ca63395b7ead601cb339d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package comod/0.1.0/comod-0.1.0.tar.gz

---
+++ packages/comod/comod-0.1.0.tar.gz/comod-0.1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script"""
+import os
+from setuptools import setup, find_packages
+
+if os.environ.get('READTHEDOCS') == 'True':
+    requirements = []
+else:
+    requirements = ["numpy", "scipy", "python-igraph"]
+
+setup(author="Dih5",
+      author_email='dihedralfive@gmail.com',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      description='Compartmental modelling Python package',
+      extras_require={
+          "docs": ["nbsphinx", "sphinx-rtd-theme", "IPython"],
+          "test": ["pytest"],
+      },
+      keywords=[],
+      name='comod',
+      packages=find_packages(include=['comod'], exclude=["demos", "tests", "docs"]),
+      install_requires=requirements,
+      url='https://github.com/dih5/comod',
+      version='0.1.0',
+
+      )
