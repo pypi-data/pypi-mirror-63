5f617cdb65b954cb2e8cb9d6d36e0cf706ff8eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:01 +0000

    Add package simplegan/0.2/simplegan-0.2.tar.gz

---
+++ packages/simplegan/simplegan-0.2.tar.gz/simplegan-0.2/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+  name = 'simplegan',         
+  packages = ['simplegan'],   
+  version = '0.2',    
+  license='MIT',      
+  description = 'Framework to ease training of generative models based on TensorFlow',   
+  author = 'Rohith Gandhi G',             
+  author_email = 'grohith327@gmail.com',  
+  url = 'https://github.com/grohith327',  
+  download_url = 'https://github.com/grohith327/simplegan/archive/v0.2.tar.gz',
+  keywords = ['GAN', 'Computer Vision', 'Deep Learning', 'TensorFlow', 'Generative Models', 'Neural Networks', 'AI'],
+  install_requires=[            
+          'tensorflow',
+          'tqdm',
+          'numpy',
+          'opencv-python',
+          'imageio',
+          'tensorflow-datasets'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Image Recognition',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
