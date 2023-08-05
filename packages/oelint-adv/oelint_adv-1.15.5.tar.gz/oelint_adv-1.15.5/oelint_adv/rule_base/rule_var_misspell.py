81e61e9d57cda8edf6ee963b3aee513b7cd89422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package oelint-adv/1.15.5/oelint_adv-1.15.5.tar.gz

---
+++ packages/oelint-adv/oelint_adv-1.15.5.tar.gz/oelint_adv-1.15.5/oelint_adv/rule_base/rule_var_misspell.py
@@ -0,0 +1,33 @@
+from oelint_adv.cls_item import Variable
+from oelint_adv.cls_rule import Rule
+from oelint_adv.const_vars import get_known_vars
+from difflib import SequenceMatcher
+
+
+class VarMisspell(Rule):
+    def __init__(self):
+        super().__init__(id="oelint.vars.mispell",
+                         severity="warning",
+                         message="<FOO>")
+
+    def get_best_match(self, item, _list, minconfidence=0.8):
+        _dict = sorted([(SequenceMatcher(None, item, k).ratio(), k)
+                        for k in _list], key=lambda x: x[0], reverse=True)
+        if _dict and _dict[0][0] >= minconfidence:
+            return _dict[0][1]
+        return ""
+
+    def check(self, _file, stash):
+        res = []
+        items = stash.GetItemsFor(filename=_file, classifier=Variable.CLASSIFIER,
+                                  attribute=Variable.ATTR_VAR)
+
+        for i in items:
+            if i.VarName in get_known_vars():
+                continue
+            _bestmatch = self.get_best_match(i.VarName, get_known_vars())
+            if _bestmatch:
+                res += self.finding(i.Origin, i.InFileLine,
+                                    "'{}' is unknown, maybe you mean '{}'".format(
+                                        i.VarName, _bestmatch))
+        return res
