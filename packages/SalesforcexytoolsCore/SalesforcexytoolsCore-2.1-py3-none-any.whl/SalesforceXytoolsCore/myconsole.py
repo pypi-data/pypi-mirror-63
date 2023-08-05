b5411b51a1802ab13074a395e77733d6e6badb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package SalesforcexytoolsCore/2.1/SalesforcexytoolsCore-2.1-py3-none-any.whl

---
+++ packages/SalesforcexytoolsCore/SalesforcexytoolsCore-2.1-py3-none-any.whl/SalesforceXytoolsCore/myconsole.py
@@ -0,0 +1,13 @@
+"""
+Create by exia.huangxy
+https://github.com/exiahuang
+"""
+
+class MyConsole(object):
+    def debug(self, msg):
+        # print(msg)
+        return
+
+    def log(self, msg):
+        # print(msg)
+        return
