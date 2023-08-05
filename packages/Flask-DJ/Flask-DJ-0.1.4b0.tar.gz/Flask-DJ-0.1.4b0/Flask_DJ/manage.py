c6421773d3d32a3098405cfc6b7b809c5b5641b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:23 +0000

    Add package Flask-DJ/0.1.4b0/Flask-DJ-0.1.4b0.tar.gz

---
+++ packages/Flask-DJ/Flask-DJ-0.1.4b0.tar.gz/Flask-DJ-0.1.4b0/Flask_DJ/manage.py
@@ -0,0 +1,48 @@
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+from importlib import import_module
+from Flask_DJ.templates import views_file, models_file, urls_file, forms_file
+from waitress import serve
+from .utils import get_project_name, create_folder, create_file
+
+"""database-methods: https://flask-migrate.readthedocs.io/en/latest/"""
+manager = None
+app_ = None
+
+commands = [
+    'startapp',
+]
+
+
+def init_manage(app):
+    global manager, app_
+    app_ = app
+    manager = Manager(app)
+
+
+def init_db(models=[]):
+    manager.add_command('db', MigrateCommand)
+    for file in models:
+        import_module(file)
+
+
+def runserver(host, port):
+    if app_.debug:
+        app_.run(host=host, port=port)
+    else:
+        serve(app_, host=host, port=port)
+
+
+def create_app_files(app_name):
+    project_name = get_project_name()
+    create_file(app_name, 'views', views_file)
+    create_file(app_name, 'models', models_file.format(project_name=project_name))
+    create_file(app_name, 'urls', urls_file.format(functions="relative_path"))
+    create_file(app_name, 'forms', forms_file)
+
+
+def startapp(name):
+    """Create folder containing forms.py, models.py, urls.py, views.py"""
+    create_folder(name)
+    create_app_files(name)
+    print(f'app {name} created')
