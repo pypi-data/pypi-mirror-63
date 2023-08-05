f12e662d3379a4f54d8e907e2d01a733c8f822c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package fingerprint/0.1.5/fingerprint-0.1.5.tar.gz

---
+++ packages/fingerprint/fingerprint-0.1.5.tar.gz/fingerprint-0.1.5/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+
+with open('README.md', 'r') as fp:
+    README = fp.read()
+
+
+setup(
+    name='fingerprint',
+    version='0.1.5',
+    description='Document fingerprint generator',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Kailash Budhathoki',
+    author_email='kailash.buki@gmail.com',
+    url='http://github.com/kailashbuki/fingerprint',
+    license='MIT License',
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
