fe6c24ddf5cfb700494cc357b73a1027508d5581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:41 +0000

    Add package devops-microsoft-mapping-sspo/0.2.3/devops_microsoft_mapping_sspo-0.2.3-py3-none-any.whl

---
+++ packages/devops-microsoft-mapping-sspo/devops_microsoft_mapping_sspo-0.2.3-py3-none-any.whl/devops_microsoft_mapping_sspo/factories.py
@@ -0,0 +1,22 @@
+import factory
+from devops_microsoft_mapping_sspo.scrum_intented_development_task import ScrumIntentedDevelopmentTask
+from devops_microsoft_mapping_sspo.scrum_performed_development_task import ScrumPerformedDevelopmentTask
+from devops_microsoft_mapping_sspo.scrum_atomic_project import ScrumAtomicProject
+from devops_microsoft_mapping_sspo.scrum_complex_project import ScrumComplexProject
+import factory
+
+class ScrumAtomicProjectFactory(factory.Factory):
+    class Meta:
+        model = ScrumAtomicProject
+
+class ScrumComplexProjectFactory(factory.Factory):
+    class Meta:
+        model = ScrumComplexProject
+
+class ScrumIntentedDevelopmentTaskFactory(factory.Factory):
+    class Meta:
+        model = ScrumIntentedDevelopmentTask
+
+class ScrumPerformedDevelopmentTaskFactory(factory.Factory):
+    class Meta:
+        model = ScrumPerformedDevelopmentTask
