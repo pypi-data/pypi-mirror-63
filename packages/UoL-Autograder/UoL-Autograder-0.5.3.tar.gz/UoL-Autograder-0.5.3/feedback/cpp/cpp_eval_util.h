9ac17e66c674be9d6a46b5c4a2e7ff44025c7952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package UoL-Autograder/0.5.3/UoL-Autograder-0.5.3.tar.gz

---
+++ packages/UoL-Autograder/UoL-Autograder-0.5.3.tar.gz/UoL-Autograder-0.5.3/feedback/cpp/cpp_eval_util.h
@@ -0,0 +1,33 @@
+#ifndef _CPP_EVAL_UTIL_H
+#define _CPP_EVAL_UTIL_H
+
+#include "json.hpp"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <tuple>
+#include <iostream>
+
+
+using namespace std;
+using namespace nlohmann;
+
+
+
+tuple<string, float> process_result(vector<string> result, vector<string> test_case, string question_specific_fail_message = "");
+bool is_within_margin(float a, float b, float margin);
+bool is_within_margin(double a, double b, double margin);
+string byte_to_binary(int x);
+string vector_to_string(vector<int> a);
+
+class Results
+{
+public:
+   void add(std::string question, std::string score, float weight, std::string feedback);
+   void output(char filename[]);
+
+private:
+   std::vector<nlohmann::json> _test_results;
+};
+
+#endif
