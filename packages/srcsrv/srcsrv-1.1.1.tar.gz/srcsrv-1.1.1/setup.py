fa6a43c8d139dde8e2e2504e1616ec6f4cf2ab24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package srcsrv/1.1.1/srcsrv-1.1.1.tar.gz

---
+++ packages/srcsrv/srcsrv-1.1.1.tar.gz/srcsrv-1.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="srcsrv",
+    version="1.1.1",
+    author="Uri Mann",
+    author_email="abba.mann@gmail.com",
+    description="Source indexing package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/urielmann/srcsrv",
+    license = "MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 4 - Beta"
+    ],
+    install_requires=[
+        'GitPython', 'svn'
+    ],
+    python_requires='>=3.6',
+    packages=['srcsrv', 'srcsrv.plugins'],
+    package_dir={'srcsrv': 'srcsrv'}
+)
