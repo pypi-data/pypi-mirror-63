e2da0455fa4981726a14258ec6af58697c3554e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:47 +0000

    Add package PyFLOTRAN/1.0.1/PyFLOTRAN-1.0.1.tar.gz

---
+++ packages/PyFLOTRAN/PyFLOTRAN-1.0.1.tar.gz/PyFLOTRAN-1.0.1/PyFLOTRAN/interpolation/SparseDataInterpolator.py
@@ -0,0 +1,17 @@
+"""
+Interpolates a given set of points into a PFLOTRAN mesh
+"""
+import numpy as np
+from scipy.interpolate import griddata
+from .BaseInterpolator import BaseInterpolator
+
+
+class SparseDataInterpolator(BaseInterpolator):
+    def interpolate(self, **kwargs):
+        self.interpolated_data = griddata(self.data[:, 0:3], self.data[:, 3], self.mesh, **kwargs)
+        return self.get_data()
+
+    def get_data(self):
+        temp_array = np.reshape(self.interpolated_data, (self.interpolated_data.shape[0], 1))
+        return np.concatenate((self.mesh, temp_array), axis=1)
+
