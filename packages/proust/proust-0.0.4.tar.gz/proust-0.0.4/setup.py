4c15dbbd75e2a8a21cf7bacc6b0477683141ae58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:51 +0000

    Add package proust/0.0.4/proust-0.0.4.tar.gz

---
+++ packages/proust/proust-0.0.4.tar.gz/proust-0.0.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="proust",
+    version="0.0.4",
+    author="Daniel Suo",
+    author_email="danielsuo@gmail.com",
+    description="A package for time series analysis",
+    url="https://github.com/danielsuo/proust",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["timeseries", "time", "series", "analysis"],
+    python_requires=">=3.5",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "cython",
+        "jax",
+        "jaxlib",
+        "scikit-learn",
+        "matplotlib",
+    ],
+    extras_require={
+        "dev": [
+            "flake8",
+            "flake8-print",
+            "flake8-bugbear",
+            "mypy",
+            "pytest",
+            "pytest-xdist",
+            "pytest-cov",
+            "pylint",
+            "black",
+            "isort",
+            "autoflake",
+            "pre-commit",
+            "pydocstring-coverage",
+            "bumpversion",
+        ]
+    },
+)
