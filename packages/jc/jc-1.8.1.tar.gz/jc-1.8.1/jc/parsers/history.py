feab048b126214e61027ee48ac8d781ebd324742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:32 +0000

    Add package jc/1.8.1/jc-1.8.1.tar.gz

---
+++ packages/jc/jc-1.8.1.tar.gz/jc-1.8.1/jc/parsers/history.py
@@ -0,0 +1,139 @@
+"""jc - JSON CLI output utility history Parser
+
+Usage:
+
+    specify --history as the first argument if the piped input is coming from history
+
+Compatibility:
+
+    'linux', 'darwin', 'cygwin', 'aix', 'freebsd'
+
+Examples:
+
+    $ history | jc --history -p
+    [
+      {
+        "line": 118,
+        "command": "sleep 100"
+      },
+      {
+        "line": 119,
+        "command": "ls /bin"
+      },
+      {
+        "line": 120,
+        "command": "echo \"hello\""
+      },
+      {
+        "line": 121,
+        "command": "docker images"
+      },
+      ...
+    ]
+
+    $ history | jc --history -p -r
+    {
+      "118": "sleep 100",
+      "119": "ls /bin",
+      "120": "echo \"hello\"",
+      "121": "docker images",
+      ...
+    }
+"""
+import jc.utils
+
+
+class info():
+    version = '1.2'
+    description = 'history command parser'
+    author = 'Kelly Brazil'
+    author_email = 'kellyjonbrazil@gmail.com'
+    details = 'Optimizations by https://github.com/philippeitis'
+
+    # compatible options: linux, darwin, cygwin, win32, aix, freebsd
+    compatible = ['linux', 'darwin', 'cygwin', 'aix', 'freebsd']
+
+
+__version__ = info.version
+
+
+def process(proc_data):
+    """
+    Final processing to conform to the schema.
+
+    Parameters:
+
+        proc_data:   (dictionary) raw structured data to process
+
+    Returns:
+
+        List of dictionaries. Structured data with the following schema:
+
+        [
+          {
+            "line":     integer,
+            "command":  string
+          }
+        ]
+    """
+
+    # rebuild output for added semantic information
+    processed = []
+    for k, v in proc_data.items():
+        proc_line = {}
+        proc_line['line'] = k
+        proc_line['command'] = v
+        processed.append(proc_line)
+
+    for entry in processed:
+        int_list = ['line']
+        for key in int_list:
+            if key in entry:
+                try:
+                    key_int = int(entry[key])
+                    entry[key] = key_int
+                except (ValueError):
+                    entry[key] = None
+
+    return processed
+
+
+def parse(data, raw=False, quiet=False):
+    """
+    Main text parsing function
+
+    Parameters:
+
+        data:        (string)  text data to parse
+        raw:         (boolean) output preprocessed JSON if True
+        quiet:       (boolean) suppress warning messages if True
+
+    Returns:
+
+        Dictionary of raw structured data or
+        list of dictionaries of processed structured data
+    """
+    if not quiet:
+        jc.utils.compatibility(__name__, info.compatible)
+
+    raw_output = {}
+
+    # split lines and clear out any non-ascii chars
+    linedata = data.encode('ascii', errors='ignore').decode().splitlines()
+
+    # Clear any blank lines
+    cleandata = list(filter(None, linedata))
+
+    if cleandata:
+        for entry in cleandata:
+            try:
+                parsed_line = entry.split(maxsplit=1)
+                raw_output[parsed_line[0]] = parsed_line[1]
+            except IndexError:
+                # need to catch indexerror in case there is weird input from prior commands
+                pass
+
+    if raw:
+        return raw_output
+    else:
+        return process(raw_output)
