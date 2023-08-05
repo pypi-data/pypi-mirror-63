b4f6c3b0664f653c615b9f3d6937659f38591668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package bond-cli/0.0.2/bond-cli-0.0.2.tar.gz

---
+++ packages/bond-cli/bond-cli-0.0.2.tar.gz/bond-cli-0.0.2/bond/app.py
@@ -0,0 +1,21 @@
+import bond.cli
+import sys
+
+COMMANDS = [
+    "discover",
+    "select",
+    "version",
+    "token",
+    "list",
+    "devices",
+    "device_create",
+    "livelog",
+    "signal",
+    "reset",
+    "wifi",
+]
+
+
+def run():
+    bond.cli.load_commands(COMMANDS)
+    bond.cli.execute_from_command_line(sys.argv)
