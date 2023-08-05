c644df861358579efc4d7b8974807c0e59f89b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package covid-py/0.1.3/covid_py-0.1.3.tar.gz

---
+++ packages/covid-py/covid_py-0.1.3.tar.gz/covid_py-0.1.3/covid_py/covid_py.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import date,datetime,timedelta
+
+file_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+
+def get_confirmed_cases():
+    return pd.read_csv(file_confirmed)
+
+def plot_cases(df,date,y_label):
+    columns = [df.columns[1]]
+    columns = columns + [c for c in df.columns[4:]]
+    df[columns].iloc[:,1:] = df[columns].iloc[:,1:].apply(pd.to_numeric)
+    result = df[columns].groupby('Country/Region').sum()
+    result = np.log(result)
+    plt.figure(figsize=(25, 6))
+    result = result.sort_values(by=date,ascending=False)
+    y = result.index
+    x = result[date]
+    plt.bar(y, x, align='center', alpha=0.5)
+    plt.ylabel(y_label)
+    plt.xticks(rotation=90)
+    plt.show()
+
+def plot_confirmed_cases():
+    plot_cases(get_confirmed_cases(),date.today().strftime("%-m/%-d/%y"),'confirmed')
+
---
+++ packages/covid-py/covid_py-0.1.3.tar.gz/covid_py-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='covid_py',
+    version='0.1.3',
+    description='Analysis of Corona Dataset',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Sirisha Cherala',
+    author_email='sirishacherala@gmail.com',
+    keywords=['Coronavirus', 'COVID']
+)
+
+install_requires = [
+    'pandas', 'numpy', 'plotly'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
