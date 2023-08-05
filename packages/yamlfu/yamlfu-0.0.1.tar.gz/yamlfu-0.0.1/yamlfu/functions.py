86c8c88ed55f8fe180aafe569c52cd63ead44aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package yamlfu/0.0.1/yamlfu-0.0.1.tar.gz

---
+++ packages/yamlfu/yamlfu-0.0.1.tar.gz/yamlfu-0.0.1/yamlfu/functions.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+import yaml
+from pprint import pprint
+
+
+def provide_yamlfu_functions(symbols):
+    symbols["render"] = render
+
+
+def render(template, *args, **kwargs):
+    from yamlfu.loader import Loader
+    loader = Loader(deepcopy(template))
+    _arguments = template["_arguments"]
+    template_args = _arguments.split()
+    assert len(template_args) == len(args)
+    render_args = {}
+    for i, value in enumerate(template_args):
+        render_args[value] = args[i]
+    result = loader.resolve(render_args)[0]
+    result["_internal_render"] = True
+    return result
