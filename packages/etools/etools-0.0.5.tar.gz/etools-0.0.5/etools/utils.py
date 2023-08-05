142fb3cd732a2b934d816029e669c6262f1783c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package etools/0.0.5/etools-0.0.5.tar.gz

---
+++ packages/etools/etools-0.0.5.tar.gz/etools-0.0.5/etools/utils.py
@@ -0,0 +1,33 @@
+import time
+import logging
+import etools.consts
+
+
+logger = logging.getLogger(etools.consts.LOGGER_NAME)
+
+
+def clear_none_in_dict(d, recursive=True):
+    clear_keys = []
+    for k, v in d.items():
+        if v is None:
+            clear_keys.append(k)
+        if isinstance(v, dict) and recursive:
+            clear_none_in_dict(v, True)
+    for k in clear_keys:
+        del d[k]
+    return d
+
+
+def bound(val, min_val, max_val):
+    return min(max(val, min_val), max_val)
+
+
+def log_run_time(action):
+    def decorator(func):
+        def inner_func(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            logger.info("action [%s] cost [%f]", action, time.time() - start)
+            return result
+        return inner_func
+    return decorator
