8d4fd68fb7cd29a3cc9bcb0bf30a573b7ab81b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package BitSet/0.1.2/BitSet-0.1.2.tar.gz

---
+++ packages/BitSet/BitSet-0.1.2.tar.gz/BitSet-0.1.2/bitset/__init__.py
@@ -0,0 +1,1 @@
+from .bitset import BitSet
---
+++ packages/BitSet/BitSet-0.1.2.tar.gz/BitSet-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="BitSet",
+    version="0.1.2",
+    description="A Tool to see and use Binary data as Int",
+    url="https://gitee.com/lty2008one/BitSet",
+    author="lty2008one",
+    author_email="lty2008one@qq.com",
+    license="MIT",
+    packages=["bitset"],
+    zip_safe=False
+)
