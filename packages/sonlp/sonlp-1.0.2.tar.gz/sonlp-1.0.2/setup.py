9522c409160a8c615708f98ee0ad8300e47d21ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:36 +0000

    Add package sonlp/1.0.2/sonlp-1.0.2.tar.gz

---
+++ packages/sonlp/sonlp-1.0.2.tar.gz/sonlp-1.0.2/setup.py
@@ -0,0 +1,40 @@
+
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name='sonlp',
+    version='1.0.2',
+    description='A simple toolkit for natural language processing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='dandanlemuria',
+    author_email='18110980003@fudan.edu.cn',
+    url='https://github.com/LemuriaChen/sonlp',
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+    keywords='abbreviation, nlp',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'click==7.0',
+        'lxml==4.4.1',
+        'prettytable==0.7.2',
+    ],
+    scripts=["bin/abbr", ],
+)
