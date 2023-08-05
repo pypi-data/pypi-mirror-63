b6243de6f441647856e57e3164a5613b78748968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:54 +0000

    Add package logicqubit-gpu/1.2.6/logicqubit_gpu-1.2.6-py3-none-any.whl

---
+++ packages/logicqubit-gpu/logicqubit_gpu-1.2.6-py3-none-any.whl/logicqubit/__init__.py
@@ -0,0 +1,10 @@
+from logicqubit import logic
+from logicqubit import gates
+from logicqubit import hilbert
+from logicqubit import utils
+from logicqubit import circuit
+from logicqubit import zhegalkin
+
+__version__ = "1.2.6"
+
+print("logicqubit-gpu version "+__version__)
