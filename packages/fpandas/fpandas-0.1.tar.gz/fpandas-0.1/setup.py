41da8f219440b9325c1034b2855c505400d6f42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package fpandas/0.1/fpandas-0.1.tar.gz

---
+++ packages/fpandas/fpandas-0.1.tar.gz/fpandas-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup  # type: ignore
+
+setup(name='fpandas',
+      version='0.1',
+      description='For a pandas with a functional design pattern.',
+      # url='',
+      author='Quinn Dougherty',
+      author_email='quinn.dougherty92@gmail.com',
+      license='MIT',
+      packages=['fpandas'],
+      zip_safe=False)
