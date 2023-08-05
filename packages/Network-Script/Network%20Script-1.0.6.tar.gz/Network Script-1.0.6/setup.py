4427f2f7c598fbf305e24345f9af1d52121d8f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package Network-Script/1.0.6/Network%20Script-1.0.6.tar.gz

---
+++ packages/Network-Script/Network%20Script-1.0.6.tar.gz/Network Script-1.0.6/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = 'Network Script',
+    version = '1.0.6',
+    url = 'https://github.com/gaming32/Network-Script',
+    author = 'Gaming32',
+    author_email = 'gaming32i64@gmail.com',
+    license = 'License :: OSI Approved :: MIT License',
+    description = 'Library for calling methods over sockets',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    packages = [
+        'netsc',
+    ],
+)
