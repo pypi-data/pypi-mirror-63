fe7e6f970075310ab84bdd76d07079ecd0acfe7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package scdxwangtaoMath/1.0/scdxwangtaoMath-1.0.tar.gz

---
+++ packages/scdxwangtaoMath/scdxwangtaoMath-1.0.tar.gz/scdxwangtaoMath-1.0/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='scdxwangtaoMath',  # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，里面只有数学方法，用于测试哦',  #描述
+    author='wangtao', # 作者
+    author_email='scdxwangtao@126.com',
+    py_modules=['scdxwangtaoMath.demo1','scdxwangtaoMath.demo2'] # 要发布的模块
+)
