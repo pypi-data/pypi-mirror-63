80c47f211b8569b2590dbd39e0b05247f30934dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package minhas-distributions/0.1/minhas_distributions-0.1.tar.gz

---
+++ packages/minhas-distributions/minhas_distributions-0.1.tar.gz/minhas_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='minhas_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['minhas_distributions'],
+      zip_safe=False)
