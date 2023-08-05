f61b85c4fd98491d5d656a431df54406f8725fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package pystatusb/1.0.2/pystatusb-1.0.2.tar.gz

---
+++ packages/pystatusb/pystatusb-1.0.2.tar.gz/pystatusb-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+# https://packaging.python.org/tutorials/packaging-projects/
+
+NAME = "pystatusb"
+setup(
+    name = NAME,
+    version = "1.0.2",
+    author = "Carl Seelye",
+    author_email = "cseelye@gmail.com",
+    description = "Control fit-statUSB devices",
+    license = "MIT",
+    keywords = "compulab statusb",
+    packages = [NAME],
+    url = "https://github.com/cseelye/{}".format(NAME),
+    include_package_data=True,
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    long_description_content_type='text/markdown',
+    install_requires = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
+)
