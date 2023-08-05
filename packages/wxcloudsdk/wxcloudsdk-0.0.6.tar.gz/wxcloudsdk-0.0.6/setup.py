0f993a37356d68268d2e4d065aa9602af5280051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package wxcloudsdk/0.0.6/wxcloudsdk-0.0.6.tar.gz

---
+++ packages/wxcloudsdk/wxcloudsdk-0.0.6.tar.gz/wxcloudsdk-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wxcloudsdk', 
+    version='0.0.6', 
+    keywords='wechat miniprogram http sdk api', 
+    description='Wechat miniprogram http api tools', 
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT License', 
+    url='',
+    author='typingcat', 
+    author_email='liuchunyao0321@gmail.com',
+    packages=find_packages(), 
+    include_package_data=True,
+    platforms='any', 
+    install_requires=["requests"],
+    python_requires='>=3.6'
+)
