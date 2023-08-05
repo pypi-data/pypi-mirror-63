094411210ea9441e2c493606d928284fe755d58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package i-impute/1.0.6/i-impute-1.0.6.tar.gz

---
+++ packages/i-impute/i-impute-1.0.6.tar.gz/i-impute-1.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="i-impute",
+    version="1.0.6",
+    author="Xikang Feng",
+    author_email="xikanfeng2@gmail.com",
+    description="I-Impute: a self-consistent method to impute single cell RNA sequencing data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xikanfeng2/I-Impute",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
