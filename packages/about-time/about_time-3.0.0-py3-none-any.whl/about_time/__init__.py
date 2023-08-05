e58ccb232b029bed73d3b24e1e0b7b61a8a4745f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package about-time/3.0.0/about_time-3.0.0-py3-none-any.whl

---
+++ packages/about-time/about_time-3.0.0-py3-none-any.whl/about_time/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+VERSION = (3, 0, 0)
+
+__author__ = 'Rogério Sampaio de Almeida'
+__email__ = 'rsalmei@gmail.com'
+__version__ = '.'.join(map(str, VERSION))
+
+__all__ = ('__author__', '__version__', 'about_time')
+
+from .core import about_time
