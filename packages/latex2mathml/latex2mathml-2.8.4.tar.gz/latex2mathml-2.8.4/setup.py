f0c0082cee9650320282993b37152c54500df7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package latex2mathml/2.8.4/latex2mathml-2.8.4.tar.gz

---
+++ packages/latex2mathml/latex2mathml-2.8.4.tar.gz/latex2mathml-2.8.4/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# __author__ = "Ronie Martinez"
+# __copyright__ = "Copyright 2016-2020, Ronie Martinez"
+# __credits__ = ["Ronie Martinez"]
+# __maintainer__ = "Ronie Martinez"
+# __email__ = "ronmarti18@gmail.com"
+from setuptools import setup
+
+setup()
