0058ed6094e3e7843eb4a498b63c468640d6173d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:40 +0000

    Add package composeml/0.1.8/composeml-0.1.8.tar.gz

---
+++ packages/composeml/composeml-0.1.8.tar.gz/composeml-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='composeml',
+    version='0.1.8',
+    author='Feature Labs, Inc.',
+    author_email='support@featurelabs.com',
+    license='BSD 3-clause',
+    url='https://compose.featurelabs.com',
+    install_requires=open('requirements.txt').readlines(),
+    tests_require=open('test-requirements.txt').readlines(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={'composeml': ['demos/*.csv']},
+    include_package_data=True,
+)
