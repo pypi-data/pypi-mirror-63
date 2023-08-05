ac985f11b3d4c932fa98f07d6eae9f5a9ed45df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:15 +0000

    Add package MyPackage-Bala/0.1/MyPackage_Bala-0.1.tar.gz

---
+++ packages/MyPackage-Bala/MyPackage_Bala-0.1.tar.gz/MyPackage_Bala-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='MyPackage_Bala',
+     version='0.1',
+     author="Bala",
+     author_email="balamca08@gmail.com",
+     description="A Trading utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/balaseenivasan/PythonPackage",
+     packages=["MyPackage_Bala"],
+     include_package_data=True,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
