dc6c7796271f84dcf2c72ad81bb112f9b2468c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:03 +0000

    Add package digits-py-lib/1.0.1/digits-py-lib-1.0.1.tar.gz

---
+++ packages/digits-py-lib/digits-py-lib-1.0.1.tar.gz/digits-py-lib-1.0.1/README.md
@@ -0,0 +1,19 @@
+# digits py lib
+
+An useful library !
+
+## installation
+
+````shell script
+pip install digits-py-lib
+````
+
+## usage
+
+````python
+from digits import digits
+
+digits.one
+digits.two
+# ...
+````
---
+++ packages/digits-py-lib/digits-py-lib-1.0.1.tar.gz/digits-py-lib-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['digits']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'digits-py-lib',
+    'version': '1.0.1',
+    'description': 'export ten first numbers',
+    'long_description': '# digits py lib\n\nAn useful library !\n\n## installation\n\n````shell script\npip install digits-py-lib\n````\n\n## usage\n\n````python\nfrom digits import digits\n\ndigits.one\ndigits.two\n# ...\n````\n',
+    'author': 'thomas-marquis',
+    'author_email': 'thomas.marquis314@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/thomas-marquis/digits-py-lib',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
