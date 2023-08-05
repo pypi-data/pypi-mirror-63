df253c09a6ce504926ab0442526338f1846ee855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:29 +0000

    Add package systemdlint/1.1.7/systemdlint-1.1.7-py3.7.egg

---
+++ packages/systemdlint/systemdlint-1.1.7-py3.7.egg/systemdlint/conf/knownUnits.py
@@ -0,0 +1,21 @@
+KNOWN_UNITS_EXT = [
+    ".automount",
+    ".conf",
+    ".link",
+    ".mount",
+    ".network",
+    ".netdev",
+    ".path",
+    ".service",
+    ".slice",
+    ".socket",
+    ".swap",
+    ".target",
+    ".timer"
+]
+KNOWN_UNITS_MUST_HAVE_UNITSECTION = [
+    ".mount",
+    ".service",
+    ".socket",
+    ".target"
+]
