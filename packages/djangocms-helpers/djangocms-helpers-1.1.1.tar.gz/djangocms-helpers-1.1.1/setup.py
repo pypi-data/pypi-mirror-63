d7477609160927d05f849cfb4c741bf261f2711f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:15 +0000

    Add package djangocms-helpers/1.1.1/djangocms-helpers-1.1.1.tar.gz

---
+++ packages/djangocms-helpers/djangocms-helpers-1.1.1.tar.gz/djangocms-helpers-1.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+setup(
+    name='djangocms-helpers',
+    version='1.1.1',
+    author='Victor',
+    author_email='victor@what.digital',
+    url='https://gitlab.com/what-digital/djangocms-helpers',
+    packages=[
+        'djangocms_helpers',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'django >= 2.2, < 3',
+        'django-cms >= 3.7, < 4.0',
+        'django-fieldsignals >= 0.4.0',
+        'django-meta',
+    ],
+)
