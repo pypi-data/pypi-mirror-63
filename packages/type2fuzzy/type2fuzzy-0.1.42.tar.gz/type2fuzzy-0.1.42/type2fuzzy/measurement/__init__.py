7afdf3b900b79ca36481518d065d928d63c03b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package type2fuzzy/0.1.42/type2fuzzy-0.1.42.tar.gz

---
+++ packages/type2fuzzy/type2fuzzy-0.1.42.tar.gz/type2fuzzy-0.1.42/type2fuzzy/measurement/__init__.py
@@ -0,0 +1,8 @@
+"""
+type2fuzzy.measurement
+
+Measurement functions by means of a decorator
+"""
+from type2fuzzy.measurement.timemeasurement import TimeMeasurement
+
+__all__ = ['TimeMeasurement']
