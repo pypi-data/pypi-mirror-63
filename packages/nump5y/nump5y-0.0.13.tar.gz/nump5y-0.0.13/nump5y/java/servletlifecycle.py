b0903dd15c1616a2b4449fd009bb6fe42f6605ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package nump5y/0.0.13/nump5y-0.0.13.tar.gz

---
+++ packages/nump5y/nump5y-0.0.13.tar.gz/nump5y-0.0.13/nump5y/java/servletlifecycle.py
@@ -0,0 +1,52 @@
+Ans="""
+'''
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletLifeCycle extends HttpServlet
+{
+public ServletLifeCycle()
+{
+System.out.println(“Am from default constructor”);
+}
+public void init(ServletConfig config)
+{
+System.out.println(“Am from Init method…!”);
+}
+public void doGet(HttpServletReuest req, HttpServletResponse res)throws ServletException, IOException
+{
+ res.setContentType(“text/html”);
+PrintWriter pw =  res.getWriter();
+pw.println(“I am from doGet method”);
+pw.close();
+}
+public void destroy()
+{
+System.out.println(“Am from Destroy methods”);
+}
+}
+
+
+Web.xml
+
+<web-app>
+<servlet>
+<servlet-name>second</servlet-name>
+<servlet-class>java4s.ServletLifeCycle</servlet-class>
+<load-on-startup>1</load-on-startup>
+</servlet>
+
+<servlet-mapping>
+<servlet-name>second</servlet-name>
+<url-pattern>/lifecycle1</url-pattern>
+</servlet-mapping>
+</web-app>
+
+
+'''
+"""
