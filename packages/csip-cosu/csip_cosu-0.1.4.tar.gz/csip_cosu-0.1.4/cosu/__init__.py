b0eb207be1d5721844d41506f6036e21c1e6789f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:26 +0000

    Add package csip-cosu/0.1.4/csip_cosu-0.1.4.tar.gz

---
+++ packages/csip-cosu/csip_cosu-0.1.4.tar.gz/csip_cosu-0.1.4/cosu/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+COSU toolkit
+=========================================
+This is COSU
+
+"""
+
+__version__ = '0.1.4'
+__built__ = '0.1.4 2e7c37eb15458fee75c6bcfb2de37f05eb21747f 2020-03-11 09:48 -0600 od'
+__author__ = 'Olaf David'
+__email__ = 'odavid@colostate.edu'
+
+from .pso import pso
+__all__ = ["pso"]
+
---
+++ packages/csip-cosu/csip_cosu-0.1.4.tar.gz/csip_cosu-0.1.4/setup.py
@@ -0,0 +1,35 @@
+"""
+ * $Id:$
+ *
+ * This file is part of the Cloud Services Integration Platform (CSIP),
+ * a Model-as-a-Service framework, API and application suite.
+ *
+ * 2012-2017, Olaf David and others, OMSLab, Colorado State University.
+ *
+ * OMSLab licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+"""
+
+from setuptools import setup, find_packages
+import sys
+sys.path.extend(['/od/projects/csip_cosu'])
+
+import cosu
+
+setup(name='csip_cosu',
+      version=cosu.__version__,
+      url='http://alm.engr.colostate.edu/cb/project/csip',
+      license='MIT',
+      author='Olaf David',
+      author_email='odavid@colostate.edu',
+      description='CSIP COSU library',
+      packages=find_packages(include=['cosu','cosu.pso','cosu.utils']),
+      long_description=open('README.md').read(),
+      install_requires=[
+            "pyswarms == 1.1.0",
+            "csip == 0.9.3",
+            "numpy",
+            "requests",
+      ],
+      zip_safe=False
+)
