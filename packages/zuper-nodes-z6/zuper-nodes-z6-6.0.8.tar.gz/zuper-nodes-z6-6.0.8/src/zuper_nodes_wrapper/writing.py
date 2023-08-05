87e68c5b2b736d5d0213e5396d466939751f6d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:09 +0000

    Add package zuper-nodes-z6/6.0.8/zuper-nodes-z6-6.0.8.tar.gz

---
+++ packages/zuper-nodes-z6/zuper-nodes-z6-6.0.8.tar.gz/zuper-nodes-z6-6.0.8/src/zuper_nodes_wrapper/writing.py
@@ -0,0 +1,31 @@
+import cbor2
+
+
+from .constants import *
+
+
+class Sink:
+    def __init__(self, of):
+        self.of = of
+
+    def write_topic_message(self, topic, data, timing):
+        """ Can raise BrokenPipeError"""
+        m = {}
+        m[FIELD_COMPAT] = [CUR_PROTOCOL]
+        m[FIELD_TOPIC] = topic
+        m[FIELD_DATA] = data
+        m[FIELD_TIMING] = timing
+        self._write_raw(m)
+
+    def write_control_message(self, code, data=None):
+        """ Can raise BrokenPipeError"""
+        m = {}
+        m[FIELD_CONTROL] = code
+        m[FIELD_DATA] = data
+        self._write_raw(m)
+
+    def _write_raw(self, m: dict):
+        """ Can raise BrokenPipeError"""
+        j = cbor2.dumps(m)
+        self.of.write(j)
+        self.of.flush()
