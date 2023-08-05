2c40357520bd9e1c6ea0a2b5336519eea7221413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package envars/0.3.3/envars-0.3.3.tar.gz

---
+++ packages/envars/envars-0.3.3.tar.gz/envars-0.3.3/envars/envars.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2017 Santosh Philip
+# =======================================================================
+#  Distributed under the MIT License.
+#  (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+# =======================================================================
+
+"""get the environmental variables
+will get the heroku env vars or the local env files"""
+# Extract the Environmental variables as a defaultdict
+from collections import defaultdict
+import os
+
+
+def getenvars(fhandle=None, defvalue=None, remoteKV=None):
+    """get the environmental variables
+    will get the heroku env vars or the local env files"""
+    if remoteKV is None:
+        remoteKV = ("ENV_NOW", "production")
+    # -
+    osdct = os.environ
+    try:
+        if osdct[remoteKV[0]] == remoteKV[1]:
+            keyval = [(key, value) for key, value in osdct.items()]
+    except KeyError:
+        if not fhandle:
+            fhandle = open(".env", 'r')
+        lines = [line.split('#')[0].strip() for line in fhandle.readlines()]
+        lines = [line for line in lines if line]
+        keyval = [line.split("=") for line in lines]
+        keyval = [(key.strip(), value.strip()) for key, value in keyval]
+    # -
+    # from https://www.accelebrate.com/blog/using-defaultdict-python/
+    dct = defaultdict(lambda: defvalue)
+    for key, value in keyval:
+        dct[key] = value
+    return dct
+
+
+def dictvalue(dct, key, defvalue=None):
+    """return the value of the dct given the key.
+    return default value if key does not exist"""
+    return dct.setdefault(key, defvalue)
---
+++ packages/envars/envars-0.3.3.tar.gz/envars-0.3.3/envars/tests/test_envars.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2017 Santosh Philip
+# =======================================================================
+#  Distributed under the MIT License.
+#  (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+# =======================================================================
+
+"""py.test for envars"""
+
+from __future__ import print_function
+
+from six import StringIO
+
+import os
+
+import envars.envars as envars
+
+
+def test_dictvalue():
+    """py.test for dictvalue"""
+    data = (
+        (dict(a=1, b=2), "a", "default", 1),
+        # dct, key, defvalue, expected
+        (dict(a=1, b=2), "c", "default", "default"),
+        # dct, key, defvalue, expected
+    )
+    for dct, key, defvalue, expected in data:
+        result = envars.dictvalue(dct, key, defvalue)
+        assert result == expected
+
+
+def test_getenvars():
+    """py.test for getenvars"""
+    data = (
+        ("""a=1
+            b=2""", 'a', "default", "1"),
+        # envtxt, key, defvalue, expected
+        ("""a=1
+            b=2""", 'c', "default", "default"),
+        # envtxt, key, defvalue, expected
+
+        # test if envars cleans up comments
+        ("""a=1 # comment
+            b=2""", 'a', "default", "1"),
+        # envtxt, key, defvalue, expected
+
+        # test for blank line
+        ("""a=1 # comment
+
+            b=2""", 'a', "default", "1"),
+        # envtxt, key, defvalue, expected
+        # test for comment only line
+        ("""a=1 # comment
+            # comment only line
+            b=2""", 'a', "default", "1"),
+        # envtxt, key, defvalue, expected
+
+    )
+    for envtxt, key, defvalue, expected in data:
+        fhandle = StringIO(envtxt)
+        dct = envars.getenvars(fhandle=fhandle, defvalue=defvalue)
+        result = dct[key]
+        assert result == expected
+    # test on os.environ -> as will happen on heroku
+    os.environ['on_heroku'] = 'Yes'
+    os.environ['another_heroku_value'] = 'avalue'
+    dct = envars.getenvars(remoteKV=('on_heroku', 'Yes'))
+    assert dct['another_heroku_value'] == 'avalue'
+    # cleanup
+    # os.environ.pop('on_heroku')
+    # os.environ.pop('another_heroku_value')
