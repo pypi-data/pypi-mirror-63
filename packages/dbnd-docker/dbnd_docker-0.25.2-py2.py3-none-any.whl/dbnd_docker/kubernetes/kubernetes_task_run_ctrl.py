1e9988a82627c316305d0a40964938ad4b316e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:34 +0000

    Add package dbnd-docker/0.25.2/dbnd_docker-0.25.2-py2.py3-none-any.whl

---
+++ packages/dbnd-docker/dbnd_docker-0.25.2-py2.py3-none-any.whl/dbnd_docker/kubernetes/kubernetes_task_run_ctrl.py
@@ -0,0 +1,54 @@
+import logging
+import shlex
+
+from typing import Optional
+
+from dbnd_docker.docker_ctrl import DockerRunCtrl
+from dbnd_docker.kubernetes.kube_dbnd_client import DbndPodCtrl
+from dbnd_docker.kubernetes.kubernetes_engine_config import KubernetesEngineConfig
+
+
+logger = logging.getLogger(__name__)
+
+
+class KubernetesTaskRunCtrl(DockerRunCtrl):
+    def __init__(self, **kwargs):
+        super(KubernetesTaskRunCtrl, self).__init__(**kwargs)
+        self.pod_ctrl = None  # type: Optional[DbndPodCtrl]
+
+    def docker_run(self):
+
+        # dont' describe in local run, do it in remote run
+        self.context.settings.system.describe = False
+        cmds = shlex.split(self.task.command)
+
+        kubernetes_config = self.task.docker_engine  # type: KubernetesEngineConfig
+        if self.task.image:
+            try:
+                container_repository, container_tag = self.task.image.split(":")
+                kubernetes_config = kubernetes_config.clone(
+                    container_repository=container_repository,
+                    container_tag=container_tag,
+                )
+            except:
+                raise
+        pod = kubernetes_config.build_pod(cmds=cmds, task_run=self.task_run)
+        kube_dbnd = kubernetes_config.build_kube_dbnd()
+
+        self.pod_ctrl = kube_dbnd.get_pod_ctrl_for_pod(pod)
+        try:
+            self.pod_ctrl.run_pod(pod=pod, task_run=self.task_run)
+            if not kubernetes_config.detach_run:
+                self.pod_ctrl.delete_pod()
+        except Exception:
+            # we should not delete pod on Keyboard interrupt
+            # external system will take care of that!
+            if not kubernetes_config.detach_run:
+                self.pod_ctrl.delete_pod()
+            raise
+
+        self.pod_ctrl = None
+
+    def on_kill(self):
+        if not self.task.docker_engine.detach_run and self.pod_ctrl:
+            self.pod_ctrl.delete_pod()
