cf08d1fd4f7e989aa6761c04e1a00737bdd5df7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:14 +0000

    Add package ont-bonito/0.0.8/ont-bonito-0.0.8.tar.gz

---
+++ packages/ont-bonito/ont-bonito-0.0.8.tar.gz/ont-bonito-0.0.8/bonito/__init__.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+
+from bonito import basecaller, evaluate, train, view, tune
+
+__version__ = '0.0.8'
+
+
+def main():
+    parser = ArgumentParser(
+        'bonito', 
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        '-v', '--version', action='version',
+        version='%(prog)s {}'.format(__version__)
+    )
+
+    subparsers = parser.add_subparsers(
+        title='subcommands', description='valid commands',
+        help='additional help', dest='command'
+    )
+    subparsers.required = True
+
+    for module in ('basecaller', 'evaluate', 'train', 'view', 'tune'):
+        mod = globals()[module]
+        p = subparsers.add_parser(module, parents=[mod.argparser()])
+        p.set_defaults(func=mod.main)
+
+    args = parser.parse_args()
+    args.func(args)
