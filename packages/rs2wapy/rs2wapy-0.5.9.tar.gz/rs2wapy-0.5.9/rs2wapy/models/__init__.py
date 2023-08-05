a8e5a69ef8ff4734bddae53765ccac8e32e4f27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package rs2wapy/0.5.9/rs2wapy-0.5.9.tar.gz

---
+++ packages/rs2wapy/rs2wapy-0.5.9.tar.gz/rs2wapy-0.5.9/rs2wapy/models/__init__.py
@@ -0,0 +1,17 @@
+from .models import *
+
+__all__ = [
+    "AllTeam",
+    "AccessPolicy",
+    "ChatMessage",
+    "ChatChannel",
+    "ChatChannelTeam",
+    "ChatChannelAll",
+    "CurrentGame",
+    "Player",
+    "PlayerScoreboard",
+    "Team",
+    "TeamScoreboard",
+    "BlueTeam",
+    "RedTeam",
+]
---
+++ packages/rs2wapy/rs2wapy-0.5.9.tar.gz/rs2wapy-0.5.9/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+import versioneer
+
+long_description = """
+Provides programmable interface to Rising Storm 2: Vietnam
+WebAdmin for building server management automation applications.
+
+The library uses PycURL internally to communicate with RS2
+WebAdmin.
+"""
+
+setuptools.setup(
+    name="rs2wapy",
+    version=versioneer.get_version(),
+    packages=setuptools.find_packages(),
+    url="https://github.com/tuokri/rs2wapy",
+    author="tuokri",
+    author_email="tuokri@tuta.io",
+    description="Rising Storm 2: Vietnam WebAdmin Python Interface",
+    long_description=long_description,
+    keywords="automation webadmin ue3",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "beautifulsoup4",
+        "bs4",
+        "Logbook",
+        "pycurl",
+        "soupsieve",
+        "requests",
+        "steam",
+    ]
+)
