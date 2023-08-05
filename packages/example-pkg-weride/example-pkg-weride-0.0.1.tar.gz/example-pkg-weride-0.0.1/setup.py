f5c5ef430374357c53f7cc753ea6e2b491e76735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package example-pkg-weride/0.0.1/example-pkg-weride-0.0.1.tar.gz

---
+++ packages/example-pkg-weride/example-pkg-weride-0.0.1.tar.gz/example-pkg-weride-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="example-pkg-weride",
+  version="0.0.1",
+  author="weride author",
+  author_email="test@example.com",
+  description="A small example package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/xxx",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
