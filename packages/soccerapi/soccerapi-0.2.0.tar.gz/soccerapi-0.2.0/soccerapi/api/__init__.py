51d83f142df6262234628bf2bc41c97c50b86f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package soccerapi/0.2.0/soccerapi-0.2.0.tar.gz

---
+++ packages/soccerapi/soccerapi-0.2.0.tar.gz/soccerapi-0.2.0/soccerapi/api/__init__.py
@@ -0,0 +1,6 @@
+import importlib
+
+package = 'soccerapi.api'
+
+Api888Sport = importlib.import_module('.888sport', package).Api888Sport
+ApiBet365 = importlib.import_module('.bet365', package).ApiBet365
