fd57f06b96d5d7ce19198960666a203d8fb35e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package duckietown-tokens/5.3.1/duckietown-tokens-5.3.1.tar.gz

---
+++ packages/duckietown-tokens/duckietown-tokens-5.3.1.tar.gz/duckietown-tokens-5.3.1/src/duckietown_tokens/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger("duckietown-tokens ")
+logger.setLevel(logging.INFO)
+
+__version__ = "5.3.1"
+
+logger.info("duckietown-tokens %s" % __version__)
+
+from .duckietown_tokens import *
+from .tokens_cli import *
