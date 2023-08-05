d300bb9a1f53c04b90159ed678e86b1c78f53e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:56 +0000

    Add package graphene-django-tools/0.20.1/graphene_django_tools-0.20.1-py3-none-any.whl

---
+++ packages/graphene-django-tools/graphene_django_tools-0.20.1-py3-none-any.whl/graphene_django_tools/__init__.py
@@ -0,0 +1,6 @@
+"""https://github.com/NateScarlet/graphene-django-tools  """
+
+from graphene_resolver import Resolver
+
+from . import connection, queryset, utils, model_type
+from .utils import ID, convert_id
