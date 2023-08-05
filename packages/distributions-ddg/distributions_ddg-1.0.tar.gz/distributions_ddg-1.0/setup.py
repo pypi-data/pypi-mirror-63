ea423a6ede68746e093f50e7c040df067c5b5aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:52 +0000

    Add package distributions-ddg/1.0/distributions_ddg-1.0.tar.gz

---
+++ packages/distributions-ddg/distributions_ddg-1.0.tar.gz/distributions_ddg-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_ddg',
+      version='1.0',
+      author = 'DDuncan',
+      author_email ='942856733@qq.com',
+      description='Gussian and Binomial distributions',
+      packages=['distributions_ddg'],
+      zip_safe=False)
