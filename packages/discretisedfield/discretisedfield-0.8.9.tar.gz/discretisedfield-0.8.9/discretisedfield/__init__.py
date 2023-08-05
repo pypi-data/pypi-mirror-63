adaf834f21d691f959e2277038cf2db89aca5633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package discretisedfield/0.8.9/discretisedfield-0.8.9.tar.gz

---
+++ packages/discretisedfield/discretisedfield-0.8.9.tar.gz/discretisedfield-0.8.9/discretisedfield/__init__.py
@@ -0,0 +1,17 @@
+import pytest
+import pkg_resources
+import seaborn as sns
+from .region import Region
+from .mesh import Mesh
+from .field import Field
+from .line import Line
+from .operators import cross, stack
+from .interact import interact
+
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
+
+
+def test():
+    return pytest.main(['-v', '--pyargs',
+                        'discretisedfield', '-l'])  # pragma: no cover
