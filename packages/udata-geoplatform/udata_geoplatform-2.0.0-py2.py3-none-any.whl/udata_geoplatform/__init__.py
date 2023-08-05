8d0a737ac05d5330171b359a77f16c98b47df539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:08 +0000

    Add package udata-geoplatform/2.0.0/udata_geoplatform-2.0.0-py2.py3-none-any.whl

---
+++ packages/udata-geoplatform/udata_geoplatform-2.0.0-py2.py3-none-any.whl/udata_geoplatform/__init__.py
@@ -0,0 +1,9 @@
+'''
+uData Geoplatform connector
+
+Connects uData with Geoplatform (geo.data.gouv.fr)
+'''
+from __future__ import unicode_literals
+
+__version__ = '2.0.0'
+__description__ = 'Connects uData with Geoplatform (geo.data.gouv.fr)'
