47dedf3cdffd7c96dc26f36ce1c93e51d4707843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package git-gud/0.2.5/git-gud-0.2.5.tar.gz

---
+++ packages/git-gud/git-gud-0.2.5.tar.gz/git-gud-0.2.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='git-gud',
+    version='0.2.5',
+    url='https://github.com/benthayer/git-gud/',
+    description='A tool to learn git',
+    author='Ben Thayer',
+    author_email='ben@benthayer.com',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=[
+        'gitgud',
+        'gitgud.hooks',
+        'gitgud.skills',
+        'gitgud.skills.basics',
+        'gitgud.skills.extras',
+        'gitgud.skills.rampup',
+    ],
+    package_data={
+        'gitgud.skills.basics': ['_*/*'],
+        'gitgud.skills.extras': ['_*/*'],
+        'gitgud.skills.rampup': ['_*/*'],
+    },
+    python_requires='>=3.5',
+    install_requires=[
+        'gitpython',
+        'importlib_resources'
+    ],
+    entry_points={
+        "console_scripts": [
+            "git-gud=gitgud.__main__:main"
+        ]
+    }
+)
