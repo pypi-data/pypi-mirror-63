211b7531da321929dd39b4c47ef24bb70fbfc9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package technopy/0.0.16/technopy-0.0.16.tar.gz

---
+++ packages/technopy/technopy-0.0.16.tar.gz/technopy-0.0.16/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='technopy',
+    packages=find_packages(),
+    version='0.0.16',
+    author='Gareth V. Walkom',
+    author_email='walkga04@googlemail.com',
+    license='GPLv3',
+    description='technopy allows you to control your TechnoTeam Luminance Camera or Hyperspectral Camera.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/garethwalkom/technopy',
+    include_package_data = True,
+    install_requires=[
+        'luxpy',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scikit-image',
+      ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
