bf8e80a3e9cbff9cfe36069d294b7c6bbc435bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package paxter/0.1.0/paxter-0.1.0-py2.py3-none-any.whl

---
+++ packages/paxter/paxter-0.1.0-py2.py3-none-any.whl/paxter/core/exceptions.py
@@ -0,0 +1,44 @@
+"""
+Exceptions specific to Paxter language utilities
+"""
+from typing import Dict, Optional
+
+__all__ = ['PaxterBaseException', 'PaxterSyntaxError', 'PaxterConfigError']
+
+PositionMap = Dict[str, int]
+
+
+class PaxterBaseException(Exception):
+    """
+    Base exception related to Paxter language utilities.
+    """
+    message: str
+
+    def __init__(self, message: str):
+        self.message = message
+
+
+class PaxterSyntaxError(PaxterBaseException):
+    """
+    Exception for syntax error raised while parsing input text.
+    Positional index parameters indicates a mapping from position name
+    to its indexing inside the input text.
+    """
+    message: str
+    body: str
+    positions: PositionMap
+
+    def __init__(
+            self, message: str,
+            body: str, positions: Optional[PositionMap] = None,
+    ):
+        self.message = message
+        self.body = body
+        self.positions = positions or {}
+
+
+class PaxterConfigError(PaxterBaseException):
+    """
+    Exception for configuration error.
+    """
+    pass
