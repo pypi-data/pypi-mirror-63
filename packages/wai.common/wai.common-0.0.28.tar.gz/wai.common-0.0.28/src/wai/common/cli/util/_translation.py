0a67859798560f40a3b93d99d30c39493fce6c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:18 +0000

    Add package wai.common/0.0.28/wai.common-0.0.28.tar.gz

---
+++ packages/wai.common/wai.common-0.0.28.tar.gz/wai.common-0.0.28/src/wai/common/cli/util/_translation.py
@@ -0,0 +1,41 @@
+"""
+Module for count-translation functionality for the CountOption class.
+"""
+from typing import Dict
+
+from ...meta.code_repr import CodeRepresentable, from_init, CodeRepresentation
+
+
+class TranslationTable(CodeRepresentable):
+    """
+    Reversible translation between integer spaces.
+    """
+    def __init__(self, *translated_values: int):
+        # Must be at least 2 values
+        if len(translated_values) < 2:
+            raise ValueError("Must provide at least 2 values")
+
+        # The translation must be reversible
+        if len(set(translated_values)) != len(translated_values):
+            raise ValueError("Translated values are not unique")
+
+        self._code_representation: CodeRepresentation = from_init(self, locals())
+
+        self._forward_translation: Dict[int, int] = {index: value for index, value in enumerate(translated_values)}
+        self._reverse_translation: Dict[int, int] = {value: key for key, value in self._forward_translation.items()}
+
+    def translate_forward(self, value: int) -> int:
+        # Cap the value to the range of the forward translation
+        value = min(max(0, value), len(self._forward_translation) - 1)
+
+        return self._forward_translation[value]
+
+    def translate_reverse(self, value: int) -> int:
+        # Make sure the value is translatable
+        if value not in self._reverse_translation:
+            raise ValueError(f"Value not in translation table: {value}")
+
+        return self._reverse_translation[value]
+
+    def code_repr(self) -> CodeRepresentation:
+        return self._code_representation
