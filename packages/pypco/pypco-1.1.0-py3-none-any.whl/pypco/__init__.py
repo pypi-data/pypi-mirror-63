3de621daa2269c46c3a418184026fb48ea051a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package pypco/1.1.0/pypco-1.1.0-py3-none-any.whl

---
+++ packages/pypco/pypco-1.1.0-py3-none-any.whl/pypco/__init__.py
@@ -0,0 +1,30 @@
+"""
+A Pythonic Object-Oriented wrapper to the PCO API
+
+pypco is a Python wrapper for the Planning Center Online (PCO) REST API
+intended to help you accomplish useful things with Python and the PCO API
+more quickly. pypco provides simple helpers wrapping the REST calls you'll
+place against the PCO API, meaning that you'll be spending your time
+directly in the PCO API docs rather than those specific to your API wrapper
+tool of choice.
+
+usage:
+    >>> import pypco
+    >>> pco = pypco.PCO()
+
+pypco supports both OAUTH and Personal Access Token (PAT) authentication.
+"""
+
+# PyPCO Version
+__version__ = "1.1.0"
+
+# Export the interface we present to clients
+
+# The primary PCO interface object
+from .pco import PCO
+
+# Utility functions for OAUTH
+from .user_auth_helpers import *
+
+# Import exceptions
+from .exceptions import *
