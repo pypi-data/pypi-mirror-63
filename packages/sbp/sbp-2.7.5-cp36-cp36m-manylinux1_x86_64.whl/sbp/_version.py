e4c6fe629c22146605ffb1ff8c37a2994fed3ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package sbp/2.7.5/sbp-2.7.5-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/sbp/sbp-2.7.5-cp36-cp36m-manylinux1_x86_64.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '2.7.5'
+full_version = '2.7.5'
+git_revision = 'Unknown'
+is_released = True
+if not is_released:
+    version = full_version
