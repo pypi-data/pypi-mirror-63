3ee8dc2ca9aa158a920ff6ac4b0d0e0a6a817545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:05 +0000

    Add package shield34/1.0.241/shield34-1.0.241-241-py2.py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.241-241-py2.py3-none-any.whl/shield34_reporter/model/csv_rows/helpers/exception_desc.py
@@ -0,0 +1,30 @@
+class ExceptionDesc:
+
+    message = ""
+    cause = ""
+    assertionError = False
+    stackTrace = []
+    additionalInfo = ""
+
+    def __init__(self, exception):
+        message = ''
+        if getattr(exception, 'message', None) is not None:
+            message = exception.message
+        if getattr(exception, 'msg', None) is not None:
+            message = exception.msg
+        if getattr(exception, 'longrepr', None) is not None:
+            longrepr = getattr(exception, 'longrepr')
+            if getattr(longrepr, 'reprcrash', None) is not None:
+                message = longrepr.reprcrash.message
+        self.message = message
+        #self.cause = exception.cause
+        #self.assertionError = exception.assertionError
+        #self.stackTrace = exception.stackTrace
+        #self.additionalInfo = exception.additionalInfo
+
+    def gen_row_value(self, lst=['message', 'cause', 'assertionError', 'stackTrace', 'additionalInfo']):
+        row_value = {}
+
+        for a in lst:
+            row_value[a] = getattr(self, a)
+        return row_value
