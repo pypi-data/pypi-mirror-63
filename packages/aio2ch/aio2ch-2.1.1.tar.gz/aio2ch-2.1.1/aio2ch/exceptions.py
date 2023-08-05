40914bfbf2f7475bd380b3236526948c99cf4db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:45 +0000

    Add package aio2ch/2.1.1/aio2ch-2.1.1.tar.gz

---
+++ packages/aio2ch/aio2ch-2.1.1.tar.gz/aio2ch-2.1.1/aio2ch/exceptions.py
@@ -0,0 +1,14 @@
+class WrongSortMethodException(Exception):
+    pass
+
+
+class NoBoardProvidedException(Exception):
+    pass
+
+
+class InvalidThreadUrlException(Exception):
+    pass
+
+
+class InvalidBoardIdException(Exception):
+    pass
