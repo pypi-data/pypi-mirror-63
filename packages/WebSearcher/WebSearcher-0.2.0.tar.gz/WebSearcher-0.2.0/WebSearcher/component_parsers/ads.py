50efcb9f7248f46561c0f861f53885be275d3ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package WebSearcher/0.2.0/WebSearcher-0.2.0.tar.gz

---
+++ packages/WebSearcher/WebSearcher-0.2.0.tar.gz/WebSearcher-0.2.0/WebSearcher/component_parsers/ads.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2017-2020 Ronald E. Robertson <rer@ronalderobertson.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+def parse_ads(cmpt):
+    """Parse ads from ad component"""
+    subs = cmpt.find_all('li', {'class':'ads-ad'})
+    return [parse_ad(sub, sub_rank) for sub_rank, sub in enumerate(subs)]
+
+def parse_ad(sub, sub_rank=0, visible=None):
+    """Parse details of a single ad subcomponent, similar to general"""
+    parsed = {'type':'ad', 'sub_rank':sub_rank}
+
+    header = sub.find('div', {'class':'ad_cclk'})
+    parsed['title'] = header.find('h3').text
+    parsed['url'] = header.find('cite').text
+    parsed['text'] = sub.find('div', {'class':'ads-creative'}).text
+    
+    bottom_text = sub.find('ul')
+    if bottom_text:
+        bottom_li = bottom_text.find_all('li')
+        parsed['details'] = [li.get_text(separator=' ') for li in bottom_li]
+
+    return parsed
---
+++ packages/WebSearcher/WebSearcher-0.2.0.tar.gz/WebSearcher-0.2.0/setup.py
@@ -0,0 +1,48 @@
+# WebSearcher - Tools for conducting, collecting, and parsing web search
+# Copyright (C) 2017-2020 Ronald E. Robertson <rer@ronalderobertson.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import setuptools
+
+def get_readme_descriptions(fp='README.md', s='#', stop_at=2):
+    with open(fp, 'r') as infile:
+        # Extract description (title) and long description including n sections
+        readme = [l.strip() for l in infile.read().split('\n')]
+        description = readme[0].replace('# ', '')
+        heading_idx = [idx for idx, l in enumerate(readme) if l.startswith(s)]
+        long_description = '  \n'.join(readme[:heading_idx[stop_at]])
+    return description, long_description
+    
+description, long_description = get_readme_descriptions()
+
+setuptools.setup(
+    name='WebSearcher',
+    version='0.2.0',
+    url='http://github.com/gitronald/WebSearcher',
+    author='Ronald E. Robertson',
+    author_email='rer@ccs.neu.edu',
+    license='BSD-3-Clause',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License'
+    ],
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=['requests','lxml','bs4','brotli',
+                      'tldextract','emoji','pandas'],
+    python_requires='>=3.6'
+)
