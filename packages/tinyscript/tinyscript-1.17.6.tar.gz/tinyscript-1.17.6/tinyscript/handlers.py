5bfe65efc90467c06ce7b71e061ff2c885d3af4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package tinyscript/1.17.6/tinyscript-1.17.6.tar.gz

---
+++ packages/tinyscript/tinyscript-1.17.6.tar.gz/tinyscript-1.17.6/tinyscript/handlers.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Module for defining handlers and exit hook.
+
+"""
+import logging
+import sys
+from signal import signal, SIGINT, SIGTERM
+
+
+__features__ = ["at_exit", "at_graceful_exit", "at_interrupt", "at_terminate"]
+__all__ = ["_hooks"] + __features__
+
+
+class ExitHooks(object):
+    # inspired from: https://stackoverflow.com/questions/9741351/how-to-find-exi
+    #                 t-code-or-reason-when-atexit-callback-is-called-in-python
+    def __init__(self):
+        self._exit = True
+        self._orig_exit = sys.exit
+        self.code = None
+        self.exception = None
+        self.state = None
+        sys.exit = self.exit
+
+    def exit(self, code=0):
+        self.code = code
+        self._orig_exit(code)
+    
+    def quit(self, code=0):
+        if self.state != "INTERRUPTED" or self._exit:
+            self.exit(code)
+
+_hooks = ExitHooks()
+
+
+def __interrupt_handler(*args):
+    """
+    Interruption handler.
+
+    :param signal: signal number
+    :param stack: stack frame
+    :param code: exit code
+    """
+    _hooks.state = "INTERRUPTED"
+    _hooks.quit(0)
+# bind to interrupt signal (Ctrl+C)
+signal(SIGINT, __interrupt_handler)
+
+
+def __terminate_handler(*args):
+    """
+    Termination handler.
+
+    :param signal: signal number
+    :param stack: stack frame
+    :param code: exit code
+    """
+    _hooks.state = "TERMINATED"
+    _hooks.quit(0)
+# bind to terminate signal
+signal(SIGTERM, __terminate_handler)
+
+
+at_exit          = lambda: None
+at_graceful_exit = lambda: None
+at_interrupt     = lambda: None
+at_terminate     = lambda: None
---
+++ packages/tinyscript/tinyscript-1.17.6.tar.gz/tinyscript-1.17.6/tinyscript/helpers/common.py
@@ -0,0 +1,147 @@
+# -*- coding: UTF-8 -*-
+"""Common utility functions.
+
+"""
+from itertools import cycle, permutations, product
+from six import string_types
+from string import digits,printable, punctuation
+from subprocess import Popen, PIPE
+
+from .compat import b
+from .constants import PYTHON3
+
+
+__all__ = __features__ = ["bruteforce", "bruteforce_mask", "execute", "strings",
+                          "strings_from_file", "xor"]
+
+
+MASKS = {
+    'a': printable,
+    'b': "".join(chr(i) for i in range(256)),
+    'd': "0123456789",
+    'h': "0123456789abcdef",
+    'H': "0123456789ABCDEF",
+    'l': "abcdefghijklmnopqrstuvwxyz",
+    's': " " + punctuation,
+    'u': "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+}
+
+
+def bruteforce(maxlen, alphabet=tuple(map(chr, range(256))), minlen=1,
+               repeat=True):
+    """
+    Generator for bruteforcing according to minimum and maximum lengths and an
+     alphabet.
+    
+    :param maxlen:   maximum bruteforce entry length
+    :param alphabet: bruteforce alphabet to be used
+    :param minlen:   minimum bruteforce entry length (optional)  
+    :yield:          bruteforce entry
+    """
+    for i in range(minlen, maxlen + 1):
+        if repeat:
+            for c in product(alphabet, repeat=i):
+                yield c if isinstance(c[0], int) else ''.join(c)
+        else:
+            for c in permutations(alphabet, i):
+                yield c if isinstance(c[0], int) else ''.join(c)
+
+
+def bruteforce_mask(mask, charsets=None):
+    """
+    Generator for bruteforcing according to a given mask (similar to this used
+     in hashcat).
+     
+    :param mask:     bruteforce mask
+    :param charsets: custom alphabets for use with the mask
+    """
+    iterables, charset = [], False
+    masks = {k: v for k, v in MASKS.items()}
+    masks.update(charsets or {})
+    for c in mask:
+        if c == "?":
+            charset = True
+            continue
+        if charset:
+            iterables.append(masks[c])
+            charset = False
+            continue
+        iterables.append(c)
+    for c in product(*iterables):
+        yield c if isinstance(c[0], int) else ''.join(c)
+
+
+def execute(cmd, **kwargs):
+    """
+    Dummy wrapper for subprocess.Popen.
+    
+    :param cmd: command string
+    """
+    if isinstance(cmd, string_types):
+        cmd = cmd.split()
+    return Popen(cmd, stdout=PIPE, stderr=PIPE, **kwargs).communicate()
+
+
+def strings(data, minlen=4, alphabet=printable):
+    """
+    Generator yielding strings according to a charset and a minimal length from
+     a given string buffer.
+    
+    :param filename: input file
+    :param minlen:   minimal length of strings to be considered
+    :param alphabet: valid charset for the strings
+    """
+    result = ""
+    for c in b(data):
+        if c in b(alphabet):
+            result += chr(c) if PYTHON3 else c
+            continue
+        if len(result) >= minlen:
+            yield result
+        result = ""
+    if len(result) >= minlen:
+        yield result
+
+
+def strings_from_file(filename, minlen=4, alphabet=printable, offset=0):
+    """
+    Generator yielding strings according to a charset and a minimal length from
+     a given file.
+    
+    :param filename: input file
+    :param minlen:   minimal length of strings to be considered
+    :param alphabet: valid charset for the strings
+    :param offset:   start offset in the input file
+    """
+    with open(filename, 'rb') as f:
+        f.seek(offset)
+        result = ""
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+            for c in data:
+                if c in b(alphabet):
+                    result += chr(c) if PYTHON3 else c
+                    continue
+                if len(result) >= minlen:
+                    yield result
+                result = ""
+        if len(result) >= minlen:
+            yield result
+
+
+def xor(str1, str2, offset=0):
+    """
+    Function for XORing two strings of different length. Either the first of the
+     second string can be longer than the other.
+
+    :param str1:   first string, with length L1
+    :param str2:   second string, with length L2
+    :param offset: ASCII offset to be applied on each resulting character
+    """
+    r = ""
+    for c1, c2 in zip(cycle(str1) if len(str1) < len(str2) else str1,
+                      cycle(str2) if len(str2) < len(str1) else str2):
+        r += chr(((ord(c1) ^ ord(c2)) + offset) % 256)
+    return r
