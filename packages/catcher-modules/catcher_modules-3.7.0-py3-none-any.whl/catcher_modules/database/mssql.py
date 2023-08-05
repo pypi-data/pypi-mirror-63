f0429409c64a8d164e526d99e32f9476caaac3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:30 +0000

    Add package catcher-modules/3.7.0/catcher_modules-3.7.0-py3-none-any.whl

---
+++ packages/catcher-modules/catcher_modules-3.7.0-py3-none-any.whl/catcher_modules/database/mssql.py
@@ -0,0 +1,51 @@
+from catcher.steps.external_step import ExternalStep
+from catcher.steps.step import update_variables
+
+from catcher_modules.database import SqlAlchemyDb
+
+
+class MSSql(ExternalStep, SqlAlchemyDb):
+    """
+    :Input:
+
+    :conf:  mssql configuration. Can be a single line string or object. **Required**.
+
+    - dbname: name of the database to connect to
+    - user: database user
+    - host: database host
+    - password: user's password
+    - port: database port
+    :query: query to run. **Required**
+
+    :Examples:
+
+    Select all from test, use object configuration
+    ::
+        mssql:
+          request:
+              conf:
+                  dbname: test
+                  user: user
+                  password: password
+                  host: localhost
+                  port: 1433
+              query: 'select count(*) from test'
+          register: {documents: '{{ OUTPUT }}'}
+
+    Insert into test, using string configuration
+    ::
+        mssql:
+          request:
+              conf: 'user:password@localhost:5432/test'
+              query: 'insert into test(id, num) values(3, 3);'
+
+      """
+
+    @property
+    def driver(self) -> str:
+        return "mssql+pymssql"
+
+    @update_variables
+    def action(self, includes: dict, variables: dict) -> any:
+        body = self.simple_input(variables)
+        return variables, self.execute(body)
---
+++ packages/catcher-modules/catcher_modules-3.7.0-py3-none-any.whl/catcher_modules/database/mysql.py
@@ -0,0 +1,51 @@
+from catcher.steps.external_step import ExternalStep
+from catcher.steps.step import update_variables
+
+from catcher_modules.database import SqlAlchemyDb
+
+
+class MySql(ExternalStep, SqlAlchemyDb):
+    """
+    :Input:
+
+    :conf:  mysql configuration. Can be a single line string or object. **Required**.
+
+    - dbname: name of the database to connect to
+    - user: database user
+    - host: database host
+    - password: user's password
+    - port: database port
+    :query: query to run. **Required**
+
+    :Examples:
+
+    Select all from test, use object configuration
+    ::
+        mysql:
+          request:
+              conf:
+                  dbname: test
+                  user: user
+                  password: password
+                  host: localhost
+                  port: 3306
+              query: 'select count(*) from test'
+          register: {documents: '{{ OUTPUT }}'}
+
+    Insert into test, using string configuration
+    ::
+        mysql:
+          request:
+              conf: 'user:password@localhost:5432/test'
+              query: 'insert into test(id, num) values(3, 3);'
+
+      """
+
+    @property
+    def driver(self) -> str:
+        return "mysql+pymysql"
+
+    @update_variables
+    def action(self, includes: dict, variables: dict) -> any:
+        body = self.simple_input(variables)
+        return variables, self.execute(body)
