94d1a2ec38632ec6306bc0834bf79d79488703b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package foggleio/1.1/foggleio-1.1.tar.gz

---
+++ packages/foggleio/foggleio-1.1.tar.gz/foggleio-1.1/README.md
@@ -0,0 +1,21 @@
+# foggle-io
+
+Continuous deployment has never been easy but with the help of Foggle.io you can be one step closer
+
+## Installing
+
+Using npm:
+
+```bash
+$ pip install foggleio
+```
+
+## Usage
+
+```python
+
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ packages/foggleio/foggleio-1.1.tar.gz/foggleio-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='foggleio',
+      version='1.1',
+      description='Continuous deployment has never been easy but with the help of Foggle.io you can be one step closer',
+      url='http://foggle.io/',
+      author='Barend Erasmus',
+      author_email='hirebarend@gmail.com',
+      license='MIT',
+      packages=['foggleio'],
+      zip_safe=False)
