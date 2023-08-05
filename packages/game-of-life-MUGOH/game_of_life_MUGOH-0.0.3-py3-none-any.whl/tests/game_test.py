9d95abea7b8ed9098797c14c94efc1c6b6a90bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:13 +0000

    Add package game-of-life-MUGOH/0.0.3/game_of_life_MUGOH-0.0.3-py3-none-any.whl

---
+++ packages/game-of-life-MUGOH/game_of_life_MUGOH-0.0.3-py3-none-any.whl/tests/game_test.py
@@ -0,0 +1,19 @@
+"Game Environment test"
+import unittest
+
+from game_of_life.game import Universe
+
+
+class TestGame(unittest.TestCase):
+    """
+        Handles testing of game environment
+    """
+
+    def setUp(self):
+        self.world = Universe()
+
+    def test_game_created_with_world_(self):
+        """
+            The game is initialized with unpopulated
+        """
+        self.assertIsNone(self.world.world)
