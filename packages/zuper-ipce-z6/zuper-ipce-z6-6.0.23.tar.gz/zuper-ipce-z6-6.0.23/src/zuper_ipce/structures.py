fe722031300e76566a2e23af4271fe81469b049f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:54 +0000

    Add package zuper-ipce-z6/6.0.23/zuper-ipce-z6-6.0.23.tar.gz

---
+++ packages/zuper-ipce-z6/zuper-ipce-z6-6.0.23.tar.gz/zuper-ipce-z6-6.0.23/src/zuper_ipce/structures.py
@@ -0,0 +1,24 @@
+from typing import TypeVar
+
+from zuper_commons.types.exceptions import ZException
+from zuper_typing.monkey_patching_typing import my_dataclass
+from zuper_typing.zeneric2 import ZenericFix
+
+
+class CannotFindSchemaReference(ZException):
+    pass
+
+
+#
+# class CannotResolveTypeVar(ZException):
+#     pass
+
+
+KK = TypeVar("KK")
+VV = TypeVar("VV")
+
+
+@my_dataclass
+class FakeValues(ZenericFix[KK, VV]):
+    real_key: KK
+    value: VV
