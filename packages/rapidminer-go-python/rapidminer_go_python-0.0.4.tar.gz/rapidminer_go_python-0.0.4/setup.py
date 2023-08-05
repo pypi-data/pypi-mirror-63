c743d532b7ca1acd2770b72bda9845ed1ce2400f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:00 +0000

    Add package rapidminer-go-python/0.0.4/rapidminer_go_python-0.0.4.tar.gz

---
+++ packages/rapidminer-go-python/rapidminer_go_python-0.0.4.tar.gz/rapidminer_go_python-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+readme = ''
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rapidminer_go_python", # Replace with your own username
+    version="0.0.4",
+    author="RapidMiner-Labs",
+    author_email="bpatil@rapidminer.com",
+    description="A python package to interact with RapidMiner GO Engine",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rapidminer-labs/python-go",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
