fa22f3c4d37a6379a3e74ae44dafd38c01d00058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package qisquick/0.0a7/qisquick-0.0a7.tar.gz

---
+++ packages/qisquick/qisquick-0.0a7.tar.gz/qisquick-0.0a7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='qisquick',
+    packages=['qisquick'],
+    version='0.0a7',
+    license='MIT',
+    description='Utility library for automating running and analyzing transpiler experiments with IBM qiskit',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Brandon K Kamaka',
+    author_email='brandon.kamaka@gmail.com',
+    url='https://github.com/ParanoydAndroid/qisquick',
+    keywords=['transpiler', 'qiskit', 'quantum'],
+    install_requires=[
+        'qiskit',
+        'numpy',
+        'scipy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Environment :: Win32 (MS Windows)'
+    ],
+    python_requires='>=3.6',
+    data_files=[('qisquick_documentation', ['qisquick_documentation.pdf', 'README.md'])]
+)
