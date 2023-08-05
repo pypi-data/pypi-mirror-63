dea8633388ef591682057d6faeab241ad9f80619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:16 +0000

    Add package tidyfasta/1.0.3/tidyfasta-1.0.3.tar.gz

---
+++ packages/tidyfasta/tidyfasta-1.0.3.tar.gz/tidyfasta-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import pathlib
+
+curdir = pathlib.Path(__file__).parent
+
+README = (curdir / "README.md").read_text()
+
+print(README)
+
+setup(
+    name="tidyfasta",
+    version='1.0.3',
+    packages=['tidyfasta', 'tidyfasta.common'],
+    url='https://github.com/maxhebditch/tidyfasta',
+    license='MIT',
+    author='Max Hebditch',
+    author_email='max@maxhebditch.co.uk',
+    description='Sanitise protein FASTA files / data',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "tidyfasta=tidyfasta.__main__:main",
+        ]
+    }
+)
