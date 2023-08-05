a177401776f658906cdb1700c12614365ca7cba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:14 +0000

    Add package SQL-google-interface/0.0.1/SQL-google-interface-0.0.1.tar.gz

---
+++ packages/SQL-google-interface/SQL-google-interface-0.0.1.tar.gz/SQL-google-interface-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SQL-google-interface", # Replace with your own username
+    version="0.0.1",
+    author="William Kearney",
+    author_email="wtkearney@gmail.com",
+    description="An interface to interact between a SQL database and Google Drive folders",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wtkearney/SQL-google-interface",
+    download_url = 'https://github.com/wtkearney/SQL-google-interface/archive/v_01.tar.gz',
+    install_requires=[
+        "pyodbc",
+        "pandas",
+        "backoff",
+        "google-api-python-client"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
