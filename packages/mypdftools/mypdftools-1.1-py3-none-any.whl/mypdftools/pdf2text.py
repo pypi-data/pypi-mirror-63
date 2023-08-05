3bacf7f62b3870938f944277865c23fd1ccd643d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package mypdftools/1.1/mypdftools-1.1-py3-none-any.whl

---
+++ packages/mypdftools/mypdftools-1.1-py3-none-any.whl/mypdftools/pdf2text.py
@@ -0,0 +1,12 @@
+""" This module provides fucntion to convert a PDF to text """
+
+
+def convert(path):
+    """
+    Convert the given PDF to text
+    Parameters:
+    path(str): The path to a PDF file.
+    Returns:
+    str: The content of the PDF file as a text.
+    """
+    print("pdf2text")
