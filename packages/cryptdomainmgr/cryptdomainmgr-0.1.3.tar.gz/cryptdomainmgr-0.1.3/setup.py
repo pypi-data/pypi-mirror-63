375f03453acfeabf708c95f503fe11f780f9046e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package cryptdomainmgr/0.1.3/cryptdomainmgr-0.1.3.tar.gz

---
+++ packages/cryptdomainmgr/cryptdomainmgr-0.1.3.tar.gz/cryptdomainmgr-0.1.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import setuptools
+import cryptdomainmgr
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cryptdomainmgr",
+    version=cryptdomainmgr.__version__,
+    author="Stefan Helmert",
+    author_email="stefan.helmert@t-online.de",
+    description="Software managing certificate, dkim and domain updates automagically.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.entroserv.de/offene-software/cryptdomainmgr",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Jinja2",
+        "simpleloggerplus",
+        "pyOpenSSL",
+        "dnsuptools",
+        "parse",
+        "configparser"
+    ],
+    license = 'https://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
+
