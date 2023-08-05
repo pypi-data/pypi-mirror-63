e9ae99122cd71f1fd77b613ed8718867a0f8ad38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:28 +0000

    Add package bebi103/0.0.54/bebi103-0.0.54-py2.py3-none-any.whl

---
+++ packages/bebi103/bebi103-0.0.54-py2.py3-none-any.whl/bebi103/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bebi103."""
+
+# Force showing deprecation warnings.
+import re
+import warnings
+
+warnings.filterwarnings(
+    "always", category=DeprecationWarning, module="^{}\.".format(re.escape(__name__))
+)
+
+from . import hv
+
+from . import viz
+
+from . import image
+
+from .utils import *
+
+try:
+    from . import stan
+except:
+    warnings.warn(
+        "Could not import `stan` submodule. Perhaps PyStan or CmdStanPy is not properly installed."
+    )
+
+
+__author__ = """Justin Bois"""
+__email__ = "bois@caltech.edu"
+__version__ = "0.0.54"
