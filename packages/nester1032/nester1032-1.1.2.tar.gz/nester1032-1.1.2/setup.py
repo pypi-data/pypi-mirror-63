e533a7df3a866ae48788c9bd4d22e9c19de014a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package nester1032/1.1.2/nester1032-1.1.2.tar.gz

---
+++ packages/nester1032/nester1032-1.1.2.tar.gz/nester1032-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester1032',
+    version = '1.1.2',
+    py_modules = ['nester1032'],
+    author = 'zehuicheng',
+    author_email = '1032370080@qq.com',
+    url = 'http://www.no.com',
+    description = 'a simple printer of nested lists',
+    )
