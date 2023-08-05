5161355643010e70b78212307b6963a5b5219751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:27 +0000

    Add package cloudmesh-common/4.2.37/cloudmesh_common-4.2.37-py2.py3-none-any.whl

---
+++ packages/cloudmesh-common/cloudmesh_common-4.2.37-py2.py3-none-any.whl/cloudmesh/common/parameter.py
@@ -0,0 +1,59 @@
+from hostlist import expand_hostlist
+from cloudmesh.common.dotdict import dotdict
+
+class Parameter(object):
+    @classmethod
+    def expand(cls, parameter, allow_duplicates=False, sort=False):
+        """
+        Parameter.expand("a[0-1]")  -> ["a0", "a1"]
+        :param parameter:
+        :param allow_duplicates:
+        :param sort:
+        :return:
+        """
+        if parameter is None:
+            return parameter
+        else:
+            return expand_hostlist(parameter, allow_duplicates=False, sort=False)
+
+    @staticmethod
+    def find(name, *dicts):
+        """
+        Finds the value for the key name in multiple dicts
+
+        :param name: the key to find
+        :param dicts: the list of dicts
+        :return:
+        """
+        for d in dicts:
+            if type(d) == str:
+                return d
+            elif name in d and d[name] is not None:
+                return d[name]
+
+        return None
+
+    @staticmethod
+    def find_bool(name, *dicts):
+        """
+        Finds the value for the key name in multiple dicts
+
+        :param name: the key to find
+        :param dicts: the list of dicts
+        :return:
+        """
+        value = False
+
+        for d in dicts:
+            if type(d) == str:
+                value = d == 'True'
+            elif name in d:
+                value = d[name]
+            if type(value) == str:
+                value = value == 'True'
+
+            if value:
+                return True
+
+        return False
+
