8340f330f8398da0683b6db51f01ea80e7ff611a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package nlpcda/1.3.2/nlpcda-1.3.2.tar.gz

---
+++ packages/nlpcda/nlpcda-1.3.2.tar.gz/nlpcda-1.3.2/nlpcda/tools/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/5 1:52 PM
+# @Author  : xinfa.jiang
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ packages/nlpcda/nlpcda-1.3.2.tar.gz/nlpcda-1.3.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Jiang.XinFa
+# Mail: 425776024@qq.com
+# Created Time:  2019-12-29 12:38:34
+#############################################
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="nlpcda",
+    version="1.3.2",
+    keywords=("pip", "nlptool", "nlpcda", "nlp"),
+    description="NLP Chinese Data Augmentation，一键中文数据增强工具",
+    long_description=open('README.rst',encoding='utf-8').read(),
+    license="MIT Licence",
+
+    url="https://github.com/425776024/nlpcda",
+    author="Jiang.XinFa",
+    author_email="425776024@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['jieba']
+)
