868695bc334f390d532eda494bf6fc402a417ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package biosteam/2.3.2/biosteam-2.3.2.tar.gz

---
+++ packages/biosteam/biosteam-2.3.2.tar.gz/biosteam-2.3.2/biosteam/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 17:28:09 2017
+
+@author: Yoel Rene Cortes-Pena
+"""
+__all__ = ['Unit', 'PowerUtility', 'HeatUtility', 'UtilityAgent',
+           'System', 'TEA', 'CombinedTEA', 
+           'find', 'Flowsheet', 'CE']
+
+from lazypkg import LazyPkg
+#: Chemical engineering plant cost index (defaults to 567.5 at 2017)
+CE = 567.5 
+
+# %% Initialize BioSTEAM 
+
+from ._heat_utility import HeatUtility, UtilityAgent
+from ._power_utility import PowerUtility
+from ._unit import Unit
+from ._system import System
+from ._tea import CombinedTEA, TEA
+from ._flowsheet import Flowsheet, find
+
+
+LazyPkg(__name__, ['utils', 'units', 'evaluation'])
+
+
---
+++ packages/biosteam/biosteam-2.3.2.tar.gz/biosteam-2.3.2/biosteam/units/_mixer.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 14:26:41 2018
+
+@author: yoelr
+"""
+from .. import Unit
+
+__all__ = ('Mixer',)
+
+class Mixer(Unit):
+    """
+    Create a mixer that mixes any number of streams together.
+    
+    Parameters
+    ----------
+    ins : streams
+        Inlet fluids to be mixed.
+    outs : stream
+        Mixed outlet fluid.
+    Examples
+    --------
+    Mix two streams:
+    
+    >>> from biosteam import units
+    >>> import thermosteam as tmo
+    >>> chemicals = tmo.Chemicals(['Ethanol', 'Water'])
+    >>> tmo.settings.set_thermo(chemicals)
+    >>> s1 = tmo.Stream('s1', Water=20, T=350)
+    >>> s2 = tmo.Stream('s2', Ethanol=30, T=300)
+    >>> M1 = units.Mixer('M1', ins=(s1, s2), outs='s3')
+    >>> M1.simulate()
+    >>> M1.show()
+    Mixer: M1
+    ins...
+    [0] s1
+        phase: 'l', T: 350 K, P: 101325 Pa
+        flow (kmol/hr): Water  20
+    [1] s2
+        phase: 'l', T: 300 K, P: 101325 Pa
+        flow (kmol/hr): Ethanol  30
+    outs...
+    [0] s3
+        phase: 'l', T: 315.11 K, P: 101325 Pa
+        flow (kmol/hr): Ethanol  30
+                        Water    20
+    
+    
+    """
+    _N_outs = 1
+    _N_ins = 2
+    _ins_size_is_fixed = False
+    
+    def _run(self):
+        s_out, = self.outs
+        s_out.mix_from(self.ins)
+
+graphics = Mixer._graphics
+graphics.edge_in *= 3
+graphics.edge_out *= 3
+graphics.node['shape'] = 'triangle'
+graphics.node['orientation'] = '270'
+graphics.edge_out[0]['tailport'] = 'e'
+del graphics
