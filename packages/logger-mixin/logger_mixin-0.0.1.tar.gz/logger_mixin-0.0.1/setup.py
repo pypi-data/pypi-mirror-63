f38bde99aa7d6ad1a725c59858a52201bd90ce64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:14 +0000

    Add package logger-mixin/0.0.1/logger_mixin-0.0.1.tar.gz

---
+++ packages/logger-mixin/logger_mixin-0.0.1.tar.gz/logger_mixin-0.0.1/setup.py
@@ -0,0 +1,32 @@
+#  logger-mixin setup.py (Last Modified 3/10/20, 10:36 AM)
+#  Copyright (C) 2020 Daniel Sullivan (daniel.sullivan@state.mn.us
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name='logger_mixin',
+    version='0.0.1',
+    packages=['logger_mixin'],
+    url='',
+    license='Lesser General Public License V3',
+    author='Daniel Sullivan',
+    author_email='daniel.sullivan@state.mn.us',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+    ]
+)
