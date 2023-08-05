99d2abbbc2898052436741791b707c5018cff4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:50 +0000

    Add package stdio/0.4/stdio-0.4.tar.gz

---
+++ packages/stdio/stdio-0.4.tar.gz/stdio-0.4/stdio/__main__.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import ssl
+import runpy
+import socket
+import logging
+import argparse
+import urllib.parse
+
+from . import connect
+from logging import critical as log
+
+
+def logfile(filename):
+    if not args.logdir:
+        return
+
+    name = args.logdir + '/' + filename
+    os.dup2(os.open(name, os.O_CREAT | os.O_WRONLY | os.O_APPEND, 0o644), 2)
+
+
+def server(addr):
+    line = urllib.parse.unquote(sys.stdin.readline())
+    sys.argv = line.split()[1:-1]
+    sys.argv[0] = sys.argv[0][1:]
+
+    os.environ['METHOD'] = line[0].strip().upper()
+    while True:
+        hdr = sys.stdin.readline().strip()
+        if not hdr:
+            break
+        k, v = hdr.split(':', 1)
+        os.environ[k.strip().upper()] = v.strip()
+
+    log('from%s cmd(%s)', addr, ' '.join(sys.argv))
+
+    logfile(sys.argv[0])
+
+    print('HTTP/1.0 200 OK\n')
+    sys.stdout.flush()
+    runpy.run_module(sys.argv[0], run_name='__main__')
+    sys.stdout.flush()
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s %(process)d : %(message)s')
+
+    if args.logdir and not os.path.isdir(args.logdir):
+        os.mkdir(args.logdir)
+
+    logfile(__loader__.name)
+
+    if not os.path.isfile('ssl.key') or not os.path.isfile('ssl.cert'):
+        log('''Please use following command to create a self signed certificate
+               openssl req -x509 -newkey rsa:2048 -keyout ssl.key -nodes
+                           -subj / -out ssl.cert -sha256 -days 1000''')
+        exit()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', args.port))
+    sock.listen()
+
+    while True:
+        conn, addr = sock.accept()
+
+        if 0 == os.fork():
+            break
+
+        conn.close()
+
+    sock.close()
+    conn = ssl.wrap_socket(conn, 'ssl.key', 'ssl.cert', True)
+    sys.stdin = conn.makefile('r')
+    sys.stdout = conn.makefile('w')
+    server(addr)
+
+
+def cmd(ip, port, cmd):
+    sock = connect(ip, port, cmd)
+
+    if not os.isatty(0):
+        sock.sendall(sys.stdin.read().encode())
+
+    while True:
+        buf = sock.recv()
+        if not buf:
+            break
+
+        os.write(1, buf)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--ip', dest='ip')
+    args.add_argument('--cmd', dest='cmd')
+    args.add_argument('--port', dest='port', type=int)
+    args.add_argument('--logdir', dest='logdir', default='')
+    args = args.parse_args()
+
+    if args.cmd:
+        cmd(args.ip, args.port, args.cmd)
+    else:
+        main()
---
+++ packages/stdio/stdio-0.4.tar.gz/stdio-0.4/stdio/tools.py
@@ -0,0 +1,26 @@
+import sys
+import time
+import hashlib
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cmd', dest='cmd')
+    args = parser.parse_args()
+
+    if 'ping' == args.cmd:
+        print('ok')
+
+    if 'hash' == args.cmd:
+        print(hashlib.md5(sys.stdin.readline().encode()).hexdigest())
+
+    if 'echo' == args.cmd:
+        for line in sys.stdin:
+            print(line)
+
+    print(time.strftime('%c'))
+
+
+if __name__ == '__main__':
+    main()
