f2405955bb673aaaab566c14429b771f4adcbcc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package kylinpy/2.4.1/kylinpy-2.4.1.tar.gz

---
+++ packages/kylinpy/kylinpy-2.4.1.tar.gz/kylinpy-2.4.1/kylinpy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .kylinpy import KylinCluster
+
+__version__ = '2.4.1'
+
+__all__ = [
+    'KylinCluster',
+]
