055edcdca8d0ebfabe50e94362e7f1df906cce3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:22 +0000

    Add package pframe/20200310.2/pframe-20200310.2.tar.gz

---
+++ packages/pframe/pframe-20200310.2.tar.gz/pframe-20200310.2/pframe/encoding/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/local/python/bin/python
+# -*- coding: utf-8 -*-
+# (C) Wu Dong, 2019
+# All rights reserved
+# @Author: 'Wu Dong <wudong@eastwu.cn>'
+# @Time: '2020-03-09 13:04'
+
+# JSON编码
+from .pjson import JSONEncoder
---
+++ packages/pframe/pframe-20200310.2.tar.gz/pframe-20200310.2/pframe/system/__init__.py
@@ -0,0 +1,10 @@
+# !/usr/local/python/bin/python
+# -*- coding: utf-8 -*-
+# (C) Wu Dong, 2019
+# All rights reserved
+# @Author: 'Wu Dong <wudong@eastwu.cn>'
+# @Time: '2020-03-09 13:05'
+
+# 系统相关
+from .platform import get_os
+from .platform import Platform
