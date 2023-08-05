2e9299b4c5101edfb24efb4a9386ef034c98d0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:29 +0000

    Add package swh.storage/0.0.177/swh.storage-0.0.177.tar.gz

---
+++ packages/swh.storage/swh.storage-0.0.177.tar.gz/swh.storage-0.0.177/swh/storage/exc.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2015-2016  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+class StorageDBError(Exception):
+    """Specific storage db error (connection, erroneous queries, etc...)
+
+    """
+
+    def __str__(self):
+        return 'An unexpected error occurred in the backend: %s' % self.args
+
+
+class StorageAPIError(Exception):
+    """Specific internal storage api (mainly connection)
+
+    """
+
+    def __str__(self):
+        args = self.args
+        return 'An unexpected error occurred in the api backend: %s' % args
+
+
+class StorageArgumentException(Exception):
+    """Argument passed to a Storage endpoint is invalid."""
+    pass
