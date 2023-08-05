274c695d793363c53b2ca9cacbc239059a8c96cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package python-amazon-paapi/3.0.0/python-amazon-paapi-3.0.0.tar.gz

---
+++ packages/python-amazon-paapi/python-amazon-paapi-3.0.0.tar.gz/python-amazon-paapi-3.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='python-amazon-paapi',
+    version='3.0.0',
+    author='Sergio Abad',
+    author_email='sergio.abad@bytelix.com',
+    description='Amazon Product Advertising API 5.0 wrapper for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='GPL-3.0',
+    url='https://github.com/sergioteula/python-amazon-paapi',
+    packages=setuptools.find_packages(),
+    install_requires=['amightygirl.paapi5-python-sdk'],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=2.7',
+)
