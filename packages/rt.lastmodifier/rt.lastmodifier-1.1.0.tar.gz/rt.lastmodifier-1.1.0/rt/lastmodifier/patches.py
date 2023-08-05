b73ed9e68eb60afd7054fe4e1493108fa6de7585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package rt.lastmodifier/1.1.0/rt.lastmodifier-1.1.0.tar.gz

---
+++ packages/rt.lastmodifier/rt.lastmodifier-1.1.0.tar.gz/rt.lastmodifier-1.1.0/rt/lastmodifier/patches.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from zope.annotation.interfaces import IAnnotations
+from persistent.dict import PersistentDict
+from zope.component import getMultiAdapter
+from plone import api
+
+
+def notifyModified(self):
+    # do all of the default stuff
+    self._old_notifyModified()
+    # now save the current userid as raw last modifier
+    annotations = IAnnotations(self)
+    annotations['rt.lastmodifier'] = PersistentDict()
+    portal_state = getMultiAdapter(
+        (self, self.REQUEST), name=u'plone_portal_state'
+    )
+    # BBB: what to do if the action is someway triggered by anonymous?
+    if portal_state.anonymous():
+        annotations['rt.lastmodifier']['lastmodifier'] = 'anonymous'
+    else:
+        annotations['rt.lastmodifier'][
+            'lastmodifier'
+        ] = api.user.get_current().getId()
