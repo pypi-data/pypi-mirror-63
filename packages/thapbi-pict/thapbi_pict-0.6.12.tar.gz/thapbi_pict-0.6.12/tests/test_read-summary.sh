9b92f68a933301380f195e506d7e3a9c0eefaf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package thapbi-pict/0.6.12/thapbi_pict-0.6.12.tar.gz

---
+++ packages/thapbi-pict/thapbi_pict-0.6.12.tar.gz/thapbi_pict-0.6.12/tests/test_read-summary.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright 2019 by Peter Cock, The James Hutton Institute.
+# All rights reserved.
+# This file is part of the THAPBI Phytophthora ITS1 Classifier Tool (PICT),
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+IFS=$'\n\t'
+set -eu
+# Note not using "set -o pipefail" until after check error message with grep
+
+export TMP=${TMP:-/tmp}
+
+echo "====================="
+echo "Checking read-summary"
+echo "====================="
+set -x
+thapbi_pict read-summary 2>&1 | grep "the following arguments are required"
+thapbi_pict read-summary -o '' -i tests/classify 2>&1 | grep "No output file specified"
+set -o pipefail
+
+# One method:
+thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S95_L001.identity.tsv -m identity -o $TMP/read-summary_identity.tsv -e $TMP/read-summary_swarm_and_blast.xlsx
+
+# Two methods:
+thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/read-summary_swarm_and_blast.tsv
+
+# With metadata, using default method, -m onebp
+thapbi_pict read-summary --input tests/classify/P-infestans-T30-4.fasta tests/classify/P-infestans-T30-4.onebp.tsv -o $TMP/read-summary_onebp.tsv -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -e $TMP/read-summary_onebp.xlsx
+diff $TMP/read-summary_onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
+
+echo "$0 - test_read-summary.sh passed"
