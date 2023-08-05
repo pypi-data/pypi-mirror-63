246f3dbcacf07e85c777c656367cddd3ab49b966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:32 +0000

    Add package devops-microsoft-mapping-sspo/0.2.1/devops_microsoft_mapping_sspo-0.2.1-py3-none-any.whl

---
+++ packages/devops-microsoft-mapping-sspo/devops_microsoft_mapping_sspo-0.2.1-py3-none-any.whl/devops_microsoft_mapping_sspo/scrum_atomic_project.py
@@ -0,0 +1,20 @@
+from .scrum_project import ScrumProject
+from sspo_db.application import factories as application_factories
+from sspo_db.model import factories as model_factories
+import logging
+logging.basicConfig(level=logging.INFO)
+
+class ScrumAtomicProject(ScrumProject):
+
+    def create(self, element, organization_id):
+        try:
+            logging.info("Scrum Atomic Project: Start")
+            self.element = element
+            self.organization = organization_id
+            self.scrum_development_task = model_factories.ScrumAtomicProjectFactory()
+            self.scrum_development_application = application_factories.ScrumAtomicProjectFactory()
+            self.create()
+            logging.info("Scrum Atomic Project: End")
+        except Exception as e: 
+            logging.error("OS error: {0}".format(e))
+            logging.error(e.__dict__)
---
+++ packages/devops-microsoft-mapping-sspo/devops_microsoft_mapping_sspo-0.2.1-py3-none-any.whl/devops_microsoft_mapping_sspo/scrum_project.py
@@ -0,0 +1,32 @@
+from .base_entity import BaseEntity
+import logging
+logging.basicConfig(level=logging.INFO)
+
+class ScrumProject(BaseEntity):
+
+    def __init__(self):
+        super().__init__()
+        self.scrum_project = None
+        self.scrum_project_application = None
+        self.element = None
+        
+    def create(self):
+        try:
+            
+            self.scrum_project.name = self.element.name
+            self.scrum_project.description = self.element.description
+            self.scrum_project.organization = self.organization
+                
+            self.scrum_project_application.create(self.scrum_project)
+                
+            self.create_application_reference(
+                    self.element.id, 
+                    self.element.url,
+                    self.PROJECT_TFS, 
+                    self.scrum_project.uuid, 
+                    self.scrum_project.type)
+            
+        except Exception as e: 
+            logging.error("OS error: {0}".format(e))
+            logging.error(e.__dict__) 
+
