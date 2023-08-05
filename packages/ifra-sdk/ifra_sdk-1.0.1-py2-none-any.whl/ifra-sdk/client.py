478cad7284ac227c48409327bc8b00da6805bca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package ifra-sdk/1.0.1/ifra_sdk-1.0.1-py2-none-any.whl

---
+++ packages/ifra-sdk/ifra_sdk-1.0.1-py2-none-any.whl/ifra-sdk/client.py
@@ -0,0 +1,15 @@
+
+
+class IfraSDK:
+  def __init__(self, channel, device_id,device_secret):
+    self.device_id = device_id
+    self.device_secret = device_secret
+
+ def add_sensor(self,sensor_name,unit,value):
+     pass
+
+ def add_sensor(self,sensor_name,unit,value):
+     pass
+
+ def send(self):
+     pass
