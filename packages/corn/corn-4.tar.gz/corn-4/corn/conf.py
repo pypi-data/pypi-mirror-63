bc44c3b7f44ddec0b4a96f0b6f02af84337ba791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:28 +0000

    Add package corn/4/corn-4.tar.gz

---
+++ packages/corn/corn-4.tar.gz/corn-4/corn/conf.py
@@ -0,0 +1,20 @@
+CLI_CONFIG = {
+    "items": {
+        "positional": True,
+        "default": [],
+        "nargs": "*",
+        "help": "Print the named corn",
+        "type": str,
+    },
+    "out": {
+        "default": "nested",
+        "type": str,
+        "help": "The outputter to display the corn",
+    },
+}
+CONFIG = {}
+GLOBAL = {}
+SUBS = {}
+DYNE = {
+    "corn": ["corn"],
+}
