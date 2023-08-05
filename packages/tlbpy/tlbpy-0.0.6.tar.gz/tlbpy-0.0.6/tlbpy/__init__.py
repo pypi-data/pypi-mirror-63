ab3b609204b9dd55371d241bf695ecfb1f31a5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:41 +0000

    Add package tlbpy/0.0.6/tlbpy-0.0.6.tar.gz

---
+++ packages/tlbpy/tlbpy-0.0.6.tar.gz/tlbpy-0.0.6/tlbpy/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    My personnal toolbox for Python
+"""
+
+__version__ = "0.0.6"
+
+from tlbpy.helloworld import helloworld
+from tlbpy.cnf_parser import cnf_parser
+from tlbpy.duration import Duration
+from tlbpy.talk import Talk
