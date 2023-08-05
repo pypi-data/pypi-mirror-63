5421df099d1a1c81006329bad795ab1bc8e858a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package arxiv-auth/0.4.2/arxiv-auth-0.4.2.tar.gz

---
+++ packages/arxiv-auth/arxiv-auth-0.4.2.tar.gz/arxiv-auth-0.4.2/arxiv/users/auth/__init__.py
@@ -0,0 +1,203 @@
+"""Provides tools for working with authenticated user/client sessions."""
+
+from typing import Optional, Union, Any
+from datetime import datetime
+import warnings
+
+from pytz import UTC
+from flask import Flask, request, Response, make_response, redirect, url_for
+from werkzeug.http import parse_cookie
+from werkzeug.datastructures import MultiDict
+from werkzeug.routing import BuildError
+from retry import retry
+
+from . import decorators, middleware, scopes, tokens
+from .. import domain, legacy
+
+from arxiv.base import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Auth(object):
+    """
+    Attaches session and authn/z information to the request.
+
+    Intended for use in a Flask application factory, for example:
+
+    .. code-block:: python
+
+       from flask import Flask
+       from arxiv.users.auth import Auth
+       from someapp import routes
+
+
+       def create_web_app() -> Flask:
+          app = Flask('someapp')
+          app.config.from_pyfile('config.py')
+          Auth(app)   # Registers the base/UI blueprint.
+          app.register_blueprint(routes.blueprint)    # Your blueprint.
+       return app
+
+
+    """
+
+    def __init__(self, app: Optional[Flask] = None) -> None:
+        """
+        Initialize ``app`` with base blueprint.
+
+        Parameters
+        ----------
+        app : :class:`Flask`
+
+        """
+        if app is not None:
+            self.init_app(app)
+
+    @retry(legacy.exceptions.Unavailable, tries=3, delay=0.5, backoff=2)
+    def _get_legacy_session(self) -> Optional[domain.Session]:
+        """
+        Attempt to load a legacy auth session.
+
+        Returns
+        -------
+        :class:`domain.Session` or None
+
+        """
+        classic_cookie_key = self.app.config['CLASSIC_COOKIE_NAME']
+        classic_cookie = request.cookies.get(classic_cookie_key, None)
+        if classic_cookie is None:
+            return None
+        try:
+            with legacy.transaction():
+                return legacy.sessions.load(classic_cookie)
+        except legacy.exceptions.UnknownSession as e:
+            logger.debug('No legacy session available: %s', e)
+        except legacy.exceptions.InvalidCookie as e:
+            logger.debug('Invalid legacy cookie: %s', e)
+        except legacy.exceptions.SessionExpired as e:
+            logger.debug('Legacy session is expired: %s', e)
+        return None
+
+    def init_app(self, app: Flask) -> None:
+        """
+        Attach :meth:`.load_session` to the Flask app.
+
+        Parameters
+        ----------
+        app : :class:`Flask`
+
+        """
+        self.app = app
+        legacy.init_app(app)
+        self.app.before_request(self.load_session)
+        self.app.config.setdefault('DEFAULT_LOGOUT_REDIRECT_URL',
+                                   'https://arxiv.org')
+        self.app.config.setdefault('DEFAULT_LOGIN_REDIRECT_URL',
+                                   'https://arxiv.org')
+
+        @self.app.teardown_request
+        def teardown_request(exception: Optional[Exception]) -> None:
+            session = legacy.current_session()
+            if exception:
+                session.rollback()
+            session.remove()
+
+        @self.app.teardown_appcontext
+        def teardown_appcontext(*args: Any, **kwargs: Any) -> None:
+            session = legacy.current_session()
+            session.rollback()
+            session.remove()
+
+    def load_session(self) -> Optional[Response]:
+        """
+        Look for an active session, and attach it to the request.
+
+        The typical scenario will involve the
+        :class:`.middleware.AuthMiddleware` unpacking a session token and
+        adding it to the WSGI request environ. As a fallback, if the legacy
+        database is available, this method will also attempt to load an
+        active legacy session.
+        """
+        # Check the WSGI request environ for the ``session`` key, which
+        # is where the auth middleware puts any unpacked auth information from
+        # the request OR any exceptions that need to be raised withing the
+        # request context.
+        session: Optional[Union[domain.Session, Exception]] = \
+            request.environ.get('session')
+
+        # Middlware may have passed an exception, which needs to be raised
+        # within the app/execution context to be handled correctly.
+        if isinstance(session, Exception):
+            logger.debug('Middleware passed an exception: %s', session)
+            raise session
+
+        # If the legacy database is available, we should use it to authorize
+        # the request.
+        if legacy.is_configured():
+            logger.debug('No session; attempting to get legacy session')
+            response = self.detect_and_clobber_dupe_cookies()
+
+            # Return that is anything other than None is treated as a response;
+            # request handling stops here.
+            if response is not None:
+                return response
+            session = self._get_legacy_session()
+
+        # Attach the session to the request so that other
+        # components can access it easily.
+        if self.app.config.get('AUTH_UPDATED_SESSION_REF'):
+            request.auth = session
+        else:
+            # This clobbers the built-in Flask session interface. This is a
+            # design flaw that's blocking other work. This is deprecated and
+            # will be removed in 0.4.1.
+            warnings.warn(
+                "Accessing the authenticated session via request.auth is"
+                " deprecated, and will be removed in 0.4.1. Use request.auth"
+                " instead. ARXIVNG-1920.",
+                DeprecationWarning
+            )
+            request.session = session
+        return None
+
+    def detect_and_clobber_dupe_cookies(self) -> Optional[Response]:
+        """
+        Detect and discard duplicate cookies.
+
+        Legacy components have been known to generate dupe cookies, which
+        causes all kinds of havoc. Here we check the request for duplicates
+        of the session and permanent cookies and, if we find them, we blow
+        them away and redirect back to /login.
+        """
+        # By default, werkzeug uses a dict-based struct that supports only a
+        # single value per key. This isn't really up to speed with RFC 6265.
+        # Luckily we can just pass in an alternate struct to parse_cookie()
+        # that can cope with multiple values.
+        raw_cookie = request.environ.get('HTTP_COOKIE', None)
+        if raw_cookie is None:
+            return None
+        cookies = parse_cookie(raw_cookie, cls=MultiDict)
+        classic_cookie_name = self.app.config['CLASSIC_COOKIE_NAME']
+        perm_cookie_name = self.app.config['CLASSIC_PERMANENT_COOKIE_NAME']
+        domain = self.app.config['AUTH_SESSION_COOKIE_DOMAIN']
+
+        response = None     # If we return None, request is handled normally.
+        now = datetime.now(UTC)
+        for name in [classic_cookie_name, perm_cookie_name]:
+            if len(cookies.getlist(name)) > 1:
+                if response is None:
+                    # The ui.login route may not exist on an application using
+                    # this package, so we will fall back to the logout redirect
+                    # URL if the login route is missing. ARXIVNG-2063
+                    try:
+                        target = url_for('ui.login')
+                    except BuildError:
+                        target = self.app.config['DEFAULT_LOGOUT_REDIRECT_URL']
+                    response = make_response(redirect(target))
+                response.set_cookie(name, '', max_age=0, expires=now)
+                response.set_cookie(name, '', max_age=0, expires=now,
+                                    domain=domain.lstrip('.'))
+                response.set_cookie(name, '', max_age=0, expires=now,
+                                    domain=domain)
+        return response
---
+++ packages/arxiv-auth/arxiv-auth-0.4.2.tar.gz/arxiv-auth-0.4.2/arxiv/users/legacy/util.py
@@ -0,0 +1,145 @@
+"""Helpers and Flask application integration."""
+
+from typing import Generator, Tuple, List, Optional, Any
+from datetime import datetime
+from pytz import timezone, UTC
+from contextlib import contextmanager
+import secrets
+from base64 import b64encode, b64decode
+import hashlib
+from collections import Counter
+
+from flask import Flask
+from sqlalchemy.engine import Engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from arxiv.base import logging
+from arxiv.base.globals import get_application_config, get_application_global
+
+from ..auth import scopes
+from .. import domain
+from .models import db, DBUser, DBPolicyClass, DBEndorsement, DBSession
+from .exceptions import UnknownSession, PasswordAuthenticationFailed
+
+EASTERN = timezone('US/Eastern')
+logger = logging.getLogger(__name__)
+logger.propagate = False
+
+
+def now() -> int:
+    """Get the current epoch/unix time."""
+    return epoch(datetime.now(tz=UTC))
+
+
+def epoch(t: datetime) -> int:
+    """Convert a :class:`.datetime` to UNIX time."""
+    delta = t - datetime.fromtimestamp(0, tz=EASTERN)
+    return int(round((delta).total_seconds()))
+
+
+def from_epoch(t: int) -> datetime:
+    """Get a :class:`datetime` from an UNIX timestamp."""
+    return datetime.fromtimestamp(t, tz=EASTERN)
+
+
+@contextmanager
+def transaction() -> Generator:
+    """Context manager for database transaction."""
+    try:
+        yield db.session
+        # The caller may have explicitly committed already, in order to
+        # implement exception handling logic. We only want to commit here if
+        # there is anything remaining that is not flushed.
+        if db.session.new or db.session.dirty or db.session.deleted:
+            db.session.commit()
+    except Exception as e:
+        logger.error('Commit failed, rolling back: %s', str(e))
+        db.session.rollback()
+        raise
+
+
+def init_app(app: Flask) -> None:
+    """Set configuration defaults and attach session to the application."""
+    db.init_app(app)
+
+
+def current_session() -> Session:
+    """Get/create database session for this context."""
+    return db.session
+
+
+def create_all() -> None:
+    """Create all tables in the database."""
+    db.create_all()
+
+
+def drop_all() -> None:
+    """Drop all tables in the database."""
+    db.drop_all()
+
+
+def hash_password(password: str) -> str:
+    """Generate a secure hash of a password."""
+    salt = secrets.token_bytes(4)
+    hashed = hashlib.sha1(salt + b'-' + password.encode('utf-8')).digest()
+    return b64encode(salt + hashed).decode('ascii')
+
+
+def check_password(password: str, encrypted: bytes) -> None:
+    """Check a password against an encrypted hash."""
+    decoded = b64decode(encrypted)
+    salt = decoded[:4]
+    enc_hashed = decoded[4:]
+    pass_hashed = hashlib.sha1(salt + b'-' + password.encode('utf-8')).digest()
+    if pass_hashed != enc_hashed:
+        raise PasswordAuthenticationFailed('Incorrect password')
+
+
+def compute_capabilities(tapir_user: DBUser) -> int:
+    """Calculate the privilege level code for a user."""
+    return int(sum([2 * tapir_user.flag_edit_users,
+                    4 * tapir_user.flag_email_verified,
+                    8 * tapir_user.flag_edit_system]))
+
+
+def get_scopes(db_user: DBUser) -> List[domain.Scope]:
+    """Generate a list of authz scopes for a legacy user based on class."""
+    if db_user.policy_class == DBPolicyClass.PUBLIC_USER:
+        return scopes.GENERAL_USER
+    if db_user.policy_class == DBPolicyClass.ADMIN:
+        return scopes.ADMIN_USER
+    return []
+
+
+def is_configured() -> bool:
+    """Determine whether or not the legacy database is configured."""
+    config = get_application_config()
+    if 'CLASSIC_DATABASE_URI' in config and 'CLASSIC_SESSION_HASH' in config:
+        return True
+    return False
+
+
+def get_session_hash() -> str:
+    """Get the legacy hash secret."""
+    config = get_application_config()
+    session_hash: str = config['CLASSIC_SESSION_HASH']
+    return session_hash
+
+
+def get_session_duration() -> int:
+    """Get the session duration from the config."""
+    config = get_application_config()
+    timeout: str = config['SESSION_DURATION']
+    return int(timeout)
+
+
+def is_available(**kwargs: Any) -> bool:
+    """Check our connection to the database."""
+    try:
+        db.session.query("1").from_statement(text("SELECT 1")).all()
+    except Exception as e:
+        logger.error('Encountered an error talking to database: %s', e)
+        return False
+    return True
