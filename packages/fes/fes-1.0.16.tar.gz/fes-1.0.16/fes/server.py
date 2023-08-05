8f14c6833d9e159a8b557c15f3f40f6499152694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:05 +0000

    Add package fes/1.0.16/fes-1.0.16.tar.gz

---
+++ packages/fes/fes-1.0.16.tar.gz/fes-1.0.16/fes/server.py
@@ -0,0 +1,31 @@
+import finac as f
+import finac.api as api
+
+import logging
+import os
+import yaml
+from pathlib import Path
+
+__version__ = '1.0.16'
+
+dir_me = Path(__file__).absolute().parents[1]
+
+f.core.logger = logging.getLogger('gunicorn.error')
+api.logger = f.core.logger
+
+with open(os.environ.get('FES_CONFIG')) as fh:
+    config = yaml.load(fh.read())['fes']
+
+if 'finac' in config:
+    fc = config['finac']
+    for k, v in fc.copy().items():
+        if '-' in k:
+            fc[k.replace('-', '_')] = v
+            del fc[k]
+f.init(**config.get('finac', {}))
+api.real_ip_header = config.get('real-ip-header')
+api.key = config['key']
+app = api.app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
