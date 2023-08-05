d05a415bba8127506a2a1792d331af0c9f2996e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:26 +0000

    Add package pytchat/0.0.6.2/pytchat-0.0.6.2-py3-none-any.whl

---
+++ packages/pytchat/pytchat-0.0.6.2-py3-none-any.whl/pytchat/__init__.py
@@ -0,0 +1,27 @@
+"""
+pytchat is a python library for fetching youtube live chat without using yt api, Selenium, or BeautifulSoup.
+"""
+__copyright__    = 'Copyright (C) 2019 taizan-hokuto'
+__version__      = '0.0.6.2'
+__license__      = 'MIT'
+__author__       = 'taizan-hokuto'
+__author_email__ = '55448286+taizan-hokuto@users.noreply.github.com'
+__url__          = 'https://github.com/taizan-hokuto/pytchat'
+
+__all__ = ["core_async","core_multithread","processors"]
+
+from .api import (
+    config,
+    LiveChat,
+    LiveChatAsync,
+    ChatProcessor,
+    CompatibleProcessor,
+    DummyProcessor,
+    DefaultProcessor,
+    Extractor, 
+    JsonfileArchiver,
+    SimpleDisplayProcessor,
+    SpeedCalculator,
+    SuperchatCalculator,
+    VideoInfo
+)
