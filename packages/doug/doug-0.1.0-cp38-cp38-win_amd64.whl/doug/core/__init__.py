66fdfbfeb9af0a5d8cf4b762f5507c688199f436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:43 +0000

    Add package doug/0.1.0/doug-0.1.0-cp38-cp38-win_amd64.whl

---
+++ packages/doug/doug-0.1.0-cp38-cp38-win_amd64.whl/doug/core/__init__.py
@@ -0,0 +1,20 @@
+# from __future__ import absolute_import
+
+from .add_watermark import *
+from .copy_paste_under import *
+from .occlusion_paste_above import *
+from .delete_one_class import *
+from .delete_classes import *
+from .hsv_color import *
+from .vertical_light_gradient import *
+from .horizontal_light_gradient import *
+from .night_noise import *
+from .night_light_spot import *
+from .sample_function import *
+from .small_angle_rotate import *
+from .tremor import *
+from .zoom_one_class import *
+from .zoom_classes import *
+from .fix_jpgmask import *
+
+from .DouG import *
