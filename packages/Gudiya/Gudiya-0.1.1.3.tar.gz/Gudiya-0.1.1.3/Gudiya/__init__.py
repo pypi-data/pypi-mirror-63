17d3ebf51628186ac78956cf7291f775c1e705ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:04 +0000

    Add package Gudiya/0.1.1.3/Gudiya-0.1.1.3.tar.gz

---
+++ packages/Gudiya/Gudiya-0.1.1.3.tar.gz/Gudiya-0.1.1.3/Gudiya/__init__.py
@@ -0,0 +1,5 @@
+from Gudiya.Model import ResNet,BasicBlock,Bottleneck
+from Gudiya.Model import ResNet18
+from Gudiya.Model_test import test
+from Gudiya.Model_train import train
+from Gudiya.Utils import imshow,total_accuracy,class_accuracy,model_call
