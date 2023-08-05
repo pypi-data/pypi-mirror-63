5164c5d5d5801948e2801c6e41cdc7f795bad676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:27 +0000

    Add package ubermagtable/0.1.9/ubermagtable-0.1.9.tar.gz

---
+++ packages/ubermagtable/ubermagtable-0.1.9.tar.gz/ubermagtable-0.1.9/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ubermagtable',
+    version='0.1.9',
+    description=('Python package for manipulating '
+                 'OOMMF and mumax3 tabular data.'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://ubermag.github.io',
+    author=('Marijan Beg, Vanessa Nehruji, Sergii Mamedov, '
+            'Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr'),
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['pandas',
+                      'numpy',
+                      'pytest'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English']
+)
