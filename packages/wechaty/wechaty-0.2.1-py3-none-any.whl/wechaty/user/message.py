b2ef596da8dfeaa10624c4a455788df1be68b905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package wechaty/0.2.1/wechaty-0.2.1-py3-none-any.whl

---
+++ packages/wechaty/wechaty-0.2.1-py3-none-any.whl/wechaty/user/message.py
@@ -0,0 +1,29 @@
+"""
+message object which handle the message sending
+"""
+from __future__ import annotations
+
+
+# pylint: disable=R0903
+class Message:
+    """
+    message object which can handle the message sending
+    """
+    def __init__(self):
+        """
+        initialization
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, msg_id: str) -> Message:
+        """
+        load message
+        """
+        raise NotImplementedError
+
+    async def ready(self):
+        """
+        sync load message
+        """
+        raise NotImplementedError
