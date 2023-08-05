90e33751776bb907537753e1282629da68c43ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package pyexodus/0.1.2/pyexodus-0.1.2-py3-none-any.whl

---
+++ packages/pyexodus/pyexodus-0.1.2-py3-none-any.whl/pyexodus/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:copyright:
+    Lion Krischer (lionkrischer@gmail.com), 2016
+:license:
+    MIT License
+"""
+from __future__ import absolute_import
+
+from .core import exodus  # NOQA
+from .version import get_git_version
+
+__version__ = get_git_version()
