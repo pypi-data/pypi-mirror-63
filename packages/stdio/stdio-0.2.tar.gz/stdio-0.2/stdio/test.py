49c1def401fd8ef832b6d7a530ef67e921ccf2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:44 +0000

    Add package stdio/0.2/stdio-0.2.tar.gz

---
+++ packages/stdio/stdio-0.2.tar.gz/stdio-0.2/stdio/test.py
@@ -0,0 +1,23 @@
+import sys
+import hashlib
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cmd', dest='cmd', default='ping')
+    args = parser.parse_args()
+
+    if 'ping' == args.cmd:
+        print('ok')
+
+    if 'hash' == args.cmd:
+        print(hashlib.md5(sys.stdin.readline().encode()).hexdigest())
+
+    if 'echo' == args.cmd:
+        for line in sys.stdin:
+            print(line)
+
+
+if __name__ == '__main__':
+    main()
