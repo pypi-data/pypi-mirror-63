2d64025e83f0892ee16897dd34a5e62d0e69924a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package retworkx/0.3.1/retworkx-0.3.1.tar.gz

---
+++ packages/retworkx/retworkx-0.3.1.tar.gz/retworkx-0.3.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name="retworkx",
+    version="0.3.1",
+    description="A python graph library implemented in Rust",
+    long_description=readme(),
+    author = "Matthew Treinish",
+    author_email = "mtreinish@kortar.org",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Rust",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    url="https://github.com/mtreinish/retworkx",
+    project_urls={
+        "Bug Tracker": "https://github.com/mtreinish/retworkx/issues",
+        "Source Code": "https://github.com/mtreinish/retworkx",
+        "Documentation": "https://retworkx.readthedocs.io",
+    },
+    rust_extensions=[RustExtension("retworkx.retworkx", "Cargo.toml",
+                                   binding=Binding.PyO3)],
+    include_package_data=True,
+    packages=["retworkx"],
+    zip_safe=False,
+    python_requires=">=3.5",
+)
