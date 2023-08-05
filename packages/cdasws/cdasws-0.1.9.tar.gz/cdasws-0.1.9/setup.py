28b9a2d2fc6687a5779065601930742ca1233aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package cdasws/0.1.9/cdasws-0.1.9.tar.gz

---
+++ packages/cdasws/cdasws-0.1.9.tar.gz/cdasws-0.1.9/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cdasws",
+    version="0.1.9",
+    description="NASA's Coordinated Data Analysis System Web Service Client Library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://cdaweb.sci.gsfc.nasa.gov/WebServices/REST",
+    author="Bernie Harris",
+    author_email="gsfc-spdf-support@lists.nasa.gov",
+    license="NOSA",
+    packages=["cdasws"],
+#    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=["python-dateutil", "requests", "spacepy"],
+#    entry_points={
+#        "console_scripts": [
+#            "cdasws=cdasws.__main__:example",
+#        ]
+#    },
+)
