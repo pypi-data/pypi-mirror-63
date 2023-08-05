ce9b0410ef7d890992e229ce33390050370078bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package incremental-trees/0.2/incremental_trees-0.2.tar.gz

---
+++ packages/incremental-trees/incremental_trees-0.2.tar.gz/incremental_trees-0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(name='incremental_trees',
+                 version="0.2",
+                 author="Gareth Jones",
+                 author_email="author@example.com",
+                 description='Sklearn forests with partial fits',
+                 # long_description=long_description,
+                 # long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 url="",
+                 install_requires=['dask', 'dask_ml', 'sklearn', 'bokeh'])
