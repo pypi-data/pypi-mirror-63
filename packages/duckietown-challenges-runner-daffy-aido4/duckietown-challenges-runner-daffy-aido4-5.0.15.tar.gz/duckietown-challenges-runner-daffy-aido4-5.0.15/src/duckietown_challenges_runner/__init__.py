6a6a67a05c78ff3ab2cd26b39bc7818ad990a641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package duckietown-challenges-runner-daffy-aido4/5.0.15/duckietown-challenges-runner-daffy-aido4-5.0.15.tar.gz

---
+++ packages/duckietown-challenges-runner-daffy-aido4/duckietown-challenges-runner-daffy-aido4-5.0.15.tar.gz/duckietown-challenges-runner-daffy-aido4-5.0.15/src/duckietown_challenges_runner/__init__.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+__version__ = "5.0.15"
+import logging
+import sys
+
+logging.basicConfig()
+dclogger = logging.getLogger("dt-challenges-runner")
+dclogger.setLevel(logging.DEBUG)
+
+dclogger.info("dt-challenges-runner %s" % __version__)
+msg = f"Default encoding {sys.getdefaultencoding()}"
+dclogger.debug(msg)
+
+from requests import __version__ as requests_version
+
+dclogger.info("using: requests %s" % requests_version)
+from docker import __version__ as docker_version
+
+dclogger.info("using: docker-py %s" % docker_version)
+
+from .runner import dt_challenges_evaluator
+
+from .runner_local import runner_local_main
