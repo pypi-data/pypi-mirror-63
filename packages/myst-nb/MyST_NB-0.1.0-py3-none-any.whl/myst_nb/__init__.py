9348f94a655dfea222f1651f3564221cff6cedf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package myst-nb/0.1.0/MyST_NB-0.1.0-py3-none-any.whl

---
+++ packages/myst-nb/MyST_NB-0.1.0-py3-none-any.whl/myst_nb/__init__.py
@@ -0,0 +1,104 @@
+__version__ = "0.1.0"
+
+from docutils import nodes
+from jupyter_sphinx.ast import (  # noqa: F401
+    JupyterWidgetStateNode,
+    JupyterWidgetViewNode,
+    JupyterCell,
+)
+
+# from ipywidgets import embed
+from pathlib import Path
+
+from .parser import (
+    NotebookParser,
+    CellNode,
+    CellInputNode,
+    CellOutputNode,
+    CellOutputBundleNode,
+    CellImageNode,
+)
+from .transform import CellOutputsToNodes
+
+
+def static_path(app):
+    static_path = Path(__file__).absolute().with_name("_static")
+    app.config.html_static_path.append(str(static_path))
+
+
+def update_togglebutton_classes(app, config):
+    to_add = [
+        ".tag_hide_input div.cell_input",
+        ".tag_hide_output div.cell_output",
+        ".tag_hide_cell.cell",
+    ]
+    for selector in to_add:
+        config.togglebutton_selector += f", {selector}"
+
+
+def setup(app):
+    """Initialize Sphinx extension."""
+    # Sllow parsing ipynb files
+    app.add_source_suffix(".ipynb", "ipynb")
+    app.add_source_parser(NotebookParser)
+    app.setup_extension("sphinx_togglebutton")
+
+    # Helper functions for the registry, pulled from jupyter-sphinx
+    def skip(self, node):
+        raise nodes.SkipNode
+
+    # Used to render an element node as HTML
+    def visit_element_html(self, node):
+        self.body.append(node.html())
+        raise nodes.SkipNode
+
+    # Shortcut for registering our container nodes
+    render_container = (
+        lambda self, node: self.visit_container(node),
+        lambda self, node: self.depart_container(node),
+    )
+
+    # Register our container nodes, these should behave just like a regular container
+    for node in [CellNode, CellInputNode, CellOutputNode]:
+        app.add_node(
+            node,
+            override=True,
+            html=(render_container),
+            latex=(render_container),
+            textinfo=(render_container),
+            text=(render_container),
+            man=(render_container),
+        )
+
+    # Register the output bundle node.
+    # No translators should touch this node because we'll replace it in a post-transform
+    app.add_node(
+        CellOutputBundleNode,
+        override=True,
+        html=(skip, None),
+        latex=(skip, None),
+        textinfo=(skip, None),
+        text=(skip, None),
+        man=(skip, None),
+    )
+
+    # So that we can in-line images in HTML outputs
+    def visit_cell_image(self, node):
+        atts = {"src": f"data:image/png;base64, {node['uri']}", "alt": f"{node['alt']}"}
+        self.body.append(self.emptytag(node, "img", "\n", **atts))
+
+    app.add_node(
+        CellImageNode, override=True, html=(visit_cell_image, lambda self, node: "")
+    )
+
+    # Register our post-transform which will convert output bundles to nodes
+    app.add_post_transform(CellOutputsToNodes)
+
+    app.connect("builder-inited", static_path)
+    app.connect("config-inited", update_togglebutton_classes)
+    app.add_css_file("mystnb.css")
+    # We use `execute` here instead of `jupyter-execute`
+    app.add_directive("execute", JupyterCell)
+    app.setup_extension("jupyter_sphinx")
+
+    return {"version": __version__, "parallel_read_safe": True}
---
+++ packages/myst-nb/MyST_NB-0.1.0-py3-none-any.whl/myst_nb/parser.py
@@ -0,0 +1,148 @@
+from docutils import nodes
+import nbformat as nbf
+
+from myst_parser.docutils_renderer import SphinxRenderer, dict_to_docinfo
+from myst_parser.block_tokens import Document
+from myst_parser.sphinx_parser import MystParser
+from jupyter_sphinx.ast import get_widgets, JupyterWidgetStateNode
+from jupyter_sphinx.execute import contains_widgets
+
+
+class NotebookParser(MystParser):
+    """Docutils parser for IPynb + CommonMark + Math + Tables + RST Extensions """
+
+    supported = ("ipynb",)
+    translate_section_name = None
+
+    default_config = {"known_url_schemes": None}
+
+    config_section = "ipynb parser"
+    config_section_dependencies = ("parsers",)
+
+    def parse(self, inputstring, document):
+        self.reporter = document.reporter
+        self.config = self.default_config.copy()
+        try:
+            new_cfg = document.settings.env.config.myst_config
+            self.config.update(new_cfg)
+        except AttributeError:
+            pass
+
+        ntbk = nbf.reads(inputstring, nbf.NO_CONVERT)
+
+        # Parse notebook-level metadata as front-matter
+        # For now, only keep key/val pairs that point to int/float/string
+        metadata = ntbk.metadata
+        docinfo = dict_to_docinfo(metadata)
+        document += docinfo
+
+        # If there are widgets, this will embed the state of all widgets in a script
+        if contains_widgets(ntbk):
+            document.append(JupyterWidgetStateNode(state=get_widgets(ntbk)))
+        renderer = SphinxRenderer(document=document, current_node=None)
+        with renderer:
+            # Loop through cells and render them
+            for ii, cell in enumerate(ntbk.cells):
+                # Skip empty cells
+                if len(cell["source"]) == 0:
+                    continue
+                try:
+                    _render_cell(cell, renderer)
+                except Exception as exc:
+                    source = cell["source"][:50]
+                    if len(cell["source"]) > 50:
+                        source = source + "..."
+                    msg_node = self.reporter.error(
+                        (
+                            f"\nError parsing notebook cell #{ii+1}: {exc}\n"
+                            f"Type: {cell['cell_type']}\n"
+                            f"Source:\n{source}\n\n"
+                        )
+                    )
+                    msg_node += nodes.literal_block(cell["source"], cell["source"])
+                    renderer.current_node += [msg_node]
+                    continue
+
+
+def _render_cell(cell, renderer):
+    """Render a cell with a SphinxRenderer instance.
+
+    Returns nothing because the renderer updates itself.
+    """
+    tags = cell.metadata.get("tags", [])
+    if "remove_cell" in tags:
+        return
+
+    # If a markdown cell, simply call the Myst parser and append children
+    if cell["cell_type"] == "markdown":
+        document = Document.read(cell["source"], front_matter=False)
+        # Check for tag-specific behavior because markdown isn't wrapped in a cell
+        if "hide_input" in tags:
+            container = nodes.container()
+            container["classes"].extend(["toggle"])
+            with renderer.current_node_context(container, append=True):
+                renderer.render(document)
+        else:
+            renderer.render(document)
+
+    # If a code cell, convert the code + outputs
+    elif cell["cell_type"] == "code":
+        # Cell container will wrap whatever is in the cell
+        classes = ["cell"]
+        for tag in tags:
+            classes.append(f"tag_{tag}")
+        sphinx_cell = CellNode(classes=classes, cell_type=cell["cell_type"])
+        renderer.current_node += sphinx_cell
+        if "remove_input" not in tags:
+            cell_input = CellInputNode(classes=["cell_input"])
+            sphinx_cell += cell_input
+
+            # Input block
+            code_block = nodes.literal_block(text=cell["source"])
+            cell_input += code_block
+
+        # ==================
+        # Cell output
+        # ==================
+        if "remove_output" not in tags:
+            cell_output = CellOutputNode(classes=["cell_output"])
+            sphinx_cell += cell_output
+
+            outputs = CellOutputBundleNode(cell["outputs"])
+            cell_output += outputs
+
+
+class CellNode(nodes.container):
+    """Represent a cell in the Sphinx AST."""
+
+    def __init__(self, rawsource="", *children, **attributes):
+        super().__init__("", **attributes)
+
+
+class CellInputNode(nodes.container):
+    """Represent an input cell in the Sphinx AST."""
+
+    def __init__(self, rawsource="", *children, **attributes):
+        super().__init__("", **attributes)
+
+
+class CellOutputNode(nodes.container):
+    """Represent an output cell in the Sphinx AST."""
+
+    def __init__(self, rawsource="", *children, **attributes):
+        super().__init__("", **attributes)
+
+
+class CellOutputBundleNode(nodes.container):
+    """Represent a MimeBundle in the Sphinx AST, to be transformed later."""
+
+    def __init__(self, outputs, rawsource="", *children, **attributes):
+        self.outputs = outputs
+        super().__init__("", **attributes)
+
+
+class CellImageNode(nodes.image):
+    """An inline image that will output to an inline HTML image."""
+
+    def __init__(self, rawsource="", *children, **attributes):
+        super().__init__("", **attributes)
