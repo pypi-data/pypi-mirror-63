d1dd86b47106fd8db5747225215ffa0e84ac2943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:13 +0000

    Add package sterm/6.0.1/sterm-6.0.1.tar.gz

---
+++ packages/sterm/sterm-6.0.1.tar.gz/sterm-6.0.1/sterm/cli.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# STERM, a serial communication terminal with server capabilities        #
+# Copyright (C) 2013-2020  Ralf Stemmer (ralf.stemmer@gmx.net)           #
+#                                                                        #
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+
+import time
+import argparse
+import termios
+import sys
+import tty
+from threading      import Thread
+from sterm.uart     import UART, UARTMode
+from sterm.terminal import Terminal
+
+
+
+VERSION = "6.0.1"
+
+
+# This global variable is used to shutdown the thread used
+# for the ReceiveData function.
+ShutdownReceiver = False
+
+
+cli = argparse.ArgumentParser(
+    description="A minimal serial ternimal.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+cli.add_argument(      "--binary",      default=False,                action="store_true",
+    help="Display raw data instead of UTF-8 encoded. (read only)")
+cli.add_argument("-n", "--noecho",      default=False,                action="store_true",
+    help="Direct character transmission. Does not echo the user input to stdout.")
+cli.add_argument(      "--escape",      default="\033",     type=str, action="store",
+    help="Change the default escape character (escape)")
+cli.add_argument("-b", "--baudrate",    default=115200,     type=int, action="store",
+    help="The baudrate used for the communication.")
+cli.add_argument("-f", "--format",      default="8N1",      type=str, action="store",
+    help="Configuration-triple: xyz with x=bytelength in bits {5,6,7,8}, y=parity {N,E,O}, z=stopbits {1,2}.")
+cli.add_argument("-w", "--write",       metavar="logfile",  type=str, action="store",
+    help="Write received data into a file")
+cli.add_argument("device",                                  type=str, action="store",
+    help="Path to the serial communication device.")
+
+ESCAPECHAR  = "\033"     # Escape character to start an escape command sequence
+
+
+def ReceiveData(uart, term):
+    """
+    This function reads every 0.01 seconds all data from the serial input buffer.
+    The read data then gets printed to the screen (stdout).
+    After writing to the output buffer, the buffer gets flushed so that the data is visible to the user
+    as soon as possible.
+    The function is blocking and runs until the global variable ``ShutdownReveiver`` get set to ``False``
+    or when reading from the input buffer raises an exception.
+
+    This function is intended to run in a separate thread.
+    The following example shows how to handle this function.
+
+    .. code-block::
+
+        # Start receiver thread
+        ReceiverThread = Thread(target=ReceiveData, args=(uart, term))
+        ReceiverThread.start()
+
+        # …
+
+        # Shutdown receiver thread
+        ShutdownReceiver = True
+        if ReceiverThread.isAlive():
+            ReceiverThread.join()
+
+
+    Args:
+        uart: Instance of the ``UART`` class.
+        term: Instance of the ``Terminal`` class.
+
+
+    Returns:
+        *Nothing*
+    """
+
+    data = ""
+    while not ShutdownReceiver:
+
+        string = uart.Receive()
+        if string:
+            term.Write(string)
+        time.sleep(0.01)
+
+
+
+def ReadCommand(term):
+    """
+    This function reads an escape sequence.
+    Independent if the terminal mode is ``echo = False``,
+    all entered keys will be echoed.
+
+    Returns:
+        A command string
+    """
+    char    = ""
+    command = ""
+    if not term.echo:
+        term.Write("␛") # Print ESC Unicode character then user hits escape key
+
+    while True:
+        char = term.ReadCharacter(echo=True)
+        if char == "\r":
+            break
+        elif char == ESCAPECHAR:
+            if len(command) == 0:
+                command = ESCAPECHAR
+            break
+        else:
+            command += char
+
+    #if not term.echo:
+    #    term.Write("\n")
+    return command
+
+
+
+def HandleUserInput(uart, term):
+    r"""
+    This function handles the user input.
+    It reads the input from *stdin* byte by byte and sends it directly UTF-8 encoded to the UART device.
+
+    When the escape character gets entered, the function starts to record a command instead of sending
+    the data to the UART device.
+    The escape character is defined in the global variable ``ESCAPECHAR`` that can be set via the
+    command line parameter ``--escape``.
+    The default character is *escape* (``\e``).
+    Valid escape commands are ``exit`` to leave this function and exit ``sterm``,
+    or ``version`` to print the version number of ``sterm`` to *stdout*.
+    Enter the escape character twice send one escape character to the UART device.
+
+    This function takes care the ``"\r\n"`` sequences and ``"\n"``-only line breaks are handled correctly.
+    Currently, it always send ``"\r\n"``
+
+    The function expects UTF-8 encoded input.
+
+    Args:
+        uart: Instance of the ``UART`` class.
+        term: Instance of the ``Terminal`` class.
+
+    Returns:
+        *Nothing*
+    """
+    char = ""
+
+    while True:
+        char = term.ReadCharacter()
+
+        # Handle escape sequences
+        if char == ESCAPECHAR:
+            command = ReadCommand(term)
+
+            if command == ESCAPECHAR:
+                uart.Transmit(ESCAPECHAR.encode("utf-8"))
+
+            if command == "exit":
+                break
+
+            elif command == "version":
+                print("Version: " + VERSION, end="\r\n")
+
+        # Send character to UART-Device
+        else:
+            # In this mode, \n needs to be added manually to get a new line
+            if char == "\r":
+                char += "\n"
+            uart.Transmit(char)
+
+    return
+
+
+
+def main():
+    print("\n\033[1;31m --[ \033[1;34msterm \033[1;31m//\033[1;34m " + VERSION + "\033[1;31m ]-- \033[0m\n")
+
+    # Handle command line arguments
+    args       = cli.parse_args()
+    global ESCAPECHAR
+    ESCAPECHAR = args.escape
+
+    if args.binary:
+        uartmode = UARTMode.BINARY
+    else:
+        uartmode = UARTMode.TEXT
+
+    # Setup local terminal
+    stdinfd          = sys.stdin.fileno()
+    oldstdinsettings = termios.tcgetattr(stdinfd)
+    tty.setraw(stdinfd) # from now on, end-line must be "\r\n"
+
+    # Open remote terminal device
+    try:
+        uart = UART(args.device, args.baudrate, args.format, uartmode=uartmode, logpath=args.write)
+    except Exception as e:
+        print("Connection to device %s failed with exception \"%s\""%(args.device, str(e)), file=sys.stderr)
+        termios.tcsetattr(stdinfd, termios.TCSADRAIN, oldstdinsettings)
+        exit(1)
+
+    term = Terminal(echo= not args.noecho, escape=args.escape)
+
+    # Start receiver thread
+    ReceiverThread = Thread(target=ReceiveData, args=(uart, term))
+    ReceiverThread.start()
+
+    # this is the main loop of this software
+    try:
+        HandleUserInput(uart, term);
+    except Exception as e:
+        print("sterm exits after following error occurred: \"%s\""%(args.device, str(e)), file=sys.stderr)
+        termios.tcsetattr(stdinfd, termios.TCSADRAIN, oldstdinsettings)
+        exit(1)
+
+    # Shutdown receiver thread
+    global ShutdownReceiver
+    ShutdownReceiver = True
+    if ReceiverThread.isAlive():
+        ReceiverThread.join()
+
+    # Clean up everything
+    uart.Disconnect()
+    termios.tcsetattr(stdinfd, termios.TCSADRAIN, oldstdinsettings)
+
+
+if __name__ == '__main__':
+    main()
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
---
+++ packages/sterm/sterm-6.0.1.tar.gz/sterm-6.0.1/sterm/terminal.py
@@ -0,0 +1,144 @@
+# STERM, a serial communication terminal                                 #
+# Copyright (C) 2013-2019  Ralf Stemmer (ralf.stemmer@gmx.net)           #
+#                                                                        #
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+
+import sys
+
+
+class Terminal(object):
+    r"""
+    This class handles the Terminal I/O for seamless read/write access to remote shells.
+
+    This class expects the terminal in *raw mode*!
+    In raw mode, the enter key is represented by ``"\r"`` and new-line by ``"\r\n"``.
+    This class takes care of this situation.
+
+    When ``echo`` is ``True``, then all entered character will be printed to *stdout*.
+    Otherwise entering a character will be invisible when not echoed by the connected device.
+    When the ``escape`` character is entered, ``"␛"`` gets printed to *stdout*.
+
+    Args:
+        echo (bool): Enable or disable printing the character that got pressed by the user on the keyboard. Default is ``True``
+        escape (str): A special character used for escape sequences. Default is ``"\033"``
+
+    Raises:
+        TypeError: When echo is not of type ``bool`` and escape not of type ``str``.
+        ValueError: When escape has more than one character.
+    """
+    def __init__(self, *, echo=True, escape="\033"):
+        if type(echo) is not bool:
+            raise TypeError("Type of echo-argument must be bool!")
+
+        if type(escape) is not str:
+            raise TypeError("Type of escape-argument must be string!")
+        if len(escape) != 1:
+            raise ValueError("Escape-argument should only contain one single character!")
+
+        self.echo    = echo
+        self.escape  = escape
+        self.stdinfd = sys.stdin.fileno()
+
+
+
+    def ReadLine(self):
+        r"""
+        This method reads a whole line and returns it.
+        The end-of-line character (``\n``, ``\r``, ``\r\n``) are not included.
+
+        Returns:
+            A string the user entered until the return key got hit. The line-break is not included in the returned string.
+        """
+        string = ""
+
+        while True:
+            char = self.ReadCharacter()
+            if char == "\n":  # Ignore \n - should never appear in raw mode
+                continue
+            elif char == "\r":
+                break
+            else:
+                string += char
+
+        return string
+
+
+
+    def ReadCharacter(self, echo=None):
+        r"""
+        This method returns one single character of the users input.
+
+        Keep in mind that in raw mode, enter only produces an \r, not an \n
+
+        If echo mode is enabled, the entered character gets printed to *stdout*.
+        The echo mode of an instance of this class can be overwritten temporary by setting the ``echo`` parameter to ``True`` or ``False``.
+        (Default is ``None`` which means, that the echo mode defined on instance creation is used)
+
+        Args:
+            echo (bool): (Optional) Enable or disable echo mode temporary
+
+        Returns:
+            One singe character of type ``str``.
+
+        Raises:
+            TypeError: When the ``echo`` parameter is not ``None`` and not a boolean.
+        """
+        if echo == None:
+            echo = self.echo
+
+        if type(echo) is not bool:
+            raise TypeError("echo argument must be a boolean or None.")
+
+        char = sys.stdin.read(1)
+        if echo:
+            if char == self.escape:
+                self.Write("␛") # Print ESC Unicode character then user hits escape key
+            else:
+                self.Write(char)
+        return char
+
+
+
+    def Write(self, string):
+        r"""
+        This method handles the output and adopts the line ending to the terminal configuration
+
+        In raw mode line ending must be ``\r\n``.
+        When an input of an enter (only ``\r``) shall be echoed, an additional ``\n`` needs to be written.
+
+        This method takes care that there is always ``\r\n``, independent if only ``\r`` or ``\n`` is used as line break in the ``string`` argument.
+
+        Returns:
+            *Nothing*
+
+        Raises:
+            TypeError: When ``type(string) is not str``. The argument to this method must always be a string.
+        """
+        if type(string) is not str:
+            raise TypeError("Argument for Terminal.Write must be a string! Actual type was %s.", str(type(string)))
+
+        # In the raw input mode (no echo), the output also expects an explicit \r
+        # This is because of the changed, none-default settings of the TTY
+        # Here I take care that the \r\n sequence is correct
+        string = string.replace("\n", "\r\n").replace("\r\r", "\r")
+        string = string.replace("\r", "\r\n").replace("\n\n", "\n")
+
+        sys.stdout.write(string)
+        sys.stdout.flush()
+        return
+
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
