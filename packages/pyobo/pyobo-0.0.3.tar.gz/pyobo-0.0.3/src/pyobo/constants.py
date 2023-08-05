d9d87822556e0746fd0f901e97d226fe86150502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:35 +0000

    Add package pyobo/0.0.3/pyobo-0.0.3.tar.gz

---
+++ packages/pyobo/pyobo-0.0.3.tar.gz/pyobo-0.0.3/src/pyobo/constants.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Constants for PyOBO."""
+
+import os
+
+__all__ = [
+    'PYOBO_HOME',
+    'OUTPUT_DIRECTORY',
+]
+
+PYOBO_HOME = os.environ.get('PYOBO_HOME') or os.path.join(os.path.expanduser('~'), '.obo')
+
+OUTPUT_DIRECTORY = (
+    os.environ.get('PYOBO_OUTPUT')
+    or os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'obo'))
+)
