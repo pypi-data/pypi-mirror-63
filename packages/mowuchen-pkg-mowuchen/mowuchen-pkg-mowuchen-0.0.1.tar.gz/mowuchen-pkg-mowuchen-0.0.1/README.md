c9072aad33c2c5559a9fbaa5a1fa3ecc6175a6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:43 +0000

    Add package mowuchen-pkg-mowuchen/0.0.1/mowuchen-pkg-mowuchen-0.0.1.tar.gz

---
+++ packages/mowuchen-pkg-mowuchen/mowuchen-pkg-mowuchen-0.0.1.tar.gz/mowuchen-pkg-mowuchen-0.0.1/README.md
@@ -0,0 +1,3 @@
+#mowuchen-pkg
+
+Tis project provide a demo for package and distributing python application.
---
+++ packages/mowuchen-pkg-mowuchen/mowuchen-pkg-mowuchen-0.0.1.tar.gz/mowuchen-pkg-mowuchen-0.0.1/mowuchen_pkg/main.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+ 
+print "hello, this is a package demo by rs"
+ 
+ 
+def max_number(a, b):
+    if a > b:
+        return a
+    else:
+        return b
