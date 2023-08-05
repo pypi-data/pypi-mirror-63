4fe628c66a96929377b6997ab784e4d0b9ea7fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package ubermagutil/0.2.5/ubermagutil-0.2.5.tar.gz

---
+++ packages/ubermagutil/ubermagutil-0.2.5.tar.gz/ubermagutil-0.2.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ubermagutil',
+    version='0.2.5',
+    description=('Python utilities package used across '
+                 'most of Ubermag packages.'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://ubermag.github.io',
+    author='Marijan Beg and Hans Fangohr',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['numpy',
+                      'pytest'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English']
+)
