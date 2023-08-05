391dc3c636255386975edf1696a078ae0cfbd18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package mkm/0.8.10/mkm-0.8.10.tar.gz

---
+++ packages/mkm/mkm-0.8.10.tar.gz/mkm-0.8.10/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Ming-Ke-Ming
+    ~~~~~~~~~~~~
+
+    Common Identity Module for decentralized user identity authentication
+"""
+
+from setuptools import setup, find_packages
+
+__version__ = '0.8.10'
+__author__ = 'Albert Moky'
+__contact__ = 'albert.moky@gmail.com'
+
+with open('README.md', 'r') as fh:
+    readme = fh.read()
+
+setup(
+    name='mkm',
+    version=__version__,
+    url='https://github.com/dimchat/mkm-py',
+    license='MIT',
+    author=__author__,
+    author_email=__contact__,
+    description='A common identity module',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        '': ['res/*.js']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'base58',  # 1.0.3
+        'Crypto',  # pycryptodome==3.7.3
+        'numpy',   # 1.15.4
+    ]
+)
