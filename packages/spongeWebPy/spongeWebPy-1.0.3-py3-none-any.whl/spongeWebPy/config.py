92df289da18c100a0163c0286be8a6b804be21d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:51 +0000

    Add package spongeWebPy/1.0.3/spongeWebPy-1.0.3-py3-none-any.whl

---
+++ packages/spongeWebPy/spongeWebPy-1.0.3-py3-none-any.whl/spongeWebPy/config.py
@@ -0,0 +1,9 @@
+"""
+Setup the basic connection things
+"""
+
+#set the base url of the SPONGE-web API
+api_url_base = 'http://10.162.163.20:5000/'
+
+#set up the HTTP request headers the way the API docs describe
+headers = {'Content-Type': 'application/json'}
