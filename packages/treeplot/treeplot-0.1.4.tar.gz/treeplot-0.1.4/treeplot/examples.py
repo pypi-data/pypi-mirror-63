9b2deda92d8496bf28b6fc12cd0d0073623087fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package treeplot/0.1.4/treeplot-0.1.4.tar.gz

---
+++ packages/treeplot/treeplot-0.1.4.tar.gz/treeplot-0.1.4/treeplot/examples.py
@@ -0,0 +1,34 @@
+import treeplot
+
+
+# %% Make example dataset
+X,y = treeplot.import_example()
+
+
+# %% RF EXAMPLE
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(max_depth=2, random_state=0).fit(X, y)
+
+ax = treeplot.randomforest(model)
+
+
+# %% RF EXAMPLE
+from sklearn.ensemble import RandomForestClassifier
+model_rf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
+
+ax = treeplot.randomforest(model_rf, export='png')
+# pdf
+ax = treeplot.randomforest(model_rf, export='pdf')
+
+
+# %% EXAMPLE XGBOOST
+from xgboost import XGBClassifier
+model_xgb = XGBClassifier(n_estimators=100, max_depth=2, random_state=0).fit(X, y)
+
+ax = treeplot.xgboost(model_xgb)
+ax = treeplot.xgboost(model_xgb, plottype='vertical')
+
+
+# %% Auto detect model
+ax = treeplot.plot(model_xgb)
+ax = treeplot.plot(model_rf)
