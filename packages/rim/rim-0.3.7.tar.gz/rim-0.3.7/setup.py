8860c14bfeca426c8589b3f1579a975eb794108c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package rim/0.3.7/rim-0.3.7.tar.gz

---
+++ packages/rim/rim-0.3.7.tar.gz/rim-0.3.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='rim',
+     version='0.3.7',
+     author="Ivan Majic",
+     author_email="imajicos@gmail.com",
+     description="A package for calculating RIM between three spatial objects",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/SelfHealingMapsProject/RIM",
+     packages=setuptools.find_packages(),
+     install_requires=['numpy==1.17.4', 'Shapely==1.6.4'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    python_requires='>=3.6',
+ )
