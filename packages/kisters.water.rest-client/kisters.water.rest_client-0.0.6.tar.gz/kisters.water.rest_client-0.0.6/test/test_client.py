0950fd528c33552db725c313d345d06fdee6b70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:57 +0000

    Add package kisters.water.rest-client/0.0.6/kisters.water.rest_client-0.0.6.tar.gz

---
+++ packages/kisters.water.rest-client/kisters.water.rest_client-0.0.6.tar.gz/kisters.water.rest_client-0.0.6/test/test_client.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+
+from kisters.water.rest_client import RESTClient
+from kisters.water.rest_client.auth import OpenIDConnect
+
+client_id = os.environ["HYDRAULIC_NETWORK_CLIENT_ID"]
+client_secret = os.environ["HYDRAULIC_NETWORK_CLIENT_SECRET"]
+service_url = os.environ["HYDRAULIC_NETWORK_STORE_URL"]
+auth = OpenIDConnect(client_id, client_secret)
+
+
+@pytest.fixture
+def json_client():
+    return RESTClient(url=service_url, authentication=auth)
+
+
+def test_get_str(json_client):
+    networks = json_client.get("rest/networks")
+    assert isinstance(networks, list)
+
+
+def test_get_iter(json_client):
+    networks = json_client.get(["rest", "networks"])
+    assert isinstance(networks, list)
