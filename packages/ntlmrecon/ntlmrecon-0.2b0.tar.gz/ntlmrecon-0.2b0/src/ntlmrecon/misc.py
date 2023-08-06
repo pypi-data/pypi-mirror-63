8ae24027949b4b33dc8f854a0fca4963a7b35635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package ntlmrecon/0.2b0/ntlmrecon-0.2b0.tar.gz

---
+++ packages/ntlmrecon/ntlmrecon-0.2b0.tar.gz/ntlmrecon-0.2b0/src/ntlmrecon/misc.py
@@ -0,0 +1,60 @@
+from termcolor import colored
+
+
+def print_banner():
+    print(colored("""
+         _   _ _____ _     ___  _________                     
+        | \ | |_   _| |    |  \/  || ___ \                    
+        |  \| | | | | |    | .  . || |_/ /___  ___ ___  _ __  
+        | . ` | | | | |    | |\/| ||    // _ \/ __/ _ \| '_ \ 
+        | |\  | | | | |____| |  | || |\ \  __/ (_| (_) | | | |
+        \_| \_/ \_/ \_____/\_|  |_/\_| \_\___|\___\___/|_| |_|
+
+             """ + colored("""v.0.2 beta - Y'all still exposing NTLM endpoints?
+""", 'green'), 'red'))
+
+INTERNAL_WORDLIST = [
+    "/abs",
+    "/aspnet_client/",
+    "/Autodiscover",
+    "/Autodiscover/AutodiscoverService.svc/root",
+    "/Autodiscover/Autodiscover.xml",
+    "/AutoUpdate/",
+    "/CertEnroll/",
+    "/CertProv",
+    "/CertSrv/",
+    "/Conf/",
+    "/dialin",
+    "/ecp/",
+    "/Etc/",
+    "/EWS/",
+    "/Exchange/",
+    "/Exchweb/",
+    "/GroupExpansion/",
+    "/HybridConfig",
+    "/iwa/authenticated.aspx",
+    "/iwa/iwa_test.aspx",
+    "/mcx",
+    "/meet",
+    "/Microsoft-Server-ActiveSync/",
+    "/OAB/",
+    "/ocsp/",
+    "/owa/",
+    "/PersistentChat",
+    "/PhoneConferencing/",
+    "/PowerShell/",
+    "/Public/",
+    "/Reach/sip.svc",
+    "/RequestHandler/",
+    "/RequestHandlerExt",
+    "/RequestHandlerExt/",
+    "/Rgs/",
+    "/RgsClients",
+    "/Rpc/",
+    "/RpcWithCert/",
+    "/scheduler",
+    "/Ucwa",
+    "/UnifiedMessaging/",
+    "/WebTicket",
+    "/WebTicket/WebTicketService.svc",
+]
