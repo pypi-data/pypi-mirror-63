5ba970619d301775d583ff474fb2f0b09dff2cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:43 +0000

    Add package rawls/0.1.0/rawls-0.1.0.tar.gz

---
+++ packages/rawls/rawls-0.1.0.tar.gz/rawls-0.1.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup
+import setuptools.command.build_py
+
+class BuildTestCommand(setuptools.command.build_py.build_py):
+    """Custom build command."""
+
+    def run(self):
+
+        # run tests using doctest
+        import doctest
+        
+        # filters folder
+        from rawls import classes
+        from rawls import converter
+        from rawls import merger
+
+        print("==============================")
+        print("Runs test command...")
+
+        # pass test using doctest
+        doctest.testmod(classes)
+        doctest.testmod(converter)
+        doctest.testmod(merger)
+
+        setuptools.command.build_py.build_py.run(self)
+
+
+setup(
+    name='rawls',
+    version='0.1.0',
+    description='RAW Light Simulation file reader/converter package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities'
+    ],
+    url='https://github.com/prise-3d/rawls',
+    author='Jérôme BUISINE',
+    author_email='jerome.buisine@univ-littoral.fr',
+    license='MIT',
+    packages=['rawls', 'rawls.classes'],
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'scipy',
+    ],
+    cmdclass={
+        'build_py': BuildTestCommand,
+    },
+    zip_safe=False)
