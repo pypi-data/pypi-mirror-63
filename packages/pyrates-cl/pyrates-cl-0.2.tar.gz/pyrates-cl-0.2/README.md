4b5651bfb4f89a680eac579264a7f9c750637352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:08 +0000

    Add package pyrates-cl/0.2/pyrates-cl-0.2.tar.gz

---
+++ packages/pyrates-cl/pyrates-cl-0.2.tar.gz/pyrates-cl-0.2/README.md
@@ -0,0 +1,16 @@
+PyRates
+=============
+PyRates supports conversion between 54 currency rates
+-------
+Can be used as:
+- Package/Module
+- CLI
+- GUI
+
+The rates are fetched by webscraping x-rates.com and saved locally
+
+In order to keep get-requests to a minimum, rates are only updated if they are older than 30 minutes
+
+The base currency is by default: Euro. Work in progress to let the user change the base currency at will
+
+# [Documentation](https://github.com/Lindeneg/pyrates-cl/tree/0.2/docs)
---
+++ packages/pyrates-cl/pyrates-cl-0.2.tar.gz/pyrates-cl-0.2/pyrates/util/url_manager.py
@@ -0,0 +1,29 @@
+from typing import Mapping
+from time import sleep
+
+from requests import get, RequestException, Response
+
+from pyrates.util.constants import Types, Constants
+from pyrates.logger.logger import mLogger
+
+
+def URLManager(url: str, header: Mapping[str, str], mSleep: int = 8, rJSON: bool = False) -> Types.URLResponse:
+    """
+    Manages HTTP/HTTPS connections
+
+        Returns:
+                result (URLResponse)  : Returns response content on success
+    """
+    e: RequestException
+    try:
+        req: Response = get(url=url, params=header)
+    except RequestException as e:
+        mLogger.critical(e)
+        raise RequestException(e)
+    status: int = req.status_code
+    if status == 200:
+        if rJSON:
+            return req.json()
+        return req.content
+    mLogger.critical(f"URLManager: non-200 response: {req}")
+    raise Exception("PyRatesURLError: Failed communication with server. Check '%s/%s' for further inspection." % (Constants.logPath, Constants.logFileName))
