6f70a6a845eb75e55d82cb03818e6ae15abb149a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package cognite-extractor-utils/0.11.0/cognite_extractor_utils-0.11.0-py3-none-any.whl

---
+++ packages/cognite-extractor-utils/cognite_extractor_utils-0.11.0-py3-none-any.whl/cognite/extractorutils/_inner_util.py
@@ -0,0 +1,7 @@
+"""
+A module containing utilities meant for use inside the extractor-utils package
+"""
+
+
+def _resolve_log_level(level: str) -> int:
+    return {"NOTSET": 0, "DEBUG": 10, "INFO": 20, "WARNING": 30, "ERROR": 40, "CRITICAL": 50}[level.upper()]
