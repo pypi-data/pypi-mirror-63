a359add05e94de5ce57845483bc6fc008f5793bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:59 +0000

    Add package eloqua-python-sdk/0.3.2/eloqua-python-sdk-0.3.2.tar.gz

---
+++ packages/eloqua-python-sdk/eloqua-python-sdk-0.3.2.tar.gz/eloqua-python-sdk-0.3.2/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# PYPI UPLOAD METHOD:
+# python setup.py sdist
+
+long_description = '''
+Eloqua Python SDK wrapping Eloqua Rest API and Eloqua BULK API.
+'''
+
+setup(
+    name='eloqua-python-sdk',
+    version='0.3.2',
+    author='Ignotas Petrulis',
+    author_email='ignotas.petrulis@gmail.com',
+    packages=['eloquasdk'],
+    install_requires=[
+        'requests'
+    ],
+    description='Eloqua Python SDK',
+    long_description=long_description
+)
