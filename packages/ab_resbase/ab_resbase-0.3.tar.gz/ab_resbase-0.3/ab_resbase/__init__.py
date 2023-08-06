3d45265738abb92fa0674cb0f2834310fb4090f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:23 +0000

    Add package ab_resbase/0.3/ab_resbase-0.3.tar.gz

---
+++ packages/ab_resbase/ab_resbase-0.3.tar.gz/ab_resbase-0.3/ab_resbase/__init__.py
@@ -0,0 +1,21 @@
+import random
+def start(min=0,max=1,maxdate=10):
+	restart(min,max,maxdate)
+def restart(min=0,max=1,maxdate=10):	
+	_min_=min
+	_max_=max
+	_maxdate_=maxdate
+	_data_=[]
+	_Interval_=(_max_-_min_)/_maxdate_
+	for i in range(maxdate):
+		_data_.append(float("%2.3f"%(i*_Interval_)))
+	ans=_data_[random.randint(0,len(_data_))]
+def guess(data):
+	if type(data) == type(_data_[0]):
+		if data in _data_:
+
+			if data!=ans:
+				_data_.remove(data)
+				_Interval_=float("%2.3f"%((_max_-_min_)/len(_data_)))
+			return _Interval_
+	return None
---
+++ packages/ab_resbase/ab_resbase-0.3.tar.gz/ab_resbase-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ab_resbase',
+      version='0.3',
+      description='clinical trial information retriver',
+      url='http://github.com/tongling/ab_resbase',
+      author='Ling',
+      author_email='an520999@gmail.com',
+      license='MIT',
+      packages=['ab_resbase'],
+      zip_safe=False)
