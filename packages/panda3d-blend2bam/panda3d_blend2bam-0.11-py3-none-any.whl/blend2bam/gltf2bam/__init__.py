9c38f930851712b6dcd1bb933775c5262e483901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:19 +0000

    Add package panda3d-blend2bam/0.11/panda3d_blend2bam-0.11-py3-none-any.whl

---
+++ packages/panda3d-blend2bam/panda3d_blend2bam-0.11-py3-none-any.whl/blend2bam/gltf2bam/__init__.py
@@ -0,0 +1,40 @@
+import json
+import os
+import sys
+
+import panda3d.core as p3d
+
+from blend2bam.common import ConverterBase
+
+class ConverterGltf2Bam(ConverterBase):
+    def convert_single(self, src, dst):
+        scriptdir = os.path.dirname(os.path.abspath(__file__))
+        sys.path.insert(0, os.path.join(scriptdir, 'panda3d-gltf'))
+        import gltf.converter #pylint: disable=import-error
+
+        dstdir = os.path.dirname(dst)
+        os.makedirs(dstdir, exist_ok=True)
+
+        settings = gltf.GltfSettings(
+            physics_engine=self.settings.physics_engine,
+            skip_axis_conversion=True,
+            no_srgb=self.settings.no_srgb,
+            textures=self.settings.textures,
+        )
+        gltf.converter.convert(src, dst, settings)
+
+        binfname = dst.replace('.bam', '.bin')
+        if os.path.exists(binfname):
+            os.remove(binfname)
+
+    def convert_batch(self, srcroot, dstdir, files):
+        for gltffile in files:
+            src = gltffile
+            dst = src.replace(str(srcroot), str(dstdir))
+
+            if self.settings.append_ext:
+                dst = dst.replace('.gltf', '.blend.bam')
+            else:
+                dst = dst.replace('.gltf', '.bam')
+
+            self.convert_single(src, dst)
