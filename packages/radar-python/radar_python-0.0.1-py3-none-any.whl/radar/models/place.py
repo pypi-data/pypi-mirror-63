e3cd6451c38a92a7c9d8cb598dfd43610be91fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:45 +0000

    Add package radar-python/0.0.1/radar_python-0.0.1-py3-none-any.whl

---
+++ packages/radar-python/radar_python-0.0.1-py3-none-any.whl/radar/models/place.py
@@ -0,0 +1,10 @@
+from .model import Model
+
+
+class Place(Model):
+    OBJECT_NAME = "Place"
+    _DISPLAY_ATTRIBUTES = (
+        "_id",
+        "name",
+        "categories",
+    )
---
+++ packages/radar-python/radar_python-0.0.1-py3-none-any.whl/radar/models/region.py
@@ -0,0 +1,20 @@
+from .model import Model
+
+
+class Region(Model):
+    OBJECT_NAME = "Region"
+    _DISPLAY_ATTRIBUTES = (
+        "type",
+        "name",
+        "code",
+    )
+
+
+class Regions(Model):
+    OBJECT_NAME = "Regions"
+    _DISPLAY_ATTRIBUTES = (
+        "country",
+        "state",
+        "dma",
+        "postalCode",
+    )
