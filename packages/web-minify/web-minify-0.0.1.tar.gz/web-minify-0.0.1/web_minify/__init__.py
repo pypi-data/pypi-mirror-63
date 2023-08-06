de559e67f8fba4e9410b4bf6ffb0089d65236d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:45 +0000

    Add package web-minify/0.0.1/web-minify-0.0.1.tar.gz

---
+++ packages/web-minify/web-minify-0.0.1.tar.gz/web-minify-0.0.1/web_minify/__init__.py
@@ -0,0 +1,24 @@
+import os
+import pkg_resources
+from .files import read_file
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+__version__ = "0.0.0"
+version_filename = "VERSION"
+
+latigo_path: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+latigo_version_file = f"{latigo_path}/{version_filename}"
+
+if pkg_resources.resource_exists(__name__, version_filename):
+    __version__ = (
+        pkg_resources.resource_string(__name__, version_filename).decode("utf-8").strip()
+    )
+elif os.path.exists(latigo_version_file):
+    __version__ = read_file(latigo_version_file)
+else:
+    logger.warning("No version found")
+    pass
+
