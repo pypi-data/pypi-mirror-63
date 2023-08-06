84a4f6a15722b1de0326844757b747150bd97fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:23 +0000

    Add package hyh-probability/0.1/hyh_probability-0.1.tar.gz

---
+++ packages/hyh-probability/hyh_probability-0.1.tar.gz/hyh_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hyh_probability',
+      version='0.1',
+      description='Gaussian , Binomial distributions ',
+      packages=['hyh_probability'],
+      author = 'YehangHan',
+      author_email = 'test@gmail.com',
+      zip_safe=False)
