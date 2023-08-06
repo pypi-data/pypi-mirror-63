5278bc7cd1489c462f0d204828e3c711616bc945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:58 +0000

    Add package async-lichess-sdk/1.1.0.1/async_lichess_sdk-1.1.0.1.tar.gz

---
+++ packages/async-lichess-sdk/async_lichess_sdk-1.1.0.1.tar.gz/async_lichess_sdk-1.1.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/amasend/lichees_python_SDK/issues',
+    'Documentation': 'https://amasend.github.io/lichees_python_SDK/html/index.html',
+    'Source Code': 'https://github.com/amasend/lichees_python_SDK'
+}
+
+setuptools.setup(
+    name="async_lichess_sdk",
+    version=version,
+    author="Amaedeusz Masny",
+    author_email="amadeuszmasny@gmail.com",
+    description="Asynchronous Python API client for accessing the lichess.org API.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/amasend/lichees_python_SDK",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    license="Apache Software License",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
