1958b1d0d7a8524654912d25b21e1e3012d07050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:17 +0000

    Add package mobile-balance/0.8.0/mobile-balance-0.8.0.tar.gz

---
+++ packages/mobile-balance/mobile-balance-0.8.0.tar.gz/mobile-balance-0.8.0/mobile_balance/mts.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+import re
+
+from .exceptions import BadResponse
+from .utils import check_status_code
+
+
+def get_balance(number, password):
+    session = requests.Session()
+
+    response = session.get('https://login.mts.ru/amserver/UI/Login')
+    check_status_code(response, 401)
+
+    csrf_token = re.search(r'name="csrf.sign" value="(.*?)"', response.content)
+    csrf_ts_token = re.search(r'name="csrf.ts" value="(.*?)"', response.content) #Второй токен
+
+    if csrf_token is None:
+        raise BadResponse('CSRF token not found', response)
+
+    csrf_token = csrf_token.group(1)
+    csrf_ts_token = csrf_ts_token.group(1) #Второй токен
+
+    response = session.post('https://login.mts.ru/amserver/UI/Login',
+                      data={'IDToken1': number,
+                            'IDToken2': password,
+                            'csrf.sign': csrf_token,
+                            'csrf.ts': csrf_ts_token, #Второй токен
+                        },
+                      headers={
+                          'Accept-Language': 'ru,en;q=0.8',
+                      })
+    check_status_code(response, 200)
+
+    response = session.get('https://oauth.mts.ru/webapi-1.4/customers/@me')
+
+    check_status_code(response, 200)
+
+    data = response.json()
+    relations = data['genericRelations']
+    targets = [rel['target'] for rel in relations]
+    accounts = [target for target in targets if target['@c'] == '.Account']
+
+    if not accounts:
+        raise RuntimeError('Account not found in the data response')
+
+    balance = accounts[0].get('balance')
+
+    if balance is None:
+        raise BadResponse('Unable to get balance from JSON', response)
+
+    return float(balance)
---
+++ packages/mobile-balance/mobile-balance-0.8.0.tar.gz/mobile-balance-0.8.0/tasks.py
@@ -0,0 +1,22 @@
+from invoke import run, task
+
+@task
+def release(c):
+    run('git push')
+    run('git push --tags')
+    run('python setup.py register -r pypi')
+    run('python setup.py sdist upload -r pypi')
+    run('python setup.py bdist_wheel upload -r pypi')
+
+
+@task
+def serve_docs(c):
+    from livereload import Server, shell
+
+    build_command = 'sphinx-build -b html docs dist/docs'
+    run(build_command)
+
+    server = Server()
+    server.watch('*.rst', shell(build_command))
+    server.watch('docs/', shell(build_command))
+    server.serve(root='dist/docs')
