9583e78246d5e96bb9d10d0e48ddff8cb3cc92c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:30 +0000

    Add package CVrail/1.9.2/CVrail-1.9.2.tar.gz

---
+++ packages/CVrail/CVrail-1.9.2.tar.gz/CVrail-1.9.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='CVrail',
+    version='1.9.2',
+    author='Simão Duarte',
+    author_email='maravilha1sapo@hotmail.com',
+    url='https://gitlab.com/simaoduarte/cvrail',
+    description="Detecta a movimentação nos taludes ferroviários",
+    long_description='Tirar fotos detetar diferenças'
+)
+
