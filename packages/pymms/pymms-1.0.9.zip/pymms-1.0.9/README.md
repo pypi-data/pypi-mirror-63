a12d3b050a160b996f3ce7001310433b684ba4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:25 +0000

    Add package pymms/1.0.9/pymms-1.0.9.zip

---
+++ packages/pymms/pymms-1.0.9.zip/pymms-1.0.9/README.md
@@ -0,0 +1,1 @@
+This is demonstration code for simulation to effects of special relativity.
---
+++ packages/pymms/pymms-1.0.9.zip/pymms-1.0.9/pymms/__init__.py
@@ -0,0 +1,8 @@
+"""
+mms - The Minkovski Metric Simulation
+Special Reletivity Software
+mms is a software package that can be used to simulate of the Minkovski metric. It uses UML2 SP language to non-numeric modelingwarnings.simplefilter(action='ignore', category=FutureWarning)
+"""
+
+__author__ = 'Vasiliy I.Gurianov'
+__version__ = '1.0.9'
