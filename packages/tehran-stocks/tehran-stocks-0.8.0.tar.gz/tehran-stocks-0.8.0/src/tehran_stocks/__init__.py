c26551784cfd3d21755f2d0504f2334aae7385fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:58 +0000

    Add package tehran-stocks/0.8.0/tehran-stocks-0.8.0.tar.gz

---
+++ packages/tehran-stocks/tehran-stocks-0.8.0.tar.gz/tehran-stocks-0.8.0/src/tehran_stocks/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+import tehran_stocks.config as db
+from tehran_stocks.download import (
+    get_all_price,
+    get_stock_detail,
+    get_stock_groups,
+    get_stock_ids,
+    update_group,
+    update_stock_price,
+)
+from tehran_stocks.models import StockPrice, Stocks, get_asset
+
+from .initializer import init_db, fill_db
+
+if not os.path.isfile(db.db_path):
+    print("No database founded.")
+    init_db()
+    fill_db()
