2d19a091e9186ed5b5573db138ae0ec7ce6f85e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package mnistlib/0.0.5/mnistlib-0.0.5.tar.gz

---
+++ packages/mnistlib/mnistlib-0.0.5.tar.gz/mnistlib-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'mnistlib',
+    version = '0.0.5',
+    description = 'geometric transformations for mnist dataset',
+    license = 'MIT',
+    packages = ['mnistlib'],
+    author = 'Oleg Zyablov',
+    author_email = 'oleg.z98@mail.ru',
+    keywords = ['mnist'],
+    url='https://github.com/sedol1339/SkillFactory/tree/master/Module%201/mnistlib%20python%20module/dist/mnistlib-0.0.3.tar.gz',
+    install_requires = [
+        'mnist', 'matplotlib', 'Pillow'
+    ],
+    include_package_data = True
+)
