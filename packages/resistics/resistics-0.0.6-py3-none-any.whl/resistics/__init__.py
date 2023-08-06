2f0c6360b9e2e586d172b7424553c848197851a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package resistics/0.0.6/resistics-0.0.6-py3-none-any.whl

---
+++ packages/resistics/resistics-0.0.6-py3-none-any.whl/resistics/__init__.py
@@ -0,0 +1,14 @@
+"""A package for the processing of magnetotelluric data
+
+resistics is a package for the robust processing of magnetotelluric data. It includes several features focussed on traceability and data investigation. For more information, visit the package website at:
+
+www.resistics.io
+"""
+
+__name__ = "resistics"
+# short X.Y version
+xyversion = "0.0.6"
+# release
+release = ""
+# combined version
+__version__ = "{}{}".format(xyversion, release)
