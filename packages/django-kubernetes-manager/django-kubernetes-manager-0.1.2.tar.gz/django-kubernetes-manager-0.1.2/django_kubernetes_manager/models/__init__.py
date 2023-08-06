a9c393f83f2b5a3d91590aade010d5dad6630da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:41 +0000

    Add package django-kubernetes-manager/0.1.2/django-kubernetes-manager-0.1.2.tar.gz

---
+++ packages/django-kubernetes-manager/django-kubernetes-manager-0.1.2.tar.gz/django-kubernetes-manager-0.1.2/django_kubernetes_manager/models/__init__.py
@@ -0,0 +1,6 @@
+from .base import (KubernetesContainer, KubernetesDeployment,
+                   KubernetesIngress, KubernetesJob, KubernetesNamespace,
+                   KubernetesPodTemplate, KubernetesService, KubernetesVolume,
+                   KubernetesVolumeMount, KubernetesNamespace, KubernetesBase,
+                   KubernetesMetadataObjBase, KubernetesNetworkingBase)
+from .target_cluster import TargetCluster
