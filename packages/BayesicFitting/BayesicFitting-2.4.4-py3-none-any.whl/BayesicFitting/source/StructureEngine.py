6a118aee7144891e88909f59ba7a4264c113ae63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:47 +0000

    Add package BayesicFitting/2.4.4/BayesicFitting-2.4.4-py3-none-any.whl

---
+++ packages/BayesicFitting/BayesicFitting-2.4.4-py3-none-any.whl/BayesicFitting/source/StructureEngine.py
@@ -0,0 +1,156 @@
+import numpy as numpy
+
+from .Modifiable import Modifiable
+from .Engine import Engine
+from . import Tools
+
+__author__ = "Do Kester"
+__year__ = 2018
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Do"
+__status__ = "Development"
+
+#  *
+#  * This file is part of the BayesicFitting package.
+#  *
+#  * BayesicFitting is free software: you can redistribute it and/or modify
+#  * it under the terms of the GNU Lesser General Public License as
+#  * published by the Free Software Foundation, either version 3 of
+#  * the License, or ( at your option ) any later version.
+#  *
+#  * BayesicFitting is distributed in the hope that it will be useful,
+#  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  * GNU Lesser General Public License for more details.
+#  *
+#  * The GPL3 license can be found at <http://www.gnu.org/licenses/>.
+#  *
+#  *    2019 - 2020 Do Kester
+
+class StructureEngine( Engine ):
+    """
+    The StructureEngine varies the internal structure of the model.
+
+    Only for Models that are Modifiable.
+
+    The member is kept when the logLikelihood > lowLhood.
+
+    Attributes
+    ----------
+    None of its own
+
+    Attributes from Engine
+    ----------------------
+    walkers, errdis, maxtrials, rng, report, unitRange, unitMin, verbose
+
+    Author       Do Kester.
+
+    """
+
+    #  *********CONSTRUCTORS***************************************************
+    def __init__( self, walkers, errdis, copy=None, seed=23455, verbose=0 ) :
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        walkers : list of Walker
+            walkers to be diffused
+        errdis : ErrorDistribution
+            error distribution to be used
+        copy : StructureEngine
+            to be copied
+        seed : int
+            for random number generator
+
+        """
+        super( StructureEngine, self ).__init__( walkers, errdis, copy=copy,
+                    seed=seed, verbose=verbose )
+
+    def copy( self ):
+        """ Return copy of this.  """
+        return StructureEngine( self.walkers, self.errdis, copy=self )
+
+    def __str__( self ):
+        return str( "StructureEngine" )
+
+    #  *********EXECUTE***************************************************
+    def execute( self, walker, lowLhood ):
+        """
+        Execute the engine by changing a component.
+
+        Parameters
+        ----------
+        walker : Sample
+            walker to diffuse
+        lowLhood : float
+            lower limit in logLikelihood
+
+        Returns
+        -------
+        int : the number of successfull moves
+
+        """
+        t = 0
+        k = 0
+        while t <= walker.problem.model.ncomp and k < self.maxtrials :
+            dt = self.executeOnce( walker, lowLhood )
+            if dt > 0 :
+                t += dt
+                k = 0
+            else :
+                k += 1
+
+        return t
+
+    def executeOnce( self, walker, lowLhood ) :
+        """
+        One execution call.
+        """
+        self.reportCall()
+
+        cwalker = walker.copy()                  ## work on local copy.
+        problem = cwalker.problem
+        ptry = cwalker.allpars
+        ftry = cwalker.fitIndex
+
+        if self.verbose > 4 :
+            print( "SEN0  ", walker.id, walker.parent, len( ptry ), len( ftry ) )
+
+        off = 0
+        model = problem.model
+        while model is not None and not isinstance( model, Modifiable ) :
+            off += model.npbase
+            model = model._next
+
+        if self.verbose > 4 :
+            nc = model.ncomp
+            np = model.npbase
+            print( "SEN1  ", cwalker.id, nc, np, len( ptry ), len( ftry ) )
+
+        if not model.vary( rng=self.rng ):
+#            print( "SEN2  ", "failed" )
+            self.reportReject()
+            return 0
+
+        Ltry = self.errdis.logLikelihood( problem, ptry )
+
+        if Ltry >= lowLhood:
+            self.reportSuccess()
+            self.setWalker( cwalker, problem, ptry, Ltry, fitIndex=ftry )
+            wlkr = self.walkers[walker.id]
+
+#            Tools.printclass( walker )
+            wlkr.check( nhyp=self.errdis.nphypar )
+
+            ## check if better than Lbest in walkers[-1]
+            self.checkBest( problem, ptry, Ltry, ftry )
+
+#            return model.npbase
+            return 1
+
+        self.reportFailed()
+        return 0
+
+
---
+++ packages/BayesicFitting/BayesicFitting-2.4.4-py3-none-any.whl/BayesicFitting/test/TestDynamicModel.py
@@ -0,0 +1,459 @@
+#run with: python3 -m unittest TestCompoundModel
+
+import unittest
+import numpy as numpy
+from astropy import units
+import math
+
+from StdTests import stdModeltest
+
+from BayesicFitting import *
+from BayesicFitting import formatter as fmt
+
+__author__ = "Do Kester"
+__year__ = 2018
+__license__ = "GPL3"
+__version__ = "0.9"
+__maintainer__ = "Do"
+__status__ = "Development"
+
+#  * This file is part of the BayesicFitting package.
+#  *
+#  * BayesicFitting is free software: you can redistribute it and/or modify
+#  * it under the terms of the GNU Lesser General Public License as
+#  * published by the Free Software Foundation, either version 3 of
+#  * the License, or ( at your option ) any later version.
+#  *
+#  * BayesicFitting is distributed in the hope that it will be useful,
+#  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  * GNU Lesser General Public License for more details.
+#  *
+#  * The GPL3 license can be found at <http://www.gnu.org/licenses/>.
+#  *
+#  *    2016 Do Kester
+
+class TestDynamicModel( unittest.TestCase ):
+    """
+    Test harness for Fitter class.
+
+    Author:      Do Kester
+
+    """
+
+    def testDynamic( self ) :
+        print( "  Test Dynamic" )
+        dyn = Dynamic( dynamic=False )
+        self.assertFalse( dyn.isDynamic() )
+
+        dyn = Dynamic(  )
+        self.assertTrue( dyn.isDynamic() )
+
+        par = numpy.arange( 8, dtype=float ) * 0.1
+        print( "par      ", fmt( par, max=None ) )
+        par = dyn.alterParameters( par, 3, 1, 2, value=1.0 )
+        print( "par      ", fmt( par, max=None ) )
+        par = dyn.alterParameters( par, 4, -1, 2 )
+        print( "par      ", fmt( par, max=None ) )
+        par = dyn.alterParameters( par, 6, 2, 2, value=[1.0,2.0] )
+        print( "par      ", fmt( par, max=None ) )
+        par = dyn.alterParameters( par, 8, -1, 2 )
+        print( "par      ", fmt( par, max=None ) )
+        par = dyn.alterParameters( par, 6, -1, 2 )
+        print( "par      ", fmt( par, max=None ) )
+
+        fin = [0,1,2,3,4,5,-2,-1]
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 3, 1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 3, -1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 4, 1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 5, -1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+
+        fin = [0,2,3,4,5,-2,-1]
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 2, 1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 2, -1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 3, 1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+        fin = dyn.alterFitindex( fin, 4, -1, 2 )
+        print( "fin      ", fmt( fin, max=None ) )
+
+
+    def test1Model1( self ):
+        print( "  Test PolynomialDynamicModel" )
+        m = PolynomialDynamicModel( 0 )
+        self.growshrink( m, dnp=1 )
+
+    def test1Model2( self ):
+        print( "  Test HarmonicDynamicModel" )
+        m = HarmonicDynamicModel( 1 )
+        self.growshrink( m, dnp=2 )
+
+    def test1Model3( self ):
+        print( "  Test RepeatingModel 3" )
+        m = GaussModel()
+        r = RepeatingModel( 1, m  )
+        rc = r.copy()
+
+        self.growshrink( rc, dnp=3 )
+
+    def test1Model4( self ):
+        print( "  Test RepeatingModel 4" )
+        m = GaussModel()
+        r = RepeatingModel( 1, m, same=[2]  )
+        self.assertTrue( isinstance( r.growPrior, ExponentialPrior ) )
+
+        rc = r.copy()
+        self.assertTrue( isinstance( rc.growPrior, ExponentialPrior ) )
+
+        self.assertTrue( rc.same[0] == 2 )
+        self.assertTrue( len( rc.same ) == 1 )
+
+        self.assertTrue( rc.index[0] == 0 )
+        self.assertTrue( rc.index[1] == 1 )
+        self.assertTrue( len( rc.index ) == 2 )
+
+        Tools.printclass( r )
+
+        self.assertTrue( r.grow() )
+        self.assertTrue( r.npbase == 5 )
+        self.assertTrue( r.shrink() )
+        self.assertTrue( r.npbase == 3 )
+        r.shrink()
+        self.assertTrue( r.npbase == 0 )
+        r.grow()
+        self.assertTrue( r.npbase == 3 )
+
+
+        rc.grow()
+        rc.grow()
+        self.assertTrue( rc.npbase == 7 )
+        self.assertTrue( rc.ncomp == 3 )
+
+    def test1Model5( self ):
+        print( "  Test RepeatingModel 6" )
+        m = GaussModel()
+        r = RepeatingModel( 1, m, same=2, minComp=1, maxComp=3  )
+
+        pars = numpy.asarray( [1.0, -0.4, 0.1] )
+        self.assertTrue( isinstance( r.growPrior, UniformPrior ) )
+        self.assertTrue( r.grow() )
+        self.assertTrue( r.npbase == 5 )
+
+        pars = numpy.asarray( [1.0, -0.4, 0.1, 0.5, 0.0] )
+        self.assertTrue( r.grow() )
+        self.assertTrue( r.npchain == 7 )
+
+        pars = numpy.asarray( [1.0, -0.4, 0.1, 0.5, 0.0, 0.3, 0.4] )
+        self.assertFalse( r.grow() )
+        self.assertTrue( r.npbase == 7 )
+
+        pars = numpy.asarray( [1.0, -0.4, 0.1, 0.5, 0.0, 0.3, 0.4] )
+
+        ## location is removed in BirthEngine, normally
+        del( r.location )
+        stdModeltest( r, pars )
+
+
+        r.parameters = pars
+        print( r.parameters )
+        self.assertTrue( r.shrink() )
+        self.assertTrue( r.npbase == 5 )
+        print( r.parameters )
+
+        self.assertTrue( r.shrink() )
+        self.assertTrue( r.npbase == 3 )
+        print( r.parameters )
+
+        self.assertFalse( r.shrink() )
+        self.assertTrue( r.npbase == 3 )
+        print( r.parameters )
+
+    def test1Model6( self ):
+        print( "  Test RepeatingModel 6" )
+        m = GaussModel()
+        r = RepeatingModel( 1, m, same=[2], minComp=1, maxComp=1  )
+        self.assertFalse( r.isDynamic() )
+        self.assertFalse( r.grow() )
+        self.assertTrue( r.npbase == 3 )
+        self.assertFalse( r.shrink() )
+        self.assertTrue( r.npbase == 3 )
+
+        p = RepeatingModel( 1, m, same=[2], dynamic=False  )
+        self.assertFalse( p.isDynamic() )
+        self.assertFalse( p.grow() )
+        self.assertTrue( p.npbase == 3 )
+        self.assertFalse( p.shrink() )
+        self.assertTrue( p.npbase == 3 )
+
+    def test1Model7( self ):
+        print( "  Test RepeatingModel 7" )
+        m = GaussModel()
+        r = RepeatingModel( 1, m, minComp=1, maxComp=3  )
+
+        self.assertTrue( isinstance( r.growPrior, UniformPrior ) )
+        self.assertTrue( r.grow() )
+        self.assertTrue( r.npbase == 6 )
+        self.assertTrue( r.grow() )
+        self.assertTrue( r.npchain == 9 )
+
+        pars = numpy.asarray( [1.0, 0.1, 0.01, 2.0, 0.2, 0.02, 3.0, 0.3, 0.03] )
+
+        r.parameters = pars
+        print( r.parameters )
+        self.assertTrue( r.shrink() )
+        self.assertTrue( r.npbase == 6 )
+        print( r.parameters )
+        self.assertTrue( r.shrink() )
+        self.assertTrue( r.npbase == 3 )
+        print( r.parameters )
+
+
+    def test1Model8( self ):
+        print( "  Test RepeatingModel 8" )
+        m = GaussModel()
+        r = RepeatingModel( 3, m )
+
+        pars = numpy.arange( 9, dtype=float )
+        rng = numpy.random.RandomState( 12345 )
+        pars = r.shuffle( pars, 0, 9, rng )
+        print( pars )
+        p0 = pars[0]
+        for k,p in enumerate( pars[1:] ) :
+            self.assertTrue( p == p0+1 or k%3 == 2 )
+            p0 = p
+
+        pars = numpy.arange( 12, dtype=float )
+        rng = numpy.random.RandomState( 12345 )
+        pars = r.shuffle( pars, 2, 9, rng )
+        print( pars )
+        self.assertTrue( pars[0]==0 and pars[1]==1 and pars[-1]==11 )
+
+    def test1Model9( self ):
+        print( "  Test RepeatingModel 9" )
+        m = GaussModel()
+        r = RepeatingModel( 3, m, same=1 )
+
+        pars = numpy.array( [0,1,2,3,5,6,8], dtype=float )
+        rng = numpy.random.RandomState( 12345 )
+        pars = r.shuffle( pars, 0, 7, rng )
+        print( pars )
+        self.assertTrue( pars[1] == 1 )
+        self.assertTrue( pars[2] == pars[0]+2 )
+        self.assertTrue( pars[4] == pars[3]+2 )
+        self.assertTrue( pars[6] == pars[5]+2 )
+
+
+        pars = numpy.array( [0,1,2,3,4,5,7,8,10,11], dtype=float )
+        rng = numpy.random.RandomState( 12345 )
+        pars = r.shuffle( pars, 2, 7, rng )
+        print( pars )
+        self.assertTrue( pars[0]==0 and pars[1]==1 and pars[-1]==11 )
+        self.assertTrue( pars[3] == 3 )
+        self.assertTrue( pars[4] == pars[2]+2 )
+        self.assertTrue( pars[6] == pars[5]+2 )
+        self.assertTrue( pars[8] == pars[7]+2 )
+
+    def growshrink( self, m, dnp=1 ) :
+        self.assertTrue( m.npchain == dnp )
+        self.assertTrue( m.npbase == dnp )
+
+        Tools.printclass( m )
+
+        m.grow()
+        m.grow()
+        m.grow()
+
+        print( m.npchain, m.npbase, m.npmax )
+        self.assertTrue( m.npchain == 4 * dnp )
+        self.assertTrue( m.npbase == 4 * dnp )
+
+        m.grow()
+        m.shrink()
+
+        Tools.printclass( m )
+        self.assertTrue( m.npchain == 4 * dnp )
+        self.assertTrue( m.npbase == 4 * dnp )
+
+        m.shrink()
+        self.assertTrue( m.npchain == 3 * dnp )
+        self.assertTrue( m.npbase == 3 * dnp )
+
+
+    def test2Model( self ):
+        print( "========================" )
+        print( "  Test two models" )
+        print( "========================" )
+
+        self.assertRaises( ValueError, PolynomialDynamicModel, 0, minDegree=2, maxDegree=4 )
+
+        m = GaussModel( )
+        p = PolynomialDynamicModel( 1, minDegree=1, maxDegree=4 )
+
+        print( m.isDynamic(), p.isDynamic() )
+
+        print( p.growPrior )
+        m += p
+
+        self.assertTrue( m.isDynamic() )
+        self.assertFalse( isinstance( m, Dynamic ) )
+        self.assertTrue( isinstance( m._next, Dynamic ) )
+
+        lolim = [-10, -1, 0, -10]
+        hilim = [+10, +1, 10, 10]
+        m.setLimits( lowLimits=lolim, highLimits=hilim )
+        Tools.printclass( m )
+        Tools.printclass( p )
+
+        par = numpy.asarray( [1,-0.2,0.3,-1.2,0.2] )
+        x = numpy.linspace( -1.0, 1.0, 11 )
+        y = numpy.linspace( 0.0, 2.3, 11 )
+
+        problem = ClassicProblem( model=m, xdata=x, ydata=y )
+
+        print( problem.myEngines() )
+        print( problem.myStartEngine() )
+        print( problem.myDistribution() )
+
+        self.assertTrue( "galilean" in problem.myEngines() )
+        self.assertTrue( "birth" in problem.myEngines() )
+        self.assertTrue( "death" in problem.myEngines() )
+
+        errdis = GaussErrorDistribution( limits=[0.01,100] )
+
+        allpars = numpy.append( par, [1.0] )
+        fitIndex = [0,1,2,3,4,-1]
+        sl = WalkerList( problem, 10, allpars, fitIndex )
+
+        print( m.npchain, m.npbase, m._next.npbase, len( allpars ) )
+        print( "====================" )
+
+        seng = StartEngine( sl, errdis )
+        for s in sl :
+            print( "Walker  ", s.id, s.problem.npars, s.problem.model.npars, s.problem.model.npchain )
+            seng.execute( s, 0.0 )
+
+        logl = sl.getLogLikelihoodEvolution()
+        lowl = numpy.min( logl )
+        print( "logl  ", fmt( logl, max=None ) )
+        print( "lowl  ", fmt( lowl ) )
+
+        beng = BirthEngine( sl, errdis )
+        deng = DeathEngine( sl, errdis )
+        print( "start  np ", m.npchain )
+        for s in sl :
+            Tools.printclass( s )
+            suc = beng.execute( s, lowl )
+            print( "Birth  ", suc, s.problem.npars, s.problem.model.npars, s.problem.model.npchain )
+            Tools.printclass( s )
+            suc = deng.execute( s, lowl )
+            print( "Death  ", suc, s.problem.npars, s.problem.model.npars, s.problem.model.npchain )
+
+
+    def testNestedSampler( self, plot=False ) :
+        print( "========================" )
+        print( "  Test Nested Sampler" )
+        print( "========================" )
+
+        numpy.random.seed( 1315 )
+        N = 21
+        x = numpy.linspace( -2.0, 3.0, N, dtype=float )
+        y = 1.2 + 0.5 * x + 0.33 * x * x + 0.27 * x * x * x
+        noise = numpy.random.randn( N )
+        y += 0.02 * noise
+
+        pm = PolynomialDynamicModel( 0 )
+        lolim = [-10]
+        hilim = [+10]
+        pm.setLimits( lowLimits=lolim, highLimits=hilim )
+        Tools.printclass( pm )
+
+        engines = None
+        ns = NestedSampler( x, pm, y, seed=2031967, engines=engines )
+
+        ns.distribution.setLimits( [0.01, 100] )
+
+        logE = ns.sample( plot=plot )
+
+    def plotNestedSampler2( self ) :
+        self.testNestedSampler2( plot=True )
+
+    def testNestedSampler2( self, plot=False ) :
+        print( "========================" )
+        print( "  Test Nested Sampler 2" )
+        print( "========================" )
+
+        numpy.random.seed( 1315 )
+        N = 40
+        x = numpy.arange( N, dtype=float ) / 10
+        y = [0, 1, 3, 4, 6, 7, 5, 8, 9, 2, -2,-9,-8,-5,-7,-6,-4,-3,-1, 0] * 2
+        y - numpy.asarray( y, dtype=float )
+#        y = numpy.linspace( 0.0, 4.0, N, dtype=float ) % 2
+        noise = numpy.random.randn( N )
+        y += 0.3 * noise
+
+        pm = HarmonicDynamicModel( 1, period=2.0 )
+        pm.growPrior.scale = 6
+
+        lolim = [-10]
+        hilim = [+10]
+        pm.setLimits( lowLimits=lolim, highLimits=hilim )
+        Tools.printclass( pm )
+
+        ns = NestedSampler( x, pm, y, seed=2031967 )
+
+        ns.distribution.setLimits( [0.01, 100] )
+#        ns.distribution.scale = 0.02
+
+        logE = ns.sample( plot=plot )
+        print( "scale  ", ns.scale )
+
+    def plotNestedSampler3( self ) :
+        self.testNestedSampler3( plot=True )
+
+    def testNestedSampler3( self, plot=False ) :
+        print( "========================" )
+        print( "  Test Nested Sampler 3" )
+        print( "========================" )
+
+        numpy.random.seed( 1315 )
+        N = 200
+        x = numpy.arange( N, dtype=float ) / 40
+        y = 0.1 * numpy.random.randn( N )
+        gm = GaussModel()
+        for k in range( 1, 6 ) :
+            par = [5.0/k, k-0.5, 0.1]
+            y += gm.result( x, par )
+
+        pm = RepeatingModel( 1, gm )
+        pm.growPrior.scale = 5
+        lolim = [0.0,0.0,0.0]
+        hilim = [+10,5.0,1.0]
+        pm.setLimits( lowLimits=lolim, highLimits=hilim )
+        Tools.printclass( pm )
+
+        ns = NestedSampler( x, pm, y, seed=2031967 )
+        ns.distribution.setLimits( [0.01, 100] )
+#        ns.distribution.scale = 0.02
+
+        logE = ns.sample( plot=plot )
+        sl = ns.samples
+        print( "maxlik   ", fmt( sl.maxLikelihoodParameters, max=None ) )
+        print( "median   ", fmt( sl.medianParameters, max=None ) )
+        print( "modus    ", fmt( sl.modusParameters, max=None ) )
+        print( "scale    ", fmt( ns.scale ) )
+
+
+    def suite( cls ):
+        return unittest.TestCase.suite( TestDynamicModel.__class__ )
+
+
+if __name__ == '__main__':
+    unittest.main()
+
