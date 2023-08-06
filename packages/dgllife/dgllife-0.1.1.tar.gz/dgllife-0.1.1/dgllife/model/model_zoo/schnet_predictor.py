d80e8f7cc9a79b97a60332f7a3359cbee7327e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:16 +0000

    Add package dgllife/0.1.1/dgllife-0.1.1.tar.gz

---
+++ packages/dgllife/dgllife-0.1.1.tar.gz/dgllife-0.1.1/dgllife/model/model_zoo/schnet_predictor.py
@@ -0,0 +1,95 @@
+"""SchNet"""
+import numpy as np
+import torch.nn as nn
+
+from ..gnn import SchNetGNN
+from ..readout import MLPNodeReadout
+
+__all__ = ['SchNetPredictor']
+
+class ShiftedSoftplus(nn.Module):
+    r"""Applies the element-wise function:
+
+    .. math::
+        \text{SSP}(x) = \frac{1}{\beta} * \log(1 + \exp(\beta * x)) - \log(\text{shift})
+
+    Parameters
+    ----------
+    beta : int
+        :math:`\beta` value for the mathematical formulation. Default to 1.
+    shift : int
+        :math:`\text{shift}` value for the mathematical formulation. Default to 2.
+    """
+    def __init__(self, beta=1, shift=2, threshold=20):
+        super(ShiftedSoftplus, self).__init__()
+
+        self.shift = shift
+        self.softplus = nn.Softplus(beta=beta, threshold=threshold)
+
+    def forward(self, inputs):
+        """Applies the activation function.
+
+        Parameters
+        ----------
+        inputs : float32 tensor of shape (N, *)
+            * denotes any number of additional dimensions.
+
+        Returns
+        -------
+        float32 tensor of shape (N, *)
+            Result of applying the activation function to the input.
+        """
+        return self.softplus(inputs) - np.log(float(self.shift))
+
+class SchNetPredictor(nn.Module):
+    """SchNet for regression and classification on graphs.
+
+    SchNet is introduced in `SchNet: A continuous-filter convolutional neural network for
+    modeling quantum interactions <https://arxiv.org/abs/1706.08566>`__.
+
+    Parameters
+    ----------
+    node_feats : int
+        Size for node representations to learn. Default to 64.
+    hidden_feats : list of int
+        ``hidden_feats[i]`` gives the size of hidden representations for the i-th interaction
+        (gnn) layer. ``len(hidden_feats)`` equals the number of interaction (gnn) layers.
+        Default to ``[64, 64, 64]``.
+    classifier_hidden_feats : int
+        Size for hidden representations in the classifier. Default to 64.
+    n_tasks : int
+        Number of tasks, which is also the output size. Default to 1.
+    num_node_types : int
+        Number of node types to embed. Default to 100.
+    cutoff : float
+        Largest center in RBF expansion. Default to 30.
+    gap : float
+        Difference between two adjacent centers in RBF expansion. Default to 0.1.
+    """
+    def __init__(self, node_feats=64, hidden_feats=None, classifier_hidden_feats=64, n_tasks=1,
+                 num_node_types=100, cutoff=30., gap=0.1):
+        super(SchNetPredictor, self).__init__()
+
+        self.gnn = SchNetGNN(node_feats, hidden_feats, num_node_types, cutoff, gap)
+        self.readout = MLPNodeReadout(node_feats, classifier_hidden_feats, n_tasks,
+                                      activation=ShiftedSoftplus())
+
+    def forward(self, g, node_types, edge_dists):
+        """Graph-level regression/soft classification.
+
+        Parameters
+        ----------
+        g : DGLGraph
+            DGLGraph for a batch of graphs.
+        node_types : int64 tensor of shape (V)
+            Node types to embed, V for the number of nodes.
+        edge_dists : float32 tensor of shape (E, 1)
+            Distances between end nodes of edges, E for the number of edges.
+
+        Returns
+        -------
+        float32 tensor of shape (G, n_tasks)
+            Prediction for the graphs in the batch. G for the number of graphs.
+        """
+        node_feats = self.gnn(g, node_types, edge_dists)
+        return self.readout(g, node_feats)
