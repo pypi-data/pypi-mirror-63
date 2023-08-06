0dc0a6e6b251cd05e5c8417cc8f7516654261bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:11 +0000

    Add package rich/0.7.2/rich-0.7.2-py3-none-any.whl

---
+++ packages/rich/rich-0.7.2-py3-none-any.whl/rich/__init__.py
@@ -0,0 +1,28 @@
+from colorama import init
+
+from typing import Any, IO, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .console import Console
+
+# Global console used by alternative print
+_console: Optional["Console"] = None
+
+
+def print(
+    *objects: Any, sep=" ", end="\n", file: IO[str] = None, flush: bool = False,
+):
+    global _console
+    if _console is None:
+        from .console import Console
+
+        _console = Console(log_time=False)
+
+    write_console = _console if file is None else Console(file=file)
+    return write_console.print(*objects, sep=sep, end=end)
+
+
+init()
+
+if __name__ == "__main__":  # pragma: no cover
+    print("Hello, **World**")
