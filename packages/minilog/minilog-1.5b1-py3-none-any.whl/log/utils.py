4a769d0e35583f7c770d1ef56d6b3e7d9ac9c45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:34 +0000

    Add package minilog/1.5b1/minilog-1.5b1-py3-none-any.whl

---
+++ packages/minilog/minilog-1.5b1-py3-none-any.whl/log/utils.py
@@ -0,0 +1,44 @@
+"""Implements the "magic" to create `logging` records for the caller."""
+
+import inspect
+import logging
+import sys
+
+from . import filters, helpers, state
+
+
+def create_logger_record(level, message, *args, exc_info=None, **kwargs) -> bool:
+    if 'pytest' in sys.modules:
+        root = logging.getLogger()
+        filters.install(root)
+    elif not state.initialized:
+        helpers.init()
+
+    frame = inspect.currentframe().f_back.f_back.f_back  # type: ignore
+
+    module_name = kwargs.pop('module_name', frame.f_globals['__name__'])
+    parent_module_name = module_name.split('.')[0]
+
+    logger = logging.getLogger(module_name)
+    if not logger.level:
+        parent_logger = logging.getLogger(parent_module_name)
+        logger.level = parent_logger.level
+    if not logger.level:
+        logger.level = state.default_level
+
+    if not logger.isEnabledFor(level):
+        return False
+
+    record = logger.makeRecord(
+        module_name,
+        level,
+        fn=frame.f_globals['__file__'],
+        lno=frame.f_lineno,
+        msg=message,
+        args=args,
+        exc_info=exc_info,
+        extra=kwargs,
+        sinfo=None,
+    )
+    logger.handle(record)
+    return True
