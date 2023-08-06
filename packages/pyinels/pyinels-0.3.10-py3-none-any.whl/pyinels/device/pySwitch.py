6a45daeec88ff4abd7a14c108efc97bcb33b5fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:19 +0000

    Add package pyinels/0.3.10/pyinels-0.3.10-py3-none-any.whl

---
+++ packages/pyinels/pyinels-0.3.10-py3-none-any.whl/pyinels/device/pySwitch.py
@@ -0,0 +1,31 @@
+"""Inels switch class for iNels BUS."""
+
+from pyinels.const import (
+    ATTR_SWITCH_ON
+)
+
+
+class pySwitch:
+    """Switch class based on InelsDevice."""
+
+    def __init__(self, device):
+        """Initialize of object InelsSwitch."""
+        self.device = device
+
+    @property
+    def state(self):
+        """Return the state of the switch."""
+        if (self.device.value is None):
+            self.device.observe()
+
+        val = str(self.device.value)
+        attr = ATTR_SWITCH_ON
+
+        # it is expression of ternary operator
+        return (True if val == attr else False)
+
+    def toggle(self):
+        """Toogle the state of the switch."""
+        new_state = True if self.state is False else False
+
+        self.device.set_value(1 if new_state is True else 0)
