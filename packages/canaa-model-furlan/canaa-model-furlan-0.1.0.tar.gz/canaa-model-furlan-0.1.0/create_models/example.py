4e6af7d46f8b6faac6192aca89695d4b8e9fb684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:12 +0000

    Add package canaa-model-furlan/0.1.0/canaa-model-furlan-0.1.0.tar.gz

---
+++ packages/canaa-model-furlan/canaa-model-furlan-0.1.0.tar.gz/canaa-model-furlan-0.1.0/create_models/example.py
@@ -0,0 +1,30 @@
+def print_example(_testing_args):
+    example = """
+The file must be an CSV (separated by ; fields), UTF-8 without BOM encoding
+
+The first line defines the model names:
+    - Promax namespace
+    - Promax model name
+    - Microservice namespace
+    - Microservice model name
+
+The next lines defines the fields:
+    - Promax field name
+    - Promax field type (int, bool, string, date, datetime, time, float or classes)
+    - Microservice field name
+    - Microservice field type
+    - Extra informations: 'pk' for Primary Key, 'required' for required field
+
+promax_namespace.promax_model;microservice_namespace.microservice_model
+codigo_modelo;int;model_id;int;pk
+nome_pessoa;string;person_name;string;required
+data_nascimento;date;birth_date;date;
+ativo;bool;active;bool;
+cadastro;datetime;register;datetime
+taxa;float;rate;float
+descricao;DescricaoModel;description;DescriptionModel
+"""
+    print(example)
+    if _testing_args:
+        return True
+    exit(0)     # pragma: no cover
