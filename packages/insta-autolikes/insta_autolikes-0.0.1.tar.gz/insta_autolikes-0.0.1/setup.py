d9eecd969101779eaf9f71a993fdee3ca2b387d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:29 +0000

    Add package insta-autolikes/0.0.1/insta_autolikes-0.0.1.tar.gz

---
+++ packages/insta-autolikes/insta_autolikes-0.0.1.tar.gz/insta_autolikes-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='insta_autolikes',
+    version="0.0.1",
+    description="test autolikes instagram",
+    long_description="",
+    author='msywww',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
