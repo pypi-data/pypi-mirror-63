f78821c0e993f48bdef25b36809422af5600dd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:31 +0000

    Add package msftfdnepy/0.0.9/msftfdnepy-0.0.9.tar.gz

---
+++ packages/msftfdnepy/msftfdnepy-0.0.9.tar.gz/msftfdnepy-0.0.9/Microsoft/Finance/DataStandards/UnitTest/TestSuite/FactTestSuite.py
@@ -0,0 +1,20 @@
+from Microsoft.Finance.DataStandards.UnitTest.TestCase.CommonTestCase import CommonTestCase
+from Microsoft.Finance.DataStandards.UnitTest.TestCase.FactTestCase import FactTestCase
+from Microsoft.Finance.DataStandards.UnitTest.TestSuite.BaseTestSuite import BaseTestSuite
+from pyspark.sql.session import SparkSession
+
+class FactTestSuite(BaseTestSuite):
+
+    def __init__(self, sparkSession: SparkSession, databaseName: str, tableName: str, linkedDimensions: [(str, str)], excludeFromAggregation: [str] = []):
+        super(FactTestSuite, self).__init__(sparkSession, databaseName, tableName)
+        self.__linkedDimensions = linkedDimensions
+        self.__excludeFromAggregations = excludeFromAggregations
+
+    def queue_tests(self) -> None:
+        factTestCase = FactTestCase(self.SparkSession, self.TableDataFrame, self.FieldInfo, self.__linkedDimensions, self.DatabaseName, self.TableName, self.TableCount, self.CustomEvents, self.__excludeFromAggregations)
+        commonTestCase = CommonTestCase(self.TableDataFrame, self.FieldInfo, self.CustomEvents)
+        self.TestSuite.addTest(factTestCase)
+        self.TestSuite.addTest(commonTestCase)
+
+    def run_tests(self) -> None:
+        self.TestRunner.run(self.TestSuite)
