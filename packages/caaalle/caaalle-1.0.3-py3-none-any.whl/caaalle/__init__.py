4c8adb9a269c67da59286d9617567c8bd7894ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:02 +0000

    Add package caaalle/1.0.3/caaalle-1.0.3-py3-none-any.whl

---
+++ packages/caaalle/caaalle-1.0.3-py3-none-any.whl/caaalle/__init__.py
@@ -0,0 +1,17 @@
+"""
+caaalle
+~~~~~~
+
+The caaalle package.
+"""
+
+def add(initial: int=0, number: int=0) -> int:
+    """Return sum of *intial* and *number*.
+    :param initial: Initial value.
+    :type initial: int
+    :param number: Value to add to initial.
+    :type number: int
+    :return: Sum of initial and number.
+    :rtype: int
+    """
+    return initial + number
