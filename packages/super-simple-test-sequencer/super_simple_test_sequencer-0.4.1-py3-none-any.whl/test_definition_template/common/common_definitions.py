5615135e7595d5e692dc5eafb5f86ff3f4e43cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package super-simple-test-sequencer/0.4.1/super_simple_test_sequencer-0.4.1-py3-none-any.whl

---
+++ packages/super-simple-test-sequencer/super_simple_test_sequencer-0.4.1-py3-none-any.whl/test_definition_template/common/common_definitions.py
@@ -0,0 +1,8 @@
+"""Common definitions for all test sequencies"""
+
+# This DUT definition is used only if SN input comes from UI.
+# Otherwise DUT definition must come from prepare_test()
+DUTS = ["left", "right", "middle"]
+
+# Wait DUT serial number from UI
+SN_FROM_UI = True
