696ee5d61649a0c24e94b512dcdc6f1deaec1802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package cerami/0.1.2/cerami-0.1.2-py3-none-any.whl

---
+++ packages/cerami/cerami-0.1.2-py3-none-any.whl/cerami/datatype/expression/__init__.py
@@ -0,0 +1,5 @@
+from .base_expression import BaseExpression
+from .equality_expression import EqualityExpression
+from .arithmetic_expression import ArithmeticExpression
+from .list_append_expression import ListAppendExpression
+from .update_remove_expression import UpdateRemoveExpression
