a5fcad0f6c343fb00e26dee21fe88f647378a4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:03 +0000

    Add package distributions-jesus/0.1/distributions_jesus-0.1.tar.gz

---
+++ packages/distributions-jesus/distributions_jesus-0.1.tar.gz/distributions_jesus-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_jesus',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_jesus'],
+      author='Jesus Mira',
+      author_email='jesus.mira1@hotmail.com',
+      zip_safe=False)
