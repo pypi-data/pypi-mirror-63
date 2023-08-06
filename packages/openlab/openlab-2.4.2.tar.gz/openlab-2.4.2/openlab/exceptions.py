91183a36bf5d1fa603752d3189b80769fa9fb5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:15 +0000

    Add package openlab/2.4.2/openlab-2.4.2.tar.gz

---
+++ packages/openlab/openlab-2.4.2.tar.gz/openlab-2.4.2/openlab/exceptions.py
@@ -0,0 +1,47 @@
+class ValidationError(Exception):
+    """
+    Raised when OpenLab Simulator returns 422 error code
+    This happens when:
+    * config/sim setpoints are out of simulator boundaries
+    * Creating a configuration with name that already exists
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+class ConfigurationNotFound(Exception):
+    """
+    Raise when configuration is not found in OpenLab
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+class SimulationNotFound(Exception):
+    """
+    Raise when simulation is not found in OpenLab
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+class SimulationCompleted(Exception):
+    """
+    Raised when a running simulation is unexpectedly completed
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+class SimulationAborted(Exception):
+    """
+    Raised when a running simulation is unexpectedly aborted
+    """
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
