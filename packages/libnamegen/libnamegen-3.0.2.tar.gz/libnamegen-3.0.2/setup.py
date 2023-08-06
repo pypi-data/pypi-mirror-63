578cf031d7ebc681cdd723ff590eb549ff8df14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:53 +0000

    Add package libnamegen/3.0.2/libnamegen-3.0.2.tar.gz

---
+++ packages/libnamegen/libnamegen-3.0.2.tar.gz/libnamegen-3.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="libnamegen",
+    version="3.0.2",
+    author="BBaoVanC",
+    author_email="bbaovanc@protonmail.com",
+    description="Library containing name generation methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BBaoVanC/libnamegen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'libprogress',
+    ],
+    python_requires='>=3.6',
+)
