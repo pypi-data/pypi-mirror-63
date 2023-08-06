fe85572ffd846522056611b9669b1f675fea6ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:41 +0000

    Add package blackfynn/3.6.2/blackfynn-3.6.2-py2.py3-none-any.whl

---
+++ packages/blackfynn/blackfynn-3.6.2-py2.py3-none-any.whl/blackfynn/__init__.py
@@ -0,0 +1,35 @@
+from .config import Settings, DEFAULTS as DEFAULT_SETTINGS
+
+# main client
+from .client import Blackfynn
+
+# base models
+from .models import (
+    BaseNode,
+    Property,
+    Organization,
+    File,
+    DataPackage,
+    Collection,
+    LinkedModelProperty,
+    Model,
+    ModelFilter,
+    ModelJoin,
+    ModelProperty,
+    ModelSelect,
+    ModelTemplate,
+    Record,
+    RecordSet,
+    Dataset,
+    RelationshipType,
+    Relationship,
+    RelationshipSet,
+    Tabular,
+    TabularSchema,
+    TimeSeries,
+    TimeSeriesChannel,
+    TimeSeriesAnnotation,
+)
+
+__title__ = "blackfynn"
+__version__ = "3.6.2"
