bce193337663bc6b18ed6a48982a401782c7a23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:09 +0000

    Add package pynetworks/0.3.0/pynetworks-0.3.0.tar.gz

---
+++ packages/pynetworks/pynetworks-0.3.0.tar.gz/pynetworks-0.3.0/pynetworks/__init__.py
@@ -0,0 +1,10 @@
+'''
+pynetworks
+===============
+
+A package that provides structure for networks of interconnected nodes, using
+the DOT language for representation.
+'''
+
+__version__ = "0.3.0"
+from .core import Network, Connection, Node, graph
