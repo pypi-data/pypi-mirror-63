1345969d33d31719ca6aa1d9e074c0fc468b807a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:22 +0000

    Add package tabledata/0.10.4/tabledata-0.10.4.tar.gz

---
+++ packages/tabledata/tabledata-0.10.4.tar.gz/tabledata-0.10.4/setup.py
@@ -0,0 +1,90 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import io
+import os.path
+
+import setuptools
+
+
+MODULE_NAME = "tabledata"
+REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
+REQUIREMENT_DIR = "requirements"
+ENCODING = "utf8"
+
+pkg_info = {}
+
+
+def get_release_command_class():
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with io.open("README.rst", encoding=ENCODING) as f:
+    LONG_DESCRIPTION = f.read()
+
+with io.open(os.path.join("docs", "pages", "introduction", "summary.txt"), encoding=ENCODING) as f:
+    SUMMARY = f.read().strip()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    INSTALL_REQUIRES = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    TESTS_REQUIRES = [line.strip() for line in f if line.strip()]
+
+SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=pkg_info["__version__"],
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description=SUMMARY,
+    include_package_data=True,
+    keywords=["table"],
+    license=pkg_info["__license__"],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(exclude=["test*"]),
+    project_urls={
+        "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
+        "Source": REPOSITORY_URL,
+        "Tracker": "{:s}/issues".format(REPOSITORY_URL),
+    },
+    install_requires=SETUPTOOLS_REQUIRES + INSTALL_REQUIRES,
+    setup_requires=SETUPTOOLS_REQUIRES,
+    tests_require=TESTS_REQUIRES,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    extras_require={"logging": ["loguru>=0.4.1,<1"], "test": TESTS_REQUIRES},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    cmdclass=get_release_command_class(),
+)
---
+++ packages/tabledata/tabledata-0.10.4.tar.gz/tabledata-0.10.4/tabledata/__version__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+__author__ = "Tsuyoshi Hombashi"
+__copyright__ = "Copyright 2017, {}".format(__author__)
+__license__ = "MIT License"
+__version__ = "0.10.4"
+__maintainer__ = __author__
+__email__ = "tsuyoshi.hombashi@gmail.com"
