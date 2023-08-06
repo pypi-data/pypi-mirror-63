7e3b900f70b90c867a2e6cc7f9e64712032e64ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:04 +0000

    Add package kenpompy/0.2.1/kenpompy-0.2.1.tar.gz

---
+++ packages/kenpompy/kenpompy-0.2.1.tar.gz/kenpompy-0.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kenpompy",
+    version="0.2.1",
+    author="Jared Andrews",
+    author_email="jared.andrews07@gmail.com",
+    description="A python package for scraping kenpom.com NCAA basketball data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/j-andrews7/kenpompy",
+    packages=setuptools.find_packages(exclude=("tests", "docs")),
+    license="GNU GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers"
+    ],
+    install_requires = ["mechanicalsoup", "pandas", "bs4"],
+)
