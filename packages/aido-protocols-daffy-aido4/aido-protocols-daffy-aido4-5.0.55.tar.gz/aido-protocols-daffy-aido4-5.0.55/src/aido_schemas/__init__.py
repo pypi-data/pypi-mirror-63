eb5e650edc019d3d7ab41f17eefe685d0d883f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:43 +0000

    Add package aido-protocols-daffy-aido4/5.0.55/aido-protocols-daffy-aido4-5.0.55.tar.gz

---
+++ packages/aido-protocols-daffy-aido4/aido-protocols-daffy-aido4-5.0.55.tar.gz/aido-protocols-daffy-aido4-5.0.55/src/aido_schemas/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "5.0.55"
+
+from zuper_commons.logs import ZLogger
+
+logger = ZLogger(__name__)
+logger.info(f"aido-protocols {__version__}")
+
+from .protocols import *
+from .protocol_agent import *
+from .protocol_simulator import *
+from .schemas import *
+from .utils_leds import *
+from .utils_images import *
