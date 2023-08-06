c534219aa5fea960b027da5dae8f1b6982b5637a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:48 +0000

    Add package mloyalty/0.2.1/mloyalty-0.2.1.tar.gz

---
+++ packages/mloyalty/mloyalty-0.2.1.tar.gz/mloyalty-0.2.1/mloyalty/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client
+from .cheque import Cheque
+from .campaign import Campaign
+from .pos import POS
+from .operator import Operator
+
+__all__ = ['Client', 'Cheque', 'Campaign', 'POS', 'Operator']
