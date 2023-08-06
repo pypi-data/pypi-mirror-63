04446278d1a32e74f75565e888f82285d20c63b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package django-mp-shop-comparison/2.0/django-mp-shop-comparison-2.0.tar.gz

---
+++ packages/django-mp-shop-comparison/django-mp-shop-comparison-2.0.tar.gz/django-mp-shop-comparison-2.0/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+version = '2.0'
+url = 'https://github.com/pmaigutyak/mp-shop-comparison'
+
+
+setup(
+    name='django-mp-shop-comparison',
+    version=version,
+    description='Django shop comparison app',
+    long_description=open('README.md').read(),
+    author='Paul Maigutyak',
+    author_email='pmaigutyak@gmail.com',
+    url=url,
+    download_url='{}/archive/{}.tar.gz'.format(url, version),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT'
+)
