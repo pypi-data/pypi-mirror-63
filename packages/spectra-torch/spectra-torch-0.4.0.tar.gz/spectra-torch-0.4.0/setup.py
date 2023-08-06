da91f86c3f72d544551b271240991793cda842fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:25 +0000

    Add package spectra-torch/0.4.0/spectra-torch-0.4.0.tar.gz

---
+++ packages/spectra-torch/spectra-torch-0.4.0.tar.gz/spectra-torch-0.4.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+with open('requirements.txt', 'r') as fh:
+  requirements = fh.read().split('\n')
+
+setup(name='spectra-torch',
+      version='0.4.0',
+      author='WangRui',
+      author_email='rwang@tongji.edu.cn',
+      description='Spectra Extraction based on PyTorch',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/mechanicalsea/spectra',
+      packages=['spectra_torch'],
+      install_requires=requirements,
+      classifiers=[
+          "Programming Language :: Python :: 3.7",
+      ],
+)
+# python3 setup.py sdist build
+# python3 setup.py bdist_wheel --universal
+# twine upload dist/*
