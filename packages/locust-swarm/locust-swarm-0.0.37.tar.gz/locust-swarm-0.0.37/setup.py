6d179fd8e413f70af267661dec5da24d58d5fe37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:21 +0000

    Add package locust-swarm/0.0.37/locust-swarm-0.0.37.tar.gz

---
+++ packages/locust-swarm/locust-swarm-0.0.37.tar.gz/locust-swarm-0.0.37/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ast
+import re
+import os
+
+if os.name == "nt":
+    raise Exception("Looks like you are on windows. Only MacOS and Linux are supported :(")
+
+from setuptools import find_packages, setup
+
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+_init_file = "locust_swarm/__init__.py"
+with open(_init_file, "rb") as f:
+    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
+
+setup(
+    name="locust-swarm",
+    version=version,
+    description="Load test distribution tool for Locust (and JMeter)",
+    long_description="""https://github.com/SvenskaSpel/locust-swarm""",
+    classifiers=[
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+    ],
+    python_requires=">=3.6, <4",
+    keywords="",
+    author="Lars Holmberg",
+    url="https://github.com/SvenskaSpel/locust-swarm",
+    license="Apache-2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["locust-plugins", "psutil", "ConfigArgParse>=1.0"],
+    scripts=["bin/swarm"],
+)
