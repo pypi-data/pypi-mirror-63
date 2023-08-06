9535d482c49cad537caa395edf3003084a88597b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:18 +0000

    Add package thexp/1.1.12/thexp-1.1.12.tar.gz

---
+++ packages/thexp/thexp-1.1.12.tar.gz/thexp-1.1.12/thexp/utils/date/sampler.py
@@ -0,0 +1,40 @@
+"""
+    Copyright (C) 2020 Shandong University
+
+    This program is licensed under the GNU General Public License 3.0 
+    (https://www.gnu.org/licenses/gpl-3.0.html). 
+    Any derivative work obtained under this license must be licensed 
+    under the GNU General Public License as published by the Free 
+    Software Foundation, either Version 3 of the License, or (at your option) 
+    any later version, if this derivative work is distributed to a third party.
+
+    The copyright for the program is owned by Shandong University. 
+    For commercial projects that require the ability to distribute 
+    the code of this program as part of a program that cannot be 
+    distributed under the GNU General Public License, please contact 
+            
+            sailist@outlook.com
+             
+    to purchase a commercial license.
+"""
+
+from torch import randperm
+from torch._utils import _accumulate
+from torch.utils.data.dataset import Subset
+
+
+def random_split_with_indices(dataset, lengths,indices=None):
+    r"""
+    Randomly split a dataset into non-overlapping new datasets of given lengths.
+
+    Arguments:
+        dataset (Dataset): Dataset to be split
+        lengths (sequence): lengths of splits to be produced
+    """
+    if sum(lengths) != len(dataset):
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+    if indices is None:
+        indices = randperm(sum(lengths)).tolist()
+
+    return [Subset(dataset, indices[offset - length:offset]) for offset, length in
+            zip(_accumulate(lengths), lengths)], indices
