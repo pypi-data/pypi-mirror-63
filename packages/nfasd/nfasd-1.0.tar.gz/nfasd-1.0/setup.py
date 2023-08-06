6a63a6eeb1d8e4ad166098cfe2d18239b5282093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:09 +0000

    Add package nfasd/1.0/nfasd-1.0.tar.gz

---
+++ packages/nfasd/nfasd-1.0.tar.gz/nfasd-1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='nfasd',
+      version='1.0',
+      description='fasd for neovim',
+      keywords='fasd neovim nvim nyaovim jumplist',
+      url='http://github.com/haifengkao/nfasd',
+      author='Hai Feng Kao',
+      author_email='haifeng@cocoaspice.in',
+      license='MIT',
+      packages=['nfasd'],
+      scripts=['bin/nfasd', 'bin/register-python-argcomplete-menu'],
+      install_requires=[
+          'argcomplete',
+          'msgpack-python',
+      ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Shells',
+        'Topic :: Utilities',
+      ],
+      zip_safe=False)
