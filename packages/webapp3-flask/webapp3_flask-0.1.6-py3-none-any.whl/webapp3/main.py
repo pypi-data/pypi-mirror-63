ae1a5a2b16fe46d2b64873a6d6e1185fca2ffdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:55 +0000

    Add package webapp3-flask/0.1.6/webapp3_flask-0.1.6-py3-none-any.whl

---
+++ packages/webapp3-flask/webapp3_flask-0.1.6-py3-none-any.whl/webapp3/main.py
@@ -0,0 +1,58 @@
+from flask import Flask, session,request,redirect, url_for
+
+
+
+def webapp(arg):
+    if request.method == 'GET':    
+        return webappGet(arg)
+    if request.method == 'POST':
+        return webappPost(arg)
+
+class Response:
+  def __init__(self, handler):
+    self.handler = handler
+
+  def write(self, stuffAdd):
+    self.handler.data += stuffAdd
+
+class DaRequest:
+  def __init__(self):
+    self.remote_addr = request.remote_addr
+
+  def get(self,arg):
+    return request.form.get(arg)
+
+class BaseHandler:
+  
+  def __init__(self):
+    self.data = ""
+    self.ChangePage="None"
+    self.response = Response(self)
+    self.session = session
+    self.request= DaRequest()
+  def redirect(self,arg):
+    self.ChangePage=arg
+
+class RequestHandler(BaseHandler):
+  pass
+
+class webapp2:
+  RequestHandler = RequestHandler
+
+def webappGet(daPage):
+    daPage.get()
+    if daPage.ChangePage=="None":
+        return daPage.data
+    else:
+        return redirect(daPage.ChangePage)
+
+def webappPost(daPage):
+    daPage.post()
+    if daPage.ChangePage=="None":
+        return daPage.data
+    else:
+        return redirect(daPage.ChangePage)
+    
+
+
+
