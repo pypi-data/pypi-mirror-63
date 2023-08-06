2dc7ff6d18860532208c806e62090d55d6c8bc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package reprit/0.2.1/reprit-0.2.1.tar.gz

---
+++ packages/reprit/reprit-0.2.1.tar.gz/reprit-0.2.1/setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from setuptools import (find_packages,
+                        setup)
+
+import reprit
+
+project_base_url = 'https://github.com/lycantropos/reprit/'
+
+tests_require = Path('requirements-tests.txt').read_text()
+
+setup(name=reprit.__name__,
+      packages=find_packages(exclude=('tests', 'tests.*')),
+      version=reprit.__version__,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+      ],
+      license='MIT License',
+      description=reprit.__doc__,
+      long_description=Path('README.md').read_text(encoding='utf-8'),
+      long_description_content_type='text/markdown',
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      url=project_base_url,
+      download_url=project_base_url + 'archive/master.zip',
+      python_requires='>=3.5.3',
+      tests_require=tests_require)
