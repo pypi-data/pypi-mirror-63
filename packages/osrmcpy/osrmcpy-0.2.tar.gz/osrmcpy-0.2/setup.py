54046ba9d17202030cfdb5eba3394137f227f0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:15 +0000

    Add package osrmcpy/0.2/osrmcpy-0.2.tar.gz

---
+++ packages/osrmcpy/osrmcpy-0.2.tar.gz/osrmcpy-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='osrmcpy',
+    version='0.2',
+    python_requires='>=3.5',
+    url='https://github.com/Paraboly/libosrmc',
+    author='Geographica',
+    author_email='dev@paraboly.com',
+    description='OSRM Python binding',
+    packages=['osrmcpy'],
+    install_requires=[],
+)
