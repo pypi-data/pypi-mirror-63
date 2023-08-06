f230f5a3a0a5bd4b2263896649a56ec6adacb4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:34 +0000

    Add package negmas/0.6.8/negmas-0.6.8.tar.gz

---
+++ packages/negmas/negmas-0.6.8.tar.gz/negmas-0.6.8/negmas/tests/test_integration.py
@@ -0,0 +1,37 @@
+import os
+import time
+from pprint import pprint
+from random import seed
+
+# seed(0)
+import pytest
+
+import negmas
+from negmas import (
+    HyperRectangleUtilityFunction,
+    LimitedOutcomesNegotiator,
+    SAOMechanism,
+)
+from negmas import UtilityDistribution as U
+
+start = time.monotonic()
+
+print(f"Import took {time.monotonic()-start}")
+
+
+def test_a_session():
+    start = time.monotonic()
+    p = SAOMechanism(outcomes=50, n_steps=50)
+    for _ in range(4):
+        p.add(
+            LimitedOutcomesNegotiator(outcomes=50, p_ending=0.01, name=f"agent {_}"),
+            ufun=HyperRectangleUtilityFunction([None], [lambda x: x[0]]),
+        )
+    p.run()
+    # print(f'{len(p.negotiators)} negotiators')
+    assert len(p.history) > 0
+    # print(f'Took {time.monotonic()-start}')
+
+
+if __name__ == "__main__":
+    pytest.main(args=[__file__])
