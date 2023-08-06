d92b3fb122487ee0e53e89eba041036b7869ffdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package pybaf/0.0.1.5/pybaf-0.0.1.5.tar.gz

---
+++ packages/pybaf/pybaf-0.0.1.5.tar.gz/pybaf-0.0.1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybaf", # Replace with your own username
+    version="0.0.1.5",
+    author="joaovitocn",
+    author_email="joaovitor.cn@gmail.com",
+    description="Python Distance Calculator Bing Api facilitator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joaovitocn/pybaf",
+    packages=setuptools.find_packages(),
+    install_requires=['Pandas','request','json'],
+    classifiers=[
+                    "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
