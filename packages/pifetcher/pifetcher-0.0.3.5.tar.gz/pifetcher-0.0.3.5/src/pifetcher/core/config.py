e92095f06bdc8a178cee73e218f96842ef223ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:56 +0000

    Add package pifetcher/0.0.3.5/pifetcher-0.0.3.5.tar.gz

---
+++ packages/pifetcher/pifetcher-0.0.3.5.tar.gz/pifetcher-0.0.3.5/src/pifetcher/core/config.py
@@ -0,0 +1,23 @@
+import json
+from os import path
+
+
+class Config:
+    initialized = False
+    browser = None
+    queue = None
+    logger = None
+    fetcher = None
+
+    @staticmethod
+    def use(config_path):
+        if not path.exists(config_path):
+            raise Exception(f'config file path does not exist. please create one')
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+
+            Config.browser = config["browser"]
+            Config.queue = config["queue"]
+            Config.logger = config["logger"]
+            Config.fetcher = config["fetcher"]
+            Config.initialized = True
