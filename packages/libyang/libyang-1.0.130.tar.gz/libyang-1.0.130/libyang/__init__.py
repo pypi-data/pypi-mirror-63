a71615ea9db758e1c5c4a2f45af24b64e64f39da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:25 +0000

    Add package libyang/1.0.130/libyang-1.0.130.tar.gz

---
+++ packages/libyang/libyang-1.0.130.tar.gz/libyang-1.0.130/libyang/__init__.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2018-2019 Robin Jarry
+# SPDX-License-Identifier: MIT
+
+import logging
+import os
+
+from _libyang import ffi
+from _libyang import lib
+
+from .schema import Module
+from .schema import Node
+from .util import LibyangError
+from .util import c2str
+from .util import str2c
+
+
+#------------------------------------------------------------------------------
+class Context(object):
+
+    def __init__(self, search_path=None,
+                 options=lib.LY_CTX_DISABLE_SEARCHDIR_CWD):
+        self._ctx = ffi.gc(lib.ly_ctx_new(ffi.NULL, options),
+                           lambda c: lib.ly_ctx_destroy(c, ffi.NULL))
+        if not self._ctx:
+            raise self.error('cannot create context')
+
+        search_dirs = []
+        if 'YANGPATH' in os.environ:
+            search_dirs.extend(
+                os.environ['YANGPATH'].strip(': \t\r\n\'"').split(':'))
+        elif 'YANG_MODPATH' in os.environ:
+            search_dirs.extend(
+                os.environ['YANG_MODPATH'].strip(': \t\r\n\'"').split(':'))
+        if search_path:
+            search_dirs.extend(search_path.strip(': \t\r\n\'"').split(':'))
+
+        for path in search_dirs:
+            if not os.path.isdir(path):
+                continue
+            if lib.ly_ctx_set_searchdir(self._ctx, str2c(path)) != 0:
+                raise self.error('cannot set search dir')
+
+    def error(self, msg, *args):
+        errors = []
+        try:
+            err = lib.ly_err_first(self._ctx)
+            while err:
+                e = []
+                if err.path:
+                    e.append(c2str(err.path))
+                if err.msg:
+                    e.append(c2str(err.msg))
+                if err.apptag:
+                    e.append(c2str(err.apptag))
+                if e:
+                    errors.append(': '.join(e))
+                err = err.next
+        finally:
+            lib.ly_err_clean(self._ctx, ffi.NULL)
+
+        msg %= args
+        if errors:
+            msg += ': ' + ' '.join(errors)
+
+        return LibyangError(msg)
+
+    def load_module(self, name):
+        mod = lib.ly_ctx_load_module(self._ctx, str2c(name), ffi.NULL)
+        if not mod:
+            raise self.error('cannot load module')
+
+        return Module(self, mod)
+
+    def get_module(self, name):
+        mod = lib.ly_ctx_get_module(self._ctx, str2c(name), ffi.NULL, False)
+        if not mod:
+            raise self.error('cannot get module')
+
+        return Module(self, mod)
+
+    def find_path(self, path):
+        node_set = ffi.gc(lib.ly_ctx_find_path(self._ctx, str2c(path)),
+                          lib.ly_set_free)
+        if not node_set:
+            raise self.error('cannot find path')
+
+        for i in range(node_set.number):
+            yield Node.new(self, node_set.set.s[i])
+
+    def __iter__(self):
+        """
+        Return an iterator that yields all implemented modules from the context
+        """
+        idx = ffi.new('uint32_t *')
+        mod = lib.ly_ctx_get_module_iter(self._ctx, idx)
+        while mod:
+            yield Module(self, mod)
+            mod = lib.ly_ctx_get_module_iter(self._ctx, idx)
+
+
+#------------------------------------------------------------------------------
+LOG_LEVELS = {
+    lib.LY_LLERR: logging.ERROR,
+    lib.LY_LLWRN: logging.WARNING,
+    lib.LY_LLVRB: logging.INFO,
+    lib.LY_LLDBG: logging.DEBUG,
+}
+
+
+@ffi.def_extern(name='lypy_log_cb')
+def libyang_c_logging_callback(level, msg, path):
+    args = [c2str(msg)]
+    if path:
+        fmt = '%s: %s'
+        args.append(c2str(path))
+    else:
+        fmt = '%s'
+    LOG.log(LOG_LEVELS.get(level, logging.NOTSET), fmt, *args)
+
+
+def set_log_level(level):
+    for ly_lvl, py_lvl in LOG_LEVELS.items():
+        if py_lvl == level:
+            lib.ly_verb(ly_lvl)
+            return
+
+
+set_log_level(logging.ERROR)
+lib.ly_set_log_clb(lib.lypy_log_cb, True)
+lib.ly_log_options(lib.LY_LOLOG | lib.LY_LOSTORE)
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.NullHandler())
