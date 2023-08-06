21467d2f257b12ba0ed6d7c1a7cd739b2a7829d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:08 +0000

    Add package django-abnorm/0.3.0/django_abnorm-0.3.0-py2.py3-none-any.whl

---
+++ packages/django-abnorm/django_abnorm-0.3.0-py2.py3-none-any.whl/abnorm_tests/settings.py
@@ -0,0 +1,38 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+DATABASES = {
+    'default': {
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'),
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+SITE_ID = 1
+
+USE_TZ = True
+
+USE_I18N = True
+USE_L10N = True
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+MEDIA_URL = '/media/'
+
+SECRET_KEY = '0'
+
+ROOT_URLCONF = 'abnorm_tests.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'abnorm',
+    'abnorm_tests.tests',
+)
+
+
+MIDDLEWARE_CLASSES = ()
+
+ABNORM_IGNORE_MODELS = [
+    'tests.IgnoredTestObj',
+]
