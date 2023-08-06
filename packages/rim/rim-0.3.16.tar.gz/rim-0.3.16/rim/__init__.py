8b342b008cacfc1b2ba98a8f2707ad1d41b8bd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:41 +0000

    Add package rim/0.3.16/rim-0.3.16.tar.gz

---
+++ packages/rim/rim-0.3.16.tar.gz/rim-0.3.16/rim/__init__.py
@@ -0,0 +1,8 @@
+""""rim package is the Python implementation of the Ray Intersection Model 
+(RIM) - for analyzing spatial relationships (e.g. betweenness, intervisibility)
+of triplets of spatial objects."""
+
+__version__ = '0.3.16'
+__release__ = __version__ + '-dev'
+
+from rim.RIM import RIM
---
+++ packages/rim/rim-0.3.16.tar.gz/rim-0.3.16/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# fetch version from the main code file
+__version__ = ''
+with open('rim/__init__.py') as initpy:
+  for line in initpy:
+      if line.startswith('__version__'):
+          exec(line.strip())
+          break
+
+setuptools.setup(
+     name='rim',
+     version=__version__,
+     author="Ivan Majic",
+     author_email="imajicos@gmail.com",
+     description="A package for calculating RIM between three spatial objects",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/SelfHealingMapsProject/RIM",
+     packages=setuptools.find_packages(),
+     install_requires=['numpy>=1.17.4', 'Shapely>=1.6.4'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    python_requires='>=3.5',
+ )
