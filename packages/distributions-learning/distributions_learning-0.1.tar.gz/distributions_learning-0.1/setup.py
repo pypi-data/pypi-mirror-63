7e214835d4ac8bc1fde0153f4963fc1d4a425002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:58 +0000

    Add package distributions-learning/0.1/distributions_learning-0.1.tar.gz

---
+++ packages/distributions-learning/distributions_learning-0.1.tar.gz/distributions_learning-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_learning',
+      version='0.1',
+      description='Learning of Gaussian and binomial distributions',
+      packages=['distributions_learning'],
+      author_email = 'sudipta.mal87@gmail.com',
+      zip_safe=False)
