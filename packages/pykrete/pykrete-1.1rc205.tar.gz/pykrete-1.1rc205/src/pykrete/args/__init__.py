1024820f28d621612c6c9f013760909da6ef5b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:36 +0000

    Add package pykrete/1.1rc205/pykrete-1.1rc205.tar.gz

---
+++ packages/pykrete/pykrete-1.1rc205.tar.gz/pykrete-1.1rc205/src/pykrete/args/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pykrete arguments
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+
+from .args import build_version, exiting_file, yes_or_no
+from .environ import environ
+
+__all__ = ['build_version', 'exiting_file', 'yes_or_no', 'environ']
