031f63744c5965936210060201dd3babcbde39ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:43 +0000

    Add package distributionsjstar414370153/0.1/distributionsjstar414370153-0.1.tar.gz

---
+++ packages/distributionsjstar414370153/distributionsjstar414370153-0.1.tar.gz/distributionsjstar414370153-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsjstar414370153',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsjstar414370153'],
+      zip_safe=False)
