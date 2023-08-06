5a4ea02a6f69d48adaef693524ab99305d9cc5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:00 +0000

    Add package asranger/0.0.2/asranger-0.0.2.tar.gz

---
+++ packages/asranger/asranger-0.0.2.tar.gz/asranger-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='asranger',
+    version='0.0.2',
+    description='Ranger - a synergistic optimizer using RAdam '
+                '(Rectified Adam) and LookAhead in one codebase ',
+    license='Apache',
+    install_requires=['torch'],
+    url="https://github.com/mpariente/Ranger-Deep-Learning-Optimizer",
+    long_description='',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
