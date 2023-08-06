85f84ca9289e166bb7786a208fb1dd883df76105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package djangoautoconf/2.0.4/djangoautoconf-2.0.4-py2.py3-none-any.whl

---
+++ packages/djangoautoconf/djangoautoconf-2.0.4-py2.py3-none-any.whl/djangoautoconf/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Richard Wang'
+__email__ = 'richardwangwang@gmail.com'
+__version__ = '2.0.4'
+
+# from .django_autoconf import DjangoAutoConf
