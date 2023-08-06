acbe23b3912a444ea1e05af596004d339c7e9967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package pomace/0.1a3/pomace-0.1a3-py3-none-any.whl

---
+++ packages/pomace/pomace-0.1a3-py3-none-any.whl/pomace/config.py
@@ -0,0 +1,60 @@
+import os
+from typing import List
+from urllib.parse import urlparse
+
+import log
+from datafiles import datafile, field
+
+
+log.init(debug='pomace_DEBUG' in os.environ)
+log.silence('datafiles')
+
+
+@datafile
+class Browser:
+    name: str = ''
+    width: int = 1920
+    height: int = 1080
+    headless: bool = False
+
+
+@datafile
+class Secret:
+    domain: str
+    name: str
+    value: str
+
+
+@datafile
+class Site:
+    url: str = ''
+
+    @property
+    def domain(self) -> str:
+        return urlparse(self.url).netloc
+
+    def __str__(self):
+        return self.domain
+
+
+@datafile("./.pomace.yml", defaults=True)
+class Settings:
+    browser: Browser = field(default_factory=Browser)
+    site: Site = field(default_factory=Site)
+    secrets: List[Secret] = field(
+        default_factory=lambda: [Secret('example.com', 'password', '<value>')]
+    )
+    development_mode_enabled = False
+
+    def __repr__(self):
+        secrets = ', '.join([secret.name for secret in self.secrets])
+        return f'<{self.site.domain} secrets: {secrets}>'
+
+    def __getattr__(self, name):
+        for secret in self.secrets:
+            if secret.domain == self.site.domain and secret.name == name:
+                return secret.value
+        return object.__getattribute__(self, name)
+
+
+settings = Settings()
