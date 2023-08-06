c6e8e995d7b4c37d1907c1cc25d76b1b685e5227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:03 +0000

    Add package SalesforceEinsteinAnalytics/0.0.3/SalesforceEinsteinAnalytics-0.0.3.tar.gz

---
+++ packages/SalesforceEinsteinAnalytics/SalesforceEinsteinAnalytics-0.0.3.tar.gz/SalesforceEinsteinAnalytics-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='SalesforceEinsteinAnalytics',
+    version='0.0.3',
+    author='Adam Hilgenkamp',
+    author_email='ahilgie@gmail.com',
+    description='Python package for working with the Einstein Analytics API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ahilgenkamp/SalesforceEinsteinAnalytics',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
