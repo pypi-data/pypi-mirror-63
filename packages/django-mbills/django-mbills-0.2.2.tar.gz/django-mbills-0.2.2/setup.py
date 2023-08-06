978244226f137f368cf7c10335843601749c3a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:37 +0000

    Add package django-mbills/0.2.2/django-mbills-0.2.2.tar.gz

---
+++ packages/django-mbills/django-mbills-0.2.2.tar.gz/django-mbills-0.2.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name='django-mbills',
+  packages=['mbills', 'mbills.migrations'],
+  version='0.2.2',
+  description='Django implementation of the Hal MBills APIs.',
+  author='Boris Savic',
+  author_email='boris70@gmail.com',
+  url='https://github.com/boris-savic/django-mbills',
+  keywords=['mbills', 'halcom', 'django' ],
+  classifiers=[],
+  install_requires=[
+        'python_mbills>=0.1.6',
+    ]
+)
