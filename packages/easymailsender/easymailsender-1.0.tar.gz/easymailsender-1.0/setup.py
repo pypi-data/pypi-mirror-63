c00d3aff63507b154f707e1dd135c32959585bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:17 +0000

    Add package easymailsender/1.0/easymailsender-1.0.tar.gz

---
+++ packages/easymailsender/easymailsender-1.0.tar.gz/easymailsender-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+    name="easymailsender",
+    version="1.0",
+    author="Xie Zheyuan",
+    author_email="billy03282@163.com",
+    license="Apache License",
+    url="http://github.com/XieZheyuan/easymail",
+    packages=['easymail'],
+    descrption="Make Send Mail Easy In Python 3",
+    long_descrption=open("readme.md","r").read(),
+    long_descrption_type="text/markdown",
+classifiers=[
+
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+
+        "Topic :: Internet",
+
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+
+)
