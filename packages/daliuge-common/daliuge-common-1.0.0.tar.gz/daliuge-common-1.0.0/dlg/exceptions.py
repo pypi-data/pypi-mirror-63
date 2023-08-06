4a0c5091e64e7f7f468505be52b6e87189ec51ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:22 +0000

    Add package daliuge-common/1.0.0/daliuge-common-1.0.0.tar.gz

---
+++ packages/daliuge-common/daliuge-common-1.0.0.tar.gz/daliuge-common-1.0.0/dlg/exceptions.py
@@ -0,0 +1,128 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2016
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+"""
+Module containing common exceptions used throughout Daliuge.
+In particular DaliugeException should be the root of all our exceptions.
+"""
+
+class DaliugeException(Exception):
+    """
+    The parent of all exceptions thrown by Daliuge
+    """
+
+class InvalidDropException(DaliugeException):
+    """
+    An exception thrown when a Drop is created with a set of invalid arguments.
+    """
+    def __init__(self, drop, reason):
+        DaliugeException.__init__(self, drop, reason)
+        if isinstance(drop, (list, tuple)):
+            self.oid, self.uid = drop
+        else:
+            self.oid = drop.oid
+            self.uid = drop.uid
+        self.reason = reason
+        self.msg = "InvalidDropException <Drop %s / %s>: %s" % (self.uid, self.oid, self.reason)
+
+    def __str__(self, *args, **kwargs):
+        return self.msg
+
+class InvalidRelationshipException(DaliugeException):
+    """
+    An exception thrown when a relationship between two Drops has been
+    instructed but is invalid in nature.
+    """
+    def __init__(self, rel, reason):
+        DaliugeException.__init__(self, rel, reason)
+        self.rel = rel
+        self.reason = reason
+        self.msg = "InvalidRelationshipException <%r>: %s" % (self.rel, self.reason)
+
+    def __str__(self, *args, **kwargs):
+        return self.msg
+
+class InvalidGraphException(DaliugeException):
+    """
+    An exception thrown when an invalid graph, or part of a graph, is given to
+    Daliuge.
+    """
+
+class NoDropException(DaliugeException):
+    """
+    An exception thrown when a Drop UID is pointing to a non-existing Drop
+    """
+
+    def __init__(self, drop_uid, reason=None):
+        DaliugeException.__init__(self, drop_uid, reason)
+        self._drop_uid = drop_uid
+        self._reason = reason
+
+    def __str__(self):
+        ret = "NoDropException <drop_uid: %s>" % (self._drop_uid)
+        if self._reason:
+            ret += ". Reason: %s" % (self._reason)
+        return ret
+
+class NoSessionException(DaliugeException):
+    """
+    An exception thrown when a session ID is pointing to a non-existing session
+    """
+
+    def __init__(self, session_id, reason=None):
+        DaliugeException.__init__(self, session_id, reason)
+        self._session_id = session_id
+        self._reason = reason
+
+    def __str__(self):
+        ret = "NoSessionException <session_id: %s>" % (self._session_id)
+        if self._reason:
+            ret += ". Reason: %s" % (self._reason)
+        return ret
+
+class SessionAlreadyExistsException(DaliugeException):
+    """
+    An exception thrown when a session ID is pointing to an existing session
+    but is meant to be used as the ID of a new session.
+    """
+
+    def __init__(self, session_id, reason=None):
+        DaliugeException.__init__(self, session_id, reason)
+        self._session_id = session_id
+        self._reason = reason
+
+    def __str__(self):
+        ret = "SessionAlreadyExistsException <session_id: %s>" % (self._session_id)
+        if self._reason:
+            ret += ". Reason: %s" % (self._reason)
+        return ret
+
+class InvalidSessionState(DaliugeException):
+    """
+    An exception thrown when an operation is requested on a session that is not
+    in the expected state for that operation.
+    """
+
+class SubManagerException(DaliugeException):
+    """
+    An exception thrown by composite drop managers when there was an error
+    invoking one of their underlying drop managers.
+    """
---
+++ packages/daliuge-common/daliuge-common-1.0.0.tar.gz/daliuge-common-1.0.0/setup.py
@@ -0,0 +1,66 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2020
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+# Version information
+# We do like numpy: we have a major/minor/patch hand-written version written
+# here. If we find the git commit (either via "git" command execution or in a
+# dlg/version.py file) we append it to the VERSION later.
+# The RELEASE flag allows us to create development versions properly supported
+# by setuptools/pkg_resources or "final" versions.
+MAJOR = 1
+MINOR = 0
+PATCH = 0
+VERSION = (MAJOR, MINOR, PATCH)
+VERSION_FILE = "dlg/common/version.py"
+RELEASE = True
+
+def do_versioning():
+    # Avoid importing, the package doesn't exist as such yet
+    with open(os.path.join('dlg', 'version_helper.py')) as f:
+        code = f.read()
+    _globals = {}
+    exec(code, _globals)
+    return _globals['write_version_info'](VERSION, VERSION_FILE, RELEASE)
+
+install_requires = [
+    # 1.10 contains an important race-condition fix on lazy-loaded modules
+    "six>=1.10",
+]
+
+setup(
+    name="daliuge-common",
+    version=do_versioning(),
+    description=u"Data Activated \uF9CA (flow) Graph Engine - Common functionality",
+    long_description="The SKA-SDK prototype for the Execution Framework component",
+    author="ICRAR DIA Group",
+    author_email="dfms_prototype@googlegroups.com",
+    url="https://github.com/ICRAR/daliuge",
+    license="LGPLv2+",
+    packages=find_packages(),
+    install_requires=install_requires,
+    test_suite="test",
+    entry_points={"console_scripts": ["dlg=dlg.common.tool:run"]},  # One tool to rule them all
+)
