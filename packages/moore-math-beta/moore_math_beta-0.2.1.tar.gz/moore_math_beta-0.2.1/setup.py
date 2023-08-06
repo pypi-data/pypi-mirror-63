c4e680ce42545f777984124c829c8f9920fe4221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package moore-math-beta/0.2.1/moore_math_beta-0.2.1.tar.gz

---
+++ packages/moore-math-beta/moore_math_beta-0.2.1.tar.gz/moore_math_beta-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='moore_math_beta',
+      version='0.2.1',
+      description='quick maths',
+      author='abhinav suri',
+      author_email='suria@sas.upenn.edu',
+      license='MIT',
+      packages=['moore_math_beta'],
+      zip_safe=False)
