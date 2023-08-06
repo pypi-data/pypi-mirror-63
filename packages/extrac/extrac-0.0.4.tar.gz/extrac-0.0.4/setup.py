13f47a0524161bc62ea21d67a9fea01f4a281ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:30 +0000

    Add package extrac/0.0.4/extrac-0.0.4.tar.gz

---
+++ packages/extrac/extrac-0.0.4.tar.gz/extrac-0.0.4/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import yaml
+
+with open("versions.yaml") as f:
+    versions = yaml.safe_load(f)
+
+pypi_version = versions["pypi"]["version"]
+
+setup(
+    name="extrac",
+    version=pypi_version,
+    description="decompression files",
+    long_description_markdown_filename="README.md",
+    py_modules=["python_extrac/extrac"],
+    license="MIT",
+    url="https://github.com/belingud/python_extrac",
+    author="belingud",
+    auth_email="zyx@lte.ink",
+    packages=find_packages(),
+    plantforms="Linux",
+    python_requires=">=3.5",
+    install_requires=["Click"],
+    scripts=["python_extrac/extrac.py"],
+    entry_points={"console_scripts": ["x=extrac:cli"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
