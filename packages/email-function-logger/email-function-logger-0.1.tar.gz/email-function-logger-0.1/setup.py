12b7b3cd66880017c6759375cd5c347991a99bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:25 +0000

    Add package email-function-logger/0.1/email-function-logger-0.1.tar.gz

---
+++ packages/email-function-logger/email-function-logger-0.1.tar.gz/email-function-logger-0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="email-function-logger",
+    version="0.1",
+    author="Arthur Cerveira",
+    author_email="aacerveira@inf.ufpel.edu.br",
+    description="A decorator to log information about a function and send it to your email",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    download_url="https://github.com/arthurcerveira/Email-Function-Logger/archive/v0.1.tar.gz",
+    url="https://github.com/arthurcerveira/Email-Function-Logger",
+    packages=setuptools.find_packages(),
+    keywords=['log', 'email', 'function', 'logger'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
