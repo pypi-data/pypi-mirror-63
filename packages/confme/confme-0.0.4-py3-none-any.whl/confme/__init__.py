356525d49b45f2005db7e13c8ba1e2b8dabaebea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:43 +0000

    Add package confme/0.0.4/confme-0.0.4-py3-none-any.whl

---
+++ packages/confme/confme-0.0.4-py3-none-any.whl/confme/__init__.py
@@ -0,0 +1,19 @@
+"""ConfigurationMadeEasy package exports"""
+
+from pydantic import BaseSettings
+
+from confme import source_backend
+
+
+class BaseConfig(BaseSettings):
+
+    @classmethod
+    def load(cls, path: str) -> 'BaseConfig':
+        """Load your configuration file into your config class structure.
+        :param config_class: Root class to map the configuration file to
+        :param path: path to configuration file
+        :return: instance of config_class with all values added from the config file
+        """
+        config_content = source_backend.parse_file(path)
+
+        return cls.parse_obj(config_content)
