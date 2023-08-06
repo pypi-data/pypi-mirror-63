18428bb2b29a58156a684360f86b4a48f872ecbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:24 +0000

    Add package qz-testpypi/0.1/qz_testpypi-0.1.tar.gz

---
+++ packages/qz-testpypi/qz_testpypi-0.1.tar.gz/qz_testpypi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='qz_testpypi',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['qz_testpypi'],
+      zip_safe=False)
