ce500651ed89e688f485cd0ad48102ff1b5130de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:16 +0000

    Add package consul-lib/0.1.7.dev0/consul_lib-0.1.7.dev0.tar.gz

---
+++ packages/consul-lib/consul_lib-0.1.7.dev0.tar.gz/consul_lib-0.1.7.dev0/setup.py
@@ -0,0 +1,19 @@
+import sys
+
+from setuptools import setup, find_packages
+
+if not sys.version_info >= (3, 5):
+    sys.exit("This tool was developed on Python 3.5, please upgrade")
+
+setup(
+    name="consul_lib",
+    version="0.1.7.dev0",
+    maintainer="Syseleven Cloudstackers",
+    maintainer_email="cloudstackers@syseleven.de",
+    url="https://github.com/syseleven/consul_lib",
+    packages=find_packages(),
+    install_requires=[
+        "python-consul",
+        "requests",
+    ],
+)
