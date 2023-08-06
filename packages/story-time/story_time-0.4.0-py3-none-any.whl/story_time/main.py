25c93007a72f79adff56c7980b762303504a499c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:16 +0000

    Add package story-time/0.4.0/story_time-0.4.0-py3-none-any.whl

---
+++ packages/story-time/story_time-0.4.0-py3-none-any.whl/story_time/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""The main script with the StoryTimeApp.
+
+Run this file to run the app.
+
+TODO:
+    - PDF generation!
+    - Fix webcam glitches.
+    - Resize images (and text?) when resizing window.
+    - Indicate diary entries in calendar.
+    - Smoother UI changes (e.g. image loading)
+    - Enlarge image when clicked.
+"""
+
+import wx
+
+from story_time.user_interface import StoryTimeAppUITest
+
+
+def main():
+    """DO ALL THE STUFF.
+    """
+    app = wx.App()
+    ex = StoryTimeAppUITest(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == "__main__":
+    main()
