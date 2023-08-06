6232cf518d4ee9455ac20f08570940225ed383ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:49 +0000

    Add package jtools/1.1.0/jtools-1.1.0-py3-none-any.whl

---
+++ packages/jtools/jtools-1.1.0-py3-none-any.whl/jtools/grammar/JQLLexer.py
@@ -0,0 +1,116 @@
+# Generated from jtools/grammar/JQL.g4 by ANTLR 4.8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\30")
+        buf.write("\u0084\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\3\2\3\2")
+        buf.write("\3\3\3\3\3\4\3\4\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3")
+        buf.write("\5\3\5\3\5\3\5\5\5C\n\5\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3")
+        buf.write("\t\3\n\3\n\3\13\3\13\3\f\3\f\3\r\3\r\3\16\3\16\3\17\3")
+        buf.write("\17\3\20\3\20\3\21\6\21\\\n\21\r\21\16\21]\3\22\6\22a")
+        buf.write("\n\22\r\22\16\22b\3\23\3\23\3\23\3\23\7\23i\n\23\f\23")
+        buf.write("\16\23l\13\23\3\23\3\23\3\23\3\23\3\23\7\23s\n\23\f\23")
+        buf.write("\16\23v\13\23\3\23\5\23y\n\23\3\24\3\24\3\25\3\25\5\25")
+        buf.write("\177\n\25\3\26\3\26\3\27\3\27\2\2\30\3\3\5\4\7\5\t\6\13")
+        buf.write("\7\r\b\17\t\21\n\23\13\25\f\27\r\31\16\33\17\35\20\37")
+        buf.write("\21!\22#\23%\24\'\25)\26+\27-\30\3\2\n\3\2\62;\4\2C\\")
+        buf.write("c|\3\2$$\3\2))\3\2\"\"\4\2\13\f\16\17\4\2**\60\60\3\2")
+        buf.write("\f\f\2\u008d\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3")
+        buf.write("\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2")
+        buf.write("\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2")
+        buf.write("\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2\2\2")
+        buf.write("#\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2")
+        buf.write("\2-\3\2\2\2\3/\3\2\2\2\5\61\3\2\2\2\7\63\3\2\2\2\tB\3")
+        buf.write("\2\2\2\13D\3\2\2\2\rF\3\2\2\2\17H\3\2\2\2\21J\3\2\2\2")
+        buf.write("\23L\3\2\2\2\25N\3\2\2\2\27P\3\2\2\2\31R\3\2\2\2\33T\3")
+        buf.write("\2\2\2\35V\3\2\2\2\37X\3\2\2\2![\3\2\2\2#`\3\2\2\2%x\3")
+        buf.write("\2\2\2\'z\3\2\2\2)~\3\2\2\2+\u0080\3\2\2\2-\u0082\3\2")
+        buf.write("\2\2/\60\7/\2\2\60\4\3\2\2\2\61\62\7-\2\2\62\6\3\2\2\2")
+        buf.write("\63\64\7a\2\2\64\b\3\2\2\2\65\66\7v\2\2\66\67\7t\2\2\67")
+        buf.write("8\7w\2\28C\7g\2\29:\7h\2\2:;\7c\2\2;<\7n\2\2<=\7u\2\2")
+        buf.write("=C\7g\2\2>?\7p\2\2?@\7w\2\2@A\7n\2\2AC\7n\2\2B\65\3\2")
+        buf.write("\2\2B9\3\2\2\2B>\3\2\2\2C\n\3\2\2\2DE\7*\2\2E\f\3\2\2")
+        buf.write("\2FG\7+\2\2G\16\3\2\2\2HI\7\60\2\2I\20\3\2\2\2JK\7]\2")
+        buf.write("\2K\22\3\2\2\2LM\7_\2\2M\24\3\2\2\2NO\7.\2\2O\26\3\2\2")
+        buf.write("\2PQ\7}\2\2Q\30\3\2\2\2RS\7\177\2\2S\32\3\2\2\2TU\7<\2")
+        buf.write("\2U\34\3\2\2\2VW\7B\2\2W\36\3\2\2\2XY\7&\2\2Y \3\2\2\2")
+        buf.write("Z\\\t\2\2\2[Z\3\2\2\2\\]\3\2\2\2][\3\2\2\2]^\3\2\2\2^")
+        buf.write("\"\3\2\2\2_a\t\3\2\2`_\3\2\2\2ab\3\2\2\2b`\3\2\2\2bc\3")
+        buf.write("\2\2\2c$\3\2\2\2dj\7$\2\2ef\7^\2\2fi\7$\2\2gi\n\4\2\2")
+        buf.write("he\3\2\2\2hg\3\2\2\2il\3\2\2\2jh\3\2\2\2jk\3\2\2\2km\3")
+        buf.write("\2\2\2lj\3\2\2\2my\7$\2\2nt\7)\2\2op\7^\2\2ps\7)\2\2q")
+        buf.write("s\n\5\2\2ro\3\2\2\2rq\3\2\2\2sv\3\2\2\2tr\3\2\2\2tu\3")
+        buf.write("\2\2\2uw\3\2\2\2vt\3\2\2\2wy\7)\2\2xd\3\2\2\2xn\3\2\2")
+        buf.write("\2y&\3\2\2\2z{\t\6\2\2{(\3\2\2\2|\177\5\'\24\2}\177\t")
+        buf.write("\7\2\2~|\3\2\2\2~}\3\2\2\2\177*\3\2\2\2\u0080\u0081\n")
+        buf.write("\b\2\2\u0081,\3\2\2\2\u0082\u0083\n\t\2\2\u0083.\3\2\2")
+        buf.write("\2\f\2B]bhjrtx~\2")
+        return buf.getvalue()
+
+
+class JQLLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    PRIMITIVE = 4
+    LPAREN = 5
+    RPAREN = 6
+    DOT = 7
+    LBRACKET = 8
+    RBRACKET = 9
+    COMMA = 10
+    LBRACE = 11
+    RBRACE = 12
+    SEMI = 13
+    AT = 14
+    DOLLAR = 15
+    DIGITS = 16
+    LETTERS = 17
+    STRING = 18
+    SPACE = 19
+    WS = 20
+    IDENTIFIER = 21
+    LAST = 22
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'-'", "'+'", "'_'", "'('", "')'", "'.'", "'['", "']'", "','", 
+            "'{'", "'}'", "':'", "'@'", "'$'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "PRIMITIVE", "LPAREN", "RPAREN", "DOT", "LBRACKET", "RBRACKET", 
+            "COMMA", "LBRACE", "RBRACE", "SEMI", "AT", "DOLLAR", "DIGITS", 
+            "LETTERS", "STRING", "SPACE", "WS", "IDENTIFIER", "LAST" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "PRIMITIVE", "LPAREN", "RPAREN", 
+                  "DOT", "LBRACKET", "RBRACKET", "COMMA", "LBRACE", "RBRACE", 
+                  "SEMI", "AT", "DOLLAR", "DIGITS", "LETTERS", "STRING", 
+                  "SPACE", "WS", "IDENTIFIER", "LAST" ]
+
+    grammarFileName = "JQL.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.8")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
