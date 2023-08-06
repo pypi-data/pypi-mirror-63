7ace32003d5aa988befc2509a5af87a53c3fc78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:33 +0000

    Add package dynapi/0.0.1/dynapi-0.0.1.tar.gz

---
+++ packages/dynapi/dynapi-0.0.1.tar.gz/dynapi-0.0.1/dynapi/__init__.py
@@ -0,0 +1,59 @@
+import requests, json
+from dynalist import Dynalist as DynaFile
+from decimal import Decimal
+import getpass
+import os
+
+
+class DynalistIO:
+    """
+    Examples:
+
+    api = DynalistIO()
+    api.set_api_key(DYNALIST_API_KEY)
+
+    # api.search_files(title='Plan')
+    data = api.get_file(file_id='UdCecF09MdnK-eUJ0kYL_35k')
+    """
+
+    def __init__(self):
+        self.cache = {}
+        self.API_KEY = os.environ.get('DYNALIST_API_KEY', None)
+
+    def set_api_key(self, apikey=None):
+        if apikey is None:
+            self.API_KEY = getpass.getpass(prompt='Enter Dynalist API key (generate one at https://dynalist.io/developer) (hidden input): ')
+        else:
+            self.API_KEY = apikey
+
+    def search_files(self, title=None):
+        files = requests.post(
+            'https://dynalist.io/api/v1/file/list',
+            data=json.dumps({'token': self.API_KEY})
+        ).json()['files']
+
+        if title is not None:
+            files = list(filter(lambda x: title in x['title'], files))
+
+        return files
+
+    def search_nodes(self, file_id, title=None, node_id=None):
+        "Only search top nodes."
+        nodes = self.get_root_children(file_id)
+        if title is not None:
+            return list(filter(lambda x: title in str(x.get('content')) , nodes))
+        elif node_id is not None:
+            return next(filter(lambda x: str(x.get('id')==node_id) , nodes))
+        else:
+            return nodes
+
+    def get_root_children(self, file_id):
+        return self.get_file(file_id).get_children(node_id='root')
+
+    def get_file(self, file_id, refresh=True):
+        if file_id in self.cache and not refresh:
+            return self.cache[file_id]
+        else:
+            client = DynaFile(file_id, token=self.API_KEY)
+            self.cache[file_id] = client
+            return client
---
+++ packages/dynapi/dynapi-0.0.1.tar.gz/dynapi-0.0.1/setup.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2019 Mindey.
+# All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+setup(
+    name='dynapi',
+    version='0.0.1',
+    description='Extras for dyanlist API wrapper.',
+    url='https://gitlab.com/mindey/dynapi',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='MIT',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=[
+        "requests",
+        "dynalist",
+    ],
+    extras_require={
+        'develop': [
+            'pre-commit==1.18.3',
+            'coverage==4.5.4',
+            'flake8==3.7.8',
+            'isort==4.3.21',
+        ],
+    },
+    zip_safe=False
+)
