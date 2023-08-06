b58902b7ea92dc6955ad03ca3230486e5ecf2591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:25 +0000

    Add package pybrock/0.13/pybrock-0.13.tar.gz

---
+++ packages/pybrock/pybrock-0.13.tar.gz/pybrock-0.13/README.md
@@ -0,0 +1,11 @@
+# brock
+
+My personal python library to include snippets of code that I find helpful or do not want to keep reproducing from scratch.
+
+
+
+## Notable packages included
+
+- us
+-- https://github.com/unitedstates/python-us
+
---
+++ packages/pybrock/pybrock-0.13.tar.gz/pybrock-0.13/pybrock/data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import string
+from pybrock import utils
+
+
+# letters
+def letters(upper=False, n=None):
+  # the data
+  if upper:
+    l = list(string.ascii_uppercase)
+  else:
+    l = list(string.ascii_lowercase)
+  # if n, how many to randomly sample
+  if n > 0:
+    l = np.random.choice(l, n)
+  
+  l = list(l)
+  return( l )
+
+  
+# state_name
+def state_name():
+  # get the dict names from states in brock utils module
+  s = utils.states()
+  # return 
+  return(s.keys())
+    
+  
+# state_abbreviation
+def state_abb():
+  # get the dict names from states in brock utils module
+  s = utils.states()
+  # return 
+  return(s.values())
