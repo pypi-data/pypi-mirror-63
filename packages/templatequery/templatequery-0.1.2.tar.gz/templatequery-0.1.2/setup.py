866ecddb27fa0a51f77d449f6c6d93e5e5d906a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:00 +0000

    Add package templatequery/0.1.2/templatequery-0.1.2.tar.gz

---
+++ packages/templatequery/templatequery-0.1.2.tar.gz/templatequery-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="templatequery",
+    version="0.1.2",
+    author="Paul Donchenko",
+    author_email="pjdonch@gmail.com",
+    description="Convenient formatting for psycopg2 SQL queries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pjdon/template_query",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: "
+        "GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['psycopg2>=2.7.5']
+)
