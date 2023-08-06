74b283f57c042fae194e28f8856442a149c805ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:54 +0000

    Add package dask-yarn/0.8.1/dask-yarn-0.8.1.tar.gz

---
+++ packages/dask-yarn/dask-yarn-0.8.1.tar.gz/dask-yarn-0.8.1/dask_yarn/__init__.py
@@ -0,0 +1,11 @@
+# Load configuration
+from . import config
+
+del config
+
+from .core import YarnCluster
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
