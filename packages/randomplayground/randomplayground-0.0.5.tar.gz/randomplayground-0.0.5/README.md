77ae55c9b1e9b6bc0670c3031a8a7ef0333711d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:01 +0000

    Add package randomplayground/0.0.5/randomplayground-0.0.5.tar.gz

---
+++ packages/randomplayground/randomplayground-0.0.5.tar.gz/randomplayground-0.0.5/README.md
@@ -0,0 +1,2 @@
+# random-play-ground
+Collection of random generator type items that should be generally useful
---
+++ packages/randomplayground/randomplayground-0.0.5.tar.gz/randomplayground-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["Faker>=4", "StringGenerator>=0", "termcolor>=1"]
+
+setup(
+    name="randomplayground",
+    version="0.0.5",
+    author="@PyOctoCat",
+    author_email="davidinco@gmail.com",
+    description="A package to help with random generator methods",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RandomPlayGround/random-play-ground",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
