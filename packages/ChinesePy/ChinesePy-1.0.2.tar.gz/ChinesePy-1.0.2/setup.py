23d035b64e95f013efef9f637139a71190913a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:22 +0000

    Add package ChinesePy/1.0.2/ChinesePy-1.0.2.tar.gz

---
+++ packages/ChinesePy/ChinesePy-1.0.2.tar.gz/ChinesePy-1.0.2/setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup
+
+with open('README.md', encoding = 'UTF-8') as fh:
+	long_description = fh.read()
+
+setup(name = 'ChinesePy',
+version = '1.0.2',
+author = 'pythonnlgs',
+long_description = long_description,
+description = '中文函数，适合初学菜鸟……',
+packages = ['ChinesePy'])
