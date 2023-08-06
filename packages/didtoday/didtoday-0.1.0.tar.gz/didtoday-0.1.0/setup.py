32964a08d6e905fb05edd5419100fdd714724bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:19 +0000

    Add package didtoday/0.1.0/didtoday-0.1.0.tar.gz

---
+++ packages/didtoday/didtoday-0.1.0.tar.gz/didtoday-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['didtoday']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'didtoday',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'David Riff',
+    'author_email': 'davidriff@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
