c444a712a7002e529aa3733a0b081b59618e9051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:23 +0000

    Add package pic-cut/0.0.6/pic_cut-0.0.6.tar.gz

---
+++ packages/pic-cut/pic_cut-0.0.6.tar.gz/pic_cut-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pic_cut",
+    version="0.0.6",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Cut Long Picture.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/pic_cut",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.0',
+)
