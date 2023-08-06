d53b5127ed72587d1e5162ce52706c1a600ac8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:07 +0000

    Add package openwisp-controller/0.5.2/openwisp-controller-0.5.2.tar.gz

---
+++ packages/openwisp-controller/openwisp-controller-0.5.2.tar.gz/openwisp-controller-0.5.2/openwisp_controller/pki/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django_x509.base.admin import AbstractCaAdmin, AbstractCertAdmin
+from reversion.admin import VersionAdmin
+
+from openwisp_users.multitenancy import MultitenantOrgFilter
+
+from ..admin import MultitenantAdminMixin
+from .models import Ca, Cert
+
+
+class CaAdmin(MultitenantAdminMixin, VersionAdmin, AbstractCaAdmin):
+    pass
+
+
+CaAdmin.fields.insert(2, 'organization')
+CaAdmin.list_filter.insert(0, ('organization', MultitenantOrgFilter))
+CaAdmin.list_display.insert(1, 'organization')
+CaAdmin.Media.js += ('admin/pki/js/show-org-field.js',)
+
+
+class CertAdmin(MultitenantAdminMixin, VersionAdmin, AbstractCertAdmin):
+    multitenant_shared_relations = ('ca',)
+
+
+CertAdmin.fields.insert(2, 'organization')
+CertAdmin.list_filter.insert(0, ('organization', MultitenantOrgFilter))
+CertAdmin.list_filter.remove('ca')
+CertAdmin.list_display.insert(1, 'organization')
+CertAdmin.Media.js += ('admin/pki/js/show-org-field.js',)
+
+
+admin.site.register(Ca, CaAdmin)
+admin.site.register(Cert, CertAdmin)
