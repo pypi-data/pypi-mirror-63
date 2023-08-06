389a91322ed24e099540de8d7b48d8b79aae680a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:17 +0000

    Add package colcon-core/0.5.5/colcon-core-0.5.5.tar.gz

---
+++ packages/colcon-core/colcon-core-0.5.5.tar.gz/colcon-core-0.5.5/colcon_core/task/python/test/setuppy_test.py
@@ -0,0 +1,49 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from sys import executable
+
+from colcon_core.plugin_system import satisfies_version
+from colcon_core.task import check_call
+from colcon_core.task.python.test import PythonTestingStepExtensionPoint
+from colcon_core.verb.test import logger
+
+
+class SetuppyPythonTestingStep(PythonTestingStepExtensionPoint):
+    """Use `unittest` to test packages."""
+
+    def __init__(self):  # noqa: D107
+        super().__init__()
+        satisfies_version(
+            PythonTestingStepExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+
+    def add_arguments(self, *, parser):  # noqa: D102
+        parser.add_argument(
+            '--unittest-args',
+            nargs='*', metavar='*', type=str.lstrip,
+            help='Pass arguments to Python unittests. '
+            'Arguments matching other options must be prefixed by a space,\n'
+            'e.g. --unittest-args " --help"')
+
+    def match(self, context, env, setup_py_data):  # noqa: D102
+        return True
+
+    async def step(self, context, env, setup_py_data):  # noqa: D102
+        if context.args.retest_until_fail:
+            logger.warning(
+                "Ignored '--retest-until-fail' for package "
+                "'{context.pkg.name}' since 'unittest' does not support the "
+                'usage'.format_map(locals()))
+
+        if context.args.retest_until_pass:
+            logger.warning(
+                "Ignored '--retest-until-pass' for package "
+                "'{context.pkg.name}' since 'unittest' does not support the "
+                'usage'.format_map(locals()))
+
+        cmd = [executable, '-m', 'unittest', '-v']
+        if context.args.unittest_args is not None:
+            cmd += context.args.unittest_args
+        rc = await check_call(context, cmd, cwd=context.args.path, env=env)
+        if rc and rc.returncode:
+            return rc.returncode
