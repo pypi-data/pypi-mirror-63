c5de9e2a5a8e154d99988bf8b25005778961c0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package unholster-inbox/0.1.0/unholster-inbox-0.1.0.tar.gz

---
+++ packages/unholster-inbox/unholster-inbox-0.1.0.tar.gz/unholster-inbox-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['unholster_inbox']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aws>=0.2.5,<0.3.0', 'boto3>=1.12.23,<2.0.0', 'click>=7.1.1,<8.0.0']
+
+setup_kwargs = {
+    'name': 'unholster-inbox',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Sebastián Acuña',
+    'author_email': 'sebastian@unholster.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
