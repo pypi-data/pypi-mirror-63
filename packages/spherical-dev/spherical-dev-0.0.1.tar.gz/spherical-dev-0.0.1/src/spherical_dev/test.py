f2d8a30dbbcfa090653d3780af2bf5aabd13a9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:40 +0000

    Add package spherical-dev/0.0.1/spherical-dev-0.0.1.tar.gz

---
+++ packages/spherical-dev/spherical-dev-0.0.1.tar.gz/spherical-dev-0.0.1/src/spherical_dev/test.py
@@ -0,0 +1,37 @@
+import pathlib
+import webbrowser
+
+import invoke
+
+
+@invoke.task
+def test(
+    ctx,
+    coverage=False,
+    deprecations=False,
+    verbose=False,
+    pdb=False,
+    ipython=False,
+    key=None
+):
+    ctx.run(
+        ' '.join((
+            'pytest',
+            *(coverage and ('--cov=gbtp', '--cov-report=html',) or ()),
+            *(verbose and ('-vv',) or ()),
+            *(pdb and ('--pdb',) or ()),
+            *(ipython and ('--pdbcls=spherical_dev.debugger:Debugger',) or ()),
+            *(key and (f'-k {key}',) or ())
+        )),
+        pty=True,
+        env={} if deprecations else {
+            'PYTHONWARNINGS': (
+                'default,'
+                'ignore::DeprecationWarning,'
+                'ignore::ResourceWarning'
+            ),
+        },
+    )
+    report_index = pathlib.Path('htmlcov/index.html').resolve()
+    if coverage:
+        webbrowser.open(f'file:{report_index}')
