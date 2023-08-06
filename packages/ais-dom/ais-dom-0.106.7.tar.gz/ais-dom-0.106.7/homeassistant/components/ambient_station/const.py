9d7ff16206e8205241a61323d21a75be8021bb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:14 +0000

    Add package ais-dom/0.106.7/ais-dom-0.106.7.tar.gz

---
+++ packages/ais-dom/ais-dom-0.106.7.tar.gz/ais-dom-0.106.7/homeassistant/components/ambient_station/const.py
@@ -0,0 +1,14 @@
+"""Define constants for the Ambient PWS component."""
+DOMAIN = "ambient_station"
+
+ATTR_LAST_DATA = "last_data"
+ATTR_MONITORED_CONDITIONS = "monitored_conditions"
+
+CONF_APP_KEY = "app_key"
+
+DATA_CLIENT = "data_client"
+
+TOPIC_UPDATE = "ambient_station_data_update_{0}"
+
+TYPE_BINARY_SENSOR = "binary_sensor"
+TYPE_SENSOR = "sensor"
