2e3f1393cb199f9ab629b8ab5e195e34c90d68ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:38 +0000

    Add package ir-ldr/0.1.0/ir_ldr-0.1.0.tar.gz

---
+++ packages/ir-ldr/ir_ldr-0.1.0.tar.gz/ir_ldr-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='ir_ldr',
+      version='0.1.0',
+      description='The python package to deal with infrared LDR and Teff.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/MingjieJian/ir_ldr',
+      author='Mingjie Jian',
+      author_email='ssaajianmingjie@gmail.com',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: IPython",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Astronomy"
+      ],
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'scipy',
+      ],
+      zip_safe=False)
