edbd9319f078ccc7c4d63553a85a7b192e84f045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:16 +0000

    Add package YOLO3/1.0/YOLO3-1.0.tar.gz

---
+++ packages/YOLO3/YOLO3-1.0.tar.gz/YOLO3-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YOLO3",
+    version="1.0",
+    keywords=("pip", "yolo", "model"),
+    description="time and path tool",
+    long_description="time and path tool",
+    license="MIT Licence",
+
+    url="https://github.com/jamesjakeies/YOLO3",
+    author="snake98skill",
+    author_email="18816216903@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
