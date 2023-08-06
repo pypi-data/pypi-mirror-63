73208560b3ab8f2124bfc67645c6f278bdb1201b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:54 +0000

    Add package china-dictatorship/0.0.9/china_dictatorship-0.0.9-py2-none-any.whl

---
+++ packages/china-dictatorship/china_dictatorship-0.0.9-py2-none-any.whl/china_dictatorship-0.0.9.data/scripts/china-dictatorship.py
@@ -0,0 +1,11 @@
+#!python
+
+import pkg_resources
+import os
+
+readme_html_path = pkg_resources.resource_filename(__name__, 'README.html')
+print(readme_html_path)
+readme_html_path = os.path.join(sys.prefix, 'README.html')
+print(readme_html_path)
+with open(readme_html_path) as f:
+    print(f.read())
