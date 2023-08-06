dfa1db8d7433eeed868f185466db80c7dac3241b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package samplestamps/0.2.1/samplestamps-0.2.1.tar.gz

---
+++ packages/samplestamps/samplestamps-0.2.1.tar.gz/samplestamps-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='sample stamps',
+      version='0.2.1',
+      description='samplestamps',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/janclemenslab/samplestamps',
+      author='Jan Clemens',
+      author_email='clemensjan@googlemail.com',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=['numpy', 'scipy'],
+      include_package_data=True,
+      zip_safe=False
+      )
---
+++ packages/samplestamps/samplestamps-0.2.1.tar.gz/samplestamps-0.2.1/src/samplestamps/__init__.py
@@ -0,0 +1,3 @@
+"""For converting between timestamps."""
+from .samplestamps import *
+__version__="0.2.1"
