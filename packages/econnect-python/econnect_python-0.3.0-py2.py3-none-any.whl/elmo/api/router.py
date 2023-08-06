caaae5bb99c25fb795c3d9a4b30ff44697898c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:56 +0000

    Add package econnect-python/0.3.0/econnect_python-0.3.0-py2.py3-none-any.whl

---
+++ packages/econnect-python/econnect_python-0.3.0-py2.py3-none-any.whl/elmo/api/router.py
@@ -0,0 +1,35 @@
+class Router(object):
+    """API router class that holds a list of endpoints
+    grouped by action type.
+    """
+
+    def __init__(self, base_url):
+        self._base_url = base_url
+
+    @property
+    def auth(self):
+        return "{}/api/login".format(self._base_url)
+
+    @property
+    def descriptions(self):
+        return "{}/api/strings".format(self._base_url)
+
+    @property
+    def lock(self):
+        return "{}/api/panel/syncLogin".format(self._base_url)
+
+    @property
+    def unlock(self):
+        return "{}/api/panel/syncLogout".format(self._base_url)
+
+    @property
+    def send_command(self):
+        return "{}/api/panel/syncSendCommand".format(self._base_url)
+
+    @property
+    def sectors(self):
+        return "{}/api/areas".format(self._base_url)
+
+    @property
+    def inputs(self):
+        return "{}/api/inputs".format(self._base_url)
