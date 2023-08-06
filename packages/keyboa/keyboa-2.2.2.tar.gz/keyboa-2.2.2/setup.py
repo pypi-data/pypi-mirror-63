e25e9116966edbd68fd1c28ab3a22a7b3c99bd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:22 +0000

    Add package keyboa/2.2.2/keyboa-2.2.2.tar.gz

---
+++ packages/keyboa/keyboa-2.2.2.tar.gz/keyboa-2.2.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+setup(
+  name='keyboa',
+  packages=['keyboa'],
+  version='2.2.2',
+  license='MIT',
+  description="Telegram Inline Keyboards Generator",
+  author='torrua',
+  author_email='torrua@gmail.com',
+  url='https://github.com/torrua/keyboa',
+  download_url='https://github.com/torrua/keyboa/archive/v2.2.2.tar.gz',
+  keywords=['Generate', 'Inline', 'Keyboard', 'Telegram'],
+  install_requires=[
+          'pytelegrambotapi',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
