36be5dddd6765d3bc7f975b53f6ed334c1734716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:06 +0000

    Add package ultrastar-pitch/0.70/ultrastar_pitch-0.70.tar.gz

---
+++ packages/ultrastar-pitch/ultrastar_pitch-0.70.tar.gz/ultrastar_pitch-0.70/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='ultrastar_pitch',
+      version='0.70',
+      description='An attempt to automate the pitch detection for USDX projects',
+      long_description=readme(),
+      classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+      ],
+      url='https://github.com/paradigmn/ultrastar_pitch',
+      author='paradigm',
+      author_email='ultrastarpitch@gmail.com',
+      packages=['ultrastar_pitch'],
+      entry_points = {"console_scripts": ['ultrastar-pitch = ultrastar_pitch.ultrastar_pitch:main']},
+      install_requires=['numpy',
+                        'tensorflow>=2.0'],
+      include_package_data=True,
+      zip_safe=False)
