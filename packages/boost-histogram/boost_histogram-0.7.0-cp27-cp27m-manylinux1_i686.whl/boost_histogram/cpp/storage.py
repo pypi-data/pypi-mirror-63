5888550afe97bb8ce7e47d5fc40a7ea379b6c08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:50 +0000

    Add package boost-histogram/0.7.0/boost_histogram-0.7.0-cp27-cp27m-manylinux1_i686.whl

---
+++ packages/boost-histogram/boost_histogram-0.7.0-cp27-cp27m-manylinux1_i686.whl/boost_histogram/cpp/storage.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+del absolute_import, division, print_function
+
+from ..storage import (
+    Int64 as int64,
+    Double as double,
+    AtomicInt64 as atomic_int64,
+    Unlimited as unlimited,
+    Weight as weight,
+    Mean as mean,
+    WeightedMean as weighted_mean,
+)
