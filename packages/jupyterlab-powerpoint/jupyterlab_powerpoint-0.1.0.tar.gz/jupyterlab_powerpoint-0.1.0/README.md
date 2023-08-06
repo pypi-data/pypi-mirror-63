125095d4bf611d791a513e345677dfd0a0c5f06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:05 +0000

    Add package jupyterlab-powerpoint/0.1.0/jupyterlab_powerpoint-0.1.0.tar.gz

---
+++ packages/jupyterlab-powerpoint/jupyterlab_powerpoint-0.1.0.tar.gz/jupyterlab_powerpoint-0.1.0/README.md
@@ -0,0 +1,25 @@
+# jupyterlab_powerpoint
+Creating PowerPoints from jupyter notebooks
+
+[![Build Status](https://dev.azure.com/tpaine154/jupyter/_apis/build/status/timkpaine.jupyterlab_powerpoint?branchName=master)](https://dev.azure.com/tpaine154/jupyter/_build/latest?definitionId=12&branchName=master)
+[![Coverage](https://img.shields.io/azure-devops/coverage/tpaine154/jupyter/12)](https://dev.azure.com/tpaine154/jupyter/_build?definitionId=12&_a=summary)
+[![GitHub issues](https://img.shields.io/github/issues/timkpaine/jupyterlab_powerpoint.svg)]()
+[![PyPI](https://img.shields.io/pypi/l/jupyterlab_powerpoint.svg)](https://pypi.python.org/pypi/jupyterlab_powerpoint)
+[![PyPI](https://img.shields.io/pypi/v/jupyterlab_powerpoint.svg)](https://pypi.python.org/pypi/jupyterlab_powerpoint)
+[![npm](https://img.shields.io/npm/v/jupyterlab_powerpoint.svg)](https://www.npmjs.com/package/jupyterlab_powerpoint)
+
+
+## Install
+
+```bash
+pip install jupyterlab_powerpoint
+jupyter labextension install jupyterlab_powerpoint
+jupyter serverextension enable --py jupyterlab_powerpoint
+```
+
+## Workflow
+
+- Generate a notebook from a prexisting notebook (includes metadata necessary to reconstruct the original powerpoint)
+- Parameterize notebook, modify parts/text/data
+    + hook in to `Papermill`, leverage python, rexecute daily, etc
+- Generate a powerpoint from the notebook
---
+++ packages/jupyterlab-powerpoint/jupyterlab_powerpoint-0.1.0.tar.gz/jupyterlab_powerpoint-0.1.0/setup.py
@@ -0,0 +1,93 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+from jupyter_packaging import (
+    create_cmdclass, install_npm, ensure_targets,
+    combine_commands, ensure_python, get_version
+)
+
+pjoin = path.join
+
+ensure_python(('2.7', '>=3.3'))
+
+name = 'jupyterlab_powerpoint'
+here = path.abspath(path.dirname(__file__))
+version = get_version(pjoin(here, name, '_version.py'))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requires = [
+    'jupyterlab>=1.0.0',
+    'nbconvert>=5.5.0',
+    'nbformat>=4.4.0',
+    'python-pptx>=0.6.0',
+]
+
+dev_requires = requires + [
+    'pytest',
+    'pytest-cov',
+    'pylint',
+    'flake8',
+    'bump2version',
+    'autopep8',
+    'mock'
+]
+
+data_spec = [
+    # Lab extension installed by default:
+    ('share/jupyter/lab/extensions',
+     'lab-dist',
+     'jupyterlab_powerpoint-*.tgz'),
+    # Config to enable server extension by default:
+    ('etc/jupyter',
+     'jupyter-config',
+     '**/*.json'),
+]
+
+
+cmdclass = create_cmdclass('js', data_files_spec=data_spec)
+cmdclass['js'] = combine_commands(
+    install_npm(here, build_cmd='build:all'),
+    ensure_targets([
+        pjoin(here, 'lib', 'index.js'),
+        pjoin(here, 'style', 'index.css')
+    ]),
+)
+
+
+setup(
+    name=name,
+    version=version,
+    description='Create powerpoints from jupyter notebooks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/timkpaine/jupyterlab_powerpoint',
+    author='Tim Paine',
+    author_email='t.paine154@gmail.com',
+    license='Apache 2.0',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Jupyter',
+    ],
+
+    cmdclass=cmdclass,
+
+    keywords='jupyter jupyterlab',
+    packages=find_packages(exclude=['tests', ]),
+    install_requires=requires,
+    extras_require={
+        'dev': dev_requires
+    },
+    include_package_data=True,
+    zip_safe=False,
+
+)
