af152fa641d93fff16d0981b001fc20b70d6f69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:43 +0000

    Add package odoo10-addon-mis-builder-budget/10.0.3.4.0.99.dev7/odoo10_addon_mis_builder_budget-10.0.3.4.0.99.dev7-py2-none-any.whl

---
+++ packages/odoo10-addon-mis-builder-budget/odoo10_addon_mis_builder_budget-10.0.3.4.0.99.dev7-py2-none-any.whl/odoo/addons/mis_builder_budget/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import mis_budget
+from . import mis_budget_item
+from . import mis_report_kpi
+from . import mis_report_instance
+from . import mis_report_instance_period
+from . import mis_report_kpi_expression
