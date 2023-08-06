5cbd70e156f5548db5104f74b819000d45533d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:03 +0000

    Add package fafi/0.1.4/fafi-0.1.4.tar.gz

---
+++ packages/fafi/fafi-0.1.4.tar.gz/fafi-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fafi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['appdirs>=1.4.3,<2.0.0', 'click>=7.1.1,<8.0.0', 'newspaper3k>=0.2.8,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['fafi = fafi.fafi:cli']}
+
+setup_kwargs = {
+    'name': 'fafi',
+    'version': '0.1.4',
+    'description': 'CLI for indexing Firefox bookmarks.',
+    'long_description': None,
+    'author': 'Sander van Dragt',
+    'author_email': 'sander@vandragt.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
