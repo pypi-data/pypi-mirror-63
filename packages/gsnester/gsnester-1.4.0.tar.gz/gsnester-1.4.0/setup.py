8f0d1ff99b108bf41a99d21df301f34167c0b556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:15 +0000

    Add package gsnester/1.4.0/gsnester-1.4.0.tar.gz

---
+++ packages/gsnester/gsnester-1.4.0.tar.gz/gsnester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='gsnester',
+    version='1.4.0',
+    py_modules = ['gsnester'],
+    author = 'Geetika Sharma',
+    author_email = 'geetusharma7@gmail.com',
+    description = 'Simple printer for nested lists',
+    long_description='This is gsnester.py module, and it provides one function called print_nested_lists() which prints lists that may or may not include nested list'
+    )
