43161e38da77dac60ca6c949d6f6494cc7522a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:04 +0000

    Add package watermill/3.3.0/watermill-3.3.0-py3-none-any.whl

---
+++ packages/watermill/watermill-3.3.0-py3-none-any.whl/watermill/stream_types.py
@@ -0,0 +1,27 @@
+from typing import TypeVar
+
+StreamType = TypeVar('StreamType')
+JoinedStreamType = TypeVar('JoinedStreamType')
+ResultStreamType = TypeVar('ResultStreamType')
+
+
+class AliasType:
+    def __init__(self, origin_cls, alias):
+        self.origin_cls = origin_cls
+        self.alias = alias
+
+    def __hash__(self):
+        return hash(self.alias)
+
+    def __name__(self):
+        return self.alias
+
+    def __eq__(self, other):
+        return self.alias == other.alias
+
+    def __call__(self, *args, **kwargs):
+        return self.origin_cls(*args, **kwargs)
+
+
+def type_alias(stream_type, alias: str):
+    return AliasType(stream_type, alias)
