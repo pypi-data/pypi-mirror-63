c84154735b0dda5a2f93f550ab42cfc07573c143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:03 +0000

    Add package banditml/0.0.2/banditml-0.0.2.tar.gz

---
+++ packages/banditml/banditml-0.0.2.tar.gz/banditml-0.0.2/setup.py
@@ -0,0 +1,29 @@
+## How to release package to PyPi:
+## cd in package directory banditml/ml
+## bump version in setup.py
+## python setup.py sdist bdist_wheel
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="banditml",
+    version="0.0.2",
+    author="Edoardo Conti, Lionel Vital",
+    author_email="edoardo.conti@gmail.com",
+    description="Portable Bandit ML code for training & serving consistency.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/banditml/banditml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["pandas", "torch", "sklearn", "dill"],
+)
