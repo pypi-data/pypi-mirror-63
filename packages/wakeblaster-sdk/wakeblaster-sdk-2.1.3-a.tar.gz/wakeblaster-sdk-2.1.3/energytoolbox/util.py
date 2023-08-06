a099da609ce98b2bca3b2d521d0dab6285a33cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:43 +0000

    Add package wakeblaster-sdk/2.1.3/wakeblaster-sdk-2.1.3-a.tar.gz

---
+++ packages/wakeblaster-sdk/wakeblaster-sdk-2.1.3-a.tar.gz/wakeblaster-sdk-2.1.3/energytoolbox/util.py
@@ -0,0 +1,15 @@
+
+class SignalNames:
+    wind_speed = 'WindSpeed_avg'
+    wind_direction = 'WindDirection'
+    wind_speed_standard_deviation = 'WindSpeed_std'
+    temperature = 'AirTemperature'
+    pressure = 'AirPressure'
+    nacelle_direction = 'NacDirection'
+    operating_state = 'OperatingState'
+    monin_obukhov_length = 'MoninObukhovLength'
+
+    @classmethod
+    def is_signal(cls, signal_name):
+        """Determine whether signal_name is in the list of signal names"""
+        return signal_name in cls.__dict__.values()
