53fad345935b8ac55ffcae0a25b2edb62cd413c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:55 +0000

    Add package dtshare/1.0.3/dtshare-1.0.3-py3-none-any.whl

---
+++ packages/dtshare/dtshare-1.0.3-py3-none-any.whl/dtshare/pro/cons.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Author: Tong Du
+date: 2020/2/13 21:28
+contact: dtshare@126.com
+desc: API常量文件
+"""
+TOKEN_F_P = 'tk.csv'
+TOKEN_ERR_MSG = '请设置dtshare pro的token凭证码，如果没有请访问https://qhkch.com/注册申请'
