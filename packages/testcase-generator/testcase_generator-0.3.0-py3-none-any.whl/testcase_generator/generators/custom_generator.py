f8e0699a9c89352fc604a9f4dcd9852d31db6aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:17 +0000

    Add package testcase-generator/0.3.0/testcase_generator-0.3.0-py3-none-any.whl

---
+++ packages/testcase-generator/testcase_generator-0.3.0-py3-none-any.whl/testcase_generator/generators/custom_generator.py
@@ -0,0 +1,25 @@
+from random import Random
+
+from testcase_generator.models import BoundedConstraint
+
+
+class CustomGenerator:
+    def __init__(self, N, *args, **kwargs):
+        self.random = Random(kwargs.get('seed'))
+        self._N = N
+        self._validate()
+
+    def _validate(self):
+        if not (isinstance(self._N, int) or isinstance(self._N, BoundedConstraint)):
+            raise ValueError('N must be an integer or a BoundedConstraint, not {}.'.format(type(self._N).__name__))
+
+    def next(self):
+        raise NotImplementedError()
+
+    @property
+    def N(self):
+        if isinstance(self._N, int):
+            return self._N
+        elif isinstance(self._N, BoundedConstraint):
+            return self._N.next
+        return None
---
+++ packages/testcase-generator/testcase_generator-0.3.0-py3-none-any.whl/testcase_generator/generators/string_generator.py
@@ -0,0 +1,57 @@
+import string
+
+from testcase_generator.generators.collection_generator import CollectionGenerator
+
+
+class StringGenerator(CollectionGenerator):
+    types = ('standard', 'palindrome', 'space_separated', 'repeating')
+    default_type = 'standard'
+
+    def __init__(self, N, *args, **kwargs):
+        """
+        N: a BoundedConstraint object or an integer for the string length
+        kwargs:
+            type: type of string to generate
+                    standard: default string
+                    palindrome: palindromic string
+                    space_separated: space separated "words"
+                    repeating: string consisting of a substring that is repeated more than 1 time
+            charset: available characters to use, the default is all lowercase letters
+        """
+        self.charset = kwargs.pop('charset', string.ascii_lowercase)
+        super().__init__(N, *args, **kwargs)
+
+    def next(self):
+        return ''.join(super().next())
+
+    @property
+    def _char(self):
+        return self.random.choice(self.charset)
+
+    def standard(self, length, **kwargs):
+        return [self._char for i in range(length)]
+
+    def palindrome(self, length, **kwargs):
+        chars = self.standard(length // 2)
+        mid = [self._char] if length % 2 == 1 else []
+        return chars + mid + chars[::-1]
+
+    def space_separated(self, length, **kwargs):
+        num_spaces = self.random.randint(0, (length - 1) // 2)
+        chars = [[self._char] for i in range(num_spaces + 1)]
+        remaining_length = length - num_spaces - (num_spaces + 1)
+        for i in range(remaining_length):
+            chars[self.random.randint(0, num_spaces)].append(self._char)
+        return ' '.join(''.join(x) for x in chars)
+
+    def repeating(self, length, **kwargs):
+        if length == 1:
+            return [self._char]
+        factors = set()
+        for i in range(1, int(length**0.5) + 1):
+            if length % i == 0:
+                factors.add(i)
+                factors.add(length // i)
+        factors.remove(length)
+        repeated_length = self.random.choice(list(factors))
+        return self.standard(repeated_length) * (length // repeated_length)
