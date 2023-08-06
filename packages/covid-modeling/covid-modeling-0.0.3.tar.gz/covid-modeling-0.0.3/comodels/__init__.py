509c53fff3b0d8b11f61a7ce149be1107f101ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:56 +0000

    Add package covid-modeling/0.0.3/covid-modeling-0.0.3.tar.gz

---
+++ packages/covid-modeling/covid-modeling-0.0.3.tar.gz/covid-modeling-0.0.3/comodels/__init__.py
@@ -0,0 +1,7 @@
+"""
+comodels: Models for coronavirus prediction. Contents:
+---------------------------------------------------------
+    comodels.sir: SIR based models:
+        Penn: Penn signals model, a simple SIR model
+        PennDeath: modified Penn model, includes death
+"""
