48ba8e31e2864f66802047a0bee28837d5eccd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:52 +0000

    Add package crocs/2.0.0/crocs-2.0.0.tar.gz

---
+++ packages/crocs/crocs-2.0.0.tar.gz/crocs-2.0.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+setup(name="crocs",
+      version="2.0.0",
+      packages=["crocs", ],
+      author="Iury O. G. Figueiredo",
+      author_email="last.src@gmail.com",
+      url='https://github.com/dracs/crocs',
+      download_url='https://github.com/dracs/crocs/releases',
+      keywords=[],
+      classifiers=[],
+      description="",)
