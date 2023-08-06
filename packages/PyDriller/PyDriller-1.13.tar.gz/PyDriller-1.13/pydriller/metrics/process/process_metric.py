7de6c7435c420d1120bff0cbf8607693a1c34f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package PyDriller/1.13/PyDriller-1.13.tar.gz

---
+++ packages/PyDriller/PyDriller-1.13.tar.gz/PyDriller-1.13/pydriller/metrics/process/process_metric.py
@@ -0,0 +1,37 @@
+"""
+This module contains the abstract class to implement process metrics.
+"""
+
+from pydriller import RepositoryMining
+
+class ProcessMetric:
+    """
+    Abstract class to implement process metrics
+    """
+
+    def __init__(self, path_to_repo: str,
+                 from_commit: str,
+                 to_commit: str):
+        """
+        :path_to_repo: path to a single repo
+        :to_commit: the SHA of the commit to stop counting. If None, the
+            analysis starts from the latest commit
+        :from_commit: the SHA of the commit to start counting. If None, the
+            analysis ends to the first commit
+        """
+        if not from_commit or not to_commit:
+            raise TypeError
+
+        self.repo_miner = RepositoryMining(path_to_repo, single=from_commit)
+
+        if from_commit != to_commit:
+            self.repo_miner = RepositoryMining(path_to_repo=path_to_repo,
+                                               from_commit=from_commit,
+                                               to_commit=to_commit,
+                                               order='reverse')
+
+    def count(self):
+        """
+        Implement the main functionality of the metric
+        """
+        return 0
---
+++ packages/PyDriller/PyDriller-1.13.tar.gz/PyDriller-1.13/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+
+def read_reqs(filename: str):
+    req = []
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.strip() and not line.startswith('-r'):
+                req.append(line.strip())
+
+    return req
+
+
+install_requires = read_reqs("requirements.txt")
+test_requires = read_reqs("test-requirements.txt")
+
+
+# Get the long description from the relevant file
+long_description = 'PyDriller is a Python framework that helps developers on ' \
+                   'mining software repositories. With PyDriller' \
+                   ' you can easily extract information from any Git ' \
+                   'repository, such as commits, developers, ' \
+                   'modifications, diffs, and source codes, and ' \
+                   'quickly export CSV files.'
+
+setup(
+    name='PyDriller',
+    description='Framework for MSR',
+    long_description=long_description,
+    author='Davide Spadini',
+    author_email='spadini.davide@gmail.com',
+    version='1.13',
+    packages=find_packages('.'),
+    url='https://github.com/ishepard/pydriller',
+    license='Apache License',
+    package_dir={'pydriller': 'pydriller'},
+    python_requires='>=3.5',
+    install_requires=install_requires,
+    test_requirements=test_requires + install_requires,
+    classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            "Operating System :: OS Independent",
+            "Operating System :: POSIX",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: MacOS :: MacOS X",
+            ]
+)
