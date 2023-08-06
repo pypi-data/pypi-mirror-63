6ffd250b5ce29482a9728f0ff363f721c2d04764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:51 +0000

    Add package your-package/0.1/your_package-0.1.tar.gz

---
+++ packages/your-package/your_package-0.1.tar.gz/your_package-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='your_package',
+    version='0.1',
+    description='ac library',
+    author='ac',
+    license='MIT',
+    keywords='sample setuptools development',
+    packages=[
+        "packages"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
