7d8dcfa215be01b087e5f7bd5e2c9cebb118efd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:39 +0000

    Add package nheatmap/0.1.1/nheatmap-0.1.1.tar.gz

---
+++ packages/nheatmap/nheatmap-0.1.1.tar.gz/nheatmap-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='nheatmap',
+    version='0.1.1',
+    author='YSX',
+    author_email='xuesoso@gmail.com',
+    packages=setuptools.find_packages(),
+    url='https://github.com/xuesoso/nheatmap',
+    license='LICENSE',
+    description='A painless and customizable tool to generate heatmap with hiearchical clustering results and additional data bars.',
+    long_description=open('README.md').read(),
+    keywords = ['heatmap', 'hiearchical clustering', 'neat heatmap'],
+    install_requires=[
+       "matplotlib >= 3.0.3",
+       "scipy >= 1.3.1",
+       "numpy >= 1.17.2",
+       "pandas >= 0.25.1",
+   ],
+)
