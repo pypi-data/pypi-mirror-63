96a0c2b7c12ddd93ec794f108e49d035273d8180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:26 +0000

    Add package distbuilder/0.7.6.15/distbuilder-0.7.6.15-py3-none-any.whl

---
+++ packages/distbuilder/distbuilder-0.7.6.15-py3-none-any.whl/distbuilder/__init__.py
@@ -0,0 +1,117 @@
+from ._version import __version__ 
+
+from distbuilder.master import \
+      ConfigFactory \
+    , PyToBinPackageProcess \
+    , PyToBinInstallerProcess \
+    , RobustInstallerProcess 
+       
+from distbuilder.py_installer import \
+      PyInstallerConfig \
+    , PyInstSpec \
+    , WindowsExeVersionInfo \
+    , buildExecutable \
+    , makePyInstSpec 
+    
+from distbuilder.qt_installer import \
+      QtIfwConfig \
+    , QtIfwConfigXml \
+    , QtIfwControlScript \
+    , QtIfwPackage \
+    , QtIfwPackageXml \
+    , QtIfwPackageScript \
+    , QtIfwShortcut \
+    , QtIfwExeWrapper \
+    , installQtIfw \
+    , unInstallQtIfw \
+    , buildInstaller \
+    , findQtIfwPackage \
+    , removeQtIfwPackage \
+    , mergeQtIfwPackages \
+    , nestQtIfwPackage \
+    , joinPathQtIfw \
+    , QT_IFW_VERBOSE_SWITCH \
+    , QT_IFW_TARGET_DIR \
+    , QT_IFW_HOME_DIR \
+    , QT_IFW_DESKTOP_DIR  \
+    , QT_IFW_APPS_DIR  \
+    , QT_IFW_STARTMENU_DIR  \
+    , QT_IFW_PRODUCT_NAME 
+
+from distbuilder.pip_installer import \
+      PipConfig \
+    , installLibraries \
+    , installLibrary \
+    , uninstallLibrary \
+    , vcsUrl
+
+from distbuilder.opy_library import \
+      OpyConfigExt as OpyConfig \
+    , OpyPatch \
+    , LibToBundle \
+    , obfuscatePy \
+    , obfuscatePyLib \
+    , createStageDir 
+    
+from distbuilder.util import \
+      ExecutableScript \
+    , IS_WINDOWS \
+    , IS_LINUX \
+    , IS_MACOS \
+    , PY2 \
+    , PY3 \
+    , BIT_CONTEXT \
+    , IS_32_BIT_CONTEXT \
+    , IS_64_BIT_CONTEXT \
+    , THIS_DIR \
+    , DEBUG_ENV_VAR_NAME, DEBUG_ENV_VAR_VALUE \
+    , absPath \
+    , exists \
+    , isFile \
+    , isDir \
+    , isParentDir \
+    , copyFile \
+    , removeFile \
+    , makeDir \
+    , copyDir \
+    , removeDir \
+    , move \
+    , rename \
+    , tempDirPath \
+    , dirPath \
+    , joinPath \
+    , splitPath \
+    , splitExt \
+    , joinExt \
+    , rootFileName \
+    , normBinaryName \
+    , normIconName \
+    , copyToDir \
+    , moveToDir \
+    , removeFromDir \
+    , renameInDir \
+    , moveToDesktop \
+    , copyToDesktop \
+    , moveToHomeDir \
+    , copyToHomeDir \
+    , collectDirs \
+    , mergeDirs \
+    , getEnv \
+    , setEnv \
+    , delEnv \
+    , _run \
+    , run \
+    , runPy \
+    , toZipFile \
+    , isImportableModule \
+    , isImportableFromModule \
+    , modulePath \
+    , modulePackagePath \
+    , sitePackagePath \
+    , importFromPath \
+    , printErr \
+    , printExc \
+    , halt \
+    , download \
+    , versionTuple \
+    , versionStr
