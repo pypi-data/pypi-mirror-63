e4319831dd08cbaa26010768ad89d2dd79db90d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:03 +0000

    Add package ofonoctl/0.4.1/ofonoctl-0.4.1.tar.gz

---
+++ packages/ofonoctl/ofonoctl-0.4.1.tar.gz/ofonoctl-0.4.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='ofonoctl',
+    version='0.4.1',
+    packages=['ofonoctl'],
+    url='https://git.sr.ht/~martijnbraam/ofonoctl',
+    license='MIT',
+    author='Martijn Braam',
+    author_email='martijn@brixit.nl',
+    description='test/control application for the ofono deamon',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=[
+        'tabulate'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ofonoctl=ofonoctl.__init__:main'
+        ]
+    }
+)
