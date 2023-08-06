cea46766300e7d6bd90ab38d2503707d236b8090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package atpbar/1.0.8/atpbar-1.0.8.tar.gz

---
+++ packages/atpbar/atpbar-1.0.8.tar.gz/atpbar-1.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import versioneer
+
+import os
+import io
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='atpbar',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Progress bars for threading and multiprocessing tasks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tai Sakuma',
+    author_email='tai.sakuma@gmail.com',
+    url='https://github.com/alphatwirl/atpbar',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=find_packages(exclude=['docs', 'tests'])
+)
