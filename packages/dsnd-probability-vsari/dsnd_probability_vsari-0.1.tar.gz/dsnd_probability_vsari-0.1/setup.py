0ec38c3054baf39797a1009d99e3b2aac70bf026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:18 +0000

    Add package dsnd-probability-vsari/0.1/dsnd_probability_vsari-0.1.tar.gz

---
+++ packages/dsnd-probability-vsari/dsnd_probability_vsari-0.1.tar.gz/dsnd_probability_vsari-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_vsari',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['dsnd_probability'],
+      author='Veli Sari',
+      author_email='veli.sari@gmail.com',
+      zip_safe=False)
