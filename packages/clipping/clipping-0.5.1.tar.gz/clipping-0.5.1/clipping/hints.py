49e699057b3403c4fab2abd86aa078ebba44b3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:43 +0000

    Add package clipping/0.5.1/clipping-0.5.1.tar.gz

---
+++ packages/clipping/clipping-0.5.1.tar.gz/clipping-0.5.1/clipping/hints.py
@@ -0,0 +1,14 @@
+from numbers import Real
+from typing import (List,
+                    Tuple,
+                    Type)
+
+Coordinate = Real
+Base = Type[Coordinate]
+Point = Tuple[Coordinate, Coordinate]
+BoundingBox = Tuple[Coordinate, Coordinate, Coordinate, Coordinate]
+Segment = Tuple[Point, Point]
+Contour = List[Point]
+Polygon = Tuple[Contour, List[Contour]]
+Multipolygon = List[Polygon]
+GeometryCollection = Tuple[List[Point], List[Segment], Multipolygon]
