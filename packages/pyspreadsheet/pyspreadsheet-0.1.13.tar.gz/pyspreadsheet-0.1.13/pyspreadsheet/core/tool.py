6b318498ffa3d57abaae59e40227ed1ffc33d3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:57 +0000

    Add package pyspreadsheet/0.1.13/pyspreadsheet-0.1.13.tar.gz

---
+++ packages/pyspreadsheet/pyspreadsheet-0.1.13.tar.gz/pyspreadsheet-0.1.13/pyspreadsheet/core/tool.py
@@ -0,0 +1,21 @@
+def construct_range(worksheet_name, start, end=None):
+    range_ = "'" + worksheet_name + "'" + "!" + start
+    if end:
+        range_ = range_ + ":" + end
+    return range_
+
+
+def column_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+
+def delete_none(rows):
+    for i in range(len(rows)):
+        for j in range(len(rows[i])):
+            if rows[i][j] is None:
+                rows[i][j] = ""
+    return rows
