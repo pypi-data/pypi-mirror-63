5e44e5652107bce01758b53b2a80179407030dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:41 +0000

    Add package microprediction/0.0.15/microprediction-0.0.15-py3-none-any.whl

---
+++ packages/microprediction/microprediction-0.0.15-py3-none-any.whl/microprediction/conventions.py
@@ -0,0 +1,67 @@
+import uuid, re, sys, requests, muid
+
+CONFIG  = requests.get('https://www.microprediction.com/config.json').json()
+MIN_LEN = CONFIG['min_len']
+
+class NameConventions(object):
+
+    @staticmethod
+    def sep():
+        return '::'
+
+    @staticmethod
+    def is_plain_name(name: str):
+        return NameConventions.is_valid_name(name) and not "~" in name
+
+    @staticmethod
+    def is_valid_name(name: str):
+        name_regex = re.compile(r'^[-a-zA-Z0-9_~.:]{1,200}\.[json,html]+$', re.IGNORECASE)
+        return (re.match(name_regex, name) is not None) and (not NameConventions.sep() in name)
+
+    @staticmethod
+    def random_name():
+        return str(uuid.uuid4()) + '.json'
+
+class ValueConventions(object):
+
+    @staticmethod
+    def is_scalar_value(value):
+        try:
+            fv = float(value)
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def is_valid_value(value):
+        return isinstance(value, (str, int, float)) and sys.getsizeof(value) < 100000
+
+    @staticmethod
+    def is_small_value(value):
+        """ Used to determine how to store history for dict like values """
+        return sys.getsizeof(value) < 1200
+
+
+class StatsConventions():
+
+    @staticmethod
+    def percentile_abscissa():
+        return [-8., -5., -4, -3, -2.5, -2.0, -1.5, -1.25, -1, -0.8, -0.6, -0.4, -0.2, -0.1, -0.05, -0.02, -0.01, 0.,
+                0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1., 1.25, 1.5, 2.0, 2.5, 3., 4., 5., 8.]
+
+
+class KeyConventions():
+
+    @staticmethod
+    def is_valid_key(key):
+        return muid.validate((key))
+
+
+
+class MicroConventions(NameConventions, ValueConventions, StatsConventions, KeyConventions):
+    pass
+
+
+
+
+
