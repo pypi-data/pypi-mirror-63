053801c264b4adb9e17b4da6e8450c76e7f5b5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package posce/0.2.0/posce-0.2.0.tar.gz

---
+++ packages/posce/posce-0.2.0.tar.gz/posce-0.2.0/posce/comms/base.py
@@ -0,0 +1,24 @@
+'''
+Click base group function.
+'''
+
+import click
+
+from posce.items import Book
+from posce.tools.clui import CustomGroup
+
+SETTINGS = {
+    'help_option_names': ['-h', '--help'],
+}
+
+@click.group(cls=CustomGroup, context_settings=SETTINGS)
+@click.option('--dire', envvar='POSCE_DIR', hidden=True)
+@click.option('--ext',  envvar='POSCE_EXT', hidden=True)
+@click.pass_context
+def group(ctx, dire, ext):
+    '''
+    Posce: a note-taking toolkit for your command line.
+    See github.com/posce/posce for help and issues.
+    '''
+
+    ctx.obj = Book(dire, ext)
