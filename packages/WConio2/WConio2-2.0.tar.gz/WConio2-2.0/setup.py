2450ff3eb26cb9ff46142cbf324832de117f3ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:01 +0000

    Add package WConio2/2.0/WConio2-2.0.tar.gz

---
+++ packages/WConio2/WConio2-2.0.tar.gz/WConio2-2.0/setup.py
@@ -0,0 +1,26 @@
+# Setup for WConio
+
+from distutils.core import setup
+setup(
+  name = 'WConio2',
+  py_modules = ['WConio2'],
+  version = '2.0',
+  license='MIT',
+  description="Windows Console I/O",
+  author = 'Chris Gonnerman',
+  author_email = 'chris@gonnerman.org',
+  url = 'https://github.com/Solomoriah/WConio2',
+  download_url = 'https://github.com/Solomoriah/WConio2/archive/v20.tar.gz', 
+  keywords = [ "windows", "console", "screen", ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+  ],
+)
+
+
+# end of file.
