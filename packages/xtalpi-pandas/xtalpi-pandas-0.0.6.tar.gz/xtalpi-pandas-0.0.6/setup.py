4731786cd72fcee91f6717d23e87a8cc19f00558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:28 +0000

    Add package xtalpi-pandas/0.0.6/xtalpi-pandas-0.0.6.tar.gz

---
+++ packages/xtalpi-pandas/xtalpi-pandas-0.0.6.tar.gz/xtalpi-pandas-0.0.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='xtalpi-pandas',
+    version='0.0.6',
+    author='Wenzhi Ma',
+    author_email='wenzhi.ma@xtalpi.com',
+    description=u'XtalPi Pandas CMD',
+    packages=['xpandas'],
+    install_requires=['click', 'pandas'],
+    entry_points={
+        'console_scripts': ['xpandas=xpandas:run']
+    }
+)
