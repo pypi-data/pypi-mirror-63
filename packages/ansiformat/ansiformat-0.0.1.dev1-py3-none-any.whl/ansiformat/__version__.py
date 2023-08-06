ccfa86d6b0fb943d0cc392f44d21729b51614e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:00 +0000

    Add package ansiformat/0.0.1.dev1/ansiformat-0.0.1.dev1-py3-none-any.whl

---
+++ packages/ansiformat/ansiformat-0.0.1.dev1-py3-none-any.whl/ansiformat/__version__.py
@@ -0,0 +1,15 @@
+"""Information about ANSI Format package."""
+from datetime import datetime
+
+__title__ = 'ansiformat'
+__description__ = 'Library to pretty format CLI output'
+__version__ = '0.0.1-dev.1'
+
+__docs_url__ = 'https://ansiformat.readthedocs.io'
+__source_url__ = 'https://github.com/ddnomad/ansiformat'
+__home_url__ = __docs_url__
+
+__author__ = 'ddnomad'
+__author_email__ = 'ddnomad@protonmail.com'
+__copyright__ = '%s ddnomad' % datetime.now().year
+__license__ = 'BSD-3-Clause'
