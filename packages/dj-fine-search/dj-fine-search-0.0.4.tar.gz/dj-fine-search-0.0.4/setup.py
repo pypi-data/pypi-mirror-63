45e08746b62d0c440cd1cb5d89fc42b47c284c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:47 +0000

    Add package dj-fine-search/0.0.4/dj-fine-search-0.0.4.tar.gz

---
+++ packages/dj-fine-search/dj-fine-search-0.0.4.tar.gz/dj-fine-search-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import readme
+
+long_description = readme.long_description
+
+setuptools.setup(
+    name="dj-fine-search",
+    version="0.0.4",
+    author="Mayowa Bello",
+    author_email="bmayowa25@gmail.com",
+    description="A key word based queryset search for django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bellomusodiq/dj-fine-search",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
