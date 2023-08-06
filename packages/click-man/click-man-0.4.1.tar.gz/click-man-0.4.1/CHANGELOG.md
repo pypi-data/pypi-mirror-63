147b1ce2b067f631bf6e14c496a70937633b6457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:20 +0000

    Add package click-man/0.4.1/click-man-0.4.1.tar.gz

---
+++ packages/click-man/click-man-0.4.1.tar.gz/click-man-0.4.1/CHANGELOG.md
@@ -0,0 +1,52 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+*Nothing here yet*
+
+## [v0.4.0]
+- Discover command if not in entry_points
+
+## [v0.3.0]
+- Make compatible with click >= 7.0
+
+## [v0.2.2]
+- Release
+
+## [v0.2.1]
+### Added
+- Add information to man page for commands. Refs #3
+
+### Fixed
+- Use hyphens to split words in a command. Refs #2
+
+## [v0.2.0]
+### Added
+- Allow to specify version via command line
+- Proof-of-concept for debian packaging
+
+## [v0.1.1]
+### Fixed
+
+- Several small bugs
+- Improved README
+
+## [v0.1.0]
+### Added
+- Initial release
+
+
+## Info
+
+Please also see `git log`
+
+[Unreleased]: https://github.com/click-contrib/click-man/compare/v0.4.0...HEAD
+[v0.4.0]: https://github.com/click-contrib/click-man/compare/v0.3.0...v0.4.0
+[v0.3.0]: https://github.com/click-contrib/click-man/compare/v0.2.2...v0.3.0
+[v0.2.2]: https://github.com/click-contrib/click-man/compare/v0.2.1...v0.2.2
+[v0.2.1]: https://github.com/click-contrib/click-man/compare/v0.2.0...v0.2.1
+[v0.2.0]: https://github.com/click-contrib/click-man/compare/v0.1.1...v0.2.0
+[v0.1.1]: https://github.com/click-contrib/click-man/compare/v0.1.0...v0.1.1
+[v0.1.0]: https://github.com/click-contrib/click-man/compare/30626839cc048856f799eb0bcd9e731fff4221dc...v0.1.0
---
+++ packages/click-man/click-man-0.4.1.tar.gz/click-man-0.4.1/tests/test_man.py
@@ -0,0 +1,82 @@
+"""
+Module to test man page functionality of click-man package.
+"""
+
+
+from sure import expect
+
+from click_man.man import ManPage
+
+
+def test_simple_man_page():
+    """
+    Test creating most simple man page
+    """
+    man = ManPage('my-command')
+    man.short_help = 'Command to test man pages for click.'
+    man.date = '21-Feb-1994'
+    expect(str(man)).to.be.equal(""".TH "MY-COMMAND" "1" "21-Feb-1994" "1.0.0" "my-command Manual"
+.SH NAME
+my-command \- Command to test man pages for click.
+.SH SYNOPSIS
+.B my-command
+""")
+
+
+def test_full_man_page():
+    """
+    Test creating man page with all options set
+    """
+    man = ManPage('my-command')
+    man.short_help = 'Command to test man pages for click.'
+    man.date = '21-Feb-1994'
+    man.synopsis = '[--yolo] [--iambatman]'
+    man.description = """This is my awesome
+
+multi line description of a click-man test.
+
+Boaa, richtig geili sach."""
+
+    man.options = [
+        ('--yolo', 'Do it in yolo sytle'),
+        ('--iambatman', 'Make me think I am batman')
+    ]
+    man.commands = [
+        ('start', 'Start it'),
+        ('stop', 'Stop it'),
+        ('test', 'Pretend you are testing your code')
+    ]
+
+    expect(str(man)).to.be.equal(r""".TH "MY-COMMAND" "1" "21-Feb-1994" "1.0.0" "my-command Manual"
+.SH NAME
+my-command \- Command to test man pages for click.
+.SH SYNOPSIS
+.B my-command
+[\-\-yolo] [\-\-iambatman]
+.SH DESCRIPTION
+This is my awesome
+.PP
+multi line description of a click-man test.
+.PP
+Boaa, richtig geili sach.
+.SH OPTIONS
+.TP
+\fB\-\-yolo\fP
+Do it in yolo sytle
+.TP
+\fB\-\-iambatman\fP
+Make me think I am batman
+.SH COMMANDS
+.PP
+\fBstart\fP
+  Start it
+  See \fBmy-command-start(1)\fP for full documentation on the \fBstart\fP command.
+.PP
+\fBstop\fP
+  Stop it
+  See \fBmy-command-stop(1)\fP for full documentation on the \fBstop\fP command.
+.PP
+\fBtest\fP
+  Pretend you are testing your code
+  See \fBmy-command-test(1)\fP for full documentation on the \fBtest\fP command.
+""")
