f90aa2febf2a55e5843cb92fc13029952ad0ebbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:00 +0000

    Add package torrent-tracker-scraper/2.0.0/torrent-tracker-scraper-2.0.0.tar.gz

---
+++ packages/torrent-tracker-scraper/torrent-tracker-scraper-2.0.0.tar.gz/torrent-tracker-scraper-2.0.0/README.md
@@ -0,0 +1,102 @@
+# Torrent Tracker Scraper
+
+A UDP torrent tracker scraper written in Python 3 
+
+![Coverage SVG](docs/imgs/coverage.svg)
+[![PyPI version](https://badge.fury.io/py/torrent-tracker-scraper.svg)](https://badge.fury.io/py/torrent-tracker-scraper)
+[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)
+[![Build Status](https://travis-ci.com/49e94b8f256530dc0d41f740dfe8a4c1/torrent-tracker-scraper.svg?branch=master)](https://travis-ci.com/49e94b8f256530dc0d41f740dfe8a4c1/torrent-tracker-scraper)
+
+<img src="docs/imgs/car-thief.jpg" width="400">
+
+### Installation
+
+```bash
+pip install torrent-tracker-scraper
+```
+
+<img src="docs/imgs/thief-downloading-python-package.jpg" width="400">
+
+## Usage
+
+The package can be used either as a module within a script or straight up from the commandline.
+
+### As a python module
+
+pass in a single infohash
+
+```python
+from torrent_tracker_scraper import scraper
+
+scraper = scraper.Scraper("tracker.coppersuffer.tk", 6969, json=True)
+
+results = scraper.scrape("95105D919C10E64AE4FA31067A8D37CCD33FE92D")
+print(results)
+
+{'tracker': 'tracker.coppersuffer.tk', 'results': [{'infohash': '95105D919C10E64AE4FA31067A8D37CCD33FE92D', 'seeders': 112, 'completed': 496, 'leechers': 2}]}
+```
+
+pass in multiple infohashes separated by commas
+
+```python
+results = scraper.scrape("95105D919C10E64AE4FA31067A8D37CCD33FE92D,913EF55D5DD1A9376B738922E5104B3A1BE3754A")
+print(results)
+
+{'tracker': 'tracker.coppersuffer.tk', 'results': [{'infohash': '95105D919C10E64AE4FA31067A8D37CCD33FE92D', 'seeders': 112, 'completed': 496, 'leechers': 2}, {'infohash': '913EF55D5DD1A9376B738922E5104B3A1BE3754A', 'seeders': 334, 'completed': 989, 'leechers': 250}]}
+```
+pass in a list of infohashes
+
+```python
+results = scraper.scrape(["913EF55D5DD1A9376B738922E5104B3A1BE3754A", "95105D919C10E64AE4FA31067A8D37CCD33FE92D"])
+print(results)
+
+{'tracker': 'tracker.coppersuffer.tk', 'results': [{'infohash': '913EF55D5DD1A9376B738922E5104B3A1BE3754A', 'seeders': 334, 'completed': 989, 'leechers': 250}, {'infohash': '95105D919C10E64AE4FA31067A8D37CCD33FE92D', 'seeders': 112, 'completed': 496, 'leechers': 2}]}
+```
+
+## From the commandline
+
+``` 
+python torrent_tracker_scraper/scraper.py -i 45b3d693cff285975f622acaeb75c5626acaff6f
+
+[{'infohash': '45b3d693cff285975f622acaeb75c5626acaff6f', 'seeders': 1, 'completed': 0, 'leechers': 0}]
+
+
+python torrent_tracker_scraper/scraper.py -i 88334ec1d90afe94a22c6de5756268599f5f8ea2,5b6a484a018beed4d01f2f57e6d029a4190a9d04
+
+[{'infohash': '88334ec1d90afe94a22c6de5756268599f5f8ea2', 'seeders': 3, 'completed': 6, 'leechers': 0}, {'infohash': '5b6a484a018beed4d01f2f57e6d029a4190a9d04', 'seeders': 2, 'completed': 12, 'leechers': 0}]
+```
+
+Get your scrap information
+
+<img src="docs/imgs/thief-with-an-early.2000s-limp-bizkit-cd.jpg" width="400">
+
+### Testing
+
+```bash
+pipenv install --dev
+pipenv shell
+python -m pytest
+```
+
+<img src="docs/imgs/thief-reviewing-unit-test-reports.jpg" width="400">
+
+### Help/Contributing
+
+Use the normal GitHub bug reporting flow i.e Create an issue here 
+<https://github.com/49e94b8f256530dc0d41f740dfe8a4c1/torrent-tracker-scraper/issues>.
+
+Fork the code, make your changes and create a pull request.
+
+### Behind the scenes
+
+For a detailed quick rundown of whats going on behind the scenes
+
+<https://blog.takeshispalace.com/programming/python3/udp/2018/12/23/udp-torrent-scraper-python.html>
+
+<img src="docs/imgs/thief-tiptoe.jpg" width="400">
+
+### Contributors
+
+<https://github.com/dessalines>
+
+
---
+++ packages/torrent-tracker-scraper/torrent-tracker-scraper-2.0.0.tar.gz/torrent-tracker-scraper-2.0.0/torrent_tracker_scraper/scraper.py
@@ -0,0 +1,233 @@
+# !/usr/bin/env python
+# scrape.py
+import binascii
+import json
+import logging
+import os
+import random
+import struct
+from threading import Timer
+import logging
+import socket
+import argparse
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Connection:
+    def __init__(self, hostname, port, timeout):
+        self.hostname = hostname
+        self.port = port
+        self.sock = self.connect(
+            self.hostname, self.port, timeout)
+
+    def connect(self, hostname, port, timeout):
+        # create socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(timeout)
+        try:
+            # connect socket
+            sock.connect((self.hostname, self.port))
+            logger.debug(
+                f'Successfully connected to {self.hostname} {self.port}. Set timeout to {timeout} secs.')
+        except Exception as e:
+            # handle socket connection error
+            logger.error(
+                "Tracker udp://{0}:{1} down falling back to udp://tracker.coppersurfer.tk".format(
+                    hostname, port))
+            try:
+                sock.connect(("tracker.coppersurfer.tk", 6969))
+                return sock
+            except Exception as e:
+                sock.close()
+                logger.error(e, logging.ERROR)
+                logger.error(
+                    " Tracker udp://{0}:{1} is also down, check your Internet".format("tracker.coppersurfer.tk",
+                                                                                      6969))
+                return None
+        return sock
+
+    def close(self):
+        """
+        Closes a socket connection gracefully
+        :return: None
+        """
+        self.sock.close()
+
+
+class Scraper:
+    def __init__(self, hostname, port, json=False, **kwargs):
+        """
+        Launches a scraper bound to a particular tracker
+        :param hostname: Tracker hostname e.g. coppersuffer.tk
+        :param port: 6969, self-explanatory
+        :param json: dictates if a json object should be returned as the output
+        :param TODO: timeout: Timeout value in seconds, program exits if no response received within this period
+        """
+        self.json = json
+        self.timeout = kwargs.get('timeout', 3)
+        self.connection = Connection(hostname, port, timeout=self.timeout)
+
+    def parse_infohashes(self, infohashes):
+        if isinstance(infohashes, str):
+            if "," not in infohashes:
+                if self.is_40_char_long(infohashes):
+                    return [infohashes]
+            if "," in infohashes:
+                infohashes = infohashes.split(',')
+                return list(filter(lambda infohash: self.is_40_char_long(infohash) is True, infohashes))
+        if isinstance(infohashes, list):
+            return list(filter(lambda infohash: self.is_40_char_long(infohash) is True, infohashes))
+        return None
+
+    def scrape(self, infohashes):
+        """
+        Takes in an infohash or infohashes. Returns seeders, leechers and completed
+        information
+        :param infohashes: SHA-1 representation of the ```info``` key in the torrent file
+        :return: [(infohash, seeders, leechers, completed),...]
+        """
+
+        infohashes = self.parse_infohashes(infohashes)
+
+        if infohashes is None or len(infohashes) == 0:
+            logger.info("Nothing to do. No infohashes passed the checks")
+            return []
+
+        logger.debug(f'Scraping infohashes: {infohashes}')
+
+        tracker_url = f'udp://{self.connection.hostname}:{self.connection.port}'
+
+        # Quit scraping if there is no connection
+        if self.connection.sock is None:
+            return "Tracker {0} is down".format(tracker_url)
+
+        # Protocol says to keep it that way
+        protocol_id = 0x41727101980
+
+        # We should get the same in response
+        transaction_id = random.randrange(1, 65535)
+
+        # Send a Connect Request
+        packet = struct.pack(">QLL", protocol_id, 0, transaction_id)
+        self.connection.sock.send(packet)
+
+        # Receive a Connect Request response
+        res = self.connection.sock.recv(16)
+        _, transaction_id, connection_id = struct.unpack(">LLQ", res)
+
+        results = list()
+
+        logger.debug("Parsing list of infohashes")
+        # holds good infohashes for unpacking, used to weed out bad infohashes
+        _good_infohashes = list()
+        # holds bad error messages
+        _bad_results = list()
+        packet_hashes = bytearray(str(), 'utf-8')
+        for infohash in infohashes:
+            if not self.is_40_char_long(infohash):
+                _bad_results.append(
+                    {"infohash": infohash, "error": "Bad infohash"})
+                continue
+            try:
+                packet_hashes += binascii.unhexlify(infohash)
+                _good_infohashes.append(infohash)
+            except Exception as e:
+                _bad_results.append(
+                    {"infohash": infohash, "error": f'Error: {e}'})
+                continue
+        packet = struct.pack(">QLL", connection_id, 2,
+                             transaction_id) + packet_hashes
+        self.connection.sock.send(packet)
+
+        # Scrape response
+        try:
+            res = self.connection.sock.recv(8 + (12 * len(infohashes)))
+        except socket.timeout as e:
+            logger.debug(f'socket.timeout {e}')
+            return ['socket.timeout error']
+
+        index = 8
+        for i in range(1, len(_good_infohashes) + 1):
+            logger.debug("Offset: {} {}".format(
+                index + (i * 12) - 12, index + (i * 12)))
+            seeders, completed, leechers = struct.unpack(
+                ">LLL", res[index + (i * 12) - 12: index + (i * 12)])
+            results.append({"infohash": infohashes[i - 1],
+                            "seeders": seeders,
+                            "completed": completed,
+                            "leechers": leechers})
+        results += _bad_results
+        return results
+
+    def is_40_char_long(self, s: str):
+        """
+        Checks if the infohash is 20 bytes long, confirming its truly of SHA-1 nature
+        :param s:
+        :return: True if infohash is valid, False otherwise
+        """
+        if len(s) == 40:
+            return True
+        return False
+
+    def __del__(self):
+        """
+        Close connection if the scraper object is being destroyed
+        :return: None
+        """
+        if self.connection is not None:
+            self.connection.close()
+
+    def __repr__(self):
+        return f'{self.connection.hostname}:{self.connection.port}'
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    required.add_argument("-i",
+                          "--infohash",
+                          help="A torrents infohash or a file path consisting of infohashes",
+                          required=True)
+    optional.add_argument("-t",
+                          "--tracker",
+                          help="Entered in the format :tracker",
+                          type=str,
+                          default="tracker.leechers-paradise.org")
+    optional.add_argument("-p",
+                          "--port",
+                          help="Entered in the format :port",
+                          type=int,
+                          default=6969)
+    optional.add_argument("-j",
+                          "--json",
+                          help="Outputs in JSON format. If you are thinking of going this route, I would suggesting using the tool module option, but hey...who am I to tell you what to do",
+                          dest='json',
+                          action='store_true')
+    optional.add_argument("-to",
+                          "--timeout",
+                          help="Enter the timeout in seconds",
+                          type=int,
+                          default=3)
+    optional.add_argument("-v",
+                          "--verbose",
+                          help="Increase verbosity for debugging purposes",
+                          action="store_true")
+    parser.set_defaults(json=False)
+
+    args, unknown = parser.parse_known_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
+                        format='[%(levelname)s]: %(message)s')
+    logger.debug("Logger initialised")
+
+    scraper = Scraper(args.tracker, args.port, args.json, timeout=args.timeout)
+    results = scraper.scrape(args.infohash)
+    if args.json:
+        print(json.dumps(results))
+    else:
+        print(results)
