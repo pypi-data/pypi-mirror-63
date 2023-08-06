24822c2fad0aae7813eb6b078d824ff8bada6708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:56 +0000

    Add package napoleontoolbox/2.3.9.7/napoleontoolbox-2.3.9.7.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.3.9.7.tar.gz/napoleontoolbox-2.3.9.7/napoleontoolbox/rebalancing/time_series_forecasting.py
@@ -0,0 +1,27 @@
+from napoleontoolbox.rebalancing import rolling
+from sklearn.metrics import mean_squared_error
+import pandas as pd 
+
+def rolling_forecasting(forecasting_model, X, y, n=252, s=63, display = False, **kwargs):
+
+    assert X.shape[0] == y.shape[0]
+    idx = X.index
+    forecasting_series = pd.Series(index=idx, name='prediction')
+
+    roll = rolling._RollingMechanism(idx, n=n, s=s)
+    for slice_n, slice_s in roll():
+        # Select X
+        X_train = X.loc[slice_n].copy()
+        y_train = y.loc[slice_n].copy()
+
+        X_test = X.loc[slice_s].copy()
+        y_test = y.loc[slice_s].copy()
+
+        forecasting_model.fit(X_train, y_train)
+        y_pred = forecasting_model.predict(X_test)
+        forecasting_series.loc[slice_s] = y_pred.values
+        if display:
+            print('rmse for slice ' + str(slice_s))
+            mean_squared_error(y_test, y_pred, squared=False)
+    return forecasting_series
+
