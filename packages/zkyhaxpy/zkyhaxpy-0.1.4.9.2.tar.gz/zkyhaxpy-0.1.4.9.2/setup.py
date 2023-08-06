abbec627a6b711e75b83810f9016428c8d579543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:11 +0000

    Add package zkyhaxpy/0.1.4.9.2/zkyhaxpy-0.1.4.9.2.tar.gz

---
+++ packages/zkyhaxpy/zkyhaxpy-0.1.4.9.2.tar.gz/zkyhaxpy-0.1.4.9.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='zkyhaxpy',  
+    version='0.1.4.9.2',
+    author="Surasak Choedpasuporn",
+    author_email="surasak.cho@gmail.com",
+    description="A python package for personal utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/surasakcho/zkyhaxpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+     ],
+ )
