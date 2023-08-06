a5aa79371122239395145592c64121fc02037579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:48 +0000

    Add package django-szuprefix-saas/0.7.12/django_szuprefix_saas-0.7.12-py2-none-any.whl

---
+++ packages/django-szuprefix-saas/django_szuprefix_saas-0.7.12-py2-none-any.whl/django_szuprefix_saas/dailylog/cron.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*- 
+# author = 'denishuang'
+from __future__ import unicode_literals
+from . import models, helper
+from ..saas.models import Party
+
+
+def do_dailylog_stat(the_date=None):
+    from datetime import date
+    the_date = the_date or date.today()
+    print 'do_dailylog_stat', the_date
+    for p in Party.objects.all():
+        print p, helper.do_daily_stat(the_date, p)
