cf0584474b6d43b14fd3c5b37e7f5e0c49db3afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:26 +0000

    Add package igeFirebase/0.0.6/igeFirebase-0.0.6.tar.gz

---
+++ packages/igeFirebase/igeFirebase-0.0.6.tar.gz/igeFirebase-0.0.6/igeFirebase.h
@@ -0,0 +1,37 @@
+#include <Python.h>
+#include "Firebase.h"
+#include "FirebaseAnalytics.h"
+#include "FirebaseAuth.h"
+#include "FirebaseMessaging.h"
+#include "FirebaseRemoteConfig.h"
+
+typedef struct {
+	PyObject_HEAD
+		Firebase* firebase;
+} firebase_obj;
+
+typedef struct {
+	PyObject_HEAD
+		FirebaseAnalytics* firebaseAnalytics;
+} firebaseAnalytics_obj;
+
+typedef struct {
+	PyObject_HEAD
+		FirebaseAuth* firebaseAuth;
+} firebaseAuth_obj;
+
+typedef struct {
+	PyObject_HEAD
+		FirebaseMessaging* firebaseMessaging;
+} firebaseMessaging_obj;
+
+typedef struct {
+	PyObject_HEAD
+		FirebaseRemoteConfig* firebaseRemoteConfig;
+} firebaseRemoteConfig_obj;
+
+extern PyTypeObject FirebaseType;
+extern PyTypeObject FirebaseAnalyticsType;
+extern PyTypeObject FirebaseAuthType;
+extern PyTypeObject FirebaseMessagingType;
+extern PyTypeObject FirebaseRemoteConfigType;
