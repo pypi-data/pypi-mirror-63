1ecb0c8d0f8f91fbfd85afc2292183c06738a75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:12 +0000

    Add package pyadtpulse/0.1.1/pyadtpulse-0.1.1-py3-none-any.whl

---
+++ packages/pyadtpulse/pyadtpulse-0.1.1-py3-none-any.whl/pyadtpulse/const.py
@@ -0,0 +1,21 @@
+DEFAULT_API_HOST = 'https://portal.adtpulse.com'
+API_HOST_CA      = 'https://portal-ca.adtpulse.com' # Canada
+
+API_PREFIX = '/myhome/'
+
+ADT_LOGIN_URI      = '/access/signin.jsp?partner=adt'
+ADT_LOGOUT_URI     = '/access/signout.jsp'
+
+ADT_SUMMARY_URI    = '/summary/summary.jsp'
+ADT_ZONES_URI      = '/ajax/homeViewDevAjax.jsp'
+ADT_STATES_URI     = '/ajax/currentStates.jsp'
+ADT_SYNC_CHECK_URI = '/Ajax/SyncCheckServ'
+
+ADT_ARM_URI        = '/quickcontrol/serv/RunRRACommand'
+ADT_ARM_DISARM_URI = '/quickcontrol/armDisarm.jsp?href=rest/adt/ui/client/security/setArmState'
+
+ADT_STATUS_CHANGE_URI = '/quickcontrol/serv/ChangeVariableServ'
+
+ADT_SET_PREMISE_URI = '/site/site?action=changepremise&siteId=[NETWORKID]'
+
+ADT_SYSTEM_SETTINGS = '/system/settings.jsp'
