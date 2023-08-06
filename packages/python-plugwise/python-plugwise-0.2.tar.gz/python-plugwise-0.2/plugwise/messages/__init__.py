7b884f6d1e660336e9d1fef252b714b2d7fed9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package python-plugwise/0.2/python-plugwise-0.2.tar.gz

---
+++ packages/python-plugwise/python-plugwise-0.2.tar.gz/python-plugwise-0.2/plugwise/messages/__init__.py
@@ -0,0 +1,20 @@
+# Use of this source code is governed by the MIT license found in the LICENSE file.
+
+from plugwise.messages.requests import (
+    CircleCalibrationRequest,
+    CircleClockInfoRequest,
+    NodeInfoRequest,
+    CirclePowerUsageRequest,
+    CirclePowerBufferRequest,
+    CircleSwitchRequest,
+    StickInitRequest,
+)
+from plugwise.messages.responses import (
+    CircleCalibrationResponse,
+    CircleClockInfoResponse,
+    NodeInfoResponse,
+    CirclePowerUsageResponse,
+    CirclePowerBufferResponse,
+    CircleSwitchResponse,
+    StickInitResponse,
+)
