860eea89ae482afce0bf6187b25d50f945f8e18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:18 +0000

    Add package inoftvocal/0.38.5.4/inoftvocal-0.38.5.4.tar.gz

---
+++ packages/inoftvocal/inoftvocal-0.38.5.4.tar.gz/inoftvocal-0.38.5.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='inoftvocal',
+    version='0.38.5.4',
+    packages=['inoft_vocal_framework', 'inoft_vocal_framework.cli', 'inoft_vocal_framework.utils',
+              'inoft_vocal_framework.speechs', 'inoft_vocal_framework.databases',
+              'inoft_vocal_framework.databases.dynamodb', 'inoft_vocal_framework.skill_builder',
+              'inoft_vocal_framework.platforms_handlers', 'inoft_vocal_framework.platforms_handlers.alexa_v1',
+              'inoft_vocal_framework.platforms_handlers.alexa_v1.response',
+              'inoft_vocal_framework.platforms_handlers.simulator',
+              'inoft_vocal_framework.platforms_handlers.simulator.request_samples',
+              'inoft_vocal_framework.platforms_handlers.dialogflow_v1',
+              'inoft_vocal_framework.platforms_handlers.samsungbixby_v1',
+              'inoft_vocal_framework.platforms_handlers.endpoints_providers'],
+    install_requires=["PyYAML", "cerberus", "boto3", "click"],
+    url='https://github.com/Robinson04/inoft_vocal_framework',
+    download_url='https://github.com/Robinson04/inoft_vocal_framework/archive/0.38.5.tar.gz',
+    license='MIT',
+    author='Inoft',
+    author_email='robinson@inoft.com',
+    description='Create advanced cross-platform skills for Alexa, Google Assistant and Samsung Bixby'
+)
+
