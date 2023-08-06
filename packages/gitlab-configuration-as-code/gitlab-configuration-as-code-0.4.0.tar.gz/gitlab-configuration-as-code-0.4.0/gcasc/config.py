c2961be2e586706ee7afbb12b3b2675b95bedcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:19 +0000

    Add package gitlab-configuration-as-code/0.4.0/gitlab-configuration-as-code-0.4.0.tar.gz

---
+++ packages/gitlab-configuration-as-code/gitlab-configuration-as-code-0.4.0.tar.gz/gitlab-configuration-as-code-0.4.0/gcasc/config.py
@@ -0,0 +1,33 @@
+import os
+
+import yaml
+from gcasc.utils.yaml_include import YamlIncluderConstructor
+from gcasc.utils.yaml_env import YamlEnvConstructor
+
+YamlIncluderConstructor.add_to_loader_class(
+    loader_class=yaml.FullLoader, base_dir=os.path.dirname(os.path.realpath(__file__))
+)
+
+YamlEnvConstructor.add_to_loader_class(loader_class=yaml.FullLoader)
+
+
+class GitlabConfiguration(object):
+    def __init__(self, config):
+        # type: (dict)->GitlabConfiguration
+        if config is None:
+            raise RuntimeError("GitLab configuration is empty")
+
+        if not isinstance(config, dict):
+            raise RuntimeError("Configuration provided must be of dictionary type")
+
+        self.config = config
+
+    def get(self, configuration):
+        return self.config.get(configuration)
+
+    @staticmethod
+    def from_file(path):
+        # type: (str)->GitlabConfiguration
+        with open(path) as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+        return GitlabConfiguration(data)
