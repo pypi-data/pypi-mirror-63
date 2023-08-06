bf59ab377146fb144b1021041414157fa3a4e083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package apsg/0.6.4/apsg-0.6.4-py2.py3-none-any.whl

---
+++ packages/apsg/apsg-0.6.4-py2.py3-none-any.whl/apsg/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+
+from apsg.core import (
+    Vec3,
+    Fol,
+    Lin,
+    Pair,
+    Fault,
+    Group,
+    PairSet,
+    FaultSet,
+    Cluster,
+    StereoGrid,
+    G,
+    settings,
+)
+
+from apsg.tensors import DefGrad, VelGrad, Stress, Tensor, Ortensor, Ellipsoid
+from apsg.helpers import sind, cosd, tand, acosd, asind, atand, atan2d
+from apsg.plotting import StereoNet, VollmerPlot, RamsayPlot, FlinnPlot, HsuPlot
+from apsg.database import SDB
+
+
+__all__ = (
+    "Vec3",
+    "Fol",
+    "Lin",
+    "Pair",
+    "Fault",
+    "Group",
+    "PairSet",
+    "FaultSet",
+    "Cluster",
+    "StereoGrid",
+    "G",
+    "settings",
+    "SDB",
+    "DefGrad",
+    "VelGrad",
+    "Stress",
+    "Tensor",
+    "Ortensor",
+    "Ellipsoid",
+    "sind",
+    "cosd",
+    "tand",
+    "acosd",
+    "asind",
+    "atand",
+    "atan2d",
+    "StereoNet",
+    "VollmerPlot",
+    "RamsayPlot",
+    "FlinnPlot",
+    "HsuPlot",
+)
+
+__version__ = "0.6.4"
+__author__ = "Ondrej Lexa"
+__email__ = "lexa.ondrej@gmail.com"
