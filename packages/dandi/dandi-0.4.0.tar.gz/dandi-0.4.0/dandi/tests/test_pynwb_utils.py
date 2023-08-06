fb22e2e4039252092a4e6dde2d897c3aa2a94a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:54 +0000

    Add package dandi/0.4.0/dandi-0.4.0.tar.gz

---
+++ packages/dandi/dandi-0.4.0.tar.gz/dandi-0.4.0/dandi/tests/test_pynwb_utils.py
@@ -0,0 +1,32 @@
+import pynwb
+
+from ..pynwb_utils import metadata_nwb_subject_fields
+from ..metadata import get_metadata
+
+
+def test_get_metadata(simple1_nwb, simple1_nwb_metadata):
+    target_metadata = simple1_nwb_metadata.copy()
+    # we will also get some counts
+    target_metadata["number_of_electrodes"] = 0
+    target_metadata["number_of_units"] = 0
+    target_metadata["number_of_units"] = 0
+    # We also populate with nd_types now, although here they would be empty
+    target_metadata["nd_types"] = []
+    # we do not populate any subject fields in our simple1_nwb
+    for f in metadata_nwb_subject_fields:
+        target_metadata[f] = None
+    metadata = get_metadata(str(simple1_nwb))
+    # we also load nwb_version field, so it must not be degenerate and ATM
+    # it is 2.X.Y. And since I don't know how to query pynwb on what
+    # version it currently "supports", we will just pop it
+    assert metadata.pop("nwb_version").startswith("2.")
+    assert target_metadata == metadata
+
+
+def test_pynwb_io(simple1_nwb):
+    # To verify that our dependencies spec is sufficient to avoid
+    # stepping into known pynwb/hdmf issues
+    with pynwb.NWBHDF5IO(str(simple1_nwb), "r", load_namespaces=True) as reader:
+        nwbfile = reader.read()
+    assert repr(nwbfile)
+    assert str(nwbfile)
