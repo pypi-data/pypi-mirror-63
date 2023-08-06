2926a9a4b899aa1862c563ab9ac247af0f285552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:25 +0000

    Add package pymydao/0.0.1/pymydao-0.0.1.tar.gz

---
+++ packages/pymydao/pymydao-0.0.1.tar.gz/pymydao-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pymydao',
+      version='0.0.1',
+      description='mysql dao',
+      url='https://github.com/taohuangtao/pymydao',
+      author='huangtao',
+      author_email='tao_huangtao@qq.com',
+      license='MIT',
+      packages=['pymydao'])
