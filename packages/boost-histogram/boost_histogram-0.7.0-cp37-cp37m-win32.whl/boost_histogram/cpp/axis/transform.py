ca29b83f15bb3b54da72f0251c70423fea9365f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:56 +0000

    Add package boost-histogram/0.7.0/boost_histogram-0.7.0-cp37-cp37m-win32.whl

---
+++ packages/boost-histogram/boost_histogram-0.7.0-cp37-cp37m-win32.whl/boost_histogram/cpp/axis/transform.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+del absolute_import, division, print_function
+
+__all__ = ("log", "sqrt", "pow")
+
+from ...axis.transform import log, sqrt, Pow as pow, Function as function
