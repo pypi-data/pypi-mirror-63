6bbc0e0055444d213b11982c201345153bc226d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:51 +0000

    Add package tara/0.1.0/tara-0.1.0-py2.py3-none-any.whl

---
+++ packages/tara/tara-0.1.0-py2.py3-none-any.whl/tara/__main__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# Tara v0.1.0
+# Tara Python Wrapper
+# Copyright © 2020, Sinan Alioglu.
+# See /LICENSE for licensing information.
+
+"""
+Main routine of Tara.
+
+:Copyright: © 2020, Sinan Alioglu.
+:License: BSD (see /LICENSE).
+"""
+
+__all__ = ('main',)
+
+
+def main():
+    """Main routine of Tara."""
+    print("Hello, world!")
+    print("This is Tara.")
+    print("You should customize __main__.py to your liking (or delete it).")
+
+
+if __name__ == '__main__':
+    main()
