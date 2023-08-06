bfdb374602b1250713be64049fd8bcd583181d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:33 +0000

    Add package Rzplgrf/1.6.0/Rzplgrf-1.6.0.tar.gz

---
+++ packages/Rzplgrf/Rzplgrf-1.6.0.tar.gz/Rzplgrf-1.6.0/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='Rzplgrf',
+    version='1.6.0',
+    description='Tools to work with ZPL GRF images and CUPS',
+    long_description=read('README.rst'),
+    author='Kyle MacFarlane',
+    author_email='kyle@deletethetrees.com',
+    url='https://github.com/kylemacfarlane/zplgrf',
+    license='GPLv3',
+
+    packages=find_packages('src'),
+
+    package_dir={'': 'src'},
+    install_requires=[
+        'setuptools',
+        'pillow'
+    ],
+    tests_require=[
+        'ghostscript'
+    ],
+    extras_require={
+        'bindings': ['ghostscript']
+    },
+    test_suite='zplgrf.tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
