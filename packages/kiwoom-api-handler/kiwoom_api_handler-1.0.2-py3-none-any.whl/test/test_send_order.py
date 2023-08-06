4735c096ccea484a7f6f8e345daca76c9d89ed9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:17 +0000

    Add package kiwoom-api-handler/1.0.2/kiwoom_api_handler-1.0.2-py3-none-any.whl

---
+++ packages/kiwoom-api-handler/kiwoom_api_handler-1.0.2-py3-none-any.whl/test/test_send_order.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from PyQt5.QtWidgets import QApplication
+
+from kiwoom_api.api import Kiwoom, DataFeeder, Executor
+
+
+class OrderTest(unittest.TestCase):
+    def testSendOrder(self):
+
+        app = QApplication(sys.argv)
+
+        # API 로그인
+        kiwoom = Kiwoom()
+        kiwoom.commConnect()
+
+        feeder = DataFeeder(kiwoom)
+        executor = Executor(kiwoom)
+
+        accNo = feeder.getAccNo()
+        code = "005930"
+
+        orderSpecDict = executor.createOrderSpec(
+            rqName="test",
+            scrNo="0000",
+            accNo=accNo,
+            orderType=1,  # 신규매수
+            code=code,
+            qty=1,
+            price=0,
+            hogaType="03",
+            originOrderNo="",
+        )
+        executor.sendOrder(**orderSpecDict)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
