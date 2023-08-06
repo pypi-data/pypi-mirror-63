9858e48314b05c5908ddbf88c1ff276400081ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:18 +0000

    Add package NesterByKasinath/1.3.0/NesterByKasinath-1.3.0.tar.gz

---
+++ packages/NesterByKasinath/NesterByKasinath-1.3.0.tar.gz/NesterByKasinath-1.3.0/NesterByKasinath.py
@@ -0,0 +1,14 @@
+""" This module is written to demostrate recursion of nested list data structure"""
+def PrintList(items, isIndentRequired = false, level = 0):
+    """ Iterate through each item in the list to identify if it's a list type
+        If it's a list, recall the function again
+        Else print the item in the list """
+    for item in items:
+        if(isinstance(item, list)):
+            PrintList(item, isIndentRequired, level+1)
+        else:
+            if isIndentRequired:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(item)
+
