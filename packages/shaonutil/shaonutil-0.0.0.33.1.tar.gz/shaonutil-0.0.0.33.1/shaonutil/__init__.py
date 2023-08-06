837730fd29e65232cca0d030814f637dc1e7c1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:20 +0000

    Add package shaonutil/0.0.0.33.1/shaonutil-0.0.0.33.1.tar.gz

---
+++ packages/shaonutil/shaonutil-0.0.0.33.1.tar.gz/shaonutil-0.0.0.33.1/shaonutil/__init__.py
@@ -0,0 +1,7 @@
+#from shaonutil.stats import ClassA
+import shaonutil.stats
+import shaonutil.network
+import shaonutil.image
+import shaonutil.strings
+import shaonutil.file
+import shaonutil.windows
