fd46e47549fbf401f891971d1dcbf0c52ea73d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:38 +0000

    Add package rend/4.2/rend-4.2.tar.gz

---
+++ packages/rend/rend-4.2.tar.gz/rend-4.2/rend/rend/jinja.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Render jinja data
+"""
+# Import local libs
+import rend.exc
+
+# Import third party libs
+import jinja2
+
+
+async def render(hub, data):
+    """
+    Render the given data through Jinja2
+    """
+    if isinstance(data, bytes):
+        data = data.decode()
+    try:
+        template = jinja2.Template(data, enable_async=True)
+        ret = await template.render_async(hub=hub)
+    except jinja2.exceptions.UndefinedError as exc:
+        raise rend.exc.RenderException(f"Jinja variable {exc.message}")
+    except jinja2.exceptions.TemplateSyntaxError as exc:
+        raise rend.exc.RenderException(f"Jinja syntax error {exc.message}")
+    return ret
