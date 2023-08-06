68f8b61508b29fd20b298da3345c0df74bfaa815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:44 +0000

    Add package autoinstallcmtpack/1.0.2/autoinstallcmtpack-1.0.2.tar.gz

---
+++ packages/autoinstallcmtpack/autoinstallcmtpack-1.0.2.tar.gz/autoinstallcmtpack-1.0.2/configCmtPack/__init__.py
@@ -0,0 +1,14 @@
+import *
+import logging
+logging.basicConfig(level=logging.info,
+                    format='[%(levelname)s] %(message)s')
+print("Info")
+#log = logging.getLogger(__name__)
+# CRITICAL: 50
+# ERROR: 40
+# WARNING: 30
+# INFO: 20
+# Debug: 10
+# NOSET: 0
+# log.setLevel(0)
+#log.info("set the logging")
