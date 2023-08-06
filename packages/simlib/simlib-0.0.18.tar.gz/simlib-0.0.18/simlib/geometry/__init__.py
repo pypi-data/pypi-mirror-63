8121aa9ba7eb506fde21632dad035e279a2adc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package simlib/0.0.18/simlib-0.0.18.tar.gz

---
+++ packages/simlib/simlib-0.0.18.tar.gz/simlib-0.0.18/simlib/geometry/__init__.py
@@ -0,0 +1,20 @@
+"""
+geometry.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+from .geometry import *
+
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+__all__ = [
+    'angle',
+    'cos_angle',
+    'distance',
+    'norm',
+    'vector'
+]
+
+
