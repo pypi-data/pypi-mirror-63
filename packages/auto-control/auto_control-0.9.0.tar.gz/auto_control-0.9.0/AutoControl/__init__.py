90f315a38d8b3ded27778067b9b044c1fbe3ea05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:58 +0000

    Add package auto-control/0.9.0/auto_control-0.9.0.tar.gz

---
+++ packages/auto-control/auto_control-0.9.0.tar.gz/auto_control-0.9.0/AutoControl/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py.py    
+@Email :   601540072@qq.com
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2020/3/20 17:04    Dong        1.0         -----
+"""
+
+# import lib
---
+++ packages/auto-control/auto_control-0.9.0.tar.gz/auto_control-0.9.0/AutoControl/share_control.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   share_control.py    
+@Email :   601540072@qq.com
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2020/3/20 17:12    Dong        1.0         -----
+"""
+
+# import lib
+def desc():
+    print("share control tools")
