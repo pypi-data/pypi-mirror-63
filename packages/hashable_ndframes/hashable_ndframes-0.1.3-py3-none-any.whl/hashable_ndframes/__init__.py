3b050b9dad7fdc195e75416ddcdb61e5bae32505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package hashable_ndframes/0.1.3/hashable_ndframes-0.1.3-py3-none-any.whl

---
+++ packages/hashable_ndframes/hashable_ndframes-0.1.3-py3-none-any.whl/hashable_ndframes/__init__.py
@@ -0,0 +1,7 @@
+"""Hashable (pandas) Series and DataFrames."""
+from hashable_ndframes.hashable_ndframes import HashableDataFrame
+from hashable_ndframes.hashable_ndframes import HashableSeries
+
+
+__version__ = "0.1.3"
+_ = {HashableSeries, HashableDataFrame}
