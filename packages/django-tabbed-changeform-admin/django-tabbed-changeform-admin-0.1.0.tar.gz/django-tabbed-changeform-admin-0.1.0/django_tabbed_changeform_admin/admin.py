059cdd1a3dfeda630633a190ded6134230470efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:34 +0000

    Add package django-tabbed-changeform-admin/0.1.0/django-tabbed-changeform-admin-0.1.0.tar.gz

---
+++ packages/django-tabbed-changeform-admin/django-tabbed-changeform-admin-0.1.0.tar.gz/django-tabbed-changeform-admin-0.1.0/django_tabbed_changeform_admin/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.admin.options import csrf_protect_m
+
+
+class DjangoTabbedChangeformAdmin(admin.ModelAdmin):
+
+    class Media:
+        css = {
+            "all": [
+                "jquery-ui/jquery-ui.min.css",
+                "django-tabbed-changeform-admin/css/django-tabbed-changeform-admin.css",
+            ]
+        }
+        js = [
+            "jquery3/jquery.js",
+            "jquery-ui/jquery-ui.min.js",
+            "django-tabbed-changeform-admin/js/django-tabbed-changeform-admin.js",
+        ]
+
+    @csrf_protect_m
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["tabs"] = self.get_tabs(request, object_id, form_url, extra_context)
+        return super().changeform_view(request, object_id, form_url, extra_context)
+
+    def get_tabs(self, request, object_id, form_url, extra_context):
+        return self.tabs
