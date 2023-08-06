dc21810456a11a09e0861312897bc245c41c5963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:45 +0000

    Add package bio2bel/0.3.2/bio2bel-0.3.2-py3-none-any.whl

---
+++ packages/bio2bel/bio2bel-0.3.2-py3-none-any.whl/bio2bel/manager/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Managers for Bio2BEL."""
+
+from .abstract_manager import AbstractManager
+from .connection_manager import ConnectionManager
+
+__all__ = [
+    'ConnectionManager',
+    'AbstractManager',
+]
