e4d368ee1468fa76dea5bf443e122b3f327c3888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package django-monaco-old/0.1.1/django_monaco_old-0.1.1-py2.7.egg

---
+++ packages/django-monaco-old/django_monaco_old-0.1.1-py2.7.egg/monaco/config.py
@@ -0,0 +1,9 @@
+MONACO_STATICFILES = {
+    'js': (
+        '/static/monaco/loader.js',
+        '/static/monaco.config.js',
+    ),
+    'css': {
+        'screen': ("/static/monaco.custom.css", )
+    }
+}
