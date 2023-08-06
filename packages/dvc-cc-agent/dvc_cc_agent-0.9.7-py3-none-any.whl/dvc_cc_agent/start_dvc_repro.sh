33c308f1674b95fb0e2abbfde8d095cc6db105ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:15 +0000

    Add package dvc-cc-agent/0.9.7/dvc_cc_agent-0.9.7-py3-none-any.whl

---
+++ packages/dvc-cc-agent/dvc_cc_agent-0.9.7-py3-none-any.whl/dvc_cc_agent/start_dvc_repro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%y_%m_%d_%H_%M_%S_%N"
+}
+tmstmp=$( timestamp )
+echo "DVC REPRO $1 2>&1 | tee $3$2_$tmstmp stdout_stderr/$2_$tmstmp"
+export PYTHONUNBUFFERED=true
+dvc repro $1 2>&1 | tee $3$2_$tmstmp stdout_stderr/$2_$tmstmp
+[ $? -ne 0 ] && exit 1;
