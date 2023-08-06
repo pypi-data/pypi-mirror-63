8ffcde64734f0122ec1478ae056e39eba6f5b241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:53 +0000

    Add package openap1/1.0.dev1/openap1-1.0.dev1-py2.py3-none-any.whl

---
+++ packages/openap1/openap1-1.0.dev1-py2.py3-none-any.whl/openap/__init__.py
@@ -0,0 +1,10 @@
+from .extra import aero
+from .extra import nav
+from .extra import filters
+from .extra import statistics
+
+from .thrust import Thrust
+from .drag import Drag
+from .fuel import FuelFlow
+from .kinematic import WRAP
+from .phase import FlightPhase
