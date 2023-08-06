73e601e09177a3cc8264d891395b2855365987bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:25 +0000

    Add package django-libs/2.0.3/django-libs-2.0.3.tar.gz

---
+++ packages/django-libs/django-libs-2.0.3.tar.gz/django-libs-2.0.3/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+import django_libs
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name="django-libs",
+    version=django_libs.__version__,
+    description='A collection of things that we re-use in every Django project, such as custom middlewares, templates, templatetags, test mixins etc...',
+    long_description=read('README.rst'),
+    license='The MIT License',
+    platforms=['OS Independent'],
+    keywords='django, common, reusable, kickstart',
+    author='Martin Brochhaus',
+    author_email='mbrochh@gmail.com',
+    url="https://github.com/bitmazk/django-libs",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    tests_require=[
+        'django-nose',
+        'coverage',
+        'django-coverage',
+    ],
+    test_suite='django_libs.tests.runtests.runtests',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
