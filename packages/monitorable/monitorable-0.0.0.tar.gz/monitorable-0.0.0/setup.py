d19f2b68efdb442f86769736232b603118c7e9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:03 +0000

    Add package monitorable/0.0.0/monitorable-0.0.0.tar.gz

---
+++ packages/monitorable/monitorable-0.0.0.tar.gz/monitorable-0.0.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+  name = "monitorable",
+  version = "0.0.0",
+  description = "monitorable",
+  license = "MIT Licence",
+
+  packages = find_packages(),
+  install_requires = []
+)
