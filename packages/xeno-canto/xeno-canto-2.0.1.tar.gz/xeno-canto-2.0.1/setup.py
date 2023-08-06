aa752216d5392791927da20652a802a550033562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package xeno-canto/2.0.1/xeno-canto-2.0.1.tar.gz

---
+++ packages/xeno-canto/xeno-canto-2.0.1.tar.gz/xeno-canto-2.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='xeno-canto',
+    version='2.0.1',
+    description='xeno-canto.org API Wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ntivirikin/xeno-canto-py',
+    author='Nazariy Tivirikin',
+    author_email='n.tivirikin@gmail.com',
+    license="MIT",
+    py_modules=['xenocanto'],
+    entry_points={"console_scripts": ["xeno-canto = xenocanto:main"]},
+)
