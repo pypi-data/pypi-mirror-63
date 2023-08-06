5a82e7a38fc2d1162f3ddf85f62d974f7aa1de27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    Add package dotctrl/0.1.0rc1/dotctrl-0.1.0rc1-py2.py3-none-any.whl

---
+++ packages/dotctrl/dotctrl-0.1.0rc1-py2.py3-none-any.whl/dotctrl/__init__.py
@@ -0,0 +1,25 @@
+"""
+Dotctrl
+~~~~~~~
+
+DotCtrl is a package for managing your dotfiles on Linux.
+
+
+For more information, access: 'https://github.com/snakypy/dotctrl'
+
+:copyright: Copyright 2020-2020 by Snakypy team, see AUTHORS.
+:license: MIT license, see LICENSE for details.
+"""
+
+__version__ = '0.1.0rc1'
+__infopkg__ = {
+    'name': 'dotctrl',
+    'url': 'https://github.com/snakypy/dotctrl',
+    'author': {
+        'name': 'William C. Canin',
+        'email': 'william.costa.canin@gmail.com',
+        'website': 'https://williamcanin.github.io',
+        'github': 'https://github.com/snakypy'
+    }
+}
+
