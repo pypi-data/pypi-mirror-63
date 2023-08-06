041c452abb7bea657af9c110f0e9ac0994b1c448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:12 +0000

    Add package django-xmail-ritual/1.0.1/django-xmail-ritual-1.0.1.tar.gz

---
+++ packages/django-xmail-ritual/django-xmail-ritual-1.0.1.tar.gz/django-xmail-ritual-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django-xmail-ritual',
+    version='1.0.1',
+    packages=[
+        'grimoire.django.xmail',
+        'grimoire.django.xmail.management',
+        'grimoire.django.xmail.management.commands',
+        'grimoire.django.xmail.migrations',
+    ],
+    package_data={
+        'grimoire.django.xmail': [
+            'locale/*/LC_MESSAGES/*.*'
+        ]
+    },
+    url='https://github.com/luismasuelli/django-xmail-ritual',
+    license='LGPL',
+    author='Luis y Anita',
+    author_email='luismasuelli@hotmail.com',
+    description='The xmail library is a mail dispatching library for Django 2.2 or greater. Mails are sent asynchronously by a '
+                'management command',
+    install_requires=['python-cantrips>=1.0.0', 'Django>=2.2']
+)
