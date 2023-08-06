c136fcecf762480341b6cbd7f4faa06129f6b64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:31 +0000

    Add package stlearn/0.1.7/stlearn-0.1.7.tar.gz

---
+++ packages/stlearn/stlearn-0.1.7.tar.gz/stlearn-0.1.7/README.md
@@ -0,0 +1,40 @@
+# stLearn - A downstream analysis toolkit for Spatial Transcriptomic data (v0.1.7)
+
+# How to install
+
+### Step 0:
+
+Prepare conda environment for stlearn
+
+``` conda create -n stlearn python=3.8 ```
+
+### Step 1:
+
+``` conda config --add channels conda-forge ```
+
+``` conda install jupyterlab louvain ipywidgets```
+### Step 2 (For Windows user):
+
+Access to: 
+
+ - https://www.lfd.uci.edu/~gohlke/pythonlibs/#python-igraph
+
+ - https://www.lfd.uci.edu/~gohlke/pythonlibs/#louvain-igraph
+
+Download 2 files: python_igraph‑0.7.1.post6‑cp37‑cp37m‑win_amd64.whl and louvain‑0.6.1‑cp37‑cp37m‑win_amd64.whl
+
+You have to change to downloaded files directory and install it:
+
+``` pip install python_igraph‑0.7.1.post6‑cp37‑cp37m‑win_amd64.whl ```
+
+``` pip install louvain‑0.6.1‑cp37‑cp37m‑win_amd64.whl ```
+
+
+
+### Step 3:
+
+``` pip install stlearn```
+
+
+
+
---
+++ packages/stlearn/stlearn-0.1.7.tar.gz/stlearn-0.1.7/stlearn/spatials/trajectory/local_level.py
@@ -0,0 +1,70 @@
+from anndata import AnnData
+from typing import Optional, Union
+import numpy as np
+from stlearn.em import run_pca,run_diffmap
+from stlearn.pp import neighbors
+
+def local_level(
+    adata: AnnData,
+    use_label_lvl2: str = "louvain",
+    use_data_lvl2: str = "X_diffmap",
+    cluster: int = 9,
+    method: str = "max",
+    copy: bool = False,
+) -> Optional[AnnData]:
+    
+    print("Start construct trajectory for subcluster " + str(cluster))
+    
+    tmp=adata.obs[adata.obs[use_label_lvl2]==str(cluster)]
+    cluster_data = adata[list(tmp.index)]
+
+
+    #run_pca(cluster_data, svd_solver='arpack')
+    #neighbors(cluster_data, n_neighbors=4, n_pcs=20)
+
+    #run_diffmap(cluster_data)
+
+
+    #neighbors(cluster_data, n_neighbors=10, use_rep=use_data_lvl2)
+
+    #from stlearn.external.scanpy.api.tl import draw_graph
+
+    #draw_graph(cluster_data)
+    #from sklearn.metrics import pairwise_distances_argmin_min
+    #x = [p[0] for p in cluster_data.obsm["X_draw_graph_fa"]]
+    #y = [p[1] for p in cluster_data.obsm["X_draw_graph_fa"]]
+    #centroid = (sum(x) / len(cluster_data.obsm["X_draw_graph_fa"]), sum(y) / len(cluster_data.obsm["X_draw_graph_fa"]))
+    #closest, _ = pairwise_distances_argmin_min([centroid], cluster_data.obsm["X_draw_graph_fa"])
+    #cluster_data.uns["cluster_" +str(cluster) +'_iroot'] = closest[0]
+
+    #from stlearn.external.scanpy.api.tl import dpt
+    #dpt(cluster_data)
+
+    
+
+    average_time = {}
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler()
+    scale = scaler.fit_transform(cluster_data.obs['dpt_pseudotime'].values.reshape(-1,1)).reshape(-1,1)
+    cluster_data.obs["dpt_pseudotime"] = scale 
+    
+    adata.uns["local_cluster_"+str(cluster)] = cluster_data
+    
+    for subcl in cluster_data.obs["sub_cluster_labels"].unique():
+        if method == "max":
+            average_time[subcl] = cluster_data.obs[cluster_data.obs["sub_cluster_labels"]==subcl]["dpt_pseudotime"].max()
+        elif method == "mean":
+            average_time[subcl] = cluster_data.obs[cluster_data.obs["sub_cluster_labels"]==subcl]["dpt_pseudotime"].mean()
+        else:
+            average_time[subcl] = cluster_data.obs[cluster_data.obs["sub_cluster_labels"]==subcl]["dpt_pseudotime"].median()
+        
+
+    adata.uns["cluster_" +str(cluster) + "_dpt"] = average_time
+
+    return adata if copy else None
+
+
+        
+
+
+    
