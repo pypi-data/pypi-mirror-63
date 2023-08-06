e4cfeac853333c5b1c26597a81ce45b703117f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:29 +0000

    Add package django-fido/0.21/django_fido-0.21-py3-none-any.whl

---
+++ packages/django-fido/django_fido-0.21-py3-none-any.whl/django_fido/settings.py
@@ -0,0 +1,24 @@
+"""Django settings specific for django_fido."""
+from __future__ import unicode_literals
+
+from appsettings import AppSettings, BooleanSetting, CallablePathSetting, NestedListSetting, StringSetting
+
+
+class DjangoFidoSettings(AppSettings):
+    """Application specific settings."""
+
+    authentication_backends = NestedListSetting(
+        inner_setting=CallablePathSetting(),
+        default=('django.contrib.auth.backends.ModelBackend',),
+        transform_default=True,
+    )
+    rp_name = StringSetting(default=None)
+    two_step_auth = BooleanSetting(default=True)
+
+    class Meta:
+        """Meta class."""
+
+        setting_prefix = 'django_fido_'
+
+
+SETTINGS = DjangoFidoSettings()
