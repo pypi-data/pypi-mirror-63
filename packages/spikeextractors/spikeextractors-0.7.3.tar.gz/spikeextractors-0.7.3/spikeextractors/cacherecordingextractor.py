fb5e234542b188f5efa0f06916de53d084170218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package spikeextractors/0.7.3/spikeextractors-0.7.3.tar.gz

---
+++ packages/spikeextractors/spikeextractors-0.7.3.tar.gz/spikeextractors-0.7.3/spikeextractors/cacherecordingextractor.py
@@ -0,0 +1,42 @@
+from spikeextractors.extractors.bindatrecordingextractor import BinDatRecordingExtractor
+from spikeextractors import RecordingExtractor
+import tempfile
+from pathlib import Path
+import os
+import shutil
+
+
+class CacheRecordingExtractor(BinDatRecordingExtractor, RecordingExtractor):
+
+    extractor_name = 'CacheRecordingExtractor'
+
+    def __init__(self, recording, chunk_size=None):
+        RecordingExtractor.__init__(self)  # init tmp folder before constructing BinDatRecordingExtractor
+        tmp_folder = self.get_tmp_folder()
+        self._recording = recording
+        self._tmp_file = tempfile.NamedTemporaryFile(suffix=".dat", dir=tmp_folder).name
+        dtype = recording.get_traces(start_frame=0, end_frame=2).dtype
+        recording.write_to_binary_dat_format(save_path=self._tmp_file, dtype=dtype, chunk_size=chunk_size)
+        BinDatRecordingExtractor.__init__(self, self._tmp_file, numchan=recording.get_num_channels(),
+                                          recording_channels=recording.get_channel_ids(),
+                                          sampling_frequency=recording.get_sampling_frequency(),
+                                          dtype=dtype)
+        self.set_tmp_folder(tmp_folder)
+        self.copy_channel_properties(recording)
+
+    def __del__(self):
+        try:
+            os.remove(self._tmp_file)
+        except Exception as e:
+            print("Unable to remove temporary file", e)
+
+    @property
+    def filename(self):
+        return self._tmp_file
+
+    def save_to_file(self, save_path):
+        save_path = Path(save_path)
+        if save_path.suffix != '.dat' and save_path.suffix != '.bin':
+            save_path = save_path.with_suffix('.dat')
+        shutil.move(self._tmp_file, str(save_path))
+        self._tmp_file = str(save_path)
---
+++ packages/spikeextractors/spikeextractors-0.7.3.tar.gz/spikeextractors-0.7.3/spikeextractors/extractors/openephysextractors/openephysextractors.py
@@ -0,0 +1,88 @@
+from spikeextractors import RecordingExtractor, SortingExtractor
+import numpy as np
+
+
+try:
+    import pyopenephys
+    HAVE_OE = True
+except ImportError:
+    HAVE_OE = False
+
+class OpenEphysRecordingExtractor(RecordingExtractor):
+
+    extractor_name = 'OpenEphysRecordingExtractor'
+    has_default_locations = False
+    installed = HAVE_OE  # check at class level if installed or not
+    is_writable = False
+    mode = 'file'
+    extractor_gui_params = [
+        {'name': 'file_path', 'type': 'file', 'title': "str, Path to file"},
+        {'name': 'experiment_id', 'type': 'int', 'value':0, 'default':0, 'title': "Experiment ID"},
+        {'name': 'recording_id', 'type': 'int', 'value':0, 'default':0, 'title': "Recording ID"},
+        {'name': 'dtype', 'type': 'str',  'value':'float', 'default':'float', 'title':"dtype ('float' or 'int')"},
+    ]
+
+    installation_mesg = "To use the OpenEphys extractor, install pyopenephys: \n\n pip install pyopenephys\n\n"  # error message when not installed
+
+    def __init__(self, file_path, *, experiment_id=0, recording_id=0, dtype='float'):
+        assert HAVE_OE, "To use the OpenEphys extractor, install pyopenephys: \n\n pip install pyopenephys\n\n"
+        assert dtype == 'int16' or 'float' in dtype, "'dtype' can be int16 (memory map) or 'float' (load into memory)"
+        RecordingExtractor.__init__(self)
+        self._recording_file = file_path
+        self._recording = pyopenephys.File(file_path).experiments[experiment_id].recordings[recording_id]
+        self._dtype = dtype
+
+    def get_channel_ids(self):
+        return list(range(self._recording.analog_signals[0].signal.shape[0]))
+
+    def get_num_frames(self):
+        return self._recording.analog_signals[0].signal.shape[1]
+
+    def get_sampling_frequency(self):
+        return float(self._recording.sample_rate.rescale('Hz').magnitude)
+
+    def get_traces(self, channel_ids=None, start_frame=None, end_frame=None):
+        start_frame, end_frame = self._cast_start_end_frame(start_frame, end_frame)
+        if start_frame is None:
+            start_frame = 0
+        if end_frame is None:
+            end_frame = self.get_num_frames()
+        if channel_ids is None:
+            channel_ids = self.get_channel_ids()
+        if self._dtype == 'int16':
+            return self._recording.analog_signals[0].signal[channel_ids, start_frame:end_frame]
+        elif self._dtype == 'float':
+            return self._recording.analog_signals[0].signal[channel_ids, start_frame:end_frame] * \
+                   self._recording.analog_signals[0].gain
+
+
+class OpenEphysSortingExtractor(SortingExtractor):
+
+    extractor_name = 'OpenEphysSortingExtractor'
+    installed = HAVE_OE  # check at class level if installed or not
+    is_writable = False
+    mode = 'file'
+    installation_mesg = "To use the OpenEphys extractor, install pyopenephys: \n\n pip install pyopenephys\n\n"  # error message when not installed
+
+    def __init__(self, file_path, *, experiment_id=0, recording_id=0):
+        assert HAVE_OE, "To use the OpenEphys extractor, install pyopenephys: \n\n pip install pyopenephys\n\n"
+        SortingExtractor.__init__(self)
+        self._recording_file = file_path
+        self._recording = pyopenephys.File(file_path).experiments[experiment_id].recordings[recording_id]
+        self._spiketrains = self._recording.spiketrains
+        self._unit_ids = list([np.unique(st.clusters)[0] for st in self._spiketrains])
+        self._sampling_frequency = float(self._recording.sample_rate.rescale('Hz').magnitude)
+
+    def get_unit_ids(self):
+        return self._unit_ids
+
+    def get_unit_spike_train(self, unit_id, start_frame=None, end_frame=None):
+        start_frame, end_frame = self._cast_start_end_frame(start_frame, end_frame)
+        if start_frame is None:
+            start_frame = 0
+        if end_frame is None:
+            end_frame = np.Inf
+        st = self._spiketrains[unit_id]
+        inds = np.where((start_frame <= (st.times * self._recording.sample_rate)) &
+                        ((st.times * self._recording.sample_rate) < end_frame))
+        return (st.times[inds] * self._recording.sample_rate).magnitude
