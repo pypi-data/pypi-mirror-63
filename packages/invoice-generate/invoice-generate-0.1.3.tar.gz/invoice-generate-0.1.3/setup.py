6b0317a6ccf0685e832545e16f6b696600d3d186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:18 +0000

    Add package invoice-generate/0.1.3/invoice-generate-0.1.3.tar.gz

---
+++ packages/invoice-generate/invoice-generate-0.1.3.tar.gz/invoice-generate-0.1.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['invoice_generate']
+
+package_data = \
+{'': ['*'], 'invoice_generate': ['templates/*']}
+
+install_requires = \
+['click>=7.0,<8.0',
+ 'jinja2>=2.10.3,<3.0.0',
+ 'pdfkit>=0.6.1,<0.7.0',
+ 'python-dateutil>=2.8.1,<3.0.0',
+ 'toml>=0.10.0,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['invoice-generate = invoice_generate:main']}
+
+setup_kwargs = {
+    'name': 'invoice-generate',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Eric Prykhodko',
+    'author_email': 'eprykhodko@bcdtriptech.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
