296d2cea77396a984f45221e7de9528703e65e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:28 +0000

    Add package pymail-io/0.0.3/pymail-io-0.0.3.tar.gz

---
+++ packages/pymail-io/pymail-io-0.0.3.tar.gz/pymail-io-0.0.3/pymail_io/callables.py
@@ -0,0 +1,122 @@
+"""
+_Callables
+Low level functions that must remain unattached to any contexts
+This is because within the task queue these functions get picked.
+- format_msg
+- unit_of_work_callable
+- send_email_with_async
+"""
+import smtplib
+import ssl
+from typing import Dict, Any, Union, List, Awaitable
+from datetime import datetime
+from email.message import EmailMessage
+
+
+def format_msg(
+        subject: str,
+        body: str,
+        sender_email: str,
+        receiver_email: Union[List[str], str],
+) -> EmailMessage:
+    """
+    :param subject:
+    :param body:
+    :param sender_email:
+    :param receiver_email:
+    :return:
+    """
+    message = EmailMessage()
+    message["Subject"] = subject
+    message["From"] = sender_email
+    if isinstance(receiver_email, list):
+        message["To"] = ", ".join(receiver_email)
+    else:
+        message["To"] = receiver_email
+    message.preamble = f"{body}"
+    return message
+
+
+def unit_of_work_callable(
+        sender_email: str,
+        password: str,
+        receiver_email: str,
+        host: str,
+        port: int,
+        subject: str,
+        body: str,
+) -> Dict[str, Any]:
+    """
+    :param sender_email:
+    :param password:
+    :param receiver_email:
+    :param host:
+    :param port:
+    :param subject:
+    :param body:
+    :return:
+    """
+    time_sent = None
+    _SSL_CONTEXT = ssl.create_default_context()
+    message = format_msg(subject, body, sender_email, receiver_email)
+    with smtplib.SMTP_SSL(host, port, context=ssl.SSLContext()) as server:
+        try:
+            server.login(sender_email, password)
+            time_sent = datetime.now()
+            server.send_message(message)
+        except smtplib.SMTPAuthenticationError as err:
+            Warning(
+                "PyMailIO Error: Couldn't authenticate email senders credentials"
+            )
+        finally:
+            server.quit()
+    return {
+        "sent_email": {
+            "subject": subject,
+            "body": body,
+            "time_sent": time_sent,
+        },
+    }
+
+
+async def send_email_with_async(
+        sender_email: str,
+        password: str,
+        receiver_email: str,
+        host: str,
+        port: int,
+        subject: str,
+        body: str,
+) -> Dict[str, Any]:
+    """
+    :param sender_email:
+    :param password:
+    :param receiver_email:
+    :param host:
+    :param port:
+    :param subject:
+    :param body:
+    :return:
+    """
+    time_sent = None
+    _SSL_CONTEXT = ssl.create_default_context()
+    message = format_msg(subject, body, sender_email, receiver_email)
+
+    async with smtplib.SMTP_SSL(host, port, context=ssl.SSLContext()) as server:
+        try:
+            await server.login(sender_email, password)
+            time_sent = datetime.now()
+            await server.send_message(message)
+        except smtplib.SMTPAuthenticationError as err:
+            Warning(
+                "PyMailIO Error: Couldn't authenticate email senders credentials"
+            )
+        finally:
+            await server.quit()
+    return {
+        "sent_email": {
+            "subject": subject,
+            "body": body,
+            "time_sent": time_sent,
+        }
+    }
