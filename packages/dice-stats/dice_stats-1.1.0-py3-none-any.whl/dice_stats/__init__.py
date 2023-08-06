ca4749bade430197e39a2c8359f43dbb1307ee31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package dice-stats/1.1.0/dice_stats-1.1.0-py3-none-any.whl

---
+++ packages/dice-stats/dice_stats-1.1.0-py3-none-any.whl/dice_stats/__init__.py
@@ -0,0 +1,13 @@
+"""
+Advanced Dice statistics without the headaches.
+
+More description to come.
+"""
+
+from .dice import Dice
+from .range import Range
+
+__all__ = [
+    "Dice",
+    "Range",
+]
