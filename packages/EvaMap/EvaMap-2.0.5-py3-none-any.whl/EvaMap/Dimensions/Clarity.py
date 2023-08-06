0ef808c521804ac4cb63e08dc442f700aaf030b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package EvaMap/2.0.5/EvaMap-2.0.5-py3-none-any.whl

---
+++ packages/EvaMap/EvaMap-2.0.5-py3-none-any.whl/EvaMap/Dimensions/Clarity.py
@@ -0,0 +1,10 @@
+from EvaMap.Dimensions.Dimension import Dimension
+from EvaMap.Metrics.humanReadable import humanReadableURIs
+from EvaMap.Metrics.humanDesc import humanDesc
+from EvaMap.Metrics.longTerm import longTerm
+
+
+class Clarity(Dimension) :
+
+    def __init__(self, nom='Clarity', list_metrics=[humanReadableURIs, humanDesc]):
+        super().__init__(nom, list_metrics)
