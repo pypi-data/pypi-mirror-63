7a49c3da0fc2c57256ed2dfcedba172a3915e172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:57 +0000

    Add package predecon-exioreed/0.1/predecon-exioreed-0.1.tar.gz

---
+++ packages/predecon-exioreed/predecon-exioreed-0.1.tar.gz/predecon-exioreed-0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name='predecon-exioreed',
+    version='0.1',
+    author='Maximilian Kaulmann',
+    author_email='exioreed@ownlink.eu',
+    description='PreDeCon - An Implementation in Python, Compatible With Scikit-Learn',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/exioReed/PreDeCon.git',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'joblib>=0.14.0',
+        'numpy>=1.15.4',
+        'scikit-learn>=0.22.1',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+    ],
+    python_requires='>=3.6',
+)
