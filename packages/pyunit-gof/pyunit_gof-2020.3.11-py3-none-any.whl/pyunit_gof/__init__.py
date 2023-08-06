6ec6b2768e1450aaae29971c781aded2f05d53ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:40 +0000

    Add package pyunit-gof/2020.3.11/pyunit_gof-2020.3.11-py3-none-any.whl

---
+++ packages/pyunit-gof/pyunit_gof-2020.3.11-py3-none-any.whl/pyunit_gof/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2020/3/11 9:57
+# @Author: Jtyoui@qq.com
+from .observer import IObservable, IObserver
+from .singleton import Singleton
+from .state1 import IState1, IStated
+from .state2 import IState2, ContextSate
+from .mediator import IMediator, Mediator
+from .clone import Clone
+from .chain import Chain, IChain
+from .proxy import IProxy
+
+__version__ = '2020.3.11'
+__author__ = 'Jtyoui'
+__description__ = '设计模式接口'
+__email__ = 'jtyoui@qq.com'
+__names__ = 'pyUnit_gof'
+__url__ = 'https://github.com/PyUnit/pyunit-gof'
---
+++ packages/pyunit-gof/pyunit_gof-2020.3.11-py3-none-any.whl/pyunit_gof/state1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2020/3/14 13:03
+# @Author: Jtyoui@qq.com
+from abc import ABCMeta, abstractmethod
+
+
+class IState1(metaclass=ABCMeta):
+
+    @abstractmethod
+    def behavior(self, *args, **kwargs):
+        """具体的行为状态"""
+        pass
+
+
+class IStated(metaclass=ABCMeta):
+
+    @abstractmethod
+    def __init__(self, state: IState1):
+        self.state = state
+
+    @abstractmethod
+    def set_state(self, state: IState1):
+        """改变不同的状态类,去实现不同的功能
+
+        self.state = state
+        state.behavior(self)
+        """
+        pass
+
+    @abstractmethod
+    def flag_state(self, *args, **kwargs):
+        """根据不同的情况实现不同的状态类
+
+        if flag==1:
+            self.set_state(IState)
+        elif flag==2:
+            self.set_state(IState)
+        """
+        pass
+
+    @abstractmethod
+    def behavior(self, *args, **kwargs):
+        """具体的行为状态"""
+        pass
