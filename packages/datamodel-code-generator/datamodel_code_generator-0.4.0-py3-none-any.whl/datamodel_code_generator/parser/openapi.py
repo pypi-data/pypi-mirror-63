779d418fbdf7eac93bf27339f85b39c838026690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:09 +0000

    Add package datamodel-code-generator/0.4.0/datamodel_code_generator-0.4.0-py3-none-any.whl

---
+++ packages/datamodel-code-generator/datamodel_code_generator-0.4.0-py3-none-any.whl/datamodel_code_generator/parser/openapi.py
@@ -0,0 +1,19 @@
+from typing import Dict
+
+from prance import BaseParser
+
+from datamodel_code_generator import snooper_to_methods
+from datamodel_code_generator.parser.jsonschema import JsonSchemaParser
+
+
+@snooper_to_methods(max_variable_length=None)
+class OpenAPIParser(JsonSchemaParser):
+    def parse_raw(self) -> None:
+        base_parser = BaseParser(
+            spec_string=self.text, backend='openapi-spec-validator'
+        )
+
+        for obj_name, raw_obj in base_parser.specification['components'][
+            'schemas'
+        ].items():  # type: str, Dict
+            self.parse_raw_obj(obj_name, raw_obj)
