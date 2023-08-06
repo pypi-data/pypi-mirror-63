046a551f367a6aec0141aebcc86fbd1b789a7a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:12 +0000

    Add package sphericalpolygon/1.1.0/sphericalpolygon-1.1.0-py3-none-any.whl

---
+++ packages/sphericalpolygon/sphericalpolygon-1.1.0-py3-none-any.whl/sphericalpolygon/__init__.py
@@ -0,0 +1,11 @@
+'''
+sphericalpolygon package
+
+This package is an archive of scientific routines for spherical polygons.   
+Currently, operations on spherical polygons include:
+    calculating area or mass(if the area density is given) and perimeter
+    geometrical or physical moment of inertia tensor
+    determining whether one or more points are inside the spherical polygon.
+'''
+
+from .create_polygon import create_polygon
