27b796df4aba2321b5122a7bd74d03d842cf683f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:32 +0000

    Add package mlbench-core/2.3.0.dev58/mlbench_core-2.3.0.dev58.tar.gz

---
+++ packages/mlbench-core/mlbench_core-2.3.0.dev58.tar.gz/mlbench_core-2.3.0.dev58/mlbench_core/dataset/translation/pytorch/config.py
@@ -0,0 +1,13 @@
+PAD_TOKEN = "<pad>"
+UNK_TOKEN = "<unk>"
+BOS_TOKEN = "<s>"
+EOS_TOKEN = "<\s>"
+
+
+VOCAB_FNAME = "vocab.bpe.32000"
+
+TRAIN_FNAME = "train.tok.clean.bpe.32000"
+VAL_FNAME = "newstest2013.tok.bpe.32000"
+TEST_FNAME = "newstest2014.tok.bpe.32000"
+
+EXTS = (".en", ".de")
