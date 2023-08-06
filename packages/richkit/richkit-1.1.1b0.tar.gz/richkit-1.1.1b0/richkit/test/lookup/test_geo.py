afcb9a159a9f37b72e7af08b179113c328b2f4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:21 +0000

    Add package richkit/1.1.1b0/richkit-1.1.1b0.tar.gz

---
+++ packages/richkit/richkit-1.1.1b0.tar.gz/richkit-1.1.1b0/richkit/test/lookup/test_geo.py
@@ -0,0 +1,13 @@
+from richkit import lookup
+import unittest
+
+
+class LookupTestCase(unittest.TestCase):
+
+    def test_country(self):
+        country = lookup.country("8.8.8.8")
+        assert country == 'US'
+
+    def test_asn(self):
+        asn = lookup.asn("8.8.8.8")
+        assert asn == 'AS15169'
