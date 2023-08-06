edbb4941efd7497d8652e8e59b63d08c9bd70d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:12 +0000

    Add package nss3/4.5.16/nss3-4.5.16.tar.gz

---
+++ packages/nss3/nss3-4.5.16.tar.gz/nss3-4.5.16/setup.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+'''
+setup(
+     name="nanshansql",
+     version="3.7.1",
+     author="JingRui Zhu",
+     author_email="870239526@qq.com",
+     url='https://pypi.org/',
+     description="Simple and fast database operation",
+     long_description=open("README.rst").read(),
+     license="MIT",
+     #url="https://github.com/desion/tidy_page",
+     packages=['nssql'],
+     install_requires=[],
+     classifiers=[
+          "Topic :: Text Processing :: Indexing"
+          ],
+     )
+mport setuptools
+
+ 
+
+'''
+ 
+
+setup(
+    name="nss3",
+    version="4.5.16",
+    author="Jingrui Zhu",
+    author_email="870239526@qq.com",
+    description="Simple and fast database operation",
+    long_description=open("README.rst").read(),
+    url="https://pypi.org/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
