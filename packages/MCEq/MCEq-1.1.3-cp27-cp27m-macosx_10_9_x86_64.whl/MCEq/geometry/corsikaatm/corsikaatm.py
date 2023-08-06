ed06bb29de1e03480ede8bcd787b87faa7764726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package MCEq/1.1.3/MCEq-1.1.3-cp27-cp27m-macosx_10_9_x86_64.whl

---
+++ packages/MCEq/MCEq-1.1.3-cp27-cp27m-macosx_10_9_x86_64.whl/MCEq/geometry/corsikaatm/corsikaatm.py
@@ -0,0 +1,36 @@
+from ctypes import (cdll, Structure, c_int, c_double, POINTER)
+import os
+import sysconfig
+
+base = os.path.dirname(os.path.abspath(__file__))
+suffix = sysconfig.get_config_var('EXT_SUFFIX')
+
+for fn in os.listdir(base):
+    if 'libcorsikaatm' in fn and fn.endswith(suffix):
+        corsika_acc = cdll.LoadLibrary(os.path.join(base, fn))
+        break
+
+for func in [
+        corsika_acc.corsika_get_density, corsika_acc.planar_rho_inv,
+        corsika_acc.corsika_get_m_overburden
+]:
+    func.restype = c_double
+
+
+def corsika_get_density(h_cm, a, b, c, t, hl):
+    """Wrap arguments for ctypes function"""
+    return corsika_acc.corsika_get_density(
+        c_double(h_cm), a.ctypes, b.ctypes, c.ctypes, t.ctypes, hl.ctypes)
+
+
+def planar_rho_inv(X, cos_theta, a, b, c, t, hl):
+    """Wrap arguments for ctypes function"""
+    return corsika_acc.planar_rho_inv(
+        c_double(X), c_double(cos_theta), a.ctypes, b.ctypes, c.ctypes,
+        t.ctypes, hl.ctypes)
+
+
+def corsika_get_m_overburden(h_cm, a, b, c, t, hl):
+    """Wrap arguments for ctypes function"""
+    return corsika_acc.corsika_get_m_overburden(
+        c_double(h_cm), a.ctypes, b.ctypes, c.ctypes, t.ctypes, hl.ctypes)
