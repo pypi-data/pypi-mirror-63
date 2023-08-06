733a6c1584103947873f6d6e657914c79ca9075e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package caaalle/3.0.0/caaalle-3.0.0-py3-none-any.whl

---
+++ packages/caaalle/caaalle-3.0.0-py3-none-any.whl/caaalle/math.py
@@ -0,0 +1,34 @@
+def add(initial: int=0, number: int=0) -> int:
+    """Return sum of *intial* and *number*.
+    :param initial: Initial value.
+    :type initial: int
+    :param number: Value to add to initial.
+    :type number: int
+    :return: Sum of initial and number.
+    :rtype: int
+    """
+    return initial + number
+
+
+def multiply(initial: int=0, number: int=0) -> int:
+    """Return product of *intial* and *number*.
+    :param initial: Initial value.
+    :type initial: int
+    :param number: Value to add to initial.
+    :type number: int
+    :return: Product of initial and number.
+    :rtype: int
+    """
+    return initial * number
+
+
+def pow(initial: int=0, number: int=0) -> int:
+    """Return power of *intial* and *number*.
+    :param initial: Initial value.
+    :type initial: int
+    :param number: Value to add to initial.
+    :type number: int
+    :return: Power of initial and number.
+    :rtype: int
+    """
+    return initial ** number
