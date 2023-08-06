881dbddba776de25e012237c9af95c18a0f02995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:12 +0000

    Add package rx7/1.0.0/rx7-1.0.0.tar.gz

---
+++ packages/rx7/rx7-1.0.0.tar.gz/rx7-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+s = setup(
+    name="rx7",
+    version="1.0.0",
+    license="MIT",
+    description="RX7 Module",
+    url="https://github.com/Ramin-RX7/rx7Module",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires = ">= 3.4",
+    author="Ramin RX7",
+    author_email="rawmin.rx@gmail.com",
+    )
