1d16b80127258ccb5702b1d7b7260621406adcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:17 +0000

    Add package ezcad-plugins/0.2.3/ezcad_plugins-0.2.3-cp37-cp37m-win_amd64.whl

---
+++ packages/ezcad-plugins/ezcad_plugins-0.2.3-cp37-cp37m-win_amd64.whl/ezcad_plugins/gotsurf/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Ezcad Development Team. All Rights Reserved.
+
+#: Default surface style.
+SURF_STYLE = {
+    'face_pure': True,
+    'face_color': (0, 0, 255, 255),
+    'draw_edge': True,
+    'draw_face': True,
+    'edge_color': (255, 0, 0, 255),
+    'shader': None,
+    'smooth': False,
+    'computeNormals': False,
+}
