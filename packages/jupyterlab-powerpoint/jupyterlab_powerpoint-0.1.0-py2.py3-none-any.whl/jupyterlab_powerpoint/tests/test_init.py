74695393d9358f5c469073df81cff1446faa53b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:05 +0000

    Add package jupyterlab-powerpoint/0.1.0/jupyterlab_powerpoint-0.1.0-py2.py3-none-any.whl

---
+++ packages/jupyterlab-powerpoint/jupyterlab_powerpoint-0.1.0-py2.py3-none-any.whl/jupyterlab_powerpoint/tests/test_init.py
@@ -0,0 +1,7 @@
+# for Coverage
+from jupyterlab_powerpoint import _jupyter_server_extension_paths
+
+
+class TestInit:
+    def test__jupyter_server_extension_paths(self):
+        assert _jupyter_server_extension_paths() == [{"module": "jupyterlab_powerpoint.extension"}]
