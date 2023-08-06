9a6e62d1b8eb262a4efcc3237bf35c7e79e4f2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:03 +0000

    Add package spkit/0.0.8/spkit-0.0.8.tar.gz

---
+++ packages/spkit/spkit-0.0.8.tar.gz/spkit-0.0.8/docs/index.rst
@@ -0,0 +1,9 @@
+Signal Processing Toolkit Documentation!
+=====================================
+
+This package includes following components:
+
+.. toctree::
+   :maxdepth: 2
+
+   README
---
+++ packages/spkit/spkit-0.0.8.tar.gz/spkit-0.0.8/spkit/__init__.py
@@ -0,0 +1,19 @@
+name = "Signal Processing tool kit"
+
+__version__ = '0.0.7'
+__author__ = 'Nikesh Bajaj'
+import sys, os
+
+sys.path.append(os.path.dirname(__file__))
+
+from .infotheory import (entropy, entropy_joint, entropy_cond, mutual_Info,entropy_kld,entropy_cross,HistPlot,binSize_FD)
+#import infotheory as it
+
+#ICA
+from .matDecomposition import ICA, SVD
+import ml
+
+#LFSR
+#from .pylfsr import LFSR
+
+__all__ = ['ICA', 'SVD','pylfsr', 'ml', 'example','data']
