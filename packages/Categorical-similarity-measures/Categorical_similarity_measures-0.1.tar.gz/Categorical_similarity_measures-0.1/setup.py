074c917d06ed7343420582099901788b3e149780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:21 +0000

    Add package Categorical-similarity-measures/0.1/Categorical_similarity_measures-0.1.tar.gz

---
+++ packages/Categorical-similarity-measures/Categorical_similarity_measures-0.1.tar.gz/Categorical_similarity_measures-0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'Categorical_similarity_measures',
+  packages = ['Categorical_similarity_measures'],
+  version = '0.1',
+  license='MIT',
+  description = 'Similarity Measures Utility Package',
+  author = 'Ajay Mukund S',
+  author_email = 'ajaymukund1998@gmail.com',
+  url = 'https://github.com/AjayMukundS/Categorical_similarity_measures',
+  download_url = 'https://github.com/AjayMukundS/Categorical_similarity_measures/archive/v_01.tar.gz',
+  keywords = ['Similarity', 'Distance', 'Categorical data'],
+  install_requires=[
+          'pandas',
+          'numpy',
+          'category_encoders',
+          'math',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
