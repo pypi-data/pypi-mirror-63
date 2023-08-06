48cdf6cfd7062b3d7016cccaaf9246fde5af16dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:24 +0000

    Add package niceback/0.2.0/niceback-0.2.0.tar.gz

---
+++ packages/niceback/niceback-0.2.0.tar.gz/niceback-0.2.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from niceback import __version__
+
+setuptools.setup(
+    name="niceback",
+    version=__version__,
+    author="L. Kärkkäinen",
+    author_email="tronic@noreply.users.github.com",
+    description="Human-readable tracebacks in Jupyter notebooks",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tronic/niceback",
+    packages=setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        "html5tagger>=1.0.0",
+    ],
+    include_package_data = True,
+)
