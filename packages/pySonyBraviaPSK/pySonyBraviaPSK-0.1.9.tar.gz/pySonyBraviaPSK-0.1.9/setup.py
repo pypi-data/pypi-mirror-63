0a8a5c22bbcb708e84417bd200d143346817a359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:36 +0000

    Add package pySonyBraviaPSK/0.1.9/pySonyBraviaPSK-0.1.9.tar.gz

---
+++ packages/pySonyBraviaPSK/pySonyBraviaPSK-0.1.9.tar.gz/pySonyBraviaPSK-0.1.9/setup.py
@@ -0,0 +1,16 @@
+"""Setup sony_bravia_psk package."""
+from setuptools import setup, find_packages
+
+setup(
+    name='pySonyBraviaPSK',
+    version='0.1.9',
+    description='Library for Sony Bravia TVs with Pre-Shared Key option',
+    url='https://github.com/gerard33/sony_bravia_psk',
+    maintainer='Gerard',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords='Sony Bravia TV PSK for Home Assistant',
+    include_package_data=True,
+    zip_safe=False
+)
