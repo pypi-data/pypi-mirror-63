e47c9fdadc230ad84beada89595d567a4fe7624e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:46 +0000

    Add package zhang-hello/1.0/zhang_hello-1.0.zip

---
+++ packages/zhang-hello/zhang_hello-1.0.zip/zhang_hello-1.0/setup.py
@@ -0,0 +1,11 @@
+# from setuptools import setup
+from distutils.core import setup
+
+setup(
+    name = 'zhang_hello',
+    version = '1.0',
+    author = 'Zhang',
+    license = 'MIT',
+    packages = ['zhang_hello'],
+    description = 'My first setup files',
+    )
