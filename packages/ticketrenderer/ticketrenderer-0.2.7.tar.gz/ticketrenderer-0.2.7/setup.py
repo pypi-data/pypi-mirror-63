28c43830d396c04655434259b384c75d95987425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:22 +0000

    Add package ticketrenderer/0.2.7/ticketrenderer-0.2.7.tar.gz

---
+++ packages/ticketrenderer/ticketrenderer-0.2.7.tar.gz/ticketrenderer-0.2.7/setup.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "ticketrenderer",
+    packages = ["ticketrenderer"],
+    version = "0.2.7",
+    author = "Benoît Guigal",
+    author_email = "benoit@postcardgroup.com",
+    description = ("A library used to render Figure tickets from ticket templates"),
+    url = "https://github.com/Postcard/ticket-renderer-python",
+    download_url = 'https://github.com/Postcard/ticket-renderer-python/tarball/0.2.7',
+    py_modules=('ticketrenderer',),
+    install_requires=[
+        'jinja2==2.7.3',
+        'mock==1.0.1'
+    ]
+)
