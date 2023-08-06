a7082424270f805246e3a65ced5b074b16c255e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:56 +0000

    Add package pytonik/1.9.10/pytonik-1.9.10-py3-none-any.whl

---
+++ packages/pytonik/pytonik-1.9.10-py3-none-any.whl/pytonik/Functions/__init__.py
@@ -0,0 +1,16 @@
+###
+# Author : Betacodings
+# Author : info@betacodings.com
+# Maintainer By: Emmanuel Martins
+# Maintainer Email: emmamartinscm@gmail.com
+# Created by Betacodings on 2019.
+###
+
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+
+from pytonik.cmd import start
+from pytonik.cmd import server
+from pytonik.cmd import install
+from pytonik.cmd import doc
