5f29045a4c4bc5de73e3c5f5c1da8aed8464f9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:09 +0000

    Add package cutadapt/2.9/cutadapt-2.9.tar.gz

---
+++ packages/cutadapt/cutadapt-2.9.tar.gz/cutadapt-2.9/tests/test_adapters.py
@@ -0,0 +1,178 @@
+import pytest
+
+from dnaio import Sequence
+from cutadapt.adapters import SingleAdapter, SingleMatch, Where, LinkedAdapter, WhereToRemove
+
+
+def test_issue_52():
+    adapter = SingleAdapter(
+        sequence='GAACTCCAGTCACNNNNN',
+        where=Where.BACK,
+        remove=WhereToRemove.SUFFIX,
+        max_error_rate=0.12,
+        min_overlap=5,
+        read_wildcards=False,
+        adapter_wildcards=True)
+    read = Sequence(name="abc", sequence='CCCCAGAACTACAGTCCCGGC')
+    am = SingleMatch(astart=0, astop=17, rstart=5, rstop=21, matches=15, errors=2,
+        remove_before=False, adapter=adapter, read=read)
+    assert am.wildcards() == 'GGC'
+    """
+    The result above should actually be 'CGGC' since the correct
+    alignment is this one:
+
+    adapter         GAACTCCAGTCACNNNNN
+    mismatches           X     X
+    read       CCCCAGAACTACAGTC-CCGGC
+
+    Since we do not keep the alignment, guessing 'GGC' is the best we
+    can currently do.
+    """
+
+
+def test_issue_80():
+    # This issue turned out to not be an actual issue with the alignment
+    # algorithm. The following alignment is found because it has more matches
+    # than the 'obvious' one:
+    #
+    # TCGTATGCCGTCTTC
+    # =========X==XX=
+    # TCGTATGCCCTC--C
+    #
+    # This is correct, albeit a little surprising, since an alignment without
+    # indels would have only two errors.
+
+    adapter = SingleAdapter(
+        sequence="TCGTATGCCGTCTTC",
+        where=Where.BACK,
+        remove=WhereToRemove.SUFFIX,
+        max_error_rate=0.2,
+        min_overlap=3,
+        read_wildcards=False,
+        adapter_wildcards=False)
+    read = Sequence(name="seq2", sequence="TCGTATGCCCTCC")
+    result = adapter.match_to(read)
+    assert result.errors == 3, result
+    assert result.astart == 0, result
+    assert result.astop == 15, result
+
+
+def test_str():
+    a = SingleAdapter('ACGT', where=Where.BACK, remove=WhereToRemove.SUFFIX, max_error_rate=0.1)
+    str(a)
+    str(a.match_to(Sequence(name='seq', sequence='TTACGT')))
+
+
+def test_linked_adapter():
+    front_adapter = SingleAdapter('AAAA', where=Where.PREFIX, min_overlap=4)
+    back_adapter = SingleAdapter('TTTT', where=Where.BACK, min_overlap=3)
+
+    linked_adapter = LinkedAdapter(
+        front_adapter, back_adapter, front_required=True, back_required=False, name='name')
+    assert linked_adapter.front_adapter.min_overlap == 4
+    assert linked_adapter.back_adapter.min_overlap == 3
+
+    sequence = Sequence(name='seq', sequence='AAAACCCCCTTTT')
+    trimmed = linked_adapter.match_to(sequence).trimmed()
+    assert trimmed.name == 'seq'
+    assert trimmed.sequence == 'CCCCC'
+
+
+def test_info_record():
+    adapter = SingleAdapter(
+        sequence='GAACTCCAGTCACNNNNN',
+        where=Where.BACK,
+        max_error_rate=0.12,
+        min_overlap=5,
+        read_wildcards=False,
+        adapter_wildcards=True,
+        name="Foo")
+    read = Sequence(name="abc", sequence='CCCCAGAACTACAGTCCCGGC')
+    am = SingleMatch(astart=0, astop=17, rstart=5, rstop=21, matches=15, errors=2, remove_before=False,
+        adapter=adapter, read=read)
+    assert am.get_info_records() == [[
+        "",
+        2,
+        5,
+        21,
+        'CCCCA',
+        'GAACTACAGTCCCGGC',
+        '',
+        'Foo',
+        '',
+        '',
+        '',
+    ]]
+
+
+def test_random_match_probabilities():
+    a = SingleAdapter('A', where=Where.BACK, max_error_rate=0.1).create_statistics()
+    assert a.back.random_match_probabilities(0.5) == [1, 0.25]
+    assert a.back.random_match_probabilities(0.2) == [1, 0.4]
+
+    for s in ('ACTG', 'XMWH'):
+        a = SingleAdapter(s, where=Where.BACK, max_error_rate=0.1).create_statistics()
+        assert a.back.random_match_probabilities(0.5) == [1, 0.25, 0.25**2, 0.25**3, 0.25**4]
+        assert a.back.random_match_probabilities(0.2) == [1, 0.4, 0.4*0.1, 0.4*0.1*0.4, 0.4*0.1*0.4*0.1]
+
+    a = SingleAdapter('GTCA', where=Where.FRONT, max_error_rate=0.1).create_statistics()
+    assert a.front.random_match_probabilities(0.5) == [1, 0.25, 0.25**2, 0.25**3, 0.25**4]
+    assert a.front.random_match_probabilities(0.2) == [1, 0.4, 0.4*0.1, 0.4*0.1*0.4, 0.4*0.1*0.4*0.1]
+
+
+def test_add_adapter_statistics():
+    stats = SingleAdapter('A', name='name', where=Where.BACK, max_error_rate=0.1).create_statistics()
+    end_stats = stats.back
+    end_stats.adjacent_bases['A'] = 7
+    end_stats.adjacent_bases['C'] = 19
+    end_stats.adjacent_bases['G'] = 23
+    end_stats.adjacent_bases['T'] = 42
+    end_stats.adjacent_bases[''] = 45
+
+    end_stats.errors[10][0] = 100
+    end_stats.errors[10][1] = 11
+    end_stats.errors[10][2] = 3
+    end_stats.errors[20][0] = 600
+    end_stats.errors[20][1] = 66
+    end_stats.errors[20][2] = 6
+
+    stats2 = SingleAdapter('A', name='name', where=Where.BACK, max_error_rate=0.1).create_statistics()
+    end_stats2 = stats2.back
+    end_stats2.adjacent_bases['A'] = 43
+    end_stats2.adjacent_bases['C'] = 31
+    end_stats2.adjacent_bases['G'] = 27
+    end_stats2.adjacent_bases['T'] = 8
+    end_stats2.adjacent_bases[''] = 5
+    end_stats2.errors[10][0] = 234
+    end_stats2.errors[10][1] = 14
+    end_stats2.errors[10][3] = 5
+    end_stats2.errors[15][0] = 90
+    end_stats2.errors[15][1] = 17
+    end_stats2.errors[15][2] = 2
+
+    stats += stats2
+    r = stats.back
+
+    assert r.adjacent_bases == {'A': 50, 'C': 50, 'G': 50, 'T': 50, '': 50}
+    assert r.errors == {
+        10: {0: 334, 1: 25, 2: 3, 3: 5},
+        15: {0: 90, 1: 17, 2: 2},
+        20: {0: 600, 1: 66, 2: 6},
+    }
+
+
+def test_issue_265():
+    """Crash when accessing the matches property of non-anchored linked adapters"""
+    s = Sequence('name', 'AAAATTTT')
+    front_adapter = SingleAdapter('GGG', where=Where.FRONT)
+    back_adapter = SingleAdapter('TTT', where=Where.BACK)
+    la = LinkedAdapter(front_adapter, back_adapter, front_required=False, back_required=False, name='name')
+    assert la.match_to(s).matches == 3
+
+
+@pytest.mark.parametrize("where", [Where.PREFIX, Where.SUFFIX])
+def test_no_indels_empty_read(where):
+    # Issue #376
+    adapter = SingleAdapter('ACGT', where=where, indels=False)
+    empty = Sequence('name', '')
+    adapter.match_to(empty)
---
+++ packages/cutadapt/cutadapt-2.9.tar.gz/cutadapt-2.9/tests/test_modifiers.py
@@ -0,0 +1,59 @@
+from dnaio import Sequence
+from cutadapt.modifiers import (UnconditionalCutter, NEndTrimmer, QualityTrimmer,
+    Shortener, AdapterCutter)
+
+
+def test_unconditional_cutter():
+    UnconditionalCutter(length=5)
+    s = 'abcdefg'
+    assert UnconditionalCutter(length=2)(s, []) == 'cdefg'
+    assert UnconditionalCutter(length=-2)(s, []) == 'abcde'
+    assert UnconditionalCutter(length=100)(s, []) == ''
+    assert UnconditionalCutter(length=-100)(s, []) == ''
+
+
+def test_nend_trimmer():
+    trimmer = NEndTrimmer()
+    seqs = ['NNNNAAACCTTGGNNN', 'NNNNAAACNNNCTTGGNNN', 'NNNNNN']
+    trims = ['AAACCTTGG', 'AAACNNNCTTGG', '']
+    for seq, trimmed in zip(seqs, trims):
+        _seq = Sequence('read1', seq, qualities='#'*len(seq))
+        _trimmed = Sequence('read1', trimmed, qualities='#'*len(trimmed))
+        assert trimmer(_seq, []) == _trimmed
+
+
+def test_quality_trimmer():
+    read = Sequence('read1', 'ACGTTTACGTA', '##456789###')
+
+    qt = QualityTrimmer(10, 10, 33)
+    assert qt(read, []) == Sequence('read1', 'GTTTAC', '456789')
+
+    qt = QualityTrimmer(0, 10, 33)
+    assert qt(read, []) == Sequence('read1', 'ACGTTTAC', '##456789')
+
+    qt = QualityTrimmer(10, 0, 33)
+    assert qt(read, []) == Sequence('read1', 'GTTTACGTA', '456789###')
+
+
+def test_shortener():
+    read = Sequence('read1', 'ACGTTTACGTA', '##456789###')
+
+    shortener = Shortener(0)
+    assert shortener(read, []) == Sequence('read1', '', '')
+
+    shortener = Shortener(1)
+    assert shortener(read, []) == Sequence('read1', 'A', '#')
+
+    shortener = Shortener(5)
+    assert shortener(read, []) == Sequence('read1', 'ACGTT', '##456')
+
+    shortener = Shortener(100)
+    assert shortener(read, []) == read
+
+
+def test_adapter_cutter():
+    from cutadapt.adapters import SingleAdapter, Where
+    a1 = SingleAdapter('GTAGTCCCGC', where=Where.BACK)
+    a2 = SingleAdapter('GTAGTCCCCC', where=Where.BACK)
+    match = AdapterCutter.best_match([a1, a2], Sequence("name", "ATACCCCTGTAGTCCCC"))
+    assert match.adapter is a2
