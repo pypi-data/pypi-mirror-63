4a1add2ed7b25fc4b8c56227c3f68285f5fbb379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:25 +0000

    Add package drf-toolkit/0.0.11/drf_toolkit-0.0.11.tar.gz

---
+++ packages/drf-toolkit/drf_toolkit-0.0.11.tar.gz/drf_toolkit-0.0.11/drf_toolkit/decorators.py
@@ -0,0 +1,31 @@
+from power_dict.schema_validator import SchemaValidator
+from six import wraps
+from drf_toolkit.drf_utils import DrfUtils
+
+def validate_request(schema):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(view, *args, **kwargs):
+            request = view.request
+            context = DrfUtils.get_request_parameters(request)
+            context = SchemaValidator.validate(context, schema)
+            kwargs['context'] = context
+            return view_func(view, *args, **kwargs)
+
+        return _wrapped_view
+
+    return decorator
+
+
+def except_error():
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(view, *args, **kwargs):
+            try:
+                return view_func(view, *args, **kwargs)
+            except Exception as ex:
+                return DrfUtils.generate_bad_response(exception=ex)
+
+        return _wrapped_view
+
+    return decorator
