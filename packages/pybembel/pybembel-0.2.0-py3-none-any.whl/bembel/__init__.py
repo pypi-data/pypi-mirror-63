89cd05a9a954ed1f141e1ca024d52995e9cfc59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:24 +0000

    Add package pybembel/0.2.0/pybembel-0.2.0-py3-none-any.whl

---
+++ packages/pybembel/pybembel-0.2.0-py3-none-any.whl/bembel/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -- Content-Encoding: UTF-8 --
+# Module version
+__version_info__ = (0, 1, 0)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
