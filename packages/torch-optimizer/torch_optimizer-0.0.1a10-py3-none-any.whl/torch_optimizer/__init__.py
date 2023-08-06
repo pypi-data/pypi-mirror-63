8084d3b2e83b029b7a4cda05d27cef075d97c0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:30 +0000

    Add package torch-optimizer/0.0.1a10/torch_optimizer-0.0.1a10-py3-none-any.whl

---
+++ packages/torch-optimizer/torch_optimizer-0.0.1a10-py3-none-any.whl/torch_optimizer/__init__.py
@@ -0,0 +1,29 @@
+from .accsgd import AccSGD
+from .adabound import AdaBound
+from .adamod import AdaMod
+from .diffgrad import DiffGrad
+from .lamb import Lamb
+from .lookahead import Lookahead
+from .novograd import NovoGrad
+from .pid import PID
+from .qhm import QHM
+from .radam import RAdam
+from .sgdw import SGDW
+from .yogi import Yogi
+
+
+__all__ = (
+    'AccSGD',
+    'AdaBound',
+    'AdaMod',
+    'DiffGrad',
+    'Lamb',
+    'Lookahead',
+    'NovoGrad',
+    'PID',
+    'QHM',
+    'RAdam',
+    'SGDW',
+    'Yogi',
+)
+__version__ = '0.0.1a10'
