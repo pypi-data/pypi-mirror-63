623287aff5e0d2c848275645bf1a550044ee9e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:18 +0000

    Add package pyfrost/0.1.3/pyfrost-0.1.3-py3-none-any.whl

---
+++ packages/pyfrost/pyfrost-0.1.3-py3-none-any.whl/frost/client/client.py
@@ -0,0 +1,147 @@
+import json
+from pathlib import Path
+
+from frost.client.auth import get_auth
+from frost.client.events import (
+    Auth,
+    Msgs,
+    messages,
+    login_status,
+    register_status
+)
+from frost.client.socketio import BaseClient, threaded
+from frost.ext import Handler
+
+
+class FrostClient(BaseClient):
+    """The Frost Client.
+
+    :param ip: The IP address of the server to connect to, defaults to '127.0.0.1'
+    :type ip: str, optional
+    :param port: The port of the server to connect to, defaults to 5555
+    :type port: int, optional
+    """
+
+    def __init__(self, ip: str = '127.0.0.1', port: int = 5555) -> None:
+        """The constructor method.
+        """
+        super(FrostClient, self).__init__(ip, port)
+
+        # Load up cogs
+        Auth()
+        Msgs()
+
+        frost_file = Path('.frost')
+
+        if not frost_file.exists():
+            with open(str(frost_file), 'w') as f:
+                json.dump({}, f)
+
+    def __enter__(self) -> 'FrostClient':
+        """The __enter__ method, connects to the server.
+
+        :return: This instance of this class
+        :rtype: 'FrostClient'
+        """
+        self.connect()
+        return self
+
+    def __exit__(self, type_, value, traceback) -> None:
+        """The __exit__ method, closes the connection to the server.
+        """
+        self.close()
+
+    def connect(self) -> None:
+        """Connect to the server and begin listening for events.
+        """
+        super().connect()
+        self._listen()
+
+    @threaded(daemon=True)
+    def _listen(self) -> None:
+        """Listen for events and handle them.
+        """
+        handler = Handler()
+
+        while True:
+            handler.handle(self.recieve())
+
+    def login(self, username: str, password: str) -> int:
+        """Login to the server.
+
+        :param username: The username of the account
+        :type username: str
+        :param password: The password of the account
+        :type password: str
+        :return: Data received from the server
+        :rtype: Any
+        """
+        self.send({
+            'headers': {
+                'path': 'authentication/login'
+            },
+            'username': username,
+            'password': password
+        })
+        return login_status.get_status()
+
+    def register(self, username: str, password: str) -> int:
+        """Register an account on the server.
+
+        :param username: The desired username of the account
+        :type username: str
+        :param password: The desired password of the account
+        :type password: str
+        """
+        self.send({
+            'headers': {
+                'path': 'authentication/register'
+            },
+            'username': username,
+            'password': password
+        })
+        return register_status.get_status()
+
+    @get_auth
+    def send_msg(self, msg: str, token: str, id_: str) -> None:
+        """Send a message to other users on the server.
+
+        :param msg: The desired message to send
+        :type msg: str
+        :param token: The user's token, auto filled by :meth:`frost.client.auth.get_auth`
+        :type token: str
+        :param id_: The user's ID, auto filled by :meth:`frost.client.auth.get_auth`
+        :type id_: str
+        """
+        self.send({
+            'headers': {
+                'token': token,
+                'id': id_,
+                'path': 'messages/send_msg'
+            },
+            'msg': msg
+        })
+
+    @get_auth
+    def get_all_msgs(
+        self,
+        token: str,
+        id_: str
+    ) -> None:
+        """Get all messages from the server. Should not be needed.
+
+        :param token: The user's token, auto filled by :meth:`frost.client.auth.get_auth`
+        :type token: str
+        :param id_: The user's ID, auto filled by :meth:`frost.client.auth.get_auth`
+        :type id_: str
+        :return: All messages
+        :rtype: Dict[str, Dict[str, Union[str, Dict[str, str]]]]
+        """
+        messages.clear()
+        self.send({
+            'headers': {
+                'path': 'messages/get_all_msgs',
+                'token': token,
+                'id': id_
+            }
+        })
---
+++ packages/pyfrost/pyfrost-0.1.3-py3-none-any.whl/tests/run_client.py
@@ -0,0 +1,48 @@
+import time
+
+from frost import FrostClient
+from frost.client import Status, threaded  # NOQA: F401
+from frost.client.events import messages
+
+
+@threaded(daemon=True)
+def check_msgs() -> None:
+    while True:
+        msgs = messages.get_new_msgs()
+
+        if msgs:
+            print(messages.all_messages)
+
+        time.sleep(0.25)
+
+
+@threaded()
+def send_msg(client: 'FrostClient'):
+    while True:
+        msg = input()
+        client.send_msg(msg)
+
+
+def run_client():
+    client = FrostClient()
+    client.connect()
+
+    # reg = None
+    # while reg in (Status.DUPLICATE_USERNAME.value, None):
+    #     reg = client.register(
+    #         input('Register Username: '),
+    #         input('Register Password: ')
+    #     )
+
+    login = None
+    while login in (Status.INVALID_AUTH.value, None):
+        login = client.login(
+            input('Username: '),
+            input('Password: ')
+        )
+
+    send_msg(client)
+    check_msgs()
+
+    # client.get_all_msgs()
+    # client.close()
