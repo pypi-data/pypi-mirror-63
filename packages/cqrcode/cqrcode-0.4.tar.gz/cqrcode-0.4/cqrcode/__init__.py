fbc57ac6eef4f35321b95f5331714dd945bc84d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package cqrcode/0.4/cqrcode-0.4.tar.gz

---
+++ packages/cqrcode/cqrcode-0.4.tar.gz/cqrcode-0.4/cqrcode/__init__.py
@@ -0,0 +1,17 @@
+# coding: utf-8 
+# !/usr/bin/python
+"""
+@File       :   __init__.py.py
+@Author     :   jiaming
+@Modify Time:   2020/3/8 22:46    
+@Contact    :   https://blog.csdn.net/weixin_39541632
+@Version    :   1.0
+@Desciption :   None
+"""
+
+from cylinder_qrcode_project.cqrcode.view.GUI_welcome import hello
+
+while True:
+    x = input('请输入 s 启动...\n')
+    if x == 's' or x == 'S':
+        hello()
