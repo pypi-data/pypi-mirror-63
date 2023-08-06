5c900188ea2f461d067516f61955a0bcf1a2c4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:13 +0000

    Add package aws-cdk-dynamodb-seeder/1.28.0/aws_cdk_dynamodb_seeder-1.28.0-py3-none-any.whl

---
+++ packages/aws-cdk-dynamodb-seeder/aws_cdk_dynamodb_seeder-1.28.0-py3-none-any.whl/ElegantDevelopment/AWSCDKDynamoDBSeeder/_jsii/__init__.py
@@ -0,0 +1,19 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_lambda
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_deployment
+import aws_cdk.core
+import aws_cdk.custom_resources
+__all__ = []
+
+publication.publish()
