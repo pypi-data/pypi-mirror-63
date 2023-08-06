2a1df750027f272002a31227230c805eb9b542f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:50 +0000

    Add package hashable_lru_cache/0.1.2/hashable_lru_cache-0.1.2-py3-none-any.whl

---
+++ packages/hashable_lru_cache/hashable_lru_cache-0.1.2-py3-none-any.whl/hashable_lru_cache/__init__.py
@@ -0,0 +1,7 @@
+"""An extension of functools.lru_cache allowing unhashable objects to be
+transformed and used in the cache key."""
+from hashable_lru_cache.hashable_lru_cache import hashable_lru_cache
+
+
+__version__ = "0.1.2"
+_ = {hashable_lru_cache}
