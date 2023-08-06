ca0a8158c845831ad1a7aed40d65a01ae680ba71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:26 +0000

    Add package moore-math-beta/0.1/moore_math_beta-0.1.tar.gz

---
+++ packages/moore-math-beta/moore_math_beta-0.1.tar.gz/moore_math_beta-0.1/moore_math_beta/__init__.py
@@ -0,0 +1,13 @@
+def add(a=0.0, b=0.0):
+    """Add two numbers together
+
+    Keyword arguments:
+    a -- number 
+    b -- also a number
+    """
+    print("working to add {} and {}...".format(a,b))
+    return a + b
+
+def multiply(a, b):
+   """TODO: Very complicated multiply things!"""
+   return 0
