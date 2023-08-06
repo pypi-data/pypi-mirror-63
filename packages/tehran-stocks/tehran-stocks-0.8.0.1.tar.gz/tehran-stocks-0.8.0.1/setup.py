5c6b4a61f22887b36792909caab058aaa12ddd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package tehran-stocks/0.8.0.1/tehran-stocks-0.8.0.1.tar.gz

---
+++ packages/tehran-stocks/tehran-stocks-0.8.0.1.tar.gz/tehran-stocks-0.8.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+# read the contents of your README file
+from os import path
+
+THISDIRECTORY = path.abspath(path.dirname(__file__))
+with open(path.join(THISDIRECTORY, "README.md")) as f:
+    LONGDESC = f.read()
+
+setup(
+    name="tehran-stocks",
+    version="0.8.0.1",
+    description="Data Downloader for Tehran stock market",
+    url="http://github.com/ghodsizdeh/tehran-stocks",
+    author="Mehdi Ghodsizadeh",
+    author_email="mehdi.ghodsizadeh@gmail.com",
+    license="MIT",
+    long_description=LONGDESC,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=[
+        "tehran_stocks",
+        "tehran_stocks.download",
+        "tehran_stocks.models",
+        "tehran_stocks.config",
+    ],
+    install_requires=["wheel", "pandas", "sqlalchemy", "requests", "jdatetime"],
+    zip_safe=False,
+    python_requires=">=3.6",
+    scripts=["bin/ts-get", "bin/ts-get.bat"],
+    include_package_data=True,
+)
---
+++ packages/tehran-stocks/tehran-stocks-0.8.0.1.tar.gz/tehran-stocks-0.8.0.1/src/tehran_stocks/models/stocks.py
@@ -0,0 +1,111 @@
+from tehran_stocks.config import *
+from sqlalchemy.orm import relationship
+import pandas as pd
+
+
+class Stocks(Base):
+    __tablename__ = "stocks"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    title = Column(String)
+    group_name = Column(String)
+    group_code = Column(Integer)
+    instId = Column(String)
+    insCode = Column(String)
+    code = Column(String, unique=True)
+    sectorPe = Column(Float)
+    shareCount = Column(Float)
+    estimatedEps = Column(Float)
+    baseVol = Column(Float)
+    prices = relationship("StockPrice", backref="stock")
+    _cached = False
+    _dfcounter = 0
+
+    def __init__ (self, **kwargs):
+        super().__init__(**kwargs)
+    
+    @property
+    def df(self):
+        self._dfcounter+=1
+        if self._cached:
+            return self._df
+        query = f"select * from stock_price where code = {self.code}"
+        df = pd.read_sql(query, engine)
+        if df.empty:
+            self._cached = True
+            self._df = df
+            return self._df
+        df["date"] = pd.to_datetime(df["dtyyyymmdd"], format="%Y%m%d")
+        df = df.sort_values("date")
+        df.reset_index(drop=True, inplace=True)
+        df.set_index("date",inplace=True)
+        self._cached = True
+        self._df = df
+        return self._df
+
+    @property
+    def mpl(self):
+        self._mpl = self.df.rename(columns={"close":"Close","open":"Open","high":"High","low":"Low","vol":"Volume"})
+        return self._mpl 
+
+    def update(self):
+        from tehran_stocks.download import update_stock_price
+
+        try:
+            res = update_stock_price(self.code)
+            return res
+        except:
+            return False
+
+    def summary(self):
+        df = self.df
+        sdate = df.date.min().strftime("%Y%m%d")
+        edate = df.date.max().strftime("%Y%m%d")
+
+        print(f"Start date: {sdate}")
+        print(f"End date: {edate}")
+        print(f"Total days: {len(df)}")
+
+    def __repr__(self):
+        return f"{self.title}-{self.name}-{self.group_name}"
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def get_group():
+        codes = (
+            session.query(Stocks.group_code, Stocks.group_name)
+            .group_by(Stocks.group_code)
+            .all()
+        )
+        return codes
+
+
+class StockPrice(Base):
+    __tablename__ = "stock_price"
+
+    id = Column(Integer, primary_key=True)
+    code = Column(String, ForeignKey("stocks.code"),index=True)
+    ticker = Column(String)
+    date = Column("dtyyyymmdd", Integer,index=True)
+    date_shamsi = Column(String)
+    first = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    value = Column(Integer)
+    vol = Column(Integer)
+    openint = Column(Integer)
+    per = Column(String)
+    open = Column(Float)
+    last = Column(Float)
+
+    def __repr__(self):
+        return f"{self.stock.name}, {self.date}, {self.close:.0f}"
+
+def get_asset(name):
+    asset = Stocks.query.filter_by(name=name).first()
+    return asset
+
