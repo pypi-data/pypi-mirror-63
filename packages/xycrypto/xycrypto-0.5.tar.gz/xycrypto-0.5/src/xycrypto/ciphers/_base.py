087f5f41bc4c79b2e9792af0ec7cdb80eedacbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:43 +0000

    Add package xycrypto/0.5/xycrypto-0.5.tar.gz

---
+++ packages/xycrypto/xycrypto-0.5.tar.gz/xycrypto-0.5/src/xycrypto/ciphers/_base.py
@@ -0,0 +1,126 @@
+import abc
+
+from . import _lib, _utils
+
+
+class Cipher(metaclass=abc.ABCMeta):
+    """Abstract base class for cipher."""
+
+    @property
+    @abc.abstractmethod
+    def _algorithm(self):
+        """The algorithm of cipher."""
+
+    @abc.abstractmethod
+    def encryptor(self):
+        """Return the encryptor context."""
+
+    @abc.abstractmethod
+    def decryptor(self):
+        """Return the decryptor context."""
+
+    def encrypt(self, data):
+        """Encrypt data and return."""
+
+        encryptor = self.encryptor()
+        temp = encryptor.update(data)
+        return temp + encryptor.finalize()
+
+    def decrypt(self, data):
+        """Decrypt data and return."""
+
+        decryptor = self.decryptor()
+        temp = decryptor.update(data)
+        return temp + decryptor.finalize()
+
+
+class StreamCipher(Cipher):
+    """Abstract base class for stream cipher."""
+
+    def __init__(self, key):
+        self._cipher = _lib.Cipher(self._algorithm(key), None, _lib.backend)
+
+    def encryptor(self):
+        return self._cipher.encryptor()
+
+    def decryptor(self):
+        return self._cipher.decryptor()
+
+
+class BlockCipher(Cipher):
+    """Abstract base class for block cipher."""
+
+    @property
+    @abc.abstractmethod
+    def block_size(self):
+        """The block size in bytes of cipher."""
+
+
+class BlockCipherECB(BlockCipher):
+    """Abstract base class for block cipher in ECB mode."""
+
+    def __init__(self, key, *, padding='PKCS7'):
+        self._cipher = _lib.Cipher(self._algorithm(key), _lib.ECB(), _lib.backend)
+        self._padding = _utils._determine_padding(padding, self.block_size)
+
+    def encryptor(self):
+        return _utils._determine_encryptor(self._cipher, self._padding)
+
+    def decryptor(self):
+        return _utils._determine_decryptor(self._cipher, self._padding)
+
+
+class BlockCipherCBC(BlockCipher):
+    """Abstract base class for block cipher in CBC mode."""
+
+    def __init__(self, key, *, iv, padding='PKCS7'):
+        self._cipher = _lib.Cipher(self._algorithm(key), _lib.CBC(iv), _lib.backend)
+        self._padding = _utils._determine_padding(padding, self.block_size)
+
+    def encryptor(self):
+        return _utils._determine_encryptor(self._cipher, self._padding)
+
+    def decryptor(self):
+        return _utils._determine_decryptor(self._cipher, self._padding)
+
+
+@StreamCipher.register
+class BlockCipherCFB(BlockCipher):
+    """Abstract base class for block cipher in CFB mode."""
+
+    def __init__(self, key, *, iv):
+        self._cipher = _lib.Cipher(self._algorithm(key), _lib.CFB(iv), _lib.backend)
+
+    def encryptor(self):
+        return self._cipher.encryptor()
+
+    def decryptor(self):
+        return self._cipher.decryptor()
+
+
+@StreamCipher.register
+class BlockCipherOFB(BlockCipher):
+    """Abstract base class for block cipher in OFB mode."""
+
+    def __init__(self, key, *, iv):
+        self._cipher = _lib.Cipher(self._algorithm(key), _lib.OFB(iv), _lib.backend)
+
+    def encryptor(self):
+        return self._cipher.encryptor()
+
+    def decryptor(self):
+        return self._cipher.decryptor()
+
+
+@StreamCipher.register
+class BlockCipherCTR(BlockCipher):
+    """Abstract base class for block cipher in CTR mode."""
+
+    def __init__(self, key, *, nonce):
+        self._cipher = _lib.Cipher(self._algorithm(key), _lib.CTR(nonce), _lib.backend)
+
+    def encryptor(self):
+        return self._cipher.encryptor()
+
+    def decryptor(self):
+        return self._cipher.decryptor()
---
+++ packages/xycrypto/xycrypto-0.5.tar.gz/xycrypto-0.5/src/xycrypto/ciphers/_lib.py
@@ -0,0 +1,11 @@
+from cryptography.hazmat.backends import default_backend                # NOQA; isort:skip
+from cryptography.hazmat.primitives.ciphers import Cipher               # NOQA; isort:skip
+from cryptography.hazmat.primitives.ciphers.algorithms import (         # NOQA; isort:skip
+    ARC4, ChaCha20,
+    AES, Blowfish, Camellia, CAST5, IDEA, SEED, TripleDES
+)
+from cryptography.hazmat.primitives.ciphers.modes import (              # NOQA; isort:skip
+    ECB, CBC, CFB, OFB, CTR
+)
+
+backend = default_backend()
