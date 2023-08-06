fbb90f29942f39c62c333e86e5d554e2c8e09bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:02 +0000

    Add package yhitSuperMath/1.1/yhitSuperMath-1.1.tar.gz

---
+++ packages/yhitSuperMath/yhitSuperMath-1.1.tar.gz/yhitSuperMath-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name='yhitSuperMath', # 对外我们模块的名字
+version='1.1', # 版本号
+description='这是仪化信息对外发布的模块，测试哦', #描述
+author='shiyazhou', # 作者
+author_email='yz_shi@163.com',
+py_modules=['yhitSuperMath.module_A','yhitSuperMath.module_A'] # 要发布的模块
+)
