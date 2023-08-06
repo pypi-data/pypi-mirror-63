e9aafb523d8be5f9e07f843157f9058fde0d5590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:14 +0000

    Add package pybuildutils/0.0.1/pybuildutils-0.0.1.tar.gz

---
+++ packages/pybuildutils/pybuildutils-0.0.1.tar.gz/pybuildutils-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pybuildutils",
+    version="0.0.1",
+    author="Bilal Shaikh",
+    author_email="bilalshaikh42@gmail.com",
+    description="A collection of tools for creating python projects"
+)
