c652d0d14674d79035aabc28474ba92d262b13ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package kakeibox-database-dictionary/0.0.3/kakeibox_database_dictionary-0.0.3-py2.py3-none-any.whl

---
+++ packages/kakeibox-database-dictionary/kakeibox_database_dictionary-0.0.3-py2.py3-none-any.whl/tests/fixtures/transaction_data.py
@@ -0,0 +1,213 @@
+TRANSACTION_DATA = {
+    "65725a55-0610-4aa2-b4d1-dee8a0936ed5": {
+        "amount": 41778.94451,
+        "description": "Focus executive drop cost base my prove night.",
+        "record_hash":
+            "0b8ea21f957d7fc452ab3d4cd4cb38be394d17f0fb17d0ef62b28b83e6fe7e37",
+        "reference_number": "XGTM5260521978861",
+        "timestamp": 1563533343,
+        "transaction_subcategory_code": 'GIF',
+        "transaction_type_code": 'INC',
+        "uuid": "65725a55-0610-4aa2-b4d1-dee8a0936ed5"
+    },
+    "e81bdd0d-b9f8-4721-9708-8a5a3569d6e0": {
+        "amount": 6220.522,
+        "description":
+            "Pick together investment prove police parent election.",
+        "record_hash":
+            "5c0b7a22276db776c043ca715c659d41030fab651d9f7cace4da04d280e1693e",
+        "reference_number": "VEJT4503634011670",
+        "timestamp": 1564510129,
+        "transaction_subcategory_code": 'BOK',
+        "transaction_type_code": 'INC',
+        "uuid": "e81bdd0d-b9f8-4721-9708-8a5a3569d6e0"
+    },
+    "e209ebd3-4748-42fc-a282-8964b0795d45": {
+        "amount": 59563.5,
+        "description": "Most after table sort anyone.",
+        "record_hash":
+            "76a20aaa18d4c06a5d3bc328d1cbf658d54e73b1ce5752a6569a76b4508d9439",
+        "reference_number": "LZEF2713341746274",
+        "timestamp": 1565855698,
+        "transaction_subcategory_code": 'SHW',
+        "transaction_type_code": 'EXP',
+        "uuid": "e209ebd3-4748-42fc-a282-8964b0795d45"
+    },
+    "06f7eb1c-f1c5-42a9-94df-eaf384a91753": {
+        "amount": 76979.1673,
+        "description": "Through star safe teacher animal.",
+        "record_hash":
+            "6f15d625f40d0ed05f3b684c03dbcc2c5e3739c5fcba16989e6f54365d141c3d",
+        "reference_number": "MMHL2082573530388",
+        "timestamp": 1563080480,
+        "transaction_subcategory_code": 'TRA',
+        "transaction_type_code": 'INC',
+        "uuid": "06f7eb1c-f1c5-42a9-94df-eaf384a91753"
+    },
+    "7064b536-82f9-498d-a5f7-cd2289680c95": {
+        "amount": 21525.167,
+        "description": "Realize blood alone enter.",
+        "record_hash":
+            "cac7baed55f36665f0c235a3471f6d7644b8929c1e70a72d4d95c3dea7196089",
+        "reference_number": "KEQB8464688274218",
+        "timestamp": 1564117807,
+        "transaction_subcategory_code": 'MAG',
+        "transaction_type_code": 'EXP',
+        "uuid": "7064b536-82f9-498d-a5f7-cd2289680c95"
+    },
+    "7e6b739f-9c3a-434b-84dc-179fb841f8b0": {
+        "amount": 594.405476893894,
+        "description": "Simple join kid capital guess.",
+        "record_hash":
+            "bbe1c5d8f1a8e834bf84775a3b00b9d81034eb31656b69cad7099b391cbdc3d1",
+        "reference_number": "YTAH8256952650808",
+        "timestamp": 1562118663,
+        "transaction_subcategory_code": 'SHO',
+        "transaction_type_code": 'INC',
+        "uuid": "7e6b739f-9c3a-434b-84dc-179fb841f8b0"
+    },
+    "135353d8-a97c-46d8-80a6-e272483c2913": {
+        "amount": 29761.7948797,
+        "description": "Close into short box.",
+        "record_hash":
+            "f17a6b034c2c0a24d9bfcbe4cc4532ed1cea93dd42ea7f1b15aa2395f5079ada",
+        "reference_number": "SUHB3671579759896",
+        "timestamp": 1562203602,
+        "transaction_subcategory_code": 'TRA',
+        "transaction_type_code": 'EXP',
+        "uuid": "135353d8-a97c-46d8-80a6-e272483c2913"
+    },
+    "566d7fd9-7f22-4edf-be25-350e4a20e020": {
+        "amount": 22577.87,
+        "description": "About present night however either.",
+        "record_hash":
+            "58ee6f2d376ab09d29dd119bc0ba534d35e1a9f9f526a07bf80cfeae417322aa",
+        "reference_number": "EZYE9034389895665",
+        "timestamp": 1563475833,
+        "transaction_subcategory_code": 'MUS',
+        "transaction_type_code": 'EXP',
+        "uuid": "566d7fd9-7f22-4edf-be25-350e4a20e020"
+    },
+    "c5e6fb07-4621-49e8-bd4f-0b8b6c2815fe": {
+        "amount": 74025.946932,
+        "description": "Hold newspaper tend operation myself.",
+        "record_hash":
+            "3d7255aa7d9d90bfa0929accf9423813393e47a094886b3e2f962d27b67794ad",
+        "reference_number": "JUTS1147901870833",
+        "timestamp": 1567043216,
+        "transaction_subcategory_code": 'SHO',
+        "transaction_type_code": 'INC',
+        "uuid": "c5e6fb07-4621-49e8-bd4f-0b8b6c2815fe"
+    },
+    "92ac897b-c797-441b-bb83-4ef25aec3a28": {
+        "amount": 18098.75404,
+        "description": "Heavy clearly huge official.",
+        "record_hash":
+            "2756e6e2aa1140cc78a19ba58c9938caf3c21b4d14b9a42bfb8886e63d7bb474",
+        "reference_number": "JIWY6072154633345",
+        "timestamp": 1563386859,
+        "transaction_subcategory_code": 'FUR',
+        "transaction_type_code": 'EXP',
+        "uuid": "92ac897b-c797-441b-bb83-4ef25aec3a28"
+    },
+    "e9e5d51a-2ed4-4cea-941e-01387c29ed63": {
+        "amount": 48066.6168102,
+        "description": "Science pass make poor.",
+        "record_hash":
+            "778a9836ceac2c9f118d915d54f303b5538750e0c26e7b1d0da5cadf38dc2478",
+        "reference_number": "OSZZ2507659238321",
+        "timestamp": 1563691511,
+        "transaction_subcategory_code": 'FUR',
+        "transaction_type_code": 'INC',
+        "uuid": "e9e5d51a-2ed4-4cea-941e-01387c29ed63"
+    },
+    "a59359cb-e3dd-40ba-99c6-023b1f71dba8": {
+        "amount": 95916.2604,
+        "description": "Become air until.",
+        "record_hash":
+            "02232713a7fc0a29ddb6c3295b8307b31f989f76e8d51a2620fe541c05cd50f8",
+        "reference_number": "BSTX3371171444377",
+        "timestamp": 1565005560,
+        "transaction_subcategory_code": 'FUR',
+        "transaction_type_code": 'EXP',
+        "uuid": "a59359cb-e3dd-40ba-99c6-023b1f71dba8"
+    },
+    "09bff241-88c7-46be-bd3f-cf99311216f1": {
+        "amount": 72430,
+        "description": "List buy line three.",
+        "record_hash":
+            "b431d9620ae1c48ca7999b9fdd0ed3eb718d1272945a6e29e0c7ad15bb01d3ca",
+        "reference_number": "VTZU6120647604067",
+        "timestamp": 1564125154,
+        "transaction_subcategory_code": 'FOD',
+        "transaction_type_code": 'EXP',
+        "uuid": "09bff241-88c7-46be-bd3f-cf99311216f1"
+    },
+    "a00a90f0-861c-4bda-8d1a-b6a78a61472f": {
+        "amount": 37596.13,
+        "description": "Sure amount buy instead smile play something.",
+        "record_hash":
+            "f5b6544e4f730cb90a6be60fc8b89abb82643d0064e3bebcefe81e23f0e9b5e5",
+        "reference_number": "HNDE2061833973130",
+        "timestamp": 1562284203,
+        "transaction_subcategory_code": 'MOV',
+        "transaction_type_code": 'INC',
+        "uuid": "a00a90f0-861c-4bda-8d1a-b6a78a61472f"
+    },
+    "0afe80ca-245a-4d38-b610-b79fc96c9e81": {
+        "amount": 71142.365,
+        "description":
+            "Professor pass event short particular consider several.",
+        "record_hash":
+            "2c695ddc76fbb44b16e51055da2d2914e860281ba822393cee5466060e1508d4",
+        "reference_number": "XIAF8021720287232",
+        "timestamp": 1563050948,
+        "transaction_subcategory_code": 'TRA',
+        "transaction_type_code": 'EXP',
+        "uuid": "0afe80ca-245a-4d38-b610-b79fc96c9e81"
+    },
+    "f2a25f46-6f93-46c9-bca2-4f613c6f77cc": {
+        "amount": 81303.159,
+        "description": "Well friend because support economic Republican.",
+        "record_hash":
+            "43803c7076fcab9548003c0eb9bba7a74fa5d77e5d6afc32843cb36ef37a0acd",
+        "reference_number": "GZWP2774492450280",
+        "timestamp": 1567132581,
+        "transaction_subcategory_code": 'RES',
+        "transaction_type_code": 'EXP',
+        "uuid": "f2a25f46-6f93-46c9-bca2-4f613c6f77cc"
+    },
+    "805458cf-fb60-4bfd-b013-c12c294ce8e2": {
+        "amount": 12188.1,
+        "description": "Ever animal morning.",
+        "record_hash":
+            "a18be4d819cea919c341aafc85ddfd9071d9ccd5ee69f1fd1278d523fa85bd83",
+        "reference_number": "MGMY0829874488700",
+        "timestamp": 1563781154,
+        "transaction_subcategory_code": 'GIF',
+        "transaction_type_code": 'INC',
+        "uuid": "805458cf-fb60-4bfd-b013-c12c294ce8e2"
+    },
+    "605de309-c799-4350-9b9b-35a69aa7f4d7": {
+        "amount": 48859.78,
+        "description": "Value cover analysis smile.",
+        "record_hash":
+            "7ee740d50a3194c6aea0f39dba5bb64d310d79e4a439d56c5798014d4f88a77e",
+        "reference_number": "WXMH3690572168023",
+        "timestamp": 1564522665,
+        "transaction_subcategory_code": 'REP',
+        "transaction_type_code": 'INC',
+        "uuid": "605de309-c799-4350-9b9b-35a69aa7f4d7"
+    },
+    "3455056a-8d81-4341-95a5-6347f6bc6653": {
+        "amount": 50445.461,
+        "description": "Future if you its put team Congress last.",
+        "record_hash":
+            "735450f55e8729be18ea8ce131fac1e6155cc42e90536147fe3cbebe81f30f11",
+        "reference_number": "QYAB8226384924840",
+        "timestamp": 1564210602,
+        "transaction_subcategory_code": 'GIF',
+        "transaction_type_code": 'INC',
+        "uuid": "3455056a-8d81-4341-95a5-6347f6bc6653"
+    }
+}
