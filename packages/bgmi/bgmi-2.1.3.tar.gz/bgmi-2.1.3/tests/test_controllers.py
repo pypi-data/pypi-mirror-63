361a8c44c8a1dfbfa9ea340fd31b0c43db5a26ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:50 +0000

    Add package bgmi/2.1.3/bgmi-2.1.3.tar.gz

---
+++ packages/bgmi/bgmi-2.1.3.tar.gz/bgmi-2.1.3/tests/test_controllers.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import os
+import unittest
+
+from bgmi.lib.controllers import *
+from bgmi.main import setup
+from bgmi.lib.constants import unicode_
+
+
+class ControllersTest(unittest.TestCase):
+    def setUp(self):
+        self.bangumi_name_1 = unicode_(os.environ.get('BANGUMI_1'))
+        self.bangumi_name_2 = unicode_(os.environ.get('BANGUMI_2'))
+        pass
+
+    def test_a_cal(self):
+        r = cal()
+        self.assertIsInstance(r, dict)
+        for day in r.keys():
+            self.assertIn(day.lower(), [x.lower() for x in Bangumi.week])
+            self.assertIsInstance(r[day], list)
+            for bangumi in r[day]:
+                self.assertIn("status", bangumi)
+                self.assertIn("subtitle_group", bangumi)
+                self.assertIn("name", bangumi)
+                self.assertIn("keyword", bangumi)
+                self.assertIn("update_time", bangumi)
+                self.assertIn("cover", bangumi)
+
+    def test_b_add(self):
+        r = add(self.bangumi_name_1, 0)
+        self.assertEqual(r['status'], 'success', r['message'])
+        r = add(self.bangumi_name_1, 0)
+        self.assertEqual(r['status'], 'warning', r['message'])
+        r = delete(self.bangumi_name_1)
+        self.assertEqual(r['status'], 'warning', r['message'])
+
+    def test_c_mark(self):
+        add(self.bangumi_name_1, 0)
+
+        r = mark(self.bangumi_name_1, 1)
+        self.assertEqual(r['status'], 'success', r['message'])
+        r = mark(self.bangumi_name_1, None)
+        self.assertEqual(r['status'], 'info', r['message'])
+        r = mark(self.bangumi_name_2, 0)
+        self.assertEqual(r['status'], 'error', r['message'])
+
+    def test_d_delete(self):
+        r = delete()
+        self.assertEqual(r['status'], 'warning', r['message'])
+        r = delete(self.bangumi_name_1)
+        self.assertEqual(r['status'], 'warning', r['message'])
+        r = delete(self.bangumi_name_1)
+        self.assertEqual(r['status'], 'warning', r['message'])
+        r = delete(self.bangumi_name_2)
+        self.assertEqual(r['status'], 'error', r['message'])
+        r = delete(clear_all=True, batch=True)
+        self.assertEqual(r['status'], 'warning', r['message'])
+
+    def test_e_search(self):
+        r = search(self.bangumi_name_1, dupe=False)
+
+    @staticmethod
+    def setUpClass():
+        setup()
+        recreate_source_relatively_table()
