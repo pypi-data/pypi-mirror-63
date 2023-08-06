9f605124598735bc050ff5d59a58280169b7e0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package mnistlib/0.0.8/mnistlib-0.0.8.tar.gz

---
+++ packages/mnistlib/mnistlib-0.0.8.tar.gz/mnistlib-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'mnistlib',
+    version = '0.0.8',
+    description = 'geometric transformations for mnist dataset',
+    license = 'MIT',
+    packages = ['mnistlib'],
+    author = 'Oleg Zyablov',
+    author_email = 'oleg.z98@mail.ru',
+    keywords = ['mnist'],
+    install_requires = [
+        'mnist', 'matplotlib', 'Pillow', 'numpy'
+    ],
+    include_package_data = True
+)
