5837966c2b95adcafed057eec1b855cdb0ddd88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:23 +0000

    Add package listoapi/0.1.46/listoapi-0.1.46-py3-none-any.whl

---
+++ packages/listoapi/listoapi-0.1.46-py3-none-any.whl/listoapi/banks.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+from .api import ListoAPI, TooManyRequests
+
+
+class Banks(ListoAPI):
+    def __init__(self, token, base_url):
+        super(Banks, self).__init__(token, base_url)
+
+    def get_bank_accounts(self):
+        return self.make_request(method="GET", path="/banks/bank_transaction/facets") \
+               .json()['facets']['bank_account']
+
+    def upload_bank_files(self, filename, file_stream, bank=None, account_number=None, currency=None, rfc_id=None):
+        print('Uploading file:', filename)
+        return self.make_request(method="POST", path="/banks/upload_bank_files/sync",
+                                 files={'file': file_stream},
+                                 data={filename:
+                                    '{"bank": "%s", "account_number": "%s", "currency": "%s", "rfc_id": %s}' %
+                                     (bank, account_number, currency, rfc_id)}).json()['status']
+
+    def get_bank_transactions(self, **kwargs):
+        return self.make_request(method="GET", path="/banks/bank_transaction", params=kwargs) \
+               .json()['hits']
+
