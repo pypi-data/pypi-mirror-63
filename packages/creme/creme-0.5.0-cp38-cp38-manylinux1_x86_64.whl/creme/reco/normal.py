97ba23c7a93d4886d2f8a7b716a3616bdb102949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package creme/0.5.0/creme-0.5.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/creme/creme-0.5.0-cp38-cp38-manylinux1_x86_64.whl/creme/reco/normal.py
@@ -0,0 +1,76 @@
+from sklearn import utils
+
+from .. import stats
+
+from . import base
+
+
+__all__ = ['RandomNormal']
+
+
+class RandomNormal(base.Recommender):
+    """Predicts random values sampled from a normal distribution.
+
+    The parameters of the normal distribution are fitted with running statistics. This is
+    equivalent to using `surprise.prediction_algorithms.random_pred.NormalPredictor`. The model
+    expect dict inputs containing both a ``user`` and an ``item`` entries.
+
+    Parameters:
+        random_state (int, ``numpy.random.RandomState`` instance or None): If int, ``random_state``
+            is the seed used by the random number generator; if ``RandomState`` instance,
+            ``random_state`` is the random number generator; if ``None``, the random number
+            generator is the ``RandomState`` instance used by `numpy.random`.
+
+    Attributes:
+        mean (stats.Mean)
+        variance (stats.Var)
+
+    Example:
+
+        ::
+
+            >>> from creme import reco
+
+            >>> X_y = (
+            ...     ({'user': 'Alice', 'item': 'Superman'}, 8),
+            ...     ({'user': 'Alice', 'item': 'Terminator'}, 9),
+            ...     ({'user': 'Alice', 'item': 'Star Wars'}, 8),
+            ...     ({'user': 'Alice', 'item': 'Notting Hill'}, 2),
+            ...     ({'user': 'Alice', 'item': 'Harry Potter'}, 5),
+            ...     ({'user': 'Bob', 'item': 'Superman'}, 8),
+            ...     ({'user': 'Bob', 'item': 'Terminator'}, 9),
+            ...     ({'user': 'Bob', 'item': 'Star Wars'}, 8),
+            ...     ({'user': 'Bob', 'item': 'Notting Hill'}, 2)
+            ... )
+
+            >>> model = reco.RandomNormal(random_state=42)
+
+            >>> for x, y in X_y:
+            ...     _ = model.fit_one(x, y)
+
+            >>> model.predict_one({'user': 'Bob', 'item': 'Harry Potter'})
+            8.092809...
+
+    Note:
+        `reco.RandomNormal` model expect a dict input with a ``user`` and an ``item`` entries
+        without any type constraint on their values (i.e. can be strings or numbers). Other entries
+        are ignored.
+
+    """
+
+    def __init__(self, random_state=None):
+        super().__init__()
+        self.variance = stats.Var()
+        self.mean = stats.Mean()
+        self.random_state = utils.check_random_state(random_state)
+
+    def _fit_one(self, user, item, y):
+        y_pred = self._predict_one(user, item)
+        self.mean.update(y)
+        self.variance.update(y)
+        return y_pred
+
+    def _predict_one(self, user, item):
+        μ = self.mean.get() or 0
+        σ = (self.variance.get() or 1) ** 0.5
+        return self.random_state.normal(μ, σ)
---
+++ packages/creme/creme-0.5.0-cp38-cp38-manylinux1_x86_64.whl/creme/stats/n_unique.py
@@ -0,0 +1,94 @@
+import math
+
+import numpy as np
+from sklearn import utils
+
+from . import base
+
+
+class NUnique(base.Univariate):
+    """Approximate number of unique values counter.
+
+    This is basically an implementation of the HyperLogLog algorithm. Adapted from
+    `here <https://github.com/clarkduvall/hypy>`_. The code is a bit too terse but it will do for
+    now.
+
+    Parameters:
+        error_rate (float): Desired error rate. Memory usage is inversely proportional to this
+            value.
+        encoding (str): Encoding used for `sklearn.utils.murmurhash3_32`. Defaults to ``'utf-8'``.
+        random_state (int): Integer seed for the hashing algorithm. Defaults to `None`.
+
+    Attributes:
+        n_bits (int)
+        n_buckets (int)
+        buckets (list)
+
+    Example:
+
+        ::
+
+            >>> import string
+            >>> from creme import stats
+
+            >>> alphabet = string.ascii_lowercase
+            >>> n_unique = stats.NUnique(error_rate=0.1, seed=42)
+
+            >>> n_unique.update('a').get()
+            1
+
+            >>> n_unique.update('b').get()
+            2
+
+            >>> for letter in alphabet:
+            ...     n_unique = n_unique.update(letter)
+            >>> n_unique.get()
+            24
+
+            We can increase the precision by lowering the ``error_rate`` parameter.
+
+            >>> n_unique = stats.NUnique(error_rate=0.01, seed=42)
+            >>> for letter in alphabet:
+            ...     n_unique = n_unique.update(letter)
+            >>> n_unique.get()
+            26
+
+    References:
+        1. `My favorite algorithm (and data structure): HyperLogLog <https://odino.org/my-favorite-data-structure-hyperloglog/>`_
+        2. `Flajolet, P., Fusy, É., Gandouet, O. and Meunier, F., 2007, June. Hyperloglog: the analysis of a near-optimal cardinality estimation algorithm. <http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf>`_
+
+    """
+
+    P32 = 2 ** 32
+
+    def __init__(self, error_rate=0.01, encoding='utf-8', seed=None):
+        self.n_bits = int(math.ceil(math.log((1.04 / error_rate) ** 2, 2)))
+        self.n_buckets = 1 << self.n_bits
+        self.buckets = [0] * self.n_buckets
+        self.encoding = encoding
+        self.seed = seed
+        self._rng = np.random.RandomState(seed)
+        self._hash_seed = self._rng.randint(0, 2 ** 32 - 1)
+
+    @property
+    def name(self):
+        return 'n_unique'
+
+    def _hash_str(self, string):
+        return utils.murmurhash3_32(bytes(string, self.encoding), self._hash_seed)
+
+    def update(self, x):
+        x = self._hash_str(x)
+        i = x & NUnique.P32 - 1 >> 32 - self.n_bits
+        z = 35 - len(bin(NUnique.P32 - 1 & x << self.n_bits | 1 << self.n_bits - 1))
+        self.buckets[i] = max(self.buckets[i], z)
+        return self
+
+    def get(self):
+        a = ({16: 0.673, 32: 0.697, 64: 0.709}[self.m]
+             if self.n_buckets <= 64 else 0.7213 / (1 + 1.079 / self.n_buckets))
+        e = a * self.n_buckets * self.n_buckets / sum(1.0 / (1 << x) for x in self.buckets)
+        if e <= self.n_buckets * 2.5:
+            z = len([r for r in self.buckets if not r])
+            return int(self.n_buckets * math.log(float(self.n_buckets) / z) if z else e)
+        return int(e if e < NUnique.P32 / 30 else -NUnique.P32 * math.log(1 - e / NUnique.P32))
