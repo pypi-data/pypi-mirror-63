fcb2c1d1be034fdabc07b3b7b12e693a728f39eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:17 +0000

    Add package typish/1.4.0/typish-1.4.0-py3-none-any.whl

---
+++ packages/typish/typish-1.4.0-py3-none-any.whl/typish/__init__.py
@@ -0,0 +1,32 @@
+from typish._classes import (
+    SubscriptableType,
+    Something,
+    TypingType,
+    ClsDict,
+)
+from typish._functions import (
+    subclass_of,
+    instance_of,
+    get_origin,
+    get_args,
+    get_alias,
+    get_type,
+    common_ancestor,
+    common_ancestor_of_types,
+    get_args_and_return_type,
+    get_type_hints_of_callable,
+    is_type_annotation,
+)
+from typish._meta import (
+    __version__,
+)
+from typish._types import (
+    T,
+    KT,
+    VT,
+    Empty,
+    Unknown,
+    Module,
+    NoneType,
+    Ellipsis_,
+)
