96723b958431d15fcb712b83aa22e3b393e7f828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package posce/0.2.0/posce-0.2.0-py3-none-any.whl

---
+++ packages/posce/posce-0.2.0-py3-none-any.whl/posce/comms/__init__.py
@@ -0,0 +1,9 @@
+'''
+Package definition for 'posce.comms'.
+'''
+
+from posce.comms import base
+from posce.comms import edit
+from posce.comms import find
+from posce.comms import list
+from posce.comms import show
