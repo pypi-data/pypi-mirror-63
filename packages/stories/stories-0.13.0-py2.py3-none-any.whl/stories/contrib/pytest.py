4dffe2ae120b51162ff517029f2a28b2a020815e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:13 +0000

    Add package stories/0.13.0/stories-0.13.0-py2.py3-none-any.whl

---
+++ packages/stories/stories-0.13.0-py2.py3-none-any.whl/stories/contrib/pytest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# type: ignore
+"""
+stories.contrib.pytest
+----------------------
+
+This module contains integration with PyTest framework.
+
+:copyright: (c) 2018-2020 dry-python team.
+:license: BSD, see LICENSE for more details.
+"""
+from _stories.contrib.pytest import pytest_runtest_call
+
+
+__all__ = ["pytest_runtest_call"]
