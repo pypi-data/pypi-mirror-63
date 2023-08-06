43666c877f0728737fffc5fe3da2bb8c0d5df3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:48 +0000

    Add package proto-py/1.0.1/proto_py-1.0.1.tar.gz

---
+++ packages/proto-py/proto_py-1.0.1.tar.gz/proto_py-1.0.1/proto_py/__init__.py
@@ -0,0 +1,7 @@
+"""Базовый init файл, с импортом наиболее используемых объектов из всей библиотеки proto_py
+"""
+
+from .tcpSocket import TcpSocket
+from .tcpServer import TcpServer
+from .message import Message
+from .baseCommands import BaseCommand, REGISTRY_COMMAND
