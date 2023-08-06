b9fa9713e01acd5455e2b2da02f6c33fe48c2712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package djangoautoconf/2.0.2/djangoautoconf-2.0.2-py2.py3-none-any.whl

---
+++ packages/djangoautoconf/djangoautoconf-2.0.2-py2.py3-none-any.whl/djangoautoconf/management/commands/dump_settings.py
@@ -0,0 +1,40 @@
+import os
+
+from django.core.management import BaseCommand
+from django.conf import settings
+
+
+def dump_attrs(obj_instance):
+    for attr in dir(obj_instance):
+        if attr != attr.upper():
+            continue
+        yield attr, getattr(obj_instance, attr)
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Create command cache for environment where os.listdir is not working'
+
+    def handle(self, *args, **options):
+        try:
+            os.remove("local/total_settings.py")
+        except:
+            pass
+        print os.getcwd()
+        with open("local/total_settings.py", "w") as f:
+            print >>f, "import os"
+            for key, value in dump_attrs(settings):
+                if key in ["AUTH_LDAP_USER_SEARCH"]:
+                    # Ignore object settings as it will not work if we dump it as string
+                    continue
+                if key in ["STATIC_ROOT", "MEDIA_ROOT"]:
+                    print >>f, '%s=os.environ["%s"]' % (key, key)
+                if value is None:
+                    continue
+                if type(value) in (list, tuple, dict, bool, int, float):
+                    print >>f, key, "=", value
+                elif type(value) in (str, ):
+                    print >>f, key, "=", '"'+str(value).replace('\\', '\\\\')+'"'
+                else:
+                    print >>f, key, "=", '"'+str(value).replace('\\', '\\\\')+'"'
+        print "dump completed"
