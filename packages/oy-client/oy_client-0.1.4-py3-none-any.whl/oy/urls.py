7517e264decc28f7378b993cb48c3a55f091d5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:45 +0000

    Add package oy-client/0.1.4/oy_client-0.1.4-py3-none-any.whl

---
+++ packages/oy-client/oy_client-0.1.4-py3-none-any.whl/oy/urls.py
@@ -0,0 +1,13 @@
+"""
+    Store all Oy API URL PATH here
+    ______________________________
+"""
+URLS = {
+    "INQUIRY_ACCOUNT": "/api/inquiry",
+    "DISBURSE": "/api/remit",
+    "DISBURSE_STATUS": "/api/remit-status",
+    "GET_BALANCE": "/api/balance",
+    "GENERATE_VA": "/api/generate-static-va",
+    "VA_INFO": "/api/static-virtual-account/",
+    "VA_TRANSACTIONS": "/api/va-tx-history/",
+}
