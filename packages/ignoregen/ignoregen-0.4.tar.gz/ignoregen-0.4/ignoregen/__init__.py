08191a28b773456cb9bfef28207aa92031de9bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:25 +0000

    Add package ignoregen/0.4/ignoregen-0.4.tar.gz

---
+++ packages/ignoregen/ignoregen-0.4.tar.gz/ignoregen-0.4/ignoregen/__init__.py
@@ -0,0 +1,7 @@
+##################################################
+# Copyright (C) 2017-2020, All rights reserved.
+##################################################
+
+__project_name__ = "ignoregen"
+__version__ = "0.4"
+__description__ = "Description of package"
