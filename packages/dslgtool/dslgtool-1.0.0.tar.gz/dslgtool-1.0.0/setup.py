6a4d1a22fd5f3728b6640ce1effcc4c937f68ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package dslgtool/1.0.0/dslgtool-1.0.0.tar.gz

---
+++ packages/dslgtool/dslgtool-1.0.0.tar.gz/dslgtool-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = 'dslgtool',
+    version = '1.0.0',
+    author = 'longlamduc',
+    description = 'A tool to detect image location',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages = find_packages(),
+    install_requires=[
+        'requests',
+        'cryptography',
+        'exifread',
+        'pillow',
+        'gpsphoto',
+        'piexif',
+        'opencv-python',
+      ],
+    entry_points = {
+        'console_scripts': [
+            'dslgtool = dslgtool.__main__:main'
+        ]
+    },
+    python_requires='>=3.6',
+    )
