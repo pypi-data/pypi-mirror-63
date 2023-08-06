adb5375fe069db452ea4807571ea17494607bf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:49 +0000

    Add package pysendpulse/0.0.5/pysendpulse-0.0.5.tar.gz

---
+++ packages/pysendpulse/pysendpulse-0.0.5.tar.gz/pysendpulse-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from sys import version_info
+from pysendpulse import (
+    __author__,
+    __author_email__,
+    __version__
+)
+
+install_requires = ['python3-memcached', ]
+
+if version_info.major == 2:
+    install_requires = ['python-memcached', 'requests', 'simplejson']
+
+setup(
+    name='pysendpulse',
+    version=__version__,
+    packages=find_packages(),
+    description='A simple SendPulse REST client library and example for Python',
+    author=__author__,
+    author_email=__author_email__,
+    url='https://github.com/sendpulse/sendpulse-rest-api-python',
+    install_requires=install_requires
+)
