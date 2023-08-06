9d59319d8206a75e0f2b0b7ae5bec670320be34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:25 +0000

    Add package pytest-xrayjira/1.0/pytest_xrayjira-1.0.tar.gz

---
+++ packages/pytest-xrayjira/pytest_xrayjira-1.0.tar.gz/pytest_xrayjira-1.0/pytest_xrayjira/constants.py
@@ -0,0 +1,6 @@
+XRAY_PLUGIN = "UPLOAD_RESULTS_TO_XRAY_JIRA"
+XRAY_MARKER_NAME = "xrayjira"
+XRAY_API_BASE_URL = "https://xray.cloud.xpand-it.com"
+MARKER_ARGS = '(plan_key, test_key, **xray_kwargs)'
+MARKER_DESCRIPTION = 'marks a test to have the results published to xray-jira'
+UPLOAD_RESULTS_TO_XRAY_JIRA_FLAG = "--upload-results-to-jira-xray"
