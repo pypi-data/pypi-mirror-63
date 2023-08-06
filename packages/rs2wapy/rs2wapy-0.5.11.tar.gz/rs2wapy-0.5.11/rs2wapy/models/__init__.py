20c9332fe01164909fb8418eb69bef7638087f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package rs2wapy/0.5.11/rs2wapy-0.5.11.tar.gz

---
+++ packages/rs2wapy/rs2wapy-0.5.11.tar.gz/rs2wapy-0.5.11/rs2wapy/models/__init__.py
@@ -0,0 +1,19 @@
+from .models import *
+
+__all__ = [
+    "STEAM_ID_TYPE",
+    "AllTeam",
+    "AccessPolicy",
+    "ChatMessage",
+    "ChatChannel",
+    "ChatChannelTeam",
+    "ChatChannelAll",
+    "CurrentGame",
+    "Player",
+    "PlayerScoreboard",
+    "Team",
+    "TeamScoreboard",
+    "BlueTeam",
+    "RedTeam",
+    "MapCycle",
+]
