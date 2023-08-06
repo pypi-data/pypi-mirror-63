80a36eb0b3d71532cd1419a6efeb3ffca01e1056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:28 +0000

    Add package xtalpi-pandas/0.1.3/xtalpi-pandas-0.1.3.tar.gz

---
+++ packages/xtalpi-pandas/xtalpi-pandas-0.1.3.tar.gz/xtalpi-pandas-0.1.3/README.md
@@ -0,0 +1,36 @@
+# xtalpi-pandas v0.1.3#
+
+xtalpi-pandas是对pandas进行封装的命令行工具，用于处理csv
+
+## 安装 ##
+安装前建议升级pip，并将pip更换为国内源（清华）
+```
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+```
+通过pip进行安装
+```
+pip install xtalpi-pandas
+```
+
+## 更新 ##
+通过pip进行更新
+```
+pip install xtalpi-pandas -U
+```
+
+## 使用方式 ##
+打开终端，输入
+```
+xpandas xxx.csv
+```
+其中xxx.csv为待处理的csv路径。之后按照程序提示选择功能，执行操作。输出与输入csv所在目录相同。
+
+## 功能列表  ##
+|command|description|
+|:-----:|:-----:|
+|c|将csv均分为N个csv|
+|s|每N行切分为一个csv|
+|t|按csv中某列的值Top N行|
+|e|按csv中某列的值提取符合条件的行|
+
---
+++ packages/xtalpi-pandas/xtalpi-pandas-0.1.3.tar.gz/xtalpi-pandas-0.1.3/xpandas/module/e.py
@@ -0,0 +1,59 @@
+from xpandas.module.utils import show_col_names
+
+
+def handle_col(df):
+    col_names = show_col_names(df)
+    while True:
+        col_name = input('想要按照哪一列进行选取? :')
+        if col_name in col_names:
+            break
+        else:
+            print('列名错误，请重新输入')
+
+    while True:
+        print('\n数值型：[1.<][2.<=][3.=][4.>][5.>=]')
+        print('字符串型：[6.包含][7.不包含]')
+        cmd = input('请从1-7中选择一个操作：')
+
+        if cmd == '1':
+            threshold = input('请输入阈值：')
+            df_new = df.loc[df[col_name] < float(threshold)]
+            break
+        elif cmd == '2':
+            threshold = input('请输入阈值：')
+            df_new = df.loc[df[col_name] <= float(threshold)]
+            break
+        elif cmd == '3':
+            threshold = input('请输入阈值：')
+            df_new = df.loc[df[col_name] == float(threshold)]
+            break
+        elif cmd == '4':
+            threshold = input('请输入阈值：')
+            df_new = df.loc[df[col_name] > float(threshold)]
+            break
+        elif cmd == '5':
+            threshold = input('请输入阈值：')
+            df_new = df.loc[df[col_name] >= float(threshold)]
+            break
+        elif cmd == '6':
+            threshold = input('请输入字符：')
+            df_new = df.loc[df[col_name].str.contains(threshold) == True]
+            break
+        elif cmd == '7':
+            threshold = input('请输入字符：')
+            df_new = df.loc[df[col_name].str.contains(threshold) == False]
+            break
+        else:
+            print('输入错误！请从1至7中选择一个操作！')
+    return df_new
+
+
+def extract_csv(df, csv):
+    while True:
+        df = handle_col(df)
+        row = df.shape[0]
+        print('\n按照条件获取了%s条数据' % row)
+        go_on = input('\n是否继续提取？[1.是][2.否]：')
+        if go_on != '1':
+            df.to_csv(csv.replace('.csv', '_extract' + '.csv'), index=False)
+            break
