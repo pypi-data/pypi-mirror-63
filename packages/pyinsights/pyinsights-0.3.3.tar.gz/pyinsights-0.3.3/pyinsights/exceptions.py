ba45ffa8b7daba61c057f8feb4a6784d01bf8526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:18 +0000

    Add package pyinsights/0.3.3/pyinsights-0.3.3.tar.gz

---
+++ packages/pyinsights/pyinsights-0.3.3.tar.gz/pyinsights-0.3.3/pyinsights/exceptions.py
@@ -0,0 +1,42 @@
+class QueryNotYetStartError(Exception):
+    """Raises error if the query has not yet started"""
+
+
+class NotFetchQueryResultError(Exception):
+    """Raises error if the query result could not be fetched"""
+
+
+class QueryTimeoutError(Exception):
+    """Raises timeout error if the query timeout"""
+
+
+class QueryAlreadyCancelled(Exception):
+    """Raises if the query has already been cancelled"""
+
+
+class QueryUnknownError(Exception):
+    """Raises if the query status is Unknown"""
+
+
+class ConfigInvalidSyntaxError(Exception):
+    """Raises error if the configuration is invalid syntax"""
+
+
+class ConfigVersionUnknownError(Exception):
+    """Raises error if not specify configuration version"""
+
+
+class ConfigNotFoundError(Exception):
+    """Raises error if the configuration could not be found"""
+
+
+class InvalidVersionError(Exception):
+    """Raises error if the configuration version is invalid"""
+
+
+class InvalidDurationError(Exception):
+    """Raises error if the duration parameter is not number"""
+
+
+class InvalidQueryStringError(Exception):
+    """Raises error if query string format is invalid"""
