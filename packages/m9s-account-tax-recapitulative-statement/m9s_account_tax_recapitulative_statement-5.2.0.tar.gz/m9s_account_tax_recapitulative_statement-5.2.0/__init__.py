7130413911dccd0c0efb739f3db6b7095032404a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:37 +0000

    Add package m9s-account-tax-recapitulative-statement/5.2.0/m9s_account_tax_recapitulative_statement-5.2.0.tar.gz

---
+++ packages/m9s-account-tax-recapitulative-statement/m9s_account_tax_recapitulative_statement-5.2.0.tar.gz/m9s_account_tax_recapitulative_statement-5.2.0/__init__.py
@@ -0,0 +1,18 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+from . import invoice
+from . import tax
+
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        invoice.InvoiceLine,
+        invoice.InvoiceTax,
+        tax.Line,
+        tax.Tax,
+        tax.TaxTemplate,
+        module='account_tax_recapitulative_statement', type_='model')
