3852d20f8471fff50f8f6c497d60f4a487d934ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package manganelo/1.1.1/manganelo-1.1.1.tar.gz

---
+++ packages/manganelo/manganelo-1.1.1.tar.gz/manganelo-1.1.1/manganelo/api/api_base.py
@@ -0,0 +1,17 @@
+import requests
+import typing
+
+from bs4 import BeautifulSoup
+
+
+class APIBase:
+	SEARCH_URL = "http://manganelo.com/search/"
+
+	def _get_soup(self) -> typing.Union[BeautifulSoup, None]:
+		default_headers = requests.utils.default_headers()
+
+		r = requests.get(self._url, stream=True, timeout=5, headers=default_headers)
+
+		r.raise_for_status()
+
+		return BeautifulSoup(r.content, "html.parser") if r.status_code == requests.codes.ok else None
