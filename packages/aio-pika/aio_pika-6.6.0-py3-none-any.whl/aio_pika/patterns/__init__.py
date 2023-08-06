301a8b3f5f25db7ff5b42c6def79a0c891d08379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:26 +0000

    Add package aio-pika/6.6.0/aio_pika-6.6.0-py3-none-any.whl

---
+++ packages/aio-pika/aio_pika-6.6.0-py3-none-any.whl/aio_pika/patterns/__init__.py
@@ -0,0 +1,8 @@
+from .master import Master, Worker, NackMessage, RejectMessage, JsonMaster
+from .rpc import RPC, JsonRPC
+
+
+__all__ = (
+    'Master', 'NackMessage', 'RejectMessage',
+    'RPC', 'Worker', 'JsonMaster', 'JsonRPC',
+)
