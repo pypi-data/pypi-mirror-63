7202868573654f9884e8925c5501a700b43b80a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:00 +0000

    Add package python-ffmpeg/1.0.11/python-ffmpeg-1.0.11.tar.gz

---
+++ packages/python-ffmpeg/python-ffmpeg-1.0.11.tar.gz/python-ffmpeg-1.0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as input_file:
+    long_description = input_file.read()
+
+setup(
+    name='python-ffmpeg',
+    version='1.0.11',
+    description='A python interface for FFmpeg',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jonghwanhyeon/python-ffmpeg',
+    author='Jonghwan Hyeon',
+    author_email='hyeon0145@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    keywords='ffmpeg',
+    packages=find_packages(),
+    install_requires=['pyee'],
+)
