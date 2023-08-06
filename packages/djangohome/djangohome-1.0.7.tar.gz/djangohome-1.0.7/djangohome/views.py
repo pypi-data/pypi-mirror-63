cf39595fc51a55c7c4e87ca8351e42d1f6f55eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:31 +0000

    Add package djangohome/1.0.7/djangohome-1.0.7.tar.gz

---
+++ packages/djangohome/djangohome-1.0.7.tar.gz/djangohome-1.0.7/djangohome/views.py
@@ -0,0 +1,28 @@
+from itertools import chain
+from django.shortcuts import render
+from django.views.generic import ListView
+""" Import from External Apps. """
+from djangopost.models import ArticleModel
+from djangoarticle.models import ArticleModelScheme
+
+
+# Create your homepage views here.
+class HomePageView(ListView):
+    template_name = "djangoadmin/djangohome/homepage_view.html"
+    model = ArticleModel
+    slug_url_kwarg = "tag_slug"
+    context_object_name = "article_filter"
+
+    def get_queryset(self):
+        return ArticleModel.objects.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_promoted'] = ArticleModelScheme.objects.published()
+        post_promo = ArticleModel.objects.promo()[0:3]
+        article_promo = ArticleModelScheme.objects.promo()[0:3]
+        context['promo'] = chain(post_promo, article_promo)
+        post_trending = ArticleModel.objects.promo()[0:2]
+        article_trending = ArticleModelScheme.objects.promo()[0:2]
+        context['is_trending'] = chain(post_trending, article_trending)
+        return context
