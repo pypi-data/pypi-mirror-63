330be7e3e9c1bfca84856e70f094832598267f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:18 +0000

    Add package flocklab-tools/0.2.0a1/flocklab-tools-0.2.0a1.tar.gz

---
+++ packages/flocklab-tools/flocklab-tools-0.2.0a1.tar.gz/flocklab-tools-0.2.0a1/flocklab/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Copyright (c) 2020, ETH Zurich, Computer Engineering Group (TEC)
+"""
+
+from .flocklab import Flocklab
+from .visualization import visualizeFlocklabTrace
+from .xmlconfig import FlocklabXmlConfig, GeneralConf, TargetConf, SerialConf, GpioTracingConf, GpioActuationConf, PowerProfilingConf, ImageConf, DebugConf
---
+++ packages/flocklab-tools/flocklab-tools-0.2.0a1.tar.gz/flocklab-tools-0.2.0a1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Copyright (c) 2020, ETH Zurich, Computer Engineering Group (TEC)
+"""
+
+import setuptools
+# import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='flocklab-tools',
+    python_requires='>=3.6',
+    version='0.2.0a1',
+    author='Computer Engineering Group, ETH Zurich',
+    author_email='rtrueb@ethz.ch',
+    description='Python support for using the FlockLab 2 testbed (flocklab CLI, creating flocklab xml, visualization).',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://flocklab.ethz.ch/',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'pandas',
+        'bokeh',
+        'requests',
+        'appdirs',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            'flocklab = flocklab.__main__:main'
+        ]
+    },
+)
