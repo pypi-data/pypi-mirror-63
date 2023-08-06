74b49c1edd6e320a9cc08ff44b86417c5dc7cd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:38 +0000

    Add package moore-math-beta/0.3.1/moore_math_beta-0.3.1-py3-none-any.whl

---
+++ packages/moore-math-beta/moore_math_beta-0.3.1-py3-none-any.whl/moore_math_beta/__init__.py
@@ -0,0 +1,21 @@
+def add(a=0.0, b=0.0):
+    """Add two numbers together
+
+    Keyword arguments:
+    a -- number
+    b -- also a number
+    """
+    print("working to add {} and {}...".format(a, b))
+    return a + b
+
+
+def multiply(a, b):
+    """DONE with some extra comments"""
+    print("hi ")
+    return a * b
+
+
+def subtract(a, b):
+    """uhhh"""
+    print("hello ")
+    return a - b
