d81f6eaed08287a3d09f4896d1bcd7c601881b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:19 +0000

    Add package cord-19-tools/0.0.3/cord-19-tools-0.0.3.tar.gz

---
+++ packages/cord-19-tools/cord-19-tools-0.0.3.tar.gz/cord-19-tools-0.0.3/test.py
@@ -0,0 +1,16 @@
+import cotools
+from pprint import pprint
+
+cotools.download(dir="data")
+
+noncomm = cotools.Paperset("data/noncomm_use_subset")
+
+data = cotools.Paperset("data/comm_use_subset")
+pprint(data[0])
+pprint(data[:2])
+
+print(len(data))
+
+# takes about 5gb in memory
+alldata = [x[0] for x in data]
+
