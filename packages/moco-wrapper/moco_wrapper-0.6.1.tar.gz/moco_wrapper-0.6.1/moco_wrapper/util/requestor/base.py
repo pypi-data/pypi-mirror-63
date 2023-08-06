2752664aa1392bb2df0f2caee216fd0dd5440289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:54 +0000

    Add package moco-wrapper/0.6.1/moco_wrapper-0.6.1.tar.gz

---
+++ packages/moco-wrapper/moco_wrapper-0.6.1.tar.gz/moco_wrapper-0.6.1/moco_wrapper/util/requestor/base.py
@@ -0,0 +1,27 @@
+class BaseRequestor(object):
+    """
+    Base class all other Requestor classes inherit from
+    """
+    
+    ERROR_STATUS_CODES = [400, 401, 403, 404, 422, 429, 500]
+    SUCCESS_STATUS_CODES =  [200, 201, 204]
+
+    @property
+    def session(self):
+        return None
+
+
+    def get(self, path, params = None, **kwargs):
+        return self.request("GET", path, params=params, **kwargs)
+
+    def post(self, path, data= None, **kwargs):
+        return self.request("POST", path, data=data, **kwargs)
+
+    def put(self, path, data = None, params = None, **kwargs):
+        return self.request("PUT", path, data=data, params=params, **kwargs)
+
+    def delete(self, path, data = None, params = None, **kwargs):
+        return self.request("DELETE", path, data=data, params=params, **kwargs)
+
+    def patch(self, path, data = None, params = None, **kwargs):
+        return self.request("PATCH", path, data=data, params=params, **kwargs)
