4522553b34bb291f5eb4b1cbd90912c48aabfb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:54 +0000

    Add package PyChance/0.1.3/PyChance-0.1.3.tar.gz

---
+++ packages/PyChance/PyChance-0.1.3.tar.gz/PyChance-0.1.3/pychance/pychance_class.py
@@ -0,0 +1,21 @@
+from pychance.parsing import Parser
+
+
+class PyChance:
+    def __init__(self):
+        self.tables = {}
+        self.parser = Parser(self)
+
+    def add_table(self, name, table):
+        self.tables[name] = table
+
+
+if __name__ == '__main__':
+    from pychance.simpletable import SimpleTable
+
+    test_table = SimpleTable("foo", ["bar", "baz"])
+
+    test = PyChance()
+    test.add_table("foo", test_table)
+    print(test.parser.parse("This is a [foo] test."))
+
