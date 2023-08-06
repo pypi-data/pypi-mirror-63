6d8c5da57d2be140ffbc8ad5c457c6fa27129961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:15 +0000

    Add package enextMonitor/1.1/enextMonitor-1.1.tar.gz

---
+++ packages/enextMonitor/enextMonitor-1.1.tar.gz/enextMonitor-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='enextMonitor',
+      version='1.1',
+      description='Package that manage task-events in Enext Monitor',
+      url='https://bitbucket.org/enext/monitor-python-pip-package/src',
+      author='João Casagrande',
+      author_email='back@enext.com.br',
+      license='MIT',
+      install_requires=['requests'],
+      zip_safe=False)
