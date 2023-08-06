edc732e4d0ca66780dffdf810fab7b4334321a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:29 +0000

    Add package m9s-gift-card/5.2.1/m9s_gift_card-5.2.1-py3-none-any.whl

---
+++ packages/m9s-gift-card/m9s_gift_card-5.2.1-py3-none-any.whl/trytond/modules/gift_card/tests/test_views_depends.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class TestViewsDepends(ModuleTestCase):
+    '''
+    Test views and depends
+    '''
+
+    module = 'gift_card'
+
+
+def suite():
+    """
+    Define suite
+    """
+    test_suite = trytond.tests.test_tryton.suite()
+    test_suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase(TestViewsDepends)
+    )
+    return test_suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
