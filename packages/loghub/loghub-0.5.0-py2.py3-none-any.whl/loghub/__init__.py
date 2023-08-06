a02e5d0e2ecc7c05b696786758ab18a9f31bd441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package loghub/0.5.0/loghub-0.5.0-py2.py3-none-any.whl

---
+++ packages/loghub/loghub-0.5.0-py2.py3-none-any.whl/loghub/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) The Spyder Development Team
+#
+# Licensed under the terms of the MIT License
+# (See LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Changelog generator based on github milestones or tags."""
+
+VERSION_INFO = (0, 5, 0)
+__version__ = '.'.join(map(str, VERSION_INFO))
