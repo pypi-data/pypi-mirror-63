4ebafd6b8dbc9803badd147bbf9825e39c972814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package sql-tools/3.1.0/sql_tools-3.1.0.tar.gz

---
+++ packages/sql-tools/sql_tools-3.1.0.tar.gz/sql_tools-3.1.0/README.md
@@ -0,0 +1,27 @@
+# SQL-TOOLS
+
+<p align="center"><img alt="SQL-Tools" src="https://raw.githubusercontent.com/yogesh-aggarwal/sql-tools-lib/master/docs/media/brand.png" width="250"/></p>
+
+**Python package to help out in working with SQL database-related operation.**
+
+It is a python package which uses database (SQL) functionality to help the developer to perform SQL operations on their desired database. This project _aims_ the developer ring to focus on their main code rather than focusing on the database related headaches.
+
+This python package uses the `sqlite` functionality to store databases on the local and `My SQL` for directly connect with the database setup on the host.
+
+---
+
+**Check it out on <a href="https://pypi.org/project/sql-tools">PyPi</a> | [![Downloads](https://pepy.tech/badge/sql-tools)](https://pepy.tech/project/sql-tools)**
+
+**or install it directly with the package manager.**
+
+> pip install sql-tools
+
+---
+
+## Resources
+
+Read the full [documentation](https://yogesh-aggarwal.gitbook.io/sql-tools) for more information
+
+You can contribute to it through [GitHub](https://github.com/yogesh-aggarwal/sql-tools-lib)
+
+## View project at [GitHub](https://github.com/yogesh-aggarwal/sql-tools-lib/projects/1)
---
+++ packages/sql-tools/sql_tools-3.1.0.tar.gz/sql_tools-3.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readMe:
+    longDesc = readMe.read()
+
+setup(
+    name="sql_tools",
+    packages=find_packages(),
+    version="3.1.0",
+    license="AGPL 3.0",
+    description="An integrative library that contains tools for performing various tasks related to MySQL/sqlite3 databases.",
+    long_description=longDesc,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    author="Yogesh Aggarwal",
+    author_email="developeryogeshgit@gmail.com",
+    url="https://github.com/yogesh-aggarwal/sql-tools-lib",
+    download_url="https://raw.githubusercontent.com/yogesh-aggarwal/sql-tools-lib/master/dist/sql_tools-3.1.0.tar.gz",
+    keywords=["SQL", "DATABASES", "TABLES", "RECORDS"],
+    install_requires=["numpy", "pandas", "cryptography", "colorama"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
