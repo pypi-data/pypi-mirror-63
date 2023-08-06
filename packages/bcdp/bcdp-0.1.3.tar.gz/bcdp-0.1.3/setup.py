6fae5d06996969bb6421bab145314bd234124750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:45 +0000

    Add package bcdp/0.1.3/bcdp-0.1.3.tar.gz

---
+++ packages/bcdp/bcdp-0.1.3.tar.gz/bcdp-0.1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bcdp',
+      version='0.1.3',
+      description='Big Climate Data Pipeline',
+      author='Alex Goodman',
+      packages=['bcdp'],
+      zip_safe=False)
