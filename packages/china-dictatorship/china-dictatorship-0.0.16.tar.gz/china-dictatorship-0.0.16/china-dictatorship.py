28bac8fd8a91522e140f82b96ac9559f05e39359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:03 +0000

    Add package china-dictatorship/0.0.16/china-dictatorship-0.0.16.tar.gz

---
+++ packages/china-dictatorship/china-dictatorship-0.0.16.tar.gz/china-dictatorship-0.0.16/china-dictatorship.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import pkg_resources
+
+readme_html_path = pkg_resources.resource_filename('china_dictatorship', 'README.html')
+with open(readme_html_path) as f:
+    print(f.read())
