05fc1c56f580e5620e84b172f4870bde196fd0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:27 +0000

    Add package shared-ds/0.0.3/shared_ds-0.0.3.tar.gz

---
+++ packages/shared-ds/shared_ds-0.0.3.tar.gz/shared_ds-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools import find_packages
+
+NUMPY_MIN_VERSION = '1.16.4'
+POSIX_IP_MIN_VERSION = '1.0.4'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='shared_ds',
+      version='0.0.3',
+      description='Provide shared memory data structures!',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/storborg/kalksdjlkasdkl',
+      author='Vigen Sahakyan',
+      author_email='vigen.sahakyan@picsart.com',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+                        'numpy>={}'.format(NUMPY_MIN_VERSION),
+                        'posix_ipc>={}'.format(POSIX_IP_MIN_VERSION),
+                    ],
+      platforms=["Linux", "Mac OS-X", "Unix"],
+      zip_safe=False)
