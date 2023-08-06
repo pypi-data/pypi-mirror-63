7fbe2df1547b7b64a72fa7bcd25eb896120e495b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:25 +0000

    Add package livestyled/0.0.1.dev77/livestyled-0.0.1.dev77.tar.gz

---
+++ packages/livestyled/livestyled-0.0.1.dev77.tar.gz/livestyled-0.0.1.dev77/livestyled/schemas/user.py
@@ -0,0 +1,66 @@
+from marshmallow import EXCLUDE, fields, Schema
+
+from livestyled.schemas.cohort import CohortSchema
+from livestyled.schemas.device import DeviceSchema
+from livestyled.schemas.magic_field import MagicFieldSchema
+from livestyled.schemas.fields import RelatedResourceField, RelatedResourceLinkField
+from livestyled.models.user import User, UserSSO
+
+
+class UserCreateSchema(Schema):
+    class Meta:
+        api_type = 'users'
+        url = 'v4/users'
+        create_url = 'v4/users/register'
+        model = User
+
+    auth_type = fields.String(data_key='authType')
+    first_name = fields.String(data_key='firstName')
+    last_name = fields.String(data_key='lastName')
+    email = fields.Email()
+    password = fields.String()
+    device_id = RelatedResourceLinkField(schema=DeviceSchema, data_key='device')
+
+
+class UserSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+        api_type = 'users'
+        url = 'v4/users'
+        create_url = 'v4/users/register'
+        authorise_url = 'v4/users/{}/authorise'
+        magic_fields_url = 'v4/users/{}/magic_fields'
+        model = User
+
+    class UserInfo(Schema):
+        class Meta:
+            unknown = EXCLUDE
+        first_name = fields.String(data_key='firstName')
+        last_name = fields.String(data_key='lastName')
+        phone = fields.String(data_key='phone')
+        dob = fields.AwareDateTime()
+        gender = fields.String()
+
+    id = fields.Int()
+    auth_type = fields.String(data_key='authType')
+    email = fields.Email()
+    password = fields.String()
+    device_id = RelatedResourceLinkField(schema=DeviceSchema, data_key='device')
+    user_info = fields.Nested(UserInfo, data_key='userInfo')
+    cohorts = RelatedResourceLinkField(schema=CohortSchema, many=True)
+    magic_fields = RelatedResourceField(schema=MagicFieldSchema, many=True, data_key='magicFields')
+
+
+class UserSSOSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+        api_type = 'user_sso',
+        url = 'v4/user_ssos'
+        model = UserSSO
+
+    id = fields.Int()
+    access_token = fields.String(data_key='accessToken', missing=None, allow_none=True)
+    refresh_token = fields.String(data_key='refreshToken', missing=None, allow_none=True)
+    sub = fields.String()
+    expires = fields.AwareDateTime(missing=None, allow_none=True)
+    user_id = RelatedResourceLinkField(schema=UserSchema, data_key='user')
