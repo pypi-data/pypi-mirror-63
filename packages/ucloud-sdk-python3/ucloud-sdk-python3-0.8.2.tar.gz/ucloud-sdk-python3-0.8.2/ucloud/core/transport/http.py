419e8179944a01f174e8baf301b7959b5a68d00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package ucloud-sdk-python3/0.8.2/ucloud-sdk-python3-0.8.2.tar.gz

---
+++ packages/ucloud-sdk-python3/ucloud-sdk-python3-0.8.2.tar.gz/ucloud-sdk-python3-0.8.2/ucloud/core/transport/http.py
@@ -0,0 +1,109 @@
+import typing
+import logging
+import json as json_mod
+
+from ucloud.core.transport import utils
+from ucloud.core.utils.compat import str
+
+logger = logging.getLogger(__name__)
+
+
+class Request:
+    def __init__(
+        self,
+        url: str,
+        method: str = "GET",
+        params: dict = None,
+        data: dict = None,
+        json: dict = None,
+        headers: dict = None,
+        **kwargs
+    ):
+        self.url = url
+        self.method = method
+        self.params = params
+        self.data = data
+        self.json = json
+        self.headers = headers
+        self.request_time = 0
+
+    def payload(self):
+        payload = (self.params or {}).copy()
+        payload.update(self.data or {})
+        payload.update(self.json or {})
+        return payload
+
+
+class Response:
+    def __init__(
+        self,
+        url: str,
+        method: str,
+        request: Request = None,
+        status_code: int = None,
+        reason: str = None,
+        headers: dict = None,
+        content: bytes = None,
+        encoding: str = None,
+        **kwargs
+    ):
+        self.url = url
+        self.method = method
+        self.request = request
+        self.status_code = status_code
+        self.reason = reason
+        self.headers = headers
+        self.content = content
+        self.encoding = encoding
+        self.response_time = 0
+
+    def json(self, **kwargs) -> typing.Optional[dict]:
+        """ json will return the bytes of content
+        """
+        if not self.content:
+            return None
+
+        encoding = utils.guess_json_utf(self.content)
+        if encoding is not None:
+            try:
+                return json_mod.loads(self.content.decode(encoding), **kwargs)
+            except UnicodeDecodeError:
+                pass
+        return json_mod.loads(self.text, **kwargs)
+
+    @property
+    def text(self):
+        """ text will return the unicode string of content,
+        see `requests.Response.text`
+        """
+        if not self.content:
+            return str("")
+
+        # Decode unicode from given encoding.
+        try:
+            content = str(self.content, self.encoding, errors="replace")
+        except (LookupError, TypeError):
+            content = str(self.content, errors="replace")
+
+        return content
+
+
+class SSLOption:
+    def __init__(
+        self,
+        ssl_verify: bool = True,
+        ssl_cacert: str = None,
+        ssl_cert: str = None,
+        ssl_key: str = None,
+    ):
+        self.ssl_verify = ssl_verify
+        self.ssl_cacert = ssl_cacert
+        self.ssl_cert = ssl_cert
+        self.ssl_key = ssl_key
+
+
+class Transport:
+    """ the abstract class of transport implementation """
+
+    def send(self, req: Request, **options: typing.Any) -> Response:
+        raise NotImplementedError
