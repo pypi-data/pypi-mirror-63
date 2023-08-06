5a3629625d1eef8bff1f9ba50b6e62f0fff8bcf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:29 +0000

    Add package graphenemodeling/1.1.1/graphenemodeling-1.1.1-py3-none-any.whl

---
+++ packages/graphenemodeling/graphenemodeling-1.1.1-py3-none-any.whl/graphenemodeling/graphene/__init__.py
@@ -0,0 +1,19 @@
+"""
+===========================================
+Graphene (:mod:`graphenemodeling.graphene`)
+===========================================
+
+.. currentmodule:: graphenemodeling.graphene
+
+GrapheneModeling's ``graphene`` is the main module of the package.
+The code here replicates literature on the material graphene. Every single function must include an example which demonstrates that it replicates a resource found in the literature. 
+
+.. toctree::
+	:maxdepth: 1
+
+	graphene.monolayer
+	graphene.bilayer
+	graphene._constants
+	
+
+"""
