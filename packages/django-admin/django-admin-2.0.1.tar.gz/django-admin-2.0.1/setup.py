ee5a8fedd65236149535cd6869222b07bc1f9654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package django-admin/2.0.1/django-admin-2.0.1.tar.gz

---
+++ packages/django-admin/django-admin-2.0.1.tar.gz/django-admin-2.0.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+version = '2.0.1'
+
+
+setup(
+    name='django-admin',
+    version=version,
+    keywords='Django Admin Extensions',
+    description='Django Admin Extensions',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/django-xxx/django-admin',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['django_admin'],
+    py_modules=[],
+    install_requires=['django-excel-response2>=3.0.0'],
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
