76c89e3f007bac06cc39e468e1840e538b4b831c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:49 +0000

    Add package mbed-tools-lib/1.1.0/mbed_tools_lib-1.1.0-py3-none-any.whl

---
+++ packages/mbed-tools-lib/mbed_tools_lib-1.1.0-py3-none-any.whl/mbed_tools_lib/logging.py
@@ -0,0 +1,32 @@
+"""Helpers for logging errors according to severity of the exception."""
+import logging
+
+LOGGING_FORMAT = "%(levelname)s: %(message)s"
+
+
+def log_exception(logger: logging.Logger, exception: Exception, show_traceback: bool = False) -> None:
+    """Logs an exception in both normal and verbose forms.
+
+    Args:
+        logger: logger
+        exception: exception to log
+        show_traceback: show the full traceback.
+    """
+    logger.error(exception, exc_info=show_traceback)
+
+
+def set_log_level(verbose_count: int) -> None:
+    """Sets the log level.
+
+    Args:
+        verbose_count: number of `-v` flags used
+    """
+    if verbose_count > 2:
+        log_level = logging.DEBUG
+    elif verbose_count == 2:
+        log_level = logging.INFO
+    elif verbose_count == 1:
+        log_level = logging.WARNING
+    else:
+        log_level = logging.ERROR
+    logging.basicConfig(level=log_level, format=LOGGING_FORMAT)
