4ea394c8a5c3ab75f44ed412580a4fdcd694ae14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:03 +0000

    Add package markdown-hiko/0.2.1/markdown_hiko-0.2.1-py3-none-any.whl

---
+++ packages/markdown-hiko/markdown_hiko-0.2.1-py3-none-any.whl/test/test_open_markdown.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from src.service.markdown_file_service import MarkdownFileService
+from src.model.file_path import FilePath
+
+
+class TestOpenMarkDown(TestCase):
+    def test_open_mark_down(self):
+        markdown = """\
+        ![](./image/file/path)
+        ![](./image/file/path)
+        ![test](./image/file/path)""".replace(" ", "")
+
+        markdown_file_service = MarkdownFileService(FilePath("./test/test.md"))
+
+        opened_markdown = markdown_file_service.open_markdown()
+        self.assertEqual(opened_markdown, markdown)
+
+    def test_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            markdown_file_service = MarkdownFileService(FilePath("./test/not_found_file.md"))
+            markdown_file_service.open_markdown()
