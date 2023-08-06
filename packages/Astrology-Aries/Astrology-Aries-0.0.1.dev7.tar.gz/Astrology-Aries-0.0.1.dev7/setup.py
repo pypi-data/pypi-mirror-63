b597b92896d04c588cbc9584f85b80d70f642d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package Astrology-Aries/0.0.1.dev7/Astrology-Aries-0.0.1.dev7.tar.gz

---
+++ packages/Astrology-Aries/Astrology-Aries-0.0.1.dev7.tar.gz/Astrology-Aries-0.0.1.dev7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from Aries.files import Markdown
+
+with open("Aries/README.md", "r") as fh:
+    long_description = fh.read()
+    long_description = Markdown.from_text(long_description).make_links_absolute("https://github.com/qiuosier/Aries/")
+
+setuptools.setup(
+    name="Astrology-Aries",
+    version="0.0.1.dev7",
+    author="Qiu Qin",
+    author_email="qiuosier@gmail.com",
+    description="Python package providing shortcuts to tasks like "
+                "accessing files on the cloud, running background tasks, configuring logging, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qiuosier/Aries",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
