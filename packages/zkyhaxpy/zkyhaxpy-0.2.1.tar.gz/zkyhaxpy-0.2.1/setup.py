ba4432b624cbbdda2002ad6543d5782c9b3b0f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:45 +0000

    Add package zkyhaxpy/0.2.1/zkyhaxpy-0.2.1.tar.gz

---
+++ packages/zkyhaxpy/zkyhaxpy-0.2.1.tar.gz/zkyhaxpy-0.2.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup
+  
+__author__ = 'you'
+  
+setup(
+   name='zkyhaxpy',  
+   version='0.2.1',
+   py_modules=['zkyhaxpy'],
+   description="A python package for personal utilities",
+   url="https://github.com/surasakcho/zkyhaxpy",
+   author="Surasak Choedpasuporn",
+   author_email="surasak.cho@gmail.com",
+)
