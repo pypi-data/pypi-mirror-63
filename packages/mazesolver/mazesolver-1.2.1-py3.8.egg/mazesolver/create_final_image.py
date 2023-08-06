200bbf873a24031706794fa5531942d0adb7b171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package mazesolver/1.2.1/mazesolver-1.2.1-py3.8.egg

---
+++ packages/mazesolver/mazesolver-1.2.1-py3.8.egg/mazesolver/create_final_image.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os
+from pathlib import Path
+
+
+def main(solution_path, input_path, output_path=None):
+	"""
+	Void function that marks the solution path into the image with green and saves the image
+
+	:param solution_path: list of coordinates to the cells in the solution
+	:param input_path: User-supplied path to input maze image
+	:param output_path: User-supplied path to a directory where the image will be saved
+	"""
+	if not output_path:
+		output_path = str(Path.home()) + "/Pictures"
+
+	solution_image = Image.open(input_path)
+
+	for i in solution_path:
+		solution_image.putpixel((i[1], i[0]), (0,255,0))
+	image_name = input_path.split("/")[-1]
+	image_name = image_name.split(".")[0]
+		
+	out_path = f"{output_path}/{image_name}_out.jpg"
+
+	solution_image.save(out_path, subsampling=0, quality=100)
+
+	print(f"The solution to {input_path} was saved at {out_path}")
+
+
---
+++ packages/mazesolver/mazesolver-1.2.1-py3.8.egg/mazesolver/load_maze.py
@@ -0,0 +1,47 @@
+from PIL import Image
+from time import sleep
+
+
+def main(input_path):
+	"""
+	Converts the input image into a matrix where:
+	'#' == walls
+	'.' == paths
+	's' == start
+	'e' == end
+
+	:param input_path: A string containing a relative or absolute path to the image
+	:return: A matrix representing the maze
+	"""
+	input_img = Image.open(input_path)  # open the image
+	pixels = input_img.load()  # load the pixels from the image
+
+	width, height = input_img.size  # get width and height of image
+
+	maze: list = []  # this will contain our matrix
+
+	for y in range(height):
+		maze.append([])  # append row
+		for x in range(width):
+			current_pixel = pixels[x, y]  # get current pixel from coords
+			pixel_color = current_pixel[0] + current_pixel[1] + current_pixel[2]  # get the sum of all RGB values
+
+			if pixel_color < 600:  # if pixel is white
+				maze[-1].append("#")  # append path to most recent row
+
+			elif pixel_color >= 600:  # if pixel is black
+				maze[-1].append(".")  # append wall to most recent row
+
+			else:
+				raise ValueError("An error occured when determining the color of some pixels")
+
+	for index, cell in enumerate(maze[0]):
+		if cell == ".":
+			maze[0][index] = "s"
+
+	for index, cell in enumerate(maze[-1]):
+		if cell == ".":
+			maze[-1][index] = "e"
+
+	return maze
+
