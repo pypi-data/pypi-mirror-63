cccc7bfe66733e242f8067bc360d88cd368c555c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:49 +0000

    Add package TrollHunter/0.2.7/TrollHunter-0.2.7.tar.gz

---
+++ packages/TrollHunter/TrollHunter-0.2.7.tar.gz/TrollHunter-0.2.7/TrollHunter/news_crawler/news_entry.py
@@ -0,0 +1,24 @@
+from TrollHunter.news_crawler.database import get_sitemap_parent, get_all_sitemap, get_trust_levels
+from TrollHunter.news_crawler.sitemap import elastic_sitemap
+import time
+
+
+def news_crawler():
+    db_sitemap = {x[0]: x for x in get_all_sitemap()}
+    trust_levels = {x[0]: x[1] for x in get_trust_levels()}
+    for parent in get_sitemap_parent():
+        elastic_sitemap(parent, trust_levels, db_sitemap, sort='loc', influxdb=True)
+
+
+def scheduler_news():
+    print('Start crawler/indexer sitemap')
+    start = time.time()
+    while True:
+        try:
+            news_crawler()
+        except Exception as error:
+            print(error)
+        print('Sleeping')
+        sleep = time.time() - start
+        if sleep < 7200:
+            time.sleep(7200 - sleep)
