ff50bf06029b9390bf52fe84ad8f26373d08e564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:50 +0000

    Add package asreview/0.7.2/asreview-0.7.2-py3-none-any.whl

---
+++ packages/asreview/asreview-0.7.2-py3-none-any.whl/asreview/models/utils.py
@@ -0,0 +1,66 @@
+# Copyright 2019 The ASReview Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def get_model_class(method):
+    """Get class of model from string.
+
+    Arguments
+    ---------
+    method: str
+        Name of the model, e.g. 'svm', 'nb' or 'lstm-pool'.
+
+    Returns
+    -------
+    BaseModel:
+        Class corresponding to the method.
+    """
+    from asreview.models.nn_2_layer import NN2LayerModel
+    from asreview.models.svm import SVMModel
+    from asreview.models.nb import NBModel
+    from asreview.models.rf import RFModel
+    from asreview.models.logistic import LogisticModel
+    from asreview.models.lstm_base import LSTMBaseModel
+    from asreview.models.lstm_pool import LSTMPoolModel
+
+    models = {
+        "svm": SVMModel,
+        "nb": NBModel,
+        "rf": RFModel,
+        "nn-2-layer": NN2LayerModel,
+        "lstm-base": LSTMBaseModel,
+        "lstm-pool": LSTMPoolModel,
+        "logistic": LogisticModel,
+    }
+    try:
+        return models[method]
+    except KeyError:
+        raise ValueError(
+            f"Error: training method '{method}' is not implemented.")
+
+
+def get_model(method, *args, **kwargs):
+    """Get an instance of a model from a string.
+
+    Arguments
+    ---------
+    method: str
+        Name of the model.
+    *args:
+        Arguments for the model.
+    **kwargs:
+        Keyword arguments for the model.
+    """
+    model_class = get_model_class(method)
+    return model_class(*args, **kwargs)
---
+++ packages/asreview/asreview-0.7.2-py3-none-any.whl/test/test_balance.py
@@ -0,0 +1,57 @@
+from pytest import mark
+import numpy as np
+
+from asreview.balance_strategies.utils import get_balance_model
+
+
+def generate_data(n_feature=20, n_sample=10):
+    X = np.random.rand(n_sample, n_feature)
+    n_sample_zero = np.int(n_sample/2)
+    n_sample_one = n_sample - n_sample_zero
+    y = np.append(np.zeros(n_sample_zero), np.ones(n_sample_one))
+    np.random.shuffle(y)
+
+    return X, y
+
+
+def check_partition(X, y, X_partition, y_partition, train_idx):
+    partition_idx = []
+    for row in X_partition:
+        partition_idx.append(
+            np.where(np.all(X == row, axis=1))[0][0]
+        )
+
+    assert np.count_nonzero(y_partition == 0) > 0
+    assert np.count_nonzero(y_partition == 1) > 0
+    assert len(partition_idx) == X_partition.shape[0]
+    assert set(partition_idx) <= set(train_idx.tolist())
+    assert np.all(X[partition_idx] == X_partition)
+    assert np.all(y[partition_idx] == y_partition)
+
+
+@mark.parametrize(
+    "balance_strategy",
+    [
+        "undersample",
+        "simple",
+        "double",
+        "triple",
+    ])
+def test_balance(balance_strategy, n_partition=100, n_feature=200,
+                 n_sample=100):
+    model = get_balance_model(balance_strategy)
+    assert isinstance(model.param, dict)
+    assert model.name == balance_strategy
+    X, y = generate_data(n_feature=n_feature, n_sample=n_sample)
+    for _ in range(n_partition):
+        n_train = np.random.randint(10, n_sample)
+        while True:
+            train_idx = np.random.choice(
+                np.arange(len(y)), n_train, replace=False)
+            num_zero = np.count_nonzero(y[train_idx] == 0)
+            num_one = np.count_nonzero(y[train_idx] == 1)
+            if num_zero > 0 and num_one > 0:
+                break
+        shared = {"query_src": {}, "current_queries": {}}
+        X_train, y_train = model.sample(X, y, train_idx, shared)
+        check_partition(X, y, X_train, y_train, train_idx)
