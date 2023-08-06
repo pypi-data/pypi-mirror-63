1e6384042c1e9398c95f06cfcd1f2f05cbc8acea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:38 +0000

    Add package csaps/0.10.1/csaps-0.10.1-py3-none-any.whl

---
+++ packages/csaps/csaps-0.10.1-py3-none-any.whl/csaps/_types.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+Type-hints and type vars
+
+"""
+
+import typing as ty
+import numbers
+import numpy as np
+
+
+UnivariateDataType = ty.Union[
+    np.ndarray,
+    ty.Sequence[numbers.Number]
+]
+
+UnivariateVectorizedDataType = ty.Union[
+    UnivariateDataType,
+    # FIXME: mypy does not support recursive types
+    # https://github.com/python/mypy/issues/731
+    # ty.Sequence['UnivariateVectorizedDataType']
+]
+
+MultivariateDataType = ty.Union[
+    np.ndarray,
+    ty.Sequence[UnivariateDataType]
+]
+
+NdGridDataType = ty.Sequence[UnivariateDataType]
+
+TData = ty.TypeVar('TData', np.ndarray, ty.Sequence[np.ndarray])
+TProps = ty.TypeVar('TProps', int, ty.Tuple[int, ...])
+TSmooth = ty.TypeVar('TSmooth', float, ty.Tuple[float, ...])
+TXi = ty.TypeVar('TXi', UnivariateDataType, NdGridDataType)
+TSpline = ty.TypeVar('TSpline')
