4fc356729ef65dd95319131bf893b31441dd94c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:14 +0000

    Add package hcdk-eks/0.0.1/hcdk_eks-0.0.1.tar.gz

---
+++ packages/hcdk-eks/hcdk_eks-0.0.1.tar.gz/hcdk_eks-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setup(
+    name='hcdk_eks',
+    version='0.0.1',
+
+    description="EKS library for the Halloumi modules of the AWS CDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        "aws-cdk.aws-ec2",
+        "aws-cdk.aws-eks",
+        "aws-cdk.aws-iam",
+        "aws-cdk.core"
+    ],
+)
