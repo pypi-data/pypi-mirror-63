681d22e9a6bc7a6315bff681abad3b937c4dddce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:49 +0000

    Add package irispy/1.1/irispy-1.1.tar.gz

---
+++ packages/irispy/irispy-1.1.tar.gz/irispy-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='irispy',
+    version='1.1',
+    packages=['irispy', 'irispy.types', 'irispy.dispatcher', 'irispy.utils'],
+    url='https://github.com/zpodushkin/irispy',
+    download_url='https://github.com/zpodushkin/irispy/archive/1.1.tar.gz',
+    license='GPL-3.0',
+    author='zpodushkin',
+    description='Async and fast api wrapper for IrisCallback API',
+    install_requires=['aiohttp', 'pydantic', 'loguru', 'vbml']
+)
