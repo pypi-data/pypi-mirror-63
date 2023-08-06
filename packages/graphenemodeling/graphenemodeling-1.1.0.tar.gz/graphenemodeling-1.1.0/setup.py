2a247b34a7f57d106206c04188c1e00df940a402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:25 +0000

    Add package graphenemodeling/1.1.0/graphenemodeling-1.1.0.tar.gz

---
+++ packages/graphenemodeling/graphenemodeling-1.1.0.tar.gz/graphenemodeling-1.1.0/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+import os
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+
+with open(os.path.join(HERE,"README.md")) as file:
+    README = file.read()
+
+setup(
+        name="graphenemodeling",
+        version="1.1.0",
+        description="Code for modeling graphene",
+        long_description=README,
+        long_description_content_type="text/markdown",
+        url="https://github.com/gholdman1/graphenemodeling.git",
+        author="gholdman",
+        author_email="gholdman@protonmail.com",
+        packages=["graphenemodeling"],
+        include_package_data=True,
+        install_requires=["numpy","scipy","matplotlib"]
+        )
+
