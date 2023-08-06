101bfad052855ed79591cee7185a59c4552138c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:40 +0000

    Add package huscy.project-documents/0.1.0a0/huscy.project_documents-0.1.0a0.tar.gz

---
+++ packages/huscy.project-documents/huscy.project_documents-0.1.0a0.tar.gz/huscy.project_documents-0.1.0a0/huscy/project_documents/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from huscy.project_documents import views
+
+
+router = DefaultRouter()
+router.register('documents', views.DocumentViewSet)
+router.register('documenttypes', views.DocumentTypeViewSet)
