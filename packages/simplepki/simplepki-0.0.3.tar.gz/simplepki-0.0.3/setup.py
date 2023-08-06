621732089929c69f6ab227fc327574bbc50d4cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:28 +0000

    Add package simplepki/0.0.3/simplepki-0.0.3.tar.gz

---
+++ packages/simplepki/simplepki-0.0.3.tar.gz/simplepki-0.0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r') as r:
+    long_description = r.read()
+
+setuptools.setup(
+    name='simplepki',
+    version='0.0.3',
+    author='Jove Dahle',
+    description='A simple CLI for making a pki',
+    keywords='simple pki tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/digitalarc/simplepki',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'simplepki=simplepki.simplepki:cli',
+        ],
+    },
+    install_requires=[
+        'cryptography>=2.8',
+        'click>=7.1.1',
+        'questionary>=1.5.1'
+    ]
+)
