d3cec5acf10009fe36f94f1a116566f42005c76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:41 +0000

    Add package ipython-blocking/0.2.1/ipython_blocking-0.2.1.tar.gz

---
+++ packages/ipython-blocking/ipython_blocking-0.2.1.tar.gz/ipython_blocking-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='ipython_blocking',
+      version='0.2.1',
+      
+      author='Matt Kafonek',
+      author_email='kafonek@gmail.com',
+      url='https://github.com/kafonek/ipython_blocking',
+            
+      description='Context manager for blocking cell execution within a Jupyter notebook',
+      long_description=open('README.md', encoding='utf-8').read(),
+      long_description_content_type = 'text/markdown',
+      
+      packages=find_packages(),
+      install_requires=['IPython', 'ipywidgets'],
+      
+      classifiers = ['Framework :: IPython',
+                     'Programming Language :: Python',
+                     'Intended Audience :: Developers',
+                     'Development Status :: 3 - Alpha',
+                     ],
+      license='BSD')
+      
