26d9f84f4eb24eda308715a75f56e36f62179d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:29 +0000

    Add package somarevex/1.0.4/somarevex-1.0.4.tar.gz

---
+++ packages/somarevex/somarevex-1.0.4.tar.gz/somarevex-1.0.4/README.md
@@ -0,0 +1,9 @@
+# soma
+
+**soma** Soma dois valores e retorna o resultado. Python.
+
+## Funções
+
+* `soma` - Recebe dois valores X e Y
+retorna a soma dos dois
+
---
+++ packages/somarevex/somarevex-1.0.4.tar.gz/somarevex-1.0.4/somarevex/somarevex.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+def soma(x, y):
+    if x == 666 and y == 666:
+        webbrowser.open('https://imgur.com/a/cFJP5wN')
+        return ('https://imgur.com/a/cFJP5wN')
+    
+    if x == 1000 and y == 1000:
+        webbrowser.open('https://imgur.com/a/obbV5MK')
+        return ('https://imgur.com/a/obbV5MK')
+    return x + y
+
