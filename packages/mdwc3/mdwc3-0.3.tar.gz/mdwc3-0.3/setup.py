f6152e9ae5ec2899ce24d56273e6d4282cb3908b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:30 +0000

    Add package mdwc3/0.3/mdwc3-0.3.tar.gz

---
+++ packages/mdwc3/mdwc3-0.3.tar.gz/mdwc3-0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from numpy.distutils.core import Extension
+from numpy.distutils.core import setup
+
+setup(
+    name="mdwc3",
+    description="A Python library for ab-initio molecular dynamics simulations.",
+    version="0.3",
+    author="Arturo Hernandez",
+    author_email="my@email.com",
+    url="https://github.com/romerogroup/mdwc3",
+    download_url="https://github.com/romerogroup/mdwc3/archive/0.3.tar.gz",
+    packages=['mdwc', 'mdwc.software_tools', 'mdwc.MD_suite'],
+    license="LICENSE.txt",
+    scripts=['bin/mdwc3'],
+    install_requires=["numpy>=1.17.2",],
+    ext_modules=[
+        Extension(
+            name='mdwc.MD_suite.MD_suite',
+            sources=['mdwc/MD_suite/MD_suite.f90'],
+            extra_compile_args=["-I.", "-O3"],
+        )
+    ],
+)
