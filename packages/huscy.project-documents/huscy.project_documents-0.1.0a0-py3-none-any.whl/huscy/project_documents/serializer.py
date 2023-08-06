36f2a1b9556c7639e67383606dc21254affac26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:40 +0000

    Add package huscy.project-documents/0.1.0a0/huscy.project_documents-0.1.0a0-py3-none-any.whl

---
+++ packages/huscy.project-documents/huscy.project_documents-0.1.0a0-py3-none-any.whl/huscy/project_documents/serializer.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from huscy.project_documents import models, services
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    document_type_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Document
+        fields = (
+            'creator',
+            'document_type',
+            'document_type_name',
+            'filehandle',
+            'filename',
+            'project',
+            'uploaded_at',
+            'uploaded_by',
+        )
+        read_only_fields = 'creator', 'filename', 'uploaded_at', 'uploaded_by'
+
+    def get_document_type_name(self, document):
+        return document.document_type.name
+
+    def create(self, validated_data):
+        return services.create_document(**validated_data)
+
+
+class DocumentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.DocumentType
+        fields = (
+            'id',
+            'name',
+        )
