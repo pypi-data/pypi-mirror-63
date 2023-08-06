78b88ec8e12bc5d6457b7ef36927d96fe9221fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:22 +0000

    Add package connectionmaster/0.0.6/connectionmaster-0.0.6.tar.gz

---
+++ packages/connectionmaster/connectionmaster-0.0.6.tar.gz/connectionmaster-0.0.6/connectionmaster/__main__.py
@@ -0,0 +1,85 @@
+# Author: Nathan Merrill
+
+from blinkparse import *
+from pathlib import Path
+import yaml, os, sys, getpass
+
+HOME_PATH = Path.home()
+
+CONFIG_FILE = os.path.join(HOME_PATH, '.cmrc.yaml')
+
+with open(CONFIG_FILE, 'r') as stream:
+    config = yaml.safe_load(stream)
+
+serverArg = CommandArgument('server', list(config.keys()))
+args = Parser(
+    commands=[
+        Command('connect', [
+            serverArg
+        ], ['c']),
+        Command('execute', [
+            serverArg,
+            CommandArgument('command')
+        ], ['x']),
+        Command('scp', [
+            serverArg,
+            CommandArgument('localfile'),
+            CommandArgument('remotefile'),
+        ], ['s']),
+        Command('ping', [
+            serverArg,
+        ], ['p']),
+        Command('trace', [
+            serverArg
+        ], ['t']),
+        Command('edit', aliases=['e'])
+    ],
+    commandRequired=True
+).parse()
+
+defaultConfig = {
+    'command': 'ssh',
+    'user': getpass.getuser(),
+    'ip': 'localhost',
+    'port': '22',
+    'key': None,
+    'justRunCommand': False,
+    'jump': None
+}
+
+if args.command == 'edit':
+    os.system('nvim ' + CONFIG_FILE)
+    sys.exit()
+
+serverConfig = config[args.commandArgs['server']]
+for option in defaultConfig:
+    if option not in serverConfig:
+        serverConfig[option] = defaultConfig[option]
+
+if args.command == 'ping':
+    os.system('ping -c 1 ' + serverConfig['ip'])
+
+if args.command == 'trace':
+    os.system('traceroute ' + serverConfig['ip'])
+
+keyPart = ''
+jumpPart = ''
+if serverConfig['key'] is not None:
+    keyPart = ' -i ' + os.path.join(HOME_PATH, '.ssh', serverConfig['key']) + ' '
+if serverConfig['jump'] is not None:
+    jumpServerConfig = config[serverConfig['jump']]
+    for option in defaultConfig:
+        if option not in jumpServerConfig:
+            jumpServerConfig[option] = defaultConfig[option]
+    jumpUserIpPort = jumpServerConfig['user'] + '@' + jumpServerConfig['ip'] + ':' + jumpServerConfig['port']
+    jumpPart = ' -J ' + jumpUserIpPort + ' '
+userIpPort = serverConfig['user'] + '@' + serverConfig['ip'] + ':' + serverConfig['port']
+
+if args.command == 'connect':
+    os.system(serverConfig['command'] + keyPart + jumpPart + ' ssh://' + userIpPort)
+
+if args.command == 'execute':
+    os.system(serverConfig['command'] + keyPart + jumpPart + ' ssh://' + userIpPort + ' "' + args.commandArgs['command'] + '"')
+
+if args.command == 'scp':
+    os.system('scp ' + keyPart + args.commandArgs['localfile'] + ' scp://' + userIpPort + '/' + args.commandArgs['remotefile'])
---
+++ packages/connectionmaster/connectionmaster-0.0.6.tar.gz/connectionmaster-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="connectionmaster",
+    version="0.0.6",
+    author="Nathan Merrill",
+    author_email="mathiscool3000@gmail.com",
+    description="A python utility for remote server management",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nathansmerrill/connectionmaster",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pyyaml', 'blinkparse']
+)
