5a31bd664f03bb3717f35aae437636a16923498e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:52 +0000

    Add package rpgpack/5.3.2/rpgpack-5.3.2.tar.gz

---
+++ packages/rpgpack/rpgpack-5.3.2.tar.gz/rpgpack-5.3.2/rpgpack/types/faction.py
@@ -0,0 +1,14 @@
+from typing import *
+import enum
+
+
+class Faction(enum.Enum):
+    RED = "🔴"
+    ORANGE = "🟠"
+    YELLOW = "🟡"
+    GREEN = "🟢"
+    BLUE = "🔵"
+    PURPLE = "🟣"
+    BLACK = "⚫️"
+    WHITE = "⚪️"
+    BROWN = "🟤"
