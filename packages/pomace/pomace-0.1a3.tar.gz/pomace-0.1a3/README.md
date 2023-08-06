dd1216094ee723b00296583d04504f49671d252b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package pomace/0.1a3/pomace-0.1a3.tar.gz

---
+++ packages/pomace/pomace-0.1a3.tar.gz/pomace-0.1a3/README.md
@@ -0,0 +1,42 @@
+# Overview
+
+Dynamic page objects for browser automation.
+
+This project was generated with [cookiecutter](https://github.com/audreyr/cookiecutter) using [jacebrowning/template-python](https://github.com/jacebrowning/template-python).
+
+[![Unix Build Status](https://img.shields.io/travis/jacebrowning/pomace/develop.svg?label=unix)](https://travis-ci.org/jacebrowning/pomace)
+[![Windows Build Status](https://img.shields.io/appveyor/ci/jacebrowning/pomace/develop.svg?label=window)](https://ci.appveyor.com/project/jacebrowning/pomace)
+[![Coverage Status](https://img.shields.io/coveralls/jacebrowning/pomace/develop.svg)](https://coveralls.io/r/jacebrowning/pomace)
+[![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/jacebrowning/pomace.svg)](https://scrutinizer-ci.com/g/jacebrowning/pomace/?branch=develop)
+[![PyPI Version](https://img.shields.io/pypi/v/pomace.svg)](https://pypi.org/project/pomace)
+[![PyPI License](https://img.shields.io/pypi/l/pomace.svg)](https://pypi.org/project/pomace)
+
+# Setup
+
+## Requirements
+
+- Python 3.8+
+
+## Installation
+
+Install it directly into an activated virtual environment:
+
+```text
+$ pip install pomace
+```
+
+or add it to your [Poetry](https://poetry.eustace.io/) project:
+
+```text
+$ poetry add pomace
+```
+
+# Usage
+
+After installation, the package can imported:
+
+```text
+$ python
+>>> import pomace
+>>> pomace.__version__
+```
---
+++ packages/pomace/pomace-0.1a3.tar.gz/pomace-0.1a3/pomace/cli.py
@@ -0,0 +1,100 @@
+# pylint: disable=no-self-use
+
+import os
+from importlib import reload
+
+from bullet import Bullet, Input
+from cleo import Application, Command
+
+from . import browser, models, utils
+from .config import settings
+
+
+class RunCommand(Command):
+    """
+    Run pomace in a loop
+
+    run
+        {--browser= : Browser to use for automation}
+        {--headless : Run the specified browser in a headless mode}
+        {--domain= : Starting domain for the automation}
+        {--root= : Directory to load models from}
+        {--dev : Enable development mode to create missing pages}
+    """
+
+    def handle(self):
+        self.update_settings()
+        self.launch_browser()
+        try:
+            self.run_loop()
+        finally:
+            utils.quit_browser()
+
+    def update_settings(self):
+        if self.option('root'):
+            os.chdir(self.option('root'))
+
+        if self.option('browser'):
+            settings.browser.name = self.option('browser').lower()
+
+        settings.browser.headless = self.option('headless')
+
+        if self.option('domain'):
+            settings.site.url = "https://" + self.option('domain')
+
+        if self.option('dev'):
+            settings.development_mode_enabled = True
+
+    def launch_browser(self):
+        if not settings.browser.name:
+            cli = Bullet(
+                prompt="\nSelect a browser for automation: ",
+                bullet=" ● ",
+                choices=browser.NAMES,
+            )
+            settings.browser.name = cli.launch().lower()
+
+        if not settings.site.domain:
+            domains = [p.domain for p in models.Page.objects.all()]
+            if domains:
+                cli = Bullet(
+                    prompt="\nStarting domain: ",
+                    bullet=" ● ",
+                    choices=list(set(domains)),
+                )
+            else:
+                cli = Input(prompt="\nStarting domain: ", strip=True)
+            settings.site.url = f'https://{cli.launch()}'
+
+        utils.launch_browser()
+
+    def run_loop(self):
+        page = models.autopage()
+        self.clear_screen()
+        self.display_url(page)
+        while True:
+            choices = ['<reload>'] + dir(page)
+            cli = Bullet(prompt=f"\nSelect an action: ", bullet=" ● ", choices=choices)
+            action = cli.launch()
+            if action == '<reload>':
+                reload(models)
+                page = models.autopage()
+                self.clear_screen()
+                self.display_url(page)
+                continue
+
+            cli = Input(prompt=f"\nValue: ")
+            page, transitioned = page.perform(action, prompt=cli.launch)
+            if transitioned:
+                self.clear_screen()
+                self.display_url(page)
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def display_url(self, page):
+        self.line(f'<fg=white;options=bold>{page}</>')
+
+
+application = Application()
+application.add(RunCommand())
