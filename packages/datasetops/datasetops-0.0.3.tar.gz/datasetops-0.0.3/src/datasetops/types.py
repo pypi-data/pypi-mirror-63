0924fe382b292464ba4a68045afdc285072bbece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:21 +0000

    Add package datasetops/0.0.3/datasetops-0.0.3.tar.gz

---
+++ packages/datasetops/datasetops-0.0.3.tar.gz/datasetops-0.0.3/src/datasetops/types.py
@@ -0,0 +1,16 @@
+from typing import Callable, Dict, Sequence, Union, Any, Optional, List, Tuple, Type, TypeVar
+from datasetops.abstract import AbstractDataset
+from pathlib import Path
+
+Shape = Sequence[int]
+IdIndex = int
+Id = int
+Ids = List[Id]
+Data = Any
+IdIndexSet = Dict[Any, List[IdIndex]]
+ItemTransformFn = Callable[[Any],Any]
+DatasetTransformFn = Callable[[int, AbstractDataset], AbstractDataset] 
+DatasetTransformFnCreator = Callable[[Any], DatasetTransformFn]
+AnyPath = Union[str, Path]
+DataPredicate = Callable[[Any], bool]
+Key = Union[int,str]
