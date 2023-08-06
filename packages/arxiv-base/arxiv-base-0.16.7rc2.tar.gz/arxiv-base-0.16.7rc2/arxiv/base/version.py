412744d5988cf1efb82349fd02dcb9db06472ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:48 +0000

    Add package arxiv-base/0.16.7rc2/arxiv-base-0.16.7rc2.tar.gz

---
+++ packages/arxiv-base/arxiv-base-0.16.7rc2.tar.gz/arxiv-base-0.16.7rc2/arxiv/base/version.py
@@ -0,0 +1,5 @@
+#  Created by tag_check.write_version
+#  NEVER CHECK THIS version.py file INTO GIT.
+#  It should be generated when the package is build for distribution.
+__when__ = '16/03/2020 19:27:57'
+__version__ = '0.16.7rc2'
