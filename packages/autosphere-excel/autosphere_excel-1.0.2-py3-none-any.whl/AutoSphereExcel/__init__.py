c95ae18c2dd0e72e877c375431774c9fed804f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:05 +0000

    Add package autosphere-excel/1.0.2/autosphere_excel-1.0.2-py3-none-any.whl

---
+++ packages/autosphere-excel/autosphere_excel-1.0.2-py3-none-any.whl/AutoSphereExcel/__init__.py
@@ -0,0 +1,10 @@
+from .AutoSphereExcel import AutoSphereExcel
+
+
+class AutoSphereExcel(AutoSphereExcel):
+    """
+    This test library provides some keywords to allow
+    opening, reading, writing, and saving Excel files
+    from Robot Framework.
+    """
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
