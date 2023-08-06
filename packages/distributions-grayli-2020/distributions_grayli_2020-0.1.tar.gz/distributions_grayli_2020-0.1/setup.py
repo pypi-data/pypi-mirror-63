d7dd933a13d1db276879bf34b3a1cdec63325c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:55 +0000

    Add package distributions-grayli-2020/0.1/distributions_grayli_2020-0.1.tar.gz

---
+++ packages/distributions-grayli-2020/distributions_grayli_2020-0.1.tar.gz/distributions_grayli_2020-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_grayli_2020',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_grayli_2020'],
+      zip_safe=False)
