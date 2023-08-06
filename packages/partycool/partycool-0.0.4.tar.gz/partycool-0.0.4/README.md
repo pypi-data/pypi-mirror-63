9496590784bddc88085349b3a74759014d8b510a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:52 +0000

    Add package partycool/0.0.4/partycool-0.0.4.tar.gz

---
+++ packages/partycool/partycool-0.0.4.tar.gz/partycool-0.0.4/README.md
@@ -0,0 +1,55 @@
+# Partycool <img src="/example_images/partycool_super_smaller.jpg">
+## Particle Size Analysis Package for All Microscopy Images 
+This package can be used to analyze the particle size from microscopy images such as FEM, SEM images and optical microscopy.
+We will try to implement also the recignition of AFM images through Machine Learning.
+In addition, there is a visualization tools that can be used to plot the size distribution of the particles.
+#### Contributers: Muammer yaman, Ximin Hu, Margherita Taddei, Yangwei Shi
+#### Release date:
+
+## Organization of the project
+The project has the following structure:
+   
+   
+    partycool/
+      |- README.md
+      |- partycool/
+         |- __init__.py
+         |- core.py
+         |- tests/
+            |- __init__.py
+            |- test_core.py
+      |- doc/
+         |- technology_review.pdf
+      |- example_images/
+         |- train/
+         |- darkfield/
+         |- optic/
+         |- sem/
+      |- trial/
+         |- .gitignore
+         |- Pytorch-tutorial.ipynb
+         |- Shape_detection.ipynb
+         |- cifar_net.pth
+         |- dog.jpg
+         |- region_of_interest_scalebar.ipynb
+         |- shape.png 
+      |- .travis.yml
+      |- .gitignore
+      |- environment.yml
+      |- LICENSE
+      |- function_list.py
+      |- partycool.ipynb    
+      |- partycool_my.ipynb
+      |- requirements.txt
+      |- test
+
+
+
+
+### How to install
+```
+pip install partycool
+```
+### Software dependencies
+* Python3
+### Preview of the package
---
+++ packages/partycool/partycool-0.0.4.tar.gz/partycool-0.0.4/partycool/tests/test_partycool.py
@@ -0,0 +1,92 @@
+import cv2
+import partycool
+
+
+#Testing database
+test = cv2.imread('../../example_images/sem_1.jpg',0)
+
+#Test for boundary detection
+
+def test_boundary_detection():
+    result = partycool.boundary_detection(test)
+    
+    assert type(result) == int, 'Wrong output type in boundary detection function'
+    assert result == 593, "Wrong output result in boundary detection function"
+    return
+
+#Test for corner detection
+
+def test_corner_detection():
+    result = partycool.corner_detection(test,1)
+    
+    assert result == 1.1848341232227488, "Wrong output result of the corner detection function"
+    assert result.shape == (), "Wrong output shape of the corner detection function"
+    assert result.size == 1, "Wrong output size of the corner detection function"
+    return
+
+#Test img_pread function
+
+def test_img_pread():
+    result = partycool.img_pread(test)
+    
+    assert result.shape == (2048, 3072), "Wrong output shape of the img_pread function"
+    assert result.size == 6291456, "Wrong  output size of the img_pread function"
+    
+    return
+
+#Testing filetered dataframe used for testing the following functions
+
+
+
+#Test contour capture function
+
+def test_contour_capture():
+    test_filtered = partycool.img_pread(test)
+    result_cnt = partycool.contour_capture(test_filtered)
+    
+    assert len(result_cnt) == 316, 'wrong output'
+    
+    return
+
+#Testing dataframe from contour capture for the following functions
+
+
+
+#Test shape radar function
+
+def test_shape_radar():
+    test_filtered = partycool.img_pread(test)
+    result_cnt = partycool.contour_capture(test_filtered)
+    result = partycool.shape_radar(result_cnt, test)
+    
+    assert len(result) == 2188, 'Wrong output'
+    
+    return
+
+#Test the wrapping function of partycool summary
+
+def test_partycool_summary():
+    test_filtered = partycool.img_pread(test)
+    result_cnt = partycool.contour_capture(test_filtered)
+    result_df = partycool.partycool_summary(result_cnt)
+    
+    assert len(result_df.columns) == 5 or len(result_df.columns) == 8, 'wrong output dataframe!'
+    
+    return
+
+#Test the watershed function
+
+
+def test_watershed():
+    test_img = '../../example_images/sem_1.jpg'
+    result = partycool.watershed(test_img)
+    
+    assert result.size == 6721536, "Wrong output size of the watershed result matrix"
+    assert result.shape == (2188, 3072), "Wrong output shape of the watershade result matrix"
+    
+    return
+
+
+
+
+#THE END
